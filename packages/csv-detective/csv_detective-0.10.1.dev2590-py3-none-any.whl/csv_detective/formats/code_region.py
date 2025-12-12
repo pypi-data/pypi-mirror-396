dad4b008a5fd729b4d5ad375a70e229bf9461fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:14:04 +0000

    Add package csv-detective/0.10.1.dev2590/csv_detective-0.10.1.dev2590-py3-none-any.whl

---
+++ packages/csv-detective/csv_detective-0.10.1.dev2590-py3-none-any.whl/csv_detective/formats/code_region.py
@@ -0,0 +1,23 @@
+from frformat import CodeRegion, Millesime
+
+proportion = 1
+tags = ["fr", "geo"]
+mandatory_label = True
+labels = [
+    "code region",
+    "reg",
+    "code insee region",
+    "region",
+]
+
+_code_region = CodeRegion(Millesime.LATEST)
+
+
+def _is(val):
+    return isinstance(val, str) and _code_region.is_valid(val)
+
+
+_test_values = {
+    True: ["32"],
+    False: ["55"],
+}
---
+++ packages/csv-detective/csv_detective-0.10.1.dev2590-py3-none-any.whl/csv_detective/formats/year.py
@@ -0,0 +1,29 @@
+proportion = 1
+tags = ["temp"]
+python_type = "int"
+labels = [
+    "year",
+    "annee",
+    "annee depot",
+    "an nais",
+    "exercice",
+    "data year",
+    "annee de publication",
+    "exercice comptable",
+    "annee de naissance",
+    "annee ouverture",
+]
+
+
+def _is(val):
+    try:
+        val = int(val)
+    except ValueError:
+        return False
+    return (1800 <= val) and (val <= 2100)
+
+
+_test_values = {
+    True: ["2015"],
+    False: ["20166", "123"],
+}
