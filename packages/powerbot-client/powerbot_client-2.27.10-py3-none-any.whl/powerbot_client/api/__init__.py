6f6e2db7d32354cda560cb84784def99f1be1faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:21 +0000

    Add package powerbot-client/2.27.10/powerbot_client-2.27.10-py3-none-any.whl

---
+++ packages/powerbot-client/powerbot_client-2.27.10-py3-none-any.whl/powerbot_client/api/__init__.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from powerbot_client.api.alert_api import AlertApi
+from powerbot_client.api.algorithms_api import AlgorithmsApi
+from powerbot_client.api.auction_configuration_api import AuctionConfigurationApi
+from powerbot_client.api.auction_exchange_api import AuctionExchangeApi
+from powerbot_client.api.auction_order_api import AuctionOrderApi
+from powerbot_client.api.auction_settings_api import AuctionSettingsApi
+from powerbot_client.api.auction_trade_api import AuctionTradeApi
+from powerbot_client.api.auctions_api import AuctionsApi
+from powerbot_client.api.authentication_api import AuthenticationApi
+from powerbot_client.api.capacities_api import CapacitiesApi
+from powerbot_client.api.contract_api import ContractApi
+from powerbot_client.api.historic_data_api import HistoricDataApi
+from powerbot_client.api.logs_api import LogsApi
+from powerbot_client.api.market_api import MarketApi
+from powerbot_client.api.messages_api import MessagesApi
+from powerbot_client.api.orders_api import OrdersApi
+from powerbot_client.api.portfolios_api import PortfoliosApi
+from powerbot_client.api.report_api import ReportApi
+from powerbot_client.api.sso_api import SSOApi
+from powerbot_client.api.schema_api import SchemaApi
+from powerbot_client.api.settings_api import SettingsApi
+from powerbot_client.api.signals_api import SignalsApi
+from powerbot_client.api.subscriptions_api import SubscriptionsApi
+from powerbot_client.api.tenants_api import TenantsApi
+from powerbot_client.api.trades_api import TradesApi
