d5e77580abfe620ef04001e5366506f9651b6600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:56 +0000

    Add package copapy/0.0.2/copapy-0.0.2-cp314-cp314-win_amd64.whl

---
+++ packages/copapy/copapy-0.0.2-cp314-cp314-win_amd64.whl/copapy/backend.py
@@ -0,0 +1,22 @@
+from ._target import add_read_command
+from ._basic_types import Net, Op, Node, CPConstant, Write, stencil_db_from_package
+from ._compiler import compile_to_dag, \
+    stable_toposort, get_const_nets, get_all_dag_edges, add_read_ops, get_all_dag_edges_between, \
+    add_write_ops
+
+__all__ = [
+    "add_read_command",
+    "Net",
+    "Op",
+    "Node",
+    "CPConstant",
+    "Write",
+    "compile_to_dag",
+    "stable_toposort",
+    "get_const_nets",
+    "get_all_dag_edges",
+    "get_all_dag_edges_between",
+    "add_read_ops",
+    "add_write_ops",
+    "stencil_db_from_package"
+]
