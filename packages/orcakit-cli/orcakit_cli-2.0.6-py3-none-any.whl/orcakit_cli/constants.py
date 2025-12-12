19ca20175fe742d1adfef6c37ef1a0932b9affab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:14:04 +0000

    Add package orcakit-cli/2.0.6/orcakit_cli-2.0.6-py3-none-any.whl

---
+++ packages/orcakit-cli/orcakit_cli-2.0.6-py3-none-any.whl/orcakit_cli/constants.py
@@ -0,0 +1,12 @@
+# 配置魔法数字和字符串
+DEFAULT_CONFIG = "orcaagent.json"
+DEFAULT_PORT = 2024
+DEFAULT_CHAT_PORT = 8001
+DEFAULT_CHAT_UI_URL = "https://agentchat.vercel.app/"
+
+# 默认基础镜像
+DEFAULT_BASE_IMAGE = "mirrors.tencent.com/orcakit/orcakit-runner:0.0.1"
+
+# analytics
+SUPABASE_PUBLIC_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6cmxwcG9qaW5wY3l5YWlweG5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkyNTc1NzksImV4cCI6MjAzNDgzMzU3OX0.kkVOlLz3BxemA5nP-vat3K4qRtrDuO4SwZSR_htcX9c"
+SUPABASE_URL = "https://kzrlppojinpcyyaipxnb.supabase.co"
