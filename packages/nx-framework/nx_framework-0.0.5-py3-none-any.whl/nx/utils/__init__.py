07ecfe23701f6611fcf9defec96b30421218d01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:32 +0000

    Add package nx-framework/0.0.5/nx_framework-0.0.5-py3-none-any.whl

---
+++ packages/nx-framework/nx_framework-0.0.5-py3-none-any.whl/nx/utils/__init__.py
@@ -0,0 +1,20 @@
+__all__ = [
+    "coalesce",
+    "create_uuid",
+    "hash_data",
+    "indent",
+    "json_dumps",
+    "json_loads",
+    "normalize_uuid",
+]
+
+
+from .coalesce import coalesce
+from .utils import (
+    create_uuid,
+    hash_data,
+    indent,
+    json_dumps,
+    json_loads,
+    normalize_uuid,
+)
