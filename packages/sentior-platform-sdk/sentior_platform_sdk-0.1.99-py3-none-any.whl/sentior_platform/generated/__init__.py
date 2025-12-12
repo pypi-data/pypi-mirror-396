338e44ef9a892f39ff1b4b6152f733805d31be73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:31:47 +0000

    Add package sentior-platform-sdk/0.1.99/sentior_platform_sdk-0.1.99-py3-none-any.whl

---
+++ packages/sentior-platform-sdk/sentior_platform_sdk-0.1.99-py3-none-any.whl/sentior_platform/generated/__init__.py
@@ -0,0 +1,8 @@
+"""
+AUTO-GENERATED package initializer.
+"""
+from .entitlements import ENTITLEMENTS, Entitlement
+from .collections import COLLECTIONS
+from .events import EVENTS
+from .version import SDK_VERSION
+
---
+++ packages/sentior-platform-sdk/sentior_platform_sdk-0.1.99-py3-none-any.whl/sentior_platform/generated/entitlements.py
@@ -0,0 +1,19 @@
+"""
+AUTO-GENERATED. NIET HANDMATIG AANPASSEN.
+"""
+from typing import Literal, TypedDict, Dict, List
+
+ENTITLEMENTS: Dict[str, List[str]] = {
+  "viktor": [
+    "ft-viktor-v1",
+    "ft-viktor-01"
+  ],
+  "core": [
+    "ft-core-basic"
+  ]
+}
+
+Entitlement = Literal[
+    "ft-viktor-v1", "ft-viktor-01", "ft-core-basic"
+]
+
