f1f744ca7cf79853a3192579ff9d87b186377ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:27 +0000

    Add package tpu-commons/0.0.1/tpu_commons-0.0.1.tar.gz

---
+++ packages/tpu-commons/tpu_commons-0.0.1.tar.gz/tpu_commons-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tpu-commons",
+    version="0.0.1",
+    packages=find_packages(),
+    author="sjnscythe",
+    author_email="scytheabhi97@gmail.com",
+    description="A poc for google",
+    python_requires='>=3.6',
+)
