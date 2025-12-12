8ff0130a1d7796bd2851e10b411c6f7931cd41ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:51 +0000

    Add package nonebot-plugin-railwaytools/1.1.5/nonebot_plugin_railwaytools-1.1.5.tar.gz

---
+++ packages/nonebot-plugin-railwaytools/nonebot_plugin_railwaytools-1.1.5.tar.gz/nonebot_plugin_railwaytools-1.1.5/nonebot_plugin_railwaytools/api.py
@@ -0,0 +1,5 @@
+class API:
+    api_12306 = "https://mobile.12306.cn/wxxcx/wechat/main/travelServiceQrcodeTrainInfo"
+    api_rail_re = "https://api.rail.re/"
+    api_xiaguanzhan = "http://www.xiaguanzhan.com/uploadfiles/"
+    api_station_screen = "https://apis.uctb.cn/api/12306?city="
