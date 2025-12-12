951ecefba3fff73dda4fb315595cc9dbc1d2fd23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 06:04:10 +0000

    Add package complexipy/5.1.0/complexipy-5.1.0-cp312-cp312-win_amd64.whl

---
+++ packages/complexipy/complexipy-5.1.0-cp312-cp312-win_amd64.whl/complexipy/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from complexipy._complexipy import (
+    CodeComplexity,
+    FileComplexity,
+    FunctionComplexity,
+    LineComplexity,
+)
+from complexipy.api import (
+    code_complexity,
+    file_complexity,
+)
+
+__all__ = [
+    "CodeComplexity",
+    "FileComplexity",
+    "FunctionComplexity",
+    "LineComplexity",
+    "code_complexity",
+    "file_complexity",
+]
