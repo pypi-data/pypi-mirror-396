a3cc194b557408f6222cf51032338ef6303cca54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:31:10 +0000

    Add package eckitlib/1.32.4.8/eckitlib-1.32.4.8-cp313-cp313-macosx_13_0_x86_64.whl

---
+++ packages/eckitlib/eckitlib-1.32.4.8-cp313-cp313-macosx_13_0_x86_64.whl/eckitlib/include/eckit/cmd/StopCmd.h
@@ -0,0 +1,53 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+/// @file   StopCmd.h
+/// @author Baudouin Raoult
+/// @date   Jan 1997
+
+#ifndef eckit_cmd_StopCmd_H
+#define eckit_cmd_StopCmd_H
+
+#include "eckit/cmd/CmdResource.h"
+
+//-----------------------------------------------------------------------------
+
+namespace eckit {
+
+//-----------------------------------------------------------------------------
+
+class StopCmd : public CmdResource {
+public:
+
+    // -- Contructors
+
+    StopCmd();
+
+    // -- Destructor
+
+    ~StopCmd();
+
+private:
+
+    // -- Overridden methods
+
+    // From CmdResource
+
+    void execute(std::istream&, std::ostream&, CmdArg&) override;
+
+    void help(std::ostream&) const override;
+    Arg usage(const std::string& cmd) const override;
+};
+
+//-----------------------------------------------------------------------------
+
+}  // namespace eckit
+
+#endif
