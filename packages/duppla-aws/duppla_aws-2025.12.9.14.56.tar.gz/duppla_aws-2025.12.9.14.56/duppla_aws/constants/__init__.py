9dc8bc79e4be272e56eff02d487663b27ad02c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:07 +0000

    Add package duppla-aws/2025.12.9.14.56/duppla_aws-2025.12.9.14.56.tar.gz

---
+++ packages/duppla-aws/duppla_aws-2025.12.9.14.56.tar.gz/duppla_aws-2025.12.9.14.56/duppla_aws/constants/__init__.py
@@ -0,0 +1,31 @@
+# Buckets
+BUCKET_CONTABILIDAD = "duppla-contabilidad"
+BUCKET_CREDENTIALS = "credentials-duppla"
+BUCKET_FACHADAS_INMUEBLES = "fachadasinmuebles"
+BUCKET_FOTOS_CASOS = "relatedphotoscases"
+BUCKET_HTML_TEMPLATES = "duppla-html-templates"
+BUCKET_PHOTOS = "formulario-visitas"
+# Files
+FIDUCIARIA_FACTURAS_FILENAME = "fiduciaria_facturas.csv"
+FIDUCIARIA_RECIBOS_FILENAME = "fiduciaria_recibos.csv"
+GOOGLE_CREDENTIALS = "credentials_gsa.json"
+GOOGLE_CREDENTIALS_INVERSIONISTAS = "credentials_gsa_inversionistas.json"
+GOOGLE_CLOUD_VISION_CREDENTIALS = "credentials_vision_gsa.json"
+GOOGLE_CREDENTIALS_ALIADOS = "credentials_gsa_aliados.json"
+# Queues
+COBRE_FIFO_QUEUE = "https://sqs.us-east-1.amazonaws.com/278566750976/cobre-webhook.fifo"
+
+__all__ = [
+    "BUCKET_CONTABILIDAD",
+    "BUCKET_CREDENTIALS",
+    "BUCKET_FACHADAS_INMUEBLES",
+    "BUCKET_FOTOS_CASOS",
+    "BUCKET_HTML_TEMPLATES",
+    "BUCKET_PHOTOS",
+    #
+    "FIDUCIARIA_FACTURAS_FILENAME",
+    "FIDUCIARIA_RECIBOS_FILENAME",
+    "GOOGLE_CREDENTIALS",
+    "GOOGLE_CLOUD_VISION_CREDENTIALS",
+    "GOOGLE_CREDENTIALS_ALIADOS",
+]
