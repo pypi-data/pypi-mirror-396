a92b288d36208296d63960d7a1ccf9f0be974def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:30:50 +0000

    Add package project-diagnose/0.1.16/project_diagnose-0.1.16.tar.gz

---
+++ packages/project-diagnose/project_diagnose-0.1.16.tar.gz/project_diagnose-0.1.16/setup.py
@@ -0,0 +1,32 @@
+# DIAGNOSE/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="project-diagnose",
+    version="0.1.16",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "flask",
+        "pydantic>=2.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "project-diagnose=project_diagnose.cli:main"
+        ]
+    },
+    author="Razmakh",  # можешь поставить псевдоним
+    description="AI-анализатор хаоса проекта и web-интерфейс для просмотра отчётов.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourname/project-diagnose",  # если будет
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
+    python_requires=">=3.8",
+)
