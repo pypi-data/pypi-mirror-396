78f627ed8d37dfbd64acc3960994a90f09eed8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:42 +0000

    Add package certbot-dns-contabo-api/1.0.0/certbot_dns_contabo_api-1.0.0.tar.gz

---
+++ packages/certbot-dns-contabo-api/certbot_dns_contabo_api-1.0.0.tar.gz/certbot_dns_contabo_api-1.0.0/certbot_dns_contabo/__init__.py
@@ -0,0 +1,3 @@
+"""Contabo DNS Authenticator for Certbot"""
+
+__version__ = "1.0.0"
---
+++ packages/certbot-dns-contabo-api/certbot_dns_contabo_api-1.0.0.tar.gz/certbot_dns_contabo_api-1.0.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="certbot-dns-contabo-api",
+    version="1.0.0",
+    description="Contabo DNS Authenticator plugin for Certbot",
+    author="Custom",
+    license="Apache License 2.0",
+    python_requires=">=3.7",
+    packages=find_packages(),
+    install_requires=[
+        "certbot>=1.0.0",
+        "requests>=2.20.0",
+    ],
+    entry_points={
+        "certbot.plugins": [
+            "dns-contabo = certbot_dns_contabo.dns_contabo:Authenticator",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Security",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Networking",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
+)
