2896c21b5d12961c2266ab0919f090647c0f1d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:31:10 +0000

    Add package pyodps/0.12.5.1/pyodps-0.12.5.1-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/pyodps/pyodps-0.12.5.1-cp39-cp39-macosx_10_9_x86_64.whl/odps/ml/metrics/__init__.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+# Copyright 1999-2025 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import types
+
+from .classification import *
+from .regression import *
+from .clustering import *
+from .scorer import SCORERS
+
+from ..algolib.loader import load_defined_algorithms
+
+# required by autodoc of sphinx
+__all__ = [k for k, v in globals().items()
+           if not k.startswith('_') and isinstance(v, (types.FunctionType, type))]
+
+load_defined_algorithms(sys.modules[__name__], 'metrics')
