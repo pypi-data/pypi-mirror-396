191f1331e3dc4850ab0fa9db227895cbc779dd70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:31:35 +0000

    Add package dkist-processing-visp/5.1.2/dkist_processing_visp-5.1.2.tar.gz

---
+++ packages/dkist-processing-visp/dkist_processing_visp-5.1.2.tar.gz/dkist_processing_visp-5.1.2/dkist_processing_visp/models/task_name.py
@@ -0,0 +1,9 @@
+"""Controlled list of visp-specific task tag names."""
+
+from enum import Enum
+
+
+class VispTaskName(str, Enum):
+    """Controlled list of task tag names."""
+
+    background = "BACKGROUND"
