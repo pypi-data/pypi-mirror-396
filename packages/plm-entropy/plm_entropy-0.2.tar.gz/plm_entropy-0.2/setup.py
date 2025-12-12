1b33226a5445fc061ed8087779e99a58ade45fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:31:49 +0000

    Add package plm-entropy/0.2/plm_entropy-0.2.tar.gz

---
+++ packages/plm-entropy/plm_entropy-0.2.tar.gz/plm_entropy-0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, Extension
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+        
+        
+setup(
+    name='plm_entropy',
+    version='0.2',
+    description='The pLM entropy Python package',
+    long_description= readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/spyros-lytras/plm_entropy',
+    download_url = 'https://github.com/spyros-lytras/plm_entropy/dist/plm_entropy-0.1.tar.gz',
+    classifiers=[
+    "Intended Audience :: Science/Research",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Programming Language :: Python",
+    ],
+    keywords='pLM LLM viruses conservation protein',
+    project_urls={
+    'Documentation': 'https://github.com/spyros-lytras/plm_entropy',
+    },
+    author='Spyros Lytras',
+    author_email='spyros@ims.u-tokyo.ac.jp',
+    license='MIT',
+    packages=['plm_entropy'],
+    install_requires=[
+    'biopython==1.83',
+    'pandas==2.2.2',
+    'numpy==1.26.4',
+    'fair-esm==2.0.0',
+    'transformers==4.41.2',
+    'scipy==1.13.1',
+    'torch==2.3.1',
+    'typing-extensions==4.12.2',
+    'sentencepiece==0.2.0'],
+    python_requires="~=3.12")
