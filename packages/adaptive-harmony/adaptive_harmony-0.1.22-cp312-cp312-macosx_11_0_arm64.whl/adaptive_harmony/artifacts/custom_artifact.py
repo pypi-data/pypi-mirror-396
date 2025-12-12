5707f992f3fd3aedf208d9872403d1c515e90d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:45 +0000

    Add package adaptive-harmony/0.1.22/adaptive_harmony-0.1.22-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/adaptive-harmony/adaptive_harmony-0.1.22-cp312-cp312-macosx_11_0_arm64.whl/adaptive_harmony/artifacts/custom_artifact.py
@@ -0,0 +1,46 @@
+import uuid
+
+from adaptive_harmony.adaptive_harmony import JobArtifact
+from adaptive_harmony.runtime.context import RecipeContext
+
+
+class CustomArtifact:
+    def __init__(self, name: str, ctx: RecipeContext, file: str | None = None) -> None:
+        self._base = JobArtifact(
+            id=str(uuid.uuid4()),
+            name=name,
+            kind="custom",
+            uri=f"file://{file}" if file else None,
+        )
+        self.ctx = ctx
+        self.ctx.job.register_artifact(self._base)
+
+    @property
+    def id(self) -> str:
+        return self._base.id
+
+    @property
+    def name(self) -> str:
+        return self._base.name
+
+    @property
+    def kind(self) -> str:
+        return self._base.kind
+
+    @property
+    def uri(self) -> str:
+        assert self._base.uri is not None
+        return self._base.uri
+
+    def write_file(self, file_path: str) -> None:
+        self.ctx.file_storage.write(file_path, self.uri)
+
+    def append_file(self, file_path: str) -> None:
+        with open(file_path, "rb") as f:
+            self.ctx.file_storage.append(f.read(), self.uri)
+
+    def read_file(self, file_path: str) -> bytes:
+        return self.ctx.file_storage.read(file_path)
+
+    def __repr__(self):
+        return f"CustomArtifact(id={self.id}, name={self.name}, kind={self.kind}, uri={self.uri})"
