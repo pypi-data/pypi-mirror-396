06d3a384f30f4f8941553b476ed1409455f2a34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:42 +0000

    Add package OrcFxAPI/11.6.1/orcfxapi-11.6.1-py3-none-any.whl

---
+++ packages/OrcFxAPI/orcfxapi-11.6.1-py3-none-any.whl/OrcFxAPIConfig.py
@@ -0,0 +1,69 @@
+import sys
+
+if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 6):
+    raise ImportError("OrcFxAPI requires Python 3.6 or later")
+import os
+import os.path
+import ctypes
+import ctypes.wintypes
+import builtins
+import winreg
+from typing import Optional, Union
+
+_libPathName = "_OrcFxAPIlib"
+_libHandleName = "_OrcFxAPIlibHandle"
+_libPath = None
+_libHandle = None
+
+
+def getLibPath() -> str:
+    result = _libPath
+    if result is not None:
+        return result
+
+    result = os.environ.get(_libPathName, None)
+    if result is not None:
+        return result
+
+    try:
+        with winreg.OpenKey(
+            winreg.HKEY_LOCAL_MACHINE,
+            "Software\\Orcina\\OrcaFlex\\Installation Directory",
+            0,
+            winreg.KEY_READ | winreg.KEY_WOW64_32KEY,
+        ) as key:
+            installationDirectory = winreg.QueryValueEx(key, "Normal")[0]
+    except BaseException:  # registry key will not exist if OrcaFlex is not installed
+        raise Exception("OrcaFlex not found")
+    is64bit = ctypes.sizeof(ctypes.c_void_p) == 8
+    if is64bit:
+        platform = "Win64"
+    else:
+        platform = "Win32"
+    return os.path.join(installationDirectory, "OrcFxAPI", platform, "OrcFxAPI.dll")
+
+
+def setLibPath(value: Union[str, os.PathLike], childProcessInherit: Optional[bool] = False) -> None:
+    global _libPath
+    _libPath = os.path.abspath(str(value))
+    if childProcessInherit:
+        os.environ[_libPathName] = _libPath
+
+
+def getLibHandle() -> Optional[ctypes.wintypes.HMODULE]:
+    result = _libHandle
+    if result is None:
+        result = getattr(builtins, _libHandleName, None)
+    return result
+
+
+def setLibHandle(value: ctypes.wintypes.HMODULE) -> None:
+    global _libHandle
+    _libHandle = value
+
+
+def lib() -> ctypes.WinDLL:
+    libHandle = getLibHandle()
+    if libHandle:
+        return ctypes.WinDLL("", handle=libHandle)  # type: ignore[arg-type]
+    return ctypes.WinDLL(getLibPath())
