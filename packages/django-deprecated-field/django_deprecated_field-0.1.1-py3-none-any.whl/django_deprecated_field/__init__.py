881925d8f2c275fe96898314192831e61cf23f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:58 +0000

    Add package django-deprecated-field/0.1.1/django_deprecated_field-0.1.1-py3-none-any.whl

---
+++ packages/django-deprecated-field/django_deprecated_field-0.1.1-py3-none-any.whl/django_deprecated_field/__init__.py
@@ -0,0 +1,9 @@
+"""
+Django Deprecated Field - A field that marks model fields as deprecated.
+"""
+
+__version__ = "0.1.1"
+
+from .deprecated_field import DeprecatedField, deprecated
+
+__all__ = ["DeprecatedField", "deprecated"]
