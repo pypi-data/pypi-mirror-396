8a1ccc592c78e9ad8fab6b205451ab1e4b6ed22b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:14:11 +0000

    Add package run/0.3/run-0.3.tar.gz

---
+++ packages/run/run-0.3.tar.gz/run-0.3/setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+
+
+setup(name='run',
+    version='0.3',
+    description='Python Process Run Control',
+    author='Wijnand Modderman',
+    author_email='maze@pyth0n.org',
+    url='https://github.com/tehmaze/run',
+    license='MIT',
+    long_description=open('README').read(),
+    packages=['run'],
+)
