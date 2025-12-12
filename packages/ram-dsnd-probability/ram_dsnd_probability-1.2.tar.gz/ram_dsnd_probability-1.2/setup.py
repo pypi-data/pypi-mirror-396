29568e70b5d786b9e8881ccc06e7b3a4063b08e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:30:58 +0000

    Add package ram-dsnd-probability/1.2/ram_dsnd_probability-1.2.tar.gz

---
+++ packages/ram-dsnd-probability/ram_dsnd_probability-1.2.tar.gz/ram_dsnd_probability-1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ram_dsnd_probability",
+    version="1.2",
+    description="Gaussian and Binomial distributions",
+    packages=["ram_dsnd_probability"],
+    author="Ram Parajuli",
+    author_email="ramprasad.1991.rp@gmail.com",
+    zip_safe=False,
+)
