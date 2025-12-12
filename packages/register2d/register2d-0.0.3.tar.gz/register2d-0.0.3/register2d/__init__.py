9df28d37ed403d6107d1473cf7951ca232c0e64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:31:50 +0000

    Add package register2d/0.0.3/register2d-0.0.3.tar.gz

---
+++ packages/register2d/register2d-0.0.3.tar.gz/register2d-0.0.3/register2d/__init__.py
@@ -0,0 +1,9 @@
+from .process import find_match_pos_and_rotate, get_red_mask_image, get_rotated_and_moved_image
+from .rotate import rotate_and_crop_white_borders
+
+__all__ = [
+    "find_match_pos_and_rotate",
+    "get_red_mask_image",
+    "get_rotated_and_moved_image",
+    "rotate_and_crop_white_borders"
+]
