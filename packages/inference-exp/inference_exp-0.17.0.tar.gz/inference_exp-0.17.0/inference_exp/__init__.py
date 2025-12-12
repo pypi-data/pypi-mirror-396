359b3f0fb2b51fd0055e2afb388a6e8410982df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:03 +0000

    Add package inference-exp/0.17.0/inference_exp-0.17.0.tar.gz

---
+++ packages/inference-exp/inference_exp-0.17.0.tar.gz/inference_exp-0.17.0/inference_exp/__init__.py
@@ -0,0 +1,37 @@
+import os
+
+if os.environ.get("PYTORCH_ENABLE_MPS_FALLBACK") is None:
+    os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+
+from inference_exp.entities import ColorFormat
+from inference_exp.model_pipelines.auto_loaders.core import (
+    AutoModelPipeline,
+    PipelineModelParameters,
+)
+from inference_exp.models.auto_loaders.core import AutoModel
+from inference_exp.models.base.classification import (
+    ClassificationModel,
+    ClassificationPrediction,
+    MultiLabelClassificationModel,
+    MultiLabelClassificationPrediction,
+)
+from inference_exp.models.base.depth_estimation import DepthEstimationModel
+from inference_exp.models.base.documents_parsing import (
+    StructuredOCRModel,
+    TextOnlyOCRModel,
+)
+from inference_exp.models.base.embeddings import TextImageEmbeddingModel
+from inference_exp.models.base.instance_segmentation import (
+    InstanceDetections,
+    InstanceSegmentationModel,
+)
+from inference_exp.models.base.keypoints_detection import (
+    KeyPoints,
+    KeyPointsDetectionModel,
+)
+from inference_exp.models.base.object_detection import (
+    Detections,
+    ObjectDetectionModel,
+    OpenVocabularyObjectDetectionModel,
+)
+from inference_exp.models.base.semantic_segmentation import SemanticSegmentationModel
