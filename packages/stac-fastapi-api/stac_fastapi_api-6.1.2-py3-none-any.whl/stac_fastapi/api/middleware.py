f35e4cbd5b14f4b304a15a79e92880dcab89a808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:14:09 +0000

    Add package stac-fastapi-api/6.1.2/stac_fastapi_api-6.1.2-py3-none-any.whl

---
+++ packages/stac-fastapi-api/stac_fastapi_api-6.1.2-py3-none-any.whl/stac_fastapi/api/middleware.py
@@ -0,0 +1,135 @@
+"""Api middleware."""
+
+import contextlib
+import re
+import typing
+from http.client import HTTP_PORT, HTTPS_PORT
+from typing import List, Optional, Tuple
+
+from starlette.middleware.cors import CORSMiddleware as _CORSMiddleware
+from starlette.types import ASGIApp, Receive, Scope, Send
+
+
+class CORSMiddleware(_CORSMiddleware):
+    """Subclass of Starlette's standard CORS middleware with default values set to those
+    recommended by the STAC API spec.
+
+    https://github.com/radiantearth/stac-api-spec/blob/914cf8108302e2ec734340080a45aaae4859bb63/implementation.md#cors
+    """
+
+    def __init__(
+        self,
+        app: ASGIApp,
+        allow_origins: typing.Sequence[str] = ("*",),
+        allow_methods: typing.Sequence[str] = (
+            "OPTIONS",
+            "POST",
+            "GET",
+        ),
+        allow_headers: typing.Sequence[str] = ("Content-Type",),
+        allow_credentials: bool = False,
+        allow_origin_regex: typing.Optional[str] = None,
+        expose_headers: typing.Sequence[str] = (),
+        max_age: int = 600,
+    ) -> None:
+        """Create CORS middleware."""
+        super().__init__(
+            app,
+            allow_origins,
+            allow_methods,
+            allow_headers,
+            allow_credentials,
+            allow_origin_regex,
+            expose_headers,
+            max_age,
+        )
+
+
+_PROTO_HEADER_REGEX = re.compile(r"proto=(?P<proto>http(s)?)")
+_HOST_HEADER_REGEX = re.compile(r"host=(?P<host>[\w.-]+)(:(?P<port>\d{1,5}))?")
+
+
+class ProxyHeaderMiddleware:
+    """Account for forwarding headers when deriving base URL.
+
+    Prioritise standard Forwarded header, look for non-standard X-Forwarded-* if missing.
+    Default to what can be derived from the URL if no headers provided. Middleware updates
+    the host header that is interpreted by starlette when deriving Request.base_url.
+    """
+
+    def __init__(self, app: ASGIApp):
+        """Create proxy header middleware."""
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        """Call from stac-fastapi framework."""
+        if scope["type"] == "http":
+            proto, domain, port = self._get_forwarded_url_parts(scope)
+            scope["scheme"] = proto
+            if domain is not None:
+                port_suffix = ""
+                if port is not None:
+                    if (proto == "http" and port != HTTP_PORT) or (
+                        proto == "https" and port != HTTPS_PORT
+                    ):
+                        port_suffix = f":{port}"
+
+                scope["headers"] = self._replace_header_value_by_name(
+                    scope,
+                    "host",
+                    f"{domain}{port_suffix}",
+                )
+
+        await self.app(scope, receive, send)
+
+    def _get_forwarded_url_parts(self, scope: Scope) -> Tuple[str, str, str]:
+        proto = scope.get("scheme", "http")
+        header_host = self._get_header_value_by_name(scope, "host")
+        if header_host is None:
+            domain, port = scope.get("server")
+        else:
+            header_host_parts = header_host.split(":")
+            if len(header_host_parts) == 2:
+                domain, port = header_host_parts
+            else:
+                domain = header_host_parts[0]
+                port = None
+
+        port_str = None  # make sure it is defined in all paths since we access it later
+
+        if forwarded := self._get_header_value_by_name(scope, "forwarded"):
+            for proxy in forwarded.split(","):
+                if proto_expr := _PROTO_HEADER_REGEX.search(proxy):
+                    proto = proto_expr.group("proto")
+                if host_expr := _HOST_HEADER_REGEX.search(proxy):
+                    domain = host_expr.group("host")
+                    port_str = host_expr.group("port")  # None if not present in the match
+
+        else:
+            domain = self._get_header_value_by_name(scope, "x-forwarded-host", domain)
+            proto = self._get_header_value_by_name(scope, "x-forwarded-proto", proto)
+            port_str = self._get_header_value_by_name(scope, "x-forwarded-port", port)
+
+        with contextlib.suppress(ValueError):  # ignore ports that are not valid integers
+            port = int(port_str) if port_str is not None else port
+
+        return (proto, domain, port)
+
+    def _get_header_value_by_name(
+        self, scope: Scope, header_name: str, default_value: Optional[str] = None
+    ) -> Optional[str]:
+        headers = scope["headers"]
+        candidates = [
+            value.decode() for key, value in headers if key.decode() == header_name
+        ]
+        return candidates[0] if len(candidates) == 1 else default_value
+
+    @staticmethod
+    def _replace_header_value_by_name(
+        scope: Scope, header_name: str, new_value: str
+    ) -> List[Tuple[str, str]]:
+        return [
+            (name, value)
+            for name, value in scope["headers"]
+            if name.decode() != header_name
+        ] + [(str.encode(header_name), str.encode(new_value))]
---
+++ packages/stac-fastapi-api/stac_fastapi_api-6.1.2-py3-none-any.whl/stac_fastapi/api/models.py
@@ -0,0 +1,144 @@
+"""Api request/response models."""
+
+from typing import List, Literal, Optional, Type, Union
+
+import attr
+from fastapi import Path, Query
+from pydantic import BaseModel, create_model
+from stac_pydantic.shared import BBox
+from typing_extensions import Annotated
+
+from stac_fastapi.types.extension import ApiExtension
+from stac_fastapi.types.search import (
+    APIRequest,
+    BaseSearchGetRequest,
+    BaseSearchPostRequest,
+    DatetimeMixin,
+    DateTimeQueryType,
+    Limit,
+    _bbox_converter,
+    _validate_datetime,
+)
+
+try:
+    import orjson  # noqa
+    from fastapi.responses import ORJSONResponse as JSONResponse
+except ImportError:  # pragma: nocover
+    from starlette.responses import JSONResponse
+
+
+def create_request_model(
+    model_name="SearchGetRequest",
+    base_model: Union[Type[BaseModel], Type[APIRequest]] = BaseSearchGetRequest,
+    extensions: Optional[List[ApiExtension]] = None,
+    mixins: Optional[Union[List[Type[BaseModel]], List[Type[APIRequest]]]] = None,
+    request_type: Optional[str] = "GET",
+) -> Union[Type[BaseModel], Type[APIRequest]]:
+    """Create a pydantic model for validating request bodies."""
+    fields = {}
+    extension_models = []
+
+    # Check extensions for additional parameters to search
+    for extension in extensions or []:
+        if extension_model := extension.get_request_model(request_type):
+            extension_models.append(extension_model)
+
+    mixins = mixins or []
+
+    models = [base_model] + extension_models + mixins
+
+    # Handle GET requests
+    if all([issubclass(m, APIRequest) for m in models]):
+        return attr.make_class(model_name, attrs={}, bases=tuple(models))
+
+    # Handle POST requests
+    elif all([issubclass(m, BaseModel) for m in models]):
+        for model in models:
+            for k, field_info in model.model_fields.items():
+                fields[k] = (field_info.annotation, field_info)
+
+        return create_model(model_name, **fields, __base__=base_model)  # type: ignore
+
+    raise TypeError("Mixed Request Model types. Check extension request types.")
+
+
+def create_get_request_model(
+    extensions: Optional[List[ApiExtension]],
+    base_model: BaseSearchGetRequest = BaseSearchGetRequest,
+) -> Type[APIRequest]:
+    """Wrap create_request_model to create the GET request model."""
+
+    return create_request_model(
+        "SearchGetRequest",
+        base_model=base_model,
+        extensions=extensions,
+        request_type="GET",
+    )
+
+
+def create_post_request_model(
+    extensions: Optional[List[ApiExtension]],
+    base_model: BaseSearchPostRequest = BaseSearchPostRequest,
+) -> Type[BaseModel]:
+    """Wrap create_request_model to create the POST request model."""
+    return create_request_model(
+        "SearchPostRequest",
+        base_model=base_model,
+        extensions=extensions,
+        request_type="POST",
+    )
+
+
+@attr.s
+class CollectionUri(APIRequest):
+    """Get or delete collection."""
+
+    collection_id: Annotated[str, Path(description="Collection ID")] = attr.ib()
+
+
+@attr.s
+class ItemUri(APIRequest):
+    """Get or delete item."""
+
+    collection_id: Annotated[str, Path(description="Collection ID")] = attr.ib()
+    item_id: Annotated[str, Path(description="Item ID")] = attr.ib()
+
+
+@attr.s
+class EmptyRequest(APIRequest):
+    """Empty request."""
+
+    ...
+
+
+@attr.s
+class ItemCollectionUri(APIRequest, DatetimeMixin):
+    """Get item collection."""
+
+    collection_id: Annotated[str, Path(description="Collection ID")] = attr.ib()
+    limit: Annotated[
+        Optional[Limit],
+        Query(
+            description="Limits the number of results that are included in each page of the response (capped to 10_000)."  # noqa: E501
+        ),
+    ] = attr.ib(default=10)
+    bbox: Optional[BBox] = attr.ib(default=None, converter=_bbox_converter)  # type: ignore
+    datetime: DateTimeQueryType = attr.ib(default=None, validator=_validate_datetime)
+
+
+class GeoJSONResponse(JSONResponse):
+    """JSON with custom, vendor content-type."""
+
+    media_type = "application/geo+json"
+
+
+class JSONSchemaResponse(JSONResponse):
+    """JSON with custom, vendor content-type."""
+
+    media_type = "application/schema+json"
+
+
+class HealthCheck(BaseModel, extra="allow"):
+    """health check response model."""
+
+    status: Literal["UP", "DOWN"]
