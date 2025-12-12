6f5659d2eafe4c0fe144a5e278fdff9361e91059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:28 +0000

    Add package qat-compiler/3.1.1/qat_compiler-3.1.1.tar.gz

---
+++ packages/qat-compiler/qat_compiler-3.1.1.tar.gz/qat_compiler-3.1.1/src/qat/backend/qblox/execution.py
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024-2025 Oxford Quantum Circuits Ltd
+
+from dataclasses import dataclass, field
+
+import numpy as np
+from pydantic import ConfigDict
+
+from qat.backend.passes.purr.analysis import TriageResult
+from qat.backend.qblox.config.specification import ModuleConfig, SequencerConfig
+from qat.backend.qblox.ir import Sequence
+from qat.executables import AbstractProgram
+
+
+@dataclass
+class QbloxPackage:
+    pulse_channel_id: str | None = None
+    physical_channel_id: str | None = None
+    instrument_id: str | None = None
+    seq_idx: int | None = None
+    seq_config: SequencerConfig = field(default_factory=lambda: SequencerConfig())
+    slot_idx: int | None = None
+    mod_config: ModuleConfig = field(default_factory=lambda: ModuleConfig())
+    sequence: Sequence | None = None
+    timeline: np.ndarray | None = None
+
+
+class QbloxProgram(AbstractProgram):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    # COMPILER 828: change to pydantic objects (not dataclasses)
+    packages: dict[str, QbloxPackage]
+    triage_result: TriageResult
+
+    @property
+    def acquire_shapes(self) -> dict[str, tuple[int, ...]]:
+        return {}
