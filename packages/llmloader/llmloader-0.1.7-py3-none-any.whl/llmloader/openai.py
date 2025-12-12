7c689fdb3ba993052830ec19237768df387a6450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:30:46 +0000

    Add package llmloader/0.1.7/llmloader-0.1.7-py3-none-any.whl

---
+++ packages/llmloader/llmloader-0.1.7-py3-none-any.whl/llmloader/openai.py
@@ -0,0 +1,30 @@
+import os
+from langchain_core.language_models.llms import LLM
+
+from .loader import Loader
+
+
+class OpenAILoader(Loader):
+    def __call__(
+        self,
+        model: str,
+        temperature: float | None = None,
+        api_key: str = "",
+        max_tokens: int = None,
+        **kwargs,
+    ) -> LLM | None:
+        if not model.startswith('gpt') and not model.startswith('o1-'):
+            return None
+
+        from langchain_openai import ChatOpenAI
+
+        api_key = api_key or os.getenv("OPENAI_API_KEY", "")
+        api_key = api_key if api_key != "" else None
+
+        return ChatOpenAI(
+            openai_api_key=api_key,
+            temperature=temperature,
+            model_name=model,
+            max_tokens=max_tokens,
+            **kwargs,
+        )
