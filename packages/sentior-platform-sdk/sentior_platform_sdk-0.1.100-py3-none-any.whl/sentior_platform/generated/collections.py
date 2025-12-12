f453fa450fa967e96f8a5b6913dfc380d13dae91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 03:31:29 +0000

    Add package sentior-platform-sdk/0.1.100/sentior_platform_sdk-0.1.100-py3-none-any.whl

---
+++ packages/sentior-platform-sdk/sentior_platform_sdk-0.1.100-py3-none-any.whl/sentior_platform/generated/collections.py
@@ -0,0 +1,11 @@
+"""
+AUTO-GENERATED. NIET HANDMATIG AANPASSEN.
+"""
+from typing import Dict
+
+COLLECTIONS: Dict[str, str] = {
+  "jobs": "viktor_jobs",
+  "evaluations": "users.workspaces.evaluations",
+  "recommendations": "viktor_recommendations"
+}
+
