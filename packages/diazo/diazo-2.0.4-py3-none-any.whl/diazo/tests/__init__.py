57204664b60c6bdf4ef821cc7c3d6784eb258354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:10 +0000

    Add package diazo/2.0.4/diazo-2.0.4-py3-none-any.whl

---
+++ packages/diazo/diazo-2.0.4-py3-none-any.whl/diazo/tests/__init__.py
@@ -0,0 +1,13 @@
+def alltests():
+    from diazo.tests import test_diazo
+    from diazo.tests import test_trace
+    from diazo.tests import test_wsgi
+
+    import unittest
+
+    modules = [
+        test_trace,
+        test_wsgi,
+        test_diazo,
+    ]
+    return unittest.TestSuite([module.test_suite() for module in modules])
