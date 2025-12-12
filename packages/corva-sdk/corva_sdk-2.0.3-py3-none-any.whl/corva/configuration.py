cfbf945a5e3c906c9f3daea9fd288785bc0e201b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:26 +0000

    Add package corva-sdk/2.0.3/corva_sdk-2.0.3-py3-none-any.whl

---
+++ packages/corva-sdk/corva_sdk-2.0.3-py3-none-any.whl/corva/configuration.py
@@ -0,0 +1,50 @@
+import datetime
+
+import pydantic_settings
+from pydantic import AnyHttpUrl, BeforeValidator, TypeAdapter
+from typing_extensions import Annotated
+
+
+def validate_http_url_to_str(v: str) -> str:
+    TypeAdapter(AnyHttpUrl).validate_python(v)
+    return v
+
+
+HttpUrlStr = Annotated[str, BeforeValidator(validate_http_url_to_str)]
+
+
+class Settings(pydantic_settings.BaseSettings):
+    # api
+    API_ROOT_URL: HttpUrlStr
+    DATA_API_ROOT_URL: HttpUrlStr
+
+    # cache
+    CACHE_URL: str
+    CACHE_SKIP_MIGRATION: int = 0
+
+    # logger
+    LOG_LEVEL: str = 'INFO'
+    LOG_THRESHOLD_MESSAGE_SIZE: int = 1000
+    LOG_THRESHOLD_MESSAGE_COUNT: int = 15
+
+    # company and app
+    APP_KEY: str  # <provider-name-with-dashes>.<app-name-with-dashes>
+    PROVIDER: str
+
+    # secrets
+    SECRETS_CACHE_TTL: int = int(datetime.timedelta(minutes=5).total_seconds())
+
+    # keep-alive
+    POOL_CONNECTIONS_COUNT: int = 20  # Total pools count
+    POOL_MAX_SIZE: int = 20  # Max connections count per pool/host
+    POOL_BLOCK: bool = True  # Wait until connection released
+
+    # retry
+    MAX_RETRY_COUNT: int = 3  # If `0` then retries will be disabled
+    BACKOFF_FACTOR: float = 1.0
+
+    # OTEL
+    OTEL_LOG_SENDING_DISABLED: bool = False
+
+
+SETTINGS = Settings()  # type: ignore[call-arg]
