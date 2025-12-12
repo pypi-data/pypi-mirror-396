1d5e29a99c320340abc041f08287f243dea6a853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:43 +0000

    Add package sechubman/0.2.0/sechubman-0.2.0-py3-none-any.whl

---
+++ packages/sechubman/sechubman-0.2.0-py3-none-any.whl/sechubman/__init__.py
@@ -0,0 +1,9 @@
+"""The main package of sechubman."""
+
+from .sechubman import Rule, validate_filters, validate_updates
+
+__all__ = [
+    "Rule",
+    "validate_filters",
+    "validate_updates",
+]
