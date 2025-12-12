3d9d9c1980af4edcedb7f36485da5ef23d0b4479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:58 +0000

    Add package nomenklatura/4.3.2/nomenklatura-4.3.2-py3-none-any.whl

---
+++ packages/nomenklatura/nomenklatura-4.3.2-py3-none-any.whl/nomenklatura/__init__.py
@@ -0,0 +1,6 @@
+from nomenklatura.resolver import Resolver, Linker
+from nomenklatura.store import Store, View
+from nomenklatura.judgement import Judgement
+
+__version__ = "4.3.2"
+__all__ = ["Resolver", "Store", "View", "Judgement", "Linker"]
