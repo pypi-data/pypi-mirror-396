df571ebc493ecef67f502b6df127e9de3799d5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 05:06:29 +0000

    Add package navigator-auth/0.15.10/navigator_auth-0.15.10-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/navigator-auth/navigator_auth-0.15.10-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/navigator_auth/backends/__init__.py
@@ -0,0 +1,32 @@
+"""Navigator.
+
+Authentication Backends.
+"""
+from .noauth import NoAuth
+from .basic import BasicAuth
+from .django import DjangoAuth
+from .troc import TrocToken
+from .token import TokenAuth
+from .api import APIKeyAuth
+from .google import GoogleAuth
+from .okta import OktaAuth
+from .adfs import ADFSAuth
+from .azure import AzureAuth
+from .github import GithubAuth
+from .oauth2 import Oauth2Provider
+
+
+__all__ = (
+    "NoAuth",
+    "BasicAuth",
+    "DjangoAuth",
+    "TrocToken",
+    "TokenAuth",
+    "APIKeyAuth",
+    "GoogleAuth",
+    "OktaAuth",
+    "ADFSAuth",
+    "AzureAuth",
+    "GithubAuth",
+    "Oauth2Provider"
+)
