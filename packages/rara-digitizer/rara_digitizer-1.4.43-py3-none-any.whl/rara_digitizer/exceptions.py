addcf8fcb39710226335ed2c6e6b6bd7194c2143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-12 04:13:28 +0000

    Add package rara-digitizer/1.4.43/rara_digitizer-1.4.43-py3-none-any.whl

---
+++ packages/rara-digitizer/rara_digitizer-1.4.43-py3-none-any.whl/rara_digitizer/exceptions.py
@@ -0,0 +1,53 @@
+class MultipleMETSFilesFound(Exception):
+    pass
+
+
+class NoMETSFileFound(Exception):
+    pass
+
+
+class PathNotFound(Exception):
+    pass
+
+
+class NotLoadedOrEmpty(Exception):
+    pass
+
+
+class UnsupportedFile(Exception):
+    pass
+
+
+class FileTypeOrStructureMismatch(Exception):
+    pass
+
+
+class PageOutOfRange(Exception):
+    pass
+
+
+class ConversionFailed(Exception):
+    pass
+
+
+class DuplicateResourceKey(Exception):
+    pass
+
+
+class InvalidResourceKey(Exception):
+    pass
+
+
+class MissingEpubResource(Exception):
+    pass
+
+
+class UnknownTxtEncoding(Exception):
+    pass
+
+
+class MetsTagNotFound(Exception):
+    pass
+
+class XmlFileNotFound(Exception):
+    pass
