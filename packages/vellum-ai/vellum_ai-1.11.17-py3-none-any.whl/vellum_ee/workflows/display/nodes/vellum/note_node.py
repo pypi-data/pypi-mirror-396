7457df61a20b36cf0f3f8cc06170bb5c0f8d0400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 09:08:53 +0000

    Add package vellum-ai/1.11.17/vellum_ai-1.11.17-py3-none-any.whl

---
+++ packages/vellum-ai/vellum_ai-1.11.17-py3-none-any.whl/vellum_ee/workflows/display/nodes/vellum/note_node.py
@@ -0,0 +1,29 @@
+from typing import Any, ClassVar, Dict, Generic, TypeVar, Union
+
+from vellum.workflows.nodes import NoteNode
+from vellum.workflows.types.core import JsonObject
+from vellum_ee.workflows.display.nodes.base_node_display import BaseNodeDisplay
+from vellum_ee.workflows.display.types import WorkflowDisplayContext
+
+_NoteNodeType = TypeVar("_NoteNodeType", bound=NoteNode)
+
+
+class BaseNoteNodeDisplay(BaseNodeDisplay[_NoteNodeType], Generic[_NoteNodeType]):
+    text: ClassVar[str] = ""
+    style: ClassVar[Union[Dict[str, Any], None]] = None
+
+    def serialize(self, display_context: WorkflowDisplayContext, **kwargs: Any) -> JsonObject:
+        del kwargs  # Unused parameters
+        node_id = self.node_id
+
+        return {
+            "id": str(node_id),
+            "type": "NOTE",
+            "inputs": [],
+            "data": {
+                "label": self.label,
+                "text": self.text,
+                "style": self.style,
+            },
+            **self.serialize_generic_fields(display_context, exclude=["outputs"]),
+        }
