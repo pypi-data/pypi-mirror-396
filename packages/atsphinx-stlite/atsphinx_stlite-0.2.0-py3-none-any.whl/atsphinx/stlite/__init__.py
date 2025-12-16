2ef343a14cb95a51df352c31119a2a4fed24da63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 13:18:36 +0000

    Add package atsphinx-stlite/0.2.0/atsphinx_stlite-0.2.0-py3-none-any.whl

---
+++ packages/atsphinx-stlite/atsphinx_stlite-0.2.0-py3-none-any.whl/atsphinx/stlite/__init__.py
@@ -0,0 +1,28 @@
+"""Embed Stlite frame into Sphinx documentation."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from sphinx.util import logging
+
+from . import directives, nodes
+
+if TYPE_CHECKING:
+    from sphinx.application import Sphinx
+
+__version__ = "0.0.0"
+
+logger = logging.getLogger(__name__)
+
+
+def setup(app: Sphinx):  # noqa: D103
+    app.add_config_value("stlite_default_version", "latest", "env", str)
+    nodes._setup(app)
+    directives._setup(app)
+    return {
+        "version": __version__,
+        "env_version": 1,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
