0d34524645d7cd6127d337da12a9ced8624da5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:07:44 +0000

    Add package vhs-decode/0.3.8.1/vhs_decode-0.3.8.1-cp314-cp314-musllinux_1_2_x86_64.whl

---
+++ packages/vhs-decode/vhs_decode-0.3.8.1-cp314-cp314-musllinux_1_2_x86_64.whl/vhsdecode/load_params_json.py
@@ -0,0 +1,26 @@
+import json
+
+
+def _override_group(json_input, group, group_name, logger):
+    """Go through a group of parameters and replace the ones found in 'json_input'"""
+    changed = {}
+    if group_name in json_input:
+        for item in json_input[group_name]:
+            if item in group:
+                changed[item] = json_input[group_name][item]
+                group[item] = json_input[group_name][item]
+            else:
+                logger.info(
+                    "Item %s in params json not in group %s. Not changed!",
+                    item,
+                    group_name,
+                )
+    if len(changed) > 0:
+        logger.debug("Changed %s in %s", changed, group_name)
+
+
+def override_params(sys_params, rf_params, file, logger):
+    """Override parameters with ones from the specified json file."""
+    json_input = json.load(file)
+    _override_group(json_input, sys_params, "sys_params", logger)
+    _override_group(json_input, rf_params, "rf_params", logger)
