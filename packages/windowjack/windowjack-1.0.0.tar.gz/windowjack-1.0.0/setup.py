4e567f5a330f54767e22662ef0625ad0b9c27805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 18:06:47 +0000

    Add package windowjack/1.0.0/windowjack-1.0.0.tar.gz

---
+++ packages/windowjack/windowjack-1.0.0.tar.gz/windowjack-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="windowjack",
+    version="1.0.0",
+    description="Windows screen and window capture library with occlusion support",
+    author="WindowJack",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
