a1b6b6273c6e199ae5c50cdddba3eb6e985ef4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 08:05:22 +0000

    Add package pecos-rslib/0.8.0.dev0/pecos_rslib-0.8.0.dev0-cp310-abi3-win_amd64.whl

---
+++ packages/pecos-rslib/pecos_rslib-0.8.0.dev0-cp310-abi3-win_amd64.whl/pecos_rslib/__init__.py
@@ -0,0 +1,16 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pecos_rslib.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from .pecos_rslib import *
+
+__doc__ = pecos_rslib.__doc__
+if hasattr(pecos_rslib, "__all__"):
+    __all__ = pecos_rslib.__all__
