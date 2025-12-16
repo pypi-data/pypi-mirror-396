0f5c398496abfee7bbda1d32fafe9e751e640039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:32:04 +0000

    Add package david8/0.6.0b1/david8-0.6.0b1-py3-none-any.whl

---
+++ packages/david8/david8-0.6.0b1-py3-none-any.whl/david8/protocols/query_builder.py
@@ -0,0 +1,25 @@
+from typing import Protocol
+
+from ..protocols.dml import DeleteProtocol, InsertProtocol, SelectProtocol, UpdateProtocol
+from .ddl import CreateTableProtocol
+from .sql import AliasedProtocol, ExprProtocol, FunctionProtocol
+
+
+class QueryBuilderProtocol(Protocol):
+    def select(self, *args: str | AliasedProtocol | ExprProtocol | FunctionProtocol) -> SelectProtocol:
+        pass
+
+    def with_(self, *args: tuple[str, SelectProtocol]) -> SelectProtocol:
+        pass
+
+    def update(self) -> UpdateProtocol:
+        pass
+
+    def insert(self) -> InsertProtocol:
+        pass
+
+    def delete(self) -> DeleteProtocol:
+        pass
+
+    def create_table_as(self, query: SelectProtocol, table: str, db: str = '') -> CreateTableProtocol:
+        pass
