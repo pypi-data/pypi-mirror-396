6a7d503d0db529a6fd1e0764a78b3d3eb969e0f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 19:09:59 +0000

    Add package bingauss/0.1/bingauss-0.1.tar.gz

---
+++ packages/bingauss/bingauss-0.1.tar.gz/bingauss-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="bingauss",
+ version="0.1",
+ description="Gaussian and Binomial distributions, Single Inheritance",
+ packages=['bingauss'],
+ author='Kevin Howard',
+ author_email='howardkc@alumni.purdue.edu',
+ zip_safe=False)
