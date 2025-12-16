009d5d54380f166b187d7d146fbd8d777e449d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 14:07:22 +0000

    Add package intellex-shared-client/0.1.1/intellex_shared_client-0.1.1-py3-none-any.whl

---
+++ packages/intellex-shared-client/intellex_shared_client-0.1.1-py3-none-any.whl/intellex_shared_client/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from intellex_shared_client.api.auth_api import AuthApi
+from intellex_shared_client.api.default_api import DefaultApi
+from intellex_shared_client.api.projects_api import ProjectsApi
+from intellex_shared_client.api.users_api import UsersApi
+
