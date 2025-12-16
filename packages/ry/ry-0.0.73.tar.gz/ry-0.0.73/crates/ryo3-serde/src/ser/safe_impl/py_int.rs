601a26575a87314eda070872e295a6dc059e8ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 17:09:09 +0000

    Add package ry/0.0.73/ry-0.0.73.tar.gz

---
+++ packages/ry/ry-0.0.73.tar.gz/ry-0.0.73/crates/ryo3-serde/src/ser/safe_impl/py_int.rs
@@ -0,0 +1,34 @@
+use pyo3::prelude::*;
+use serde::ser::{Serialize, Serializer};
+
+use crate::errors::pyerr2sererr;
+
+use pyo3::Bound;
+use pyo3::types::PyInt;
+
+pub(crate) struct SerializePyInt<'a, 'py> {
+    obj: &'a Bound<'py, PyAny>,
+}
+
+impl<'a, 'py> SerializePyInt<'a, 'py> {
+    #[inline]
+    pub(crate) fn new(obj: &'a Bound<'py, PyAny>) -> Self {
+        Self { obj }
+    }
+}
+
+impl Serialize for SerializePyInt<'_, '_> {
+    #[inline(always)]
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let v: i64 = self
+            .obj
+            .cast_exact::<PyInt>()
+            .map_err(pyerr2sererr)?
+            .extract()
+            .map_err(pyerr2sererr)?;
+        serializer.serialize_i64(v)
+    }
+}
