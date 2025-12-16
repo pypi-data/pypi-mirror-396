e959bc39fa0257e7beaa5ff6fbacad9b66fd9162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 15:10:16 +0000

    Add package openlayer/0.11.2/openlayer-0.11.2.tar.gz

---
+++ packages/openlayer/openlayer-0.11.2.tar.gz/openlayer-0.11.2/src/openlayer/types/projects/inference_pipeline_create_params.py
@@ -0,0 +1,55 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import Literal, Required, Annotated, TypedDict
+
+from ..._types import SequenceNotStr
+from ..._utils import PropertyInfo
+
+__all__ = ["InferencePipelineCreateParams", "Project", "Workspace"]
+
+
+class InferencePipelineCreateParams(TypedDict, total=False):
+    description: Required[Optional[str]]
+    """The inference pipeline description."""
+
+    name: Required[str]
+    """The inference pipeline name."""
+
+    project: Optional[Project]
+
+    workspace: Optional[Workspace]
+
+
+class Project(TypedDict, total=False):
+    name: Required[str]
+    """The project name."""
+
+    task_type: Required[
+        Annotated[
+            Literal["llm-base", "tabular-classification", "tabular-regression", "text-classification"],
+            PropertyInfo(alias="taskType"),
+        ]
+    ]
+    """The task type of the project."""
+
+    description: Optional[str]
+    """The project description."""
+
+
+class Workspace(TypedDict, total=False):
+    name: Required[str]
+    """The workspace name."""
+
+    slug: Required[str]
+    """The workspace slug."""
+
+    invite_code: Annotated[str, PropertyInfo(alias="inviteCode")]
+    """The workspace invite code."""
+
+    saml_only_access: Annotated[bool, PropertyInfo(alias="samlOnlyAccess")]
+    """Whether the workspace only allows SAML authentication."""
+
+    wildcard_domains: Annotated[SequenceNotStr[str], PropertyInfo(alias="wildcardDomains")]
