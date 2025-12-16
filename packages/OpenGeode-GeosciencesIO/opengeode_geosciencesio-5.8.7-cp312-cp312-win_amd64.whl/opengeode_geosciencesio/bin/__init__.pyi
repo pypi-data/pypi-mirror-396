1cdfb7e840ec823d002ffd61f436dd0fdb229a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 09:08:55 +0000

    Add package OpenGeode-GeosciencesIO/5.8.7/opengeode_geosciencesio-5.8.7-cp312-cp312-win_amd64.whl

---
+++ packages/OpenGeode-GeosciencesIO/opengeode_geosciencesio-5.8.7-cp312-cp312-win_amd64.whl/opengeode_geosciencesio/bin/__init__.pyi
@@ -0,0 +1,4 @@
+from __future__ import annotations
+from . import opengeode_geosciencesio_py_mesh
+from . import opengeode_geosciencesio_py_model
+__all__: list[str] = ['opengeode_geosciencesio_py_mesh', 'opengeode_geosciencesio_py_model']
---
+++ packages/OpenGeode-GeosciencesIO/opengeode_geosciencesio-5.8.7-cp312-cp312-win_amd64.whl/opengeode_geosciencesio/model_geosciencesio.pyi
@@ -0,0 +1,8 @@
+from __future__ import annotations
+import opengeode as opengeode
+import opengeode_geosciences as opengeode_geosciences
+from opengeode_geosciencesio.bin.opengeode_geosciencesio_py_model import BRepGeosExporter
+from opengeode_geosciencesio.bin.opengeode_geosciencesio_py_model import GeosciencesIOModelLibrary
+from opengeode_geosciencesio.bin.opengeode_geosciencesio_py_model import StructuralModelGeosExporter
+import opengeode_io as opengeode_io
+__all__: list[str] = ['BRepGeosExporter', 'GeosciencesIOModelLibrary', 'StructuralModelGeosExporter', 'opengeode', 'opengeode_geosciences', 'opengeode_io']
