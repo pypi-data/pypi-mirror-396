3ba26cf79d91f34787e0a31a712aa6fc2636e91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:31:50 +0000

    Add package arcticdb/6.5.0/arcticdb-6.5.0-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/arcticdb/arcticdb-6.5.0-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/arcticdb/__init__.py
@@ -0,0 +1,33 @@
+import logging as _logging
+import os as _os
+
+import arcticdb_ext as _ext
+import sys as _sys
+
+from arcticdb.arctic import Arctic
+from arcticdb.options import LibraryOptions, OutputFormat, RuntimeOptions, ArrowOutputStringFormat
+from arcticdb.version_store.processing import QueryBuilder, where
+from arcticdb.version_store._store import VersionedItem
+import arcticdb.version_store.library as library
+from arcticdb.tools import set_config_from_env_vars
+from arcticdb_ext.version_store import DataError, VersionRequestType
+from arcticdb_ext.exceptions import ErrorCode, ErrorCategory
+from arcticdb.version_store.library import (
+    WritePayload,
+    UpdatePayload,
+    ReadInfoRequest,
+    ReadRequest,
+    DeleteRequest,
+    col,
+    LazyDataFrame,
+    LazyDataFrameCollection,
+    LazyDataFrameAfterJoin,
+    concat,
+    StagedDataFinalizeMethod,
+    WriteMetadataPayload,
+)
+from arcticdb.version_store.admin_tools import KeyType, Size
+
+set_config_from_env_vars(_os.environ)
+
+__version__ = "6.5.0"
