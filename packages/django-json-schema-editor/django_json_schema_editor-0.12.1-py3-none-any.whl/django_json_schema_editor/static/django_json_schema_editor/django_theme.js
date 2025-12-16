8f7966e897a482ce88fe28471834a9940d281c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 11:05:53 +0000

    Add package django-json-schema-editor/0.12.1/django_json_schema_editor-0.12.1-py3-none-any.whl

---
+++ packages/django-json-schema-editor/django_json_schema_editor-0.12.1-py3-none-any.whl/django_json_schema_editor/static/django_json_schema_editor/django_theme.js
@@ -0,0 +1,116 @@
+/* global JSONEditor */
+
+JSONEditor.defaults.themes.django = class DjangoTheme extends (
+  JSONEditor.defaults.themes.html
+) {
+  getIndentedPanel() {
+    const el = super.getIndentedPanel()
+
+    // reset styles (borders/spacing)
+    el.removeAttribute("style")
+
+    // set class for easier styling via css
+    el.setAttribute("class", "indented-panel")
+    return el
+  }
+  getButton(text, icon, title) {
+    const el = super.getButton(text, icon, title)
+    el.setAttribute("class", `${el.getAttribute("class")} button`)
+    return el
+  }
+  getButtonHolder() {
+    const el = super.getButtonHolder()
+    el.removeAttribute("style")
+    el.classList.add("je-button-holder")
+    return el
+  }
+  getHeaderButtonHolder() {
+    const el = super.getHeaderButtonHolder()
+    el.removeAttribute("style")
+    el.classList.add("je-button-holder")
+    return el
+  }
+  getTabHolder() {
+    const el = super.getTabHolder()
+    el.setAttribute("class", "je-tab-holder")
+    el.innerHTML = [
+      "<div class='je-tabs'></div>",
+      "<div class='je-content'></div>",
+      "<div style='clear:both;'></div>",
+    ].join("")
+    return el
+  }
+  getTab(span) {
+    const el = super.getTab(span)
+    el.appendChild(span)
+    el.setAttribute("class", "je-tab button")
+    el.removeAttribute("style")
+    return el
+  }
+  markTabActive(row) {
+    super.markTabActive(row)
+    row.tab.classList.add("active")
+    row.tab.removeAttribute("style")
+  }
+  getTextareaInput() {
+    // reset styles
+    const el = super.getTextareaInput()
+
+    // set class for easier styling via css
+    el.removeAttribute("style")
+    return el
+  }
+  setGridColumnSize(el, size) {
+    el.setAttribute("style", `width: ${(size / 12) * 100}%`)
+  }
+}
+
+const iconClass = "material-icons"
+const iconPrefix = ""
+const mapping = {
+  collapse: "arrow_drop_up",
+  expand: "arrow_drop_down",
+  delete: "delete",
+  edit: "edit",
+  add: "add",
+  subtract: "remove",
+  cancel: "cancel",
+  save: "save",
+  moveup: "arrow_upward",
+  movedown: "arrow_downward",
+  moveright: "arrow_forward",
+  moveleft: "arrow_back",
+  copy: "content_copy",
+  clear: "highlight_off",
+  time: "access_time",
+  calendar: "calendar_today",
+  upload: "cloud_upload",
+  edit_properties: "list",
+}
+
+JSONEditor.defaults.iconlibs.django = class DjangoTheme extends (
+  JSONEditor.AbstractIconLib
+) {
+  constructor() {
+    super(iconPrefix, mapping)
+    this.icon_class = iconClass
+  }
+
+  getIconClass(_key) {
+    /* This method is unused. */
+    return this.icon_class
+  }
+
+  getIcon(key) {
+    /* Get the mapping. */
+    const mapping = this.mapping[key]
+    if (!mapping) return null
+
+    /* @see http://materializecss.com/icons.html */
+    const i = document.createElement("i")
+    i.classList.add(this.icon_class)
+    const t = document.createTextNode(mapping)
+    i.appendChild(t)
+    return i
+  }
+}
---
+++ packages/django-json-schema-editor/django_json_schema_editor-0.12.1-py3-none-any.whl/django_json_schema_editor/static/django_json_schema_editor/prose_editor.js
@@ -0,0 +1,81 @@
+import { createEditor } from "django-prose-editor/configurable"
+
+const core = {
+  Document: true,
+  Paragraph: true,
+  HardBreak: true,
+  Text: true,
+  Menu: true,
+}
+const defaults = {
+  Bold: true,
+  Italic: true,
+  Underline: true,
+  Subscript: true,
+  Superscript: true,
+}
+
+function createJSONProseEditor(textarea, options, setClobber) {
+  setClobber(null)
+
+  let extensions = defaults
+  if (Object.hasOwn(options, "extensions")) {
+    extensions = options.extensions
+  }
+
+  createEditor(textarea, {
+    ...options,
+    extensions: { ...core, ...extensions },
+  }).then((editor) => {
+    setClobber(() => {
+      editor.destroy()
+    })
+  })
+}
+
+JSONEditor.defaults.editors.prose = class extends (
+  JSONEditor.defaults.editors.string
+) {
+  setValue(value, initial, fromTemplate) {
+    const res = super.setValue(value, initial, fromTemplate)
+
+    if (res?.changed) {
+      this.clobber?.()
+
+      createJSONProseEditor(this.input, this.options, (clobber) => {
+        this.clobber = clobber
+      })
+    }
+  }
+
+  build() {
+    this.options.format = "textarea"
+    super.build()
+    this.input_type = this.schema.format // Restore original format
+  }
+
+  afterInputReady() {
+    this.clobber?.()
+    createJSONProseEditor(this.input, this.options, (clobber) => {
+      this.clobber = clobber
+    })
+
+    this.input.addEventListener("input", (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+
+      this.value = this.input.value
+      this.onChange(true)
+    })
+  }
+
+  getNumColumns() {
+    return 6
+  }
+}
+
+JSONEditor.defaults.resolvers.unshift((schema) => {
+  if (schema.type === "string" && schema.format === "prose") {
+    return "prose"
+  }
+})
