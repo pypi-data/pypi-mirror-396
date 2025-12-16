31f7e0889113d96ac56c6610e32634ccd23c1402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 16:06:30 +0000

    Add package tirx/0.1/tirx-0.1.tar.gz

---
+++ packages/tirx/tirx-0.1.tar.gz/tirx-0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name = "tirx",
+    packages = ["tirx"],
+    version = "0.1",
+    license="Apache-2.0",
+    description = "Machine learning for systems",
+    author = "",
+    author_email = "tianqi.tchen@gmail.com",
+    url = "https://github.com/mlc-ai",
+    keywords = [],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3"
+  ],
+)
