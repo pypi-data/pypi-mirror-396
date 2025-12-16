88435efa284a15435003b89b281fce0c55ecc569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 06:04:58 +0000

    Add package ternimator/0.0.6/ternimator-0.0.6.tar.gz

---
+++ packages/ternimator/ternimator-0.0.6.tar.gz/ternimator-0.0.6/src/ternimator/__init__.py
@@ -0,0 +1,23 @@
+from . import animators
+from .utils import (
+    Animation,
+    AnimParams,
+    LazyItems,
+    Lines,
+    animate,
+    animate_iter,
+    animated,
+    consume,
+)
+
+__all__ = [
+    "AnimParams",
+    "Animation",
+    "LazyItems",
+    "Lines",
+    "animate",
+    "animate_iter",
+    "animated",
+    "animators",
+    "consume",
+]
