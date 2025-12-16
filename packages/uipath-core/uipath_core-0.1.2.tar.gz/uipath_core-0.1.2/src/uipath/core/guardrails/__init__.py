0ea95e712b025c7b233a108d56adaabb60641474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:32:23 +0000

    Add package uipath-core/0.1.2/uipath_core-0.1.2.tar.gz

---
+++ packages/uipath-core/uipath_core-0.1.2.tar.gz/uipath_core-0.1.2/src/uipath/core/guardrails/__init__.py
@@ -0,0 +1,44 @@
+"""UiPath Guardrails Models.
+
+This module contains models related to UiPath Guardrails.
+"""
+
+from ._deterministic_guardrails_service import DeterministicGuardrailsService
+from .guardrails import (
+    AllFieldsSelector,
+    ApplyTo,
+    BaseGuardrail,
+    BooleanRule,
+    DeterministicGuardrail,
+    FieldReference,
+    FieldSelector,
+    FieldSource,
+    GuardrailScope,
+    GuardrailSelector,
+    NumberRule,
+    Rule,
+    SelectorType,
+    SpecificFieldsSelector,
+    UniversalRule,
+    WordRule,
+)
+
+__all__ = [
+    "DeterministicGuardrailsService",
+    "FieldSource",
+    "ApplyTo",
+    "FieldReference",
+    "SelectorType",
+    "AllFieldsSelector",
+    "SpecificFieldsSelector",
+    "FieldSelector",
+    "BaseGuardrail",
+    "DeterministicGuardrail",
+    "WordRule",
+    "NumberRule",
+    "BooleanRule",
+    "UniversalRule",
+    "Rule",
+    "GuardrailScope",
+    "GuardrailSelector",
+]
