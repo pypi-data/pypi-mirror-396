82dc4a2fc04a5fc04f6d86114eda99bf274b0fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:32:18 +0000

    Add package blackfire/1.20.36/blackfire-1.20.36-cp313-cp313-manylinux2014_x86_64.whl

---
+++ packages/blackfire/blackfire-1.20.36-cp313-cp313-manylinux2014_x86_64.whl/blackfire/README.rst
@@ -0,0 +1,33 @@
+Blackfire SDK for Python
+========================
+
+Blackfire Python SDK is a Python library that manages the Blackfire
+Python Profiler and handles communication with Blackfire.io servers.
+
+Read the official `Blackfire
+documentation <https://docs.blackfire.io/index>`_ for more information.
+
+Installation
+------------
+
+Please follow the official `Blackfire Installation
+Guide <https://docs.blackfire.io/up-and-running/installation>`_.
+
+Usage
+-----
+
+These examples and more can be found on the official `Blackfire Python
+SDK documentation <https://docs.blackfire.io/python/integrations/sdk>`_.
+
+Resources
+---------
+
+-  `Blackfire.io <https://blackfire.io>`_
+-  `Blackfire Installation
+   Guide <https://docs.blackfire.io/up-and-running/installation>`_
+-  `Blackfire Python SDK
+   documentation <https://docs.blackfire.io/python/integrations/sdk>`_
+-  `Blackfire Django
+   Integration <https://docs.blackfire.io/python/integrations/django>`_
+-  `Blackfire Flask
+   Integration <https://docs.blackfire.io/python/integrations/flask>`_
---
+++ packages/blackfire/blackfire-1.20.36-cp313-cp313-manylinux2014_x86_64.whl/blackfire/hooks/odoo/patch.py
@@ -0,0 +1,42 @@
+import sys
+from blackfire.utils import import_module, get_logger, get_executable_path
+from blackfire.hooks.utils import patch_module
+
+log = get_logger(__name__)
+
+MIN_SUPPORTED_VERSION = '13'
+
+
+def patch():
+    # this defensive check is necessary since sometimes argv is not present
+    # during bootstrap and also needs to be done before importing odoo as we modify
+    # sys.path
+    if hasattr(sys, 'argv'):
+        # Since Odoo is not importable by default, try to add
+        # its directory in python path.
+        # Assumes that odoo-bin is in the same directory than odoo module.
+        executable_path = get_executable_path(sys.argv[0])
+        if executable_path is not None and executable_path.endswith(
+            '/odoo-bin'
+        ):
+            import os
+            odoo_path = os.path.dirname(executable_path)
+            log.debug("Detected Odoo path: %s", odoo_path)
+            sys.path.append(odoo_path)
+
+    def _patch(module):
+        from blackfire.hooks.odoo.middleware import OdooMiddleware
+
+        if module.release.version_info[0] >= 15:
+            module.http.root = OdooMiddleware(module.http.root)
+            log.debug("Patched Odoo.HTTP.root")
+        else:
+            module.service.wsgi_server.application = OdooMiddleware(
+                module.service.wsgi_server.application
+            )
+
+    module = import_module('odoo')
+    if not module:
+        return False
+
+    return patch_module('odoo', _patch, version=module.release.version)
