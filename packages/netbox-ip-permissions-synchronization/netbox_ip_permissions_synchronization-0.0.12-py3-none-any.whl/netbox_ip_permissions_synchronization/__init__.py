13ecb8740a875564ee8e6954937af83c5009c365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 12:06:27 +0000

    Add package netbox-ip-permissions-synchronization/0.0.12/netbox_ip_permissions_synchronization-0.0.12-py3-none-any.whl

---
+++ packages/netbox-ip-permissions-synchronization/netbox_ip_permissions_synchronization-0.0.12-py3-none-any.whl/netbox_ip_permissions_synchronization/__init__.py
@@ -0,0 +1,15 @@
+## __init__.py
+from netbox.plugins import PluginConfig
+
+
+class Config(PluginConfig):
+    name = 'netbox_ip_permissions_synchronization'
+    verbose_name = 'NetBox IP Permissions Synchronization'
+    description = 'Syncing permissions on IP Addresses with those from their corresponding Prefix in NetBox'
+    version = '0.0.12'
+    author = 'Loris Henrion'
+    author_email = 'loris_henrion@bce.lu'
+    base_url = 'ip-permissions'
+
+
+config = Config
