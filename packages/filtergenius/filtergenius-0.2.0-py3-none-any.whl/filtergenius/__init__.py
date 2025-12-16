6c8cfc73c918c16296763276557478ed812611cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 21:05:51 +0000

    Add package filtergenius/0.2.0/filtergenius-0.2.0-py3-none-any.whl

---
+++ packages/filtergenius/filtergenius-0.2.0-py3-none-any.whl/filtergenius/__init__.py
@@ -0,0 +1,14 @@
+from .filter import (
+    is_email,
+    is_gmail,
+    is_phone,
+    has_emoji,
+    is_int,
+    is_float,
+    is_bool,
+    is_str,
+    is_alphanumeric,
+    is_url,
+    is_whitespace,
+    has_special_char
+)
