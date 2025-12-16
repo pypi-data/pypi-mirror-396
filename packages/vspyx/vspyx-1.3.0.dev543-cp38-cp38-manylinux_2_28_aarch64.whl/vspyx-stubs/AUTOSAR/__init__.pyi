67ac220c945ce5cf0009c5ee6d6c4074455e4314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 17:08:44 +0000

    Add package vspyx/1.3.0.dev543/vspyx-1.3.0.dev543-cp38-cp38-manylinux_2_28_aarch64.whl

---
+++ packages/vspyx/vspyx-1.3.0.dev543-cp38-cp38-manylinux_2_28_aarch64.whl/vspyx-stubs/AUTOSAR/__init__.pyi
@@ -0,0 +1,8 @@
+import typing, enum, vspyx
+from . import Classic, Foundation, AcceptanceTest
+
+class Module(vspyx.Core.Module):
+	"""Module
+	"""
+	def LoadARXML(self, path: str, serialized: bool, namespaced: bool) -> vspyx.Core.ScheduledTask_6b4128c881: ...
+
