ffcc22eaeeeab6de80b62b0e68f8206e4806df15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 12:05:53 +0000

    Add package piquasso/7.1.0/piquasso-7.1.0-cp311-cp311-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/piquasso/piquasso-7.1.0-cp311-cp311-manylinux_2_17_i686.manylinux2014_i686.whl/piquasso/_simulators/fock/pure/simulator.py
@@ -0,0 +1,158 @@
+#
+# Copyright 2021-2025 Budapest Quantum Computing Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .state import PureFockState
+
+from .simulation_steps import (
+    state_vector_instruction,
+    passive_linear,
+    beamsplitter5050,
+    kerr,
+    cross_kerr,
+    cubic_phase,
+    squeezing,
+    displacement,
+    linear,
+    particle_number_measurement,
+    vacuum,
+    create,
+    annihilate,
+    batch_prepare,
+    batch_apply,
+    post_select_photons,
+    imperfect_post_select_photons,
+    homodyne_measurement,
+)
+
+from ..simulation_steps import attenuator
+
+from ...simulator import BuiltinSimulator
+from piquasso.instructions import (
+    preparations,
+    gates,
+    measurements,
+    channels,
+    batch,
+)
+
+from piquasso._simulators.connectors import (
+    NumpyConnector,
+    TensorflowConnector,
+    JaxConnector,
+)
+
+
+class PureFockSimulator(BuiltinSimulator):
+    """Performs photonic simulations using Fock representation with pure states.
+
+    The simulation (when executed) results in an instance of
+    :class:`~piquasso._simulators.fock.pure.state.PureFockState`.
+
+    Example usage::
+
+        import numpy as np
+        import piquasso as pq
+
+
+        with pq.Program() as program:
+            pq.Q(all) | pq.Vacuum()
+
+            pq.Q(0)   | pq.Squeezing(r=0.1)
+            pq.Q(1)   | pq.Squeezing(r=0.2)
+
+            pq.Q(0, 1) | pq.Beamsplitter(theta=np.pi / 3)
+
+            pq.Q(0) | pq.Kerr(xi=0.05)
+
+        simulator = pq.PureFockSimulator(d=2, config=pq.Config(cutoff=7))
+        result = simulator.execute(program)
+
+    Supported preparations:
+        :class:`~piquasso.instructions.preparations.Vacuum`,
+        :class:`~piquasso.instructions.preparations.Create`,
+        :class:`~piquasso.instructions.preparations.Annihilate`,
+        :class:`~piquasso.instructions.preparations.StateVector`.
+
+    Supported gates:
+        :class:`~piquasso.instructions.gates.Interferometer`,
+        :class:`~piquasso.instructions.gates.Beamsplitter`,
+        :class:`~piquasso.instructions.gates.Phaseshifter`,
+        :class:`~piquasso.instructions.gates.MachZehnder`,
+        :class:`~piquasso.instructions.gates.Fourier`,
+        :class:`~piquasso.instructions.gates.Kerr`,
+        :class:`~piquasso.instructions.gates.CrossKerr`,
+        :class:`~piquasso.instructions.gates.CubicPhase`,
+        :class:`~piquasso.instructions.gates.GaussianTransform`,
+        :class:`~piquasso.instructions.gates.Squeezing`,
+        :class:`~piquasso.instructions.gates.QuadraticPhase`,
+        :class:`~piquasso.instructions.gates.Squeezing2`,
+        :class:`~piquasso.instructions.gates.ControlledX`,
+        :class:`~piquasso.instructions.gates.ControlledZ`,
+        :class:`~piquasso.instructions.gates.Displacement`,
+        :class:`~piquasso.instructions.gates.PositionDisplacement`,
+        :class:`~piquasso.instructions.gates.MomentumDisplacement`.
+
+    Supported measurements:
+        :class:`~piquasso.instructions.measurements.ParticleNumberMeasurement`.
+
+    Supported channels:
+        :class:`~piquasso.instructions.channels.Attenuator`.
+    """
+
+    _state_class = PureFockState
+
+    _instruction_map = {
+        preparations.Vacuum: vacuum,
+        preparations.Create: create,
+        preparations.Annihilate: annihilate,
+        preparations.StateVector: state_vector_instruction,
+        gates.Interferometer: passive_linear,
+        gates.Beamsplitter: passive_linear,
+        gates.Beamsplitter5050: beamsplitter5050,
+        gates.Phaseshifter: passive_linear,
+        gates.MachZehnder: passive_linear,
+        gates.Fourier: passive_linear,
+        gates.Kerr: kerr,
+        gates.CrossKerr: cross_kerr,
+        gates.CubicPhase: cubic_phase,
+        gates.Squeezing: squeezing,
+        gates.QuadraticPhase: linear,
+        gates.Displacement: displacement,
+        gates.PositionDisplacement: displacement,
+        gates.MomentumDisplacement: displacement,
+        gates.Squeezing2: linear,
+        gates.GaussianTransform: linear,
+        measurements.ParticleNumberMeasurement: particle_number_measurement,
+        measurements.PostSelectPhotons: post_select_photons,
+        measurements.ImperfectPostSelectPhotons: imperfect_post_select_photons,
+        measurements.HomodyneMeasurement: homodyne_measurement,
+        channels.Attenuator: attenuator,
+        batch.BatchPrepare: batch_prepare,
+        batch.BatchApply: batch_apply,
+    }
+
+    _default_connector_class = NumpyConnector
+
+    _extra_builtin_connectors = [TensorflowConnector, JaxConnector]
+
+    _measurement_classes_allowed_mid_circuit = (
+        measurements.ParticleNumberMeasurement,
+        measurements.PostSelectPhotons,
+    )
+
+    _measurement_classes_allowed_with_shots_none = (
+        measurements.ParticleNumberMeasurement,
+        measurements.PostSelectPhotons,
+    )
---
+++ packages/piquasso/piquasso-7.1.0-cp311-cp311-manylinux_2_17_i686.manylinux2014_i686.whl/piquasso/fermionic/fock/simulation_steps.py
@@ -0,0 +1,233 @@
+#
+# Copyright 2021-2025 Budapest Quantum Computing Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from piquasso.api.branch import Branch
+
+from piquasso.api.exceptions import InvalidParameter
+
+from piquasso._math.validations import (
+    all_zero_or_one,
+    are_modes_consecutive,
+    validate_occupation_numbers,
+)
+
+from ._utils import (
+    calculate_indices_for_controlled_phase,
+    calculate_indices_for_ising_XX,
+)
+
+from .._utils import (
+    get_fock_space_index,
+    get_cutoff_fock_space_dimension,
+    next_second_quantized,
+)
+from .state import PureFockState
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List
+    from piquasso.instructions.preparations import StateVector
+    from piquasso.instructions.gates import _PassiveLinearGate, Squeezing2
+    from piquasso.fermionic.instructions import ControlledPhase, IsingXX
+
+
+def state_vector(
+    state: PureFockState, instruction: "StateVector", shots: int
+) -> "List[Branch]":
+    connector = state._connector
+
+    fallback_np = connector.fallback_np
+
+    coefficient = instruction._get_all_params(state._connector)["coefficient"]
+
+    if "occupation_numbers" in instruction._get_all_params(state._connector):
+        occupation_numbers = fallback_np.array(
+            instruction._get_all_params(state._connector)["occupation_numbers"]
+        )
+
+        if state._config.validate:
+            if not all_zero_or_one(occupation_numbers):
+                raise InvalidParameter(
+                    f"Invalid initial state specified: instruction={instruction}"
+                )
+            validate_occupation_numbers(
+                occupation_numbers,
+                state._d,
+                state._config.cutoff,
+                context=f": instruction={instruction}",
+            )
+
+        index = get_fock_space_index(occupation_numbers)
+
+        state._state_vector = connector.assign(state._state_vector, index, coefficient)
+
+    elif "fock_amplitude_map" in instruction._get_all_params(state._connector):
+        for occupation, amplitude in instruction._get_all_params(state._connector)[
+            "fock_amplitude_map"
+        ].items():
+            occ_numbers = fallback_np.array(occupation)
+
+            if state._config.validate:
+                if len(occ_numbers) != state._d or not all_zero_or_one(occ_numbers):
+                    raise InvalidParameter(
+                        f"Invalid initial state specified: instruction={instruction}"
+                    )
+                validate_occupation_numbers(
+                    occ_numbers,
+                    state._d,
+                    state._config.cutoff,
+                    context=f": instruction={instruction}",
+                )
+
+            index = get_fock_space_index(occ_numbers)
+
+            state._state_vector = connector.assign(
+                state._state_vector,
+                index,
+                coefficient * amplitude,
+            )
+
+    return [Branch(state=state)]
+
+
+def passive_linear(
+    state: PureFockState, instruction: "_PassiveLinearGate", shots: int
+) -> "List[Branch]":
+    connector = state._connector
+    config = state._config
+
+    modes = instruction.modes
+
+    if state._config.validate and not are_modes_consecutive(modes):
+        raise InvalidParameter(f"Specified modes must be consecutive: modes={modes}")
+
+    unitary = instruction._get_passive_block(connector, config)
+
+    representations = connector.calculate_interferometer_on_fermionic_fock_space(
+        unitary, config.cutoff
+    )
+
+    state._state_vector = connector.apply_fermionic_passive_linear_to_state_vector(
+        representations,
+        state._state_vector,
+        modes,
+        state._d,
+        config.cutoff,
+    )
+
+    return [Branch(state=state)]
+
+
+def squeezing2(
+    state: PureFockState, instruction: "Squeezing2", shots: int
+) -> "List[Branch]":
+    connector = state._connector
+
+    modes = instruction.modes
+
+    d = state._d
+    cutoff = state._config.cutoff
+
+    np = connector.np
+    fallback_np = connector.fallback_np
+
+    if state._config.validate and not are_modes_consecutive(modes):
+        raise InvalidParameter(f"Specified modes must be consecutive: modes={modes}")
+
+    r = instruction.params["r"]
+    phi = instruction.params["phi"]
+
+    size = get_cutoff_fock_space_dimension(d, cutoff)
+
+    index = fallback_np.zeros(d, dtype=np.int64)
+
+    U = np.array(
+        [
+            [np.cos(r / 2), np.sin(r / 2) * np.exp(-1j * phi)],
+            [-np.sin(r / 2) * np.exp(1j * phi), np.cos(r / 2)],
+        ]
+    )
+
+    for i in range(size):
+        if index[modes[0]] == 0 and index[modes[1]] == 0:
+            index[modes[0]] = 1
+            index[modes[1]] = 1
+            j = get_fock_space_index(index)
+            index[modes[0]] = 0
+            index[modes[1]] = 0
+
+            if j < size:
+                state._state_vector = connector.assign(
+                    state._state_vector, ((i, j),), U @ state._state_vector[(i, j),]
+                )
+            else:
+                state._state_vector = connector.assign(
+                    state._state_vector, i, U[0, 0] * state._state_vector[i]
+                )
+
+        index = next_second_quantized(index)
+
+    return [Branch(state=state)]
+
+
+def controlled_phase(
+    state: PureFockState, instruction: "ControlledPhase", shots: int
+) -> "List[Branch]":
+    connector = state._connector
+
+    modes = instruction.modes
+
+    d = state._d
+    cutoff = state._config.cutoff
+
+    np = connector.np
+
+    phi = instruction.params["phi"]
+
+    rotation = np.exp(1j * phi)
+
+    indices = calculate_indices_for_controlled_phase(d, cutoff, modes)
+
+    state._state_vector = connector.assign(
+        state._state_vector, indices, rotation * state._state_vector[indices]
+    )
+
+    return [Branch(state=state)]
+
+
+def ising_XX(
+    state: PureFockState, instruction: "IsingXX", shots: int
+) -> "List[Branch]":
+    connector = state._connector
+    np = connector.np
+
+    phi = instruction.params["phi"]
+    modes = instruction.modes
+
+    d = state._d
+    cutoff = state._config.cutoff
+
+    cos_phi = np.cos(phi)
+    i_sin_phi = 1j * np.sin(phi)
+
+    indices = calculate_indices_for_ising_XX(d, cutoff, modes)
+
+    for index in indices:
+        initial = state._state_vector[index]
+        final = cos_phi * initial + i_sin_phi * np.flip(initial)
+        state._state_vector = connector.assign(state._state_vector, index, final)
+
+    return [Branch(state=state)]
