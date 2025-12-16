c6e5c9922ddcf86d5159c589d67eb90c538c18b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:07:02 +0000

    Add package warpdata/0.1.33/warpdata-0.1.33-py3-none-any.whl

---
+++ packages/warpdata/warpdata-0.1.33-py3-none-any.whl/warpdata/formats/__init__.py
@@ -0,0 +1,8 @@
+"""
+Format conversion utilities for warpdata.
+
+Provides converters from internal representations (JSON AST) to various
+symbolic formats (S-expressions, proof terms, Coq, etc.).
+"""
+
+__all__ = ["lean_conversions"]
