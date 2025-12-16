9ef33eecd76d8640b10578d24cf046f0743b287a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 18:06:49 +0000

    Add package OpenTAKServer/1.7.1/opentakserver-1.7.1-py3-none-any.whl

---
+++ packages/OpenTAKServer/opentakserver-1.7.1-py3-none-any.whl/opentakserver/forms/package_form.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileRequired, FileAllowed
+from wtforms import StringField, FileField, BooleanField
+from wtforms.validators import Optional, DataRequired
+from opentakserver.functions import false_values
+
+
+class PackageForm(FlaskForm):
+    platform = StringField(default="Android")
+    plugin_type = StringField(default="plugin")
+    apk = FileField(validators=[FileRequired(), FileAllowed(['apk'])])
+    icon = FileField(validators=[Optional()])
+    description = StringField(validators=[Optional()])
+    install_on_enrollment = BooleanField(false_values=false_values)
+    install_on_connection = BooleanField(false_values=false_values)
+    atak_version = StringField(validators=[Optional()])
+
+
+class PackageUpdateForm(FlaskForm):
+    package_name = StringField(validators=[DataRequired()])
+    install_on_enrollment = BooleanField(false_values=false_values)
+    install_on_connection = BooleanField(false_values=false_values)
