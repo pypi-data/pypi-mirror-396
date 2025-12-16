f102d3db44d54406e14b61e1b5d5f0d47c8f9402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 14:07:33 +0000

    Add package tiledbsoma/2.2.0/tiledbsoma-2.2.0-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/tiledbsoma/tiledbsoma-2.2.0-cp312-cp312-manylinux_2_28_x86_64.whl/tiledbsoma/io/spatial/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) TileDB, Inc. and The Chan Zuckerberg Initiative Foundation
+#
+# Licensed under the MIT License.
+
+"""Experimental SOMA features.
+
+This module is for experimental features. Support for these features
+may be dropped.
+"""
+
+from .ingest import VisiumPaths, from_visium, register_visium_datasets
+from .outgest import to_spatialdata
+
+__all__ = ["VisiumPaths", "from_visium", "register_visium_datasets", "to_spatialdata"]
