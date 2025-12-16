d70232c71f2433ba773ed7616cb6ee12ba23f8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 16:05:37 +0000

    Add package async-tiff/0.3.0/async_tiff-0.3.0-cp311-abi3-musllinux_1_2_x86_64.whl

---
+++ packages/async-tiff/async_tiff-0.3.0-cp311-abi3-musllinux_1_2_x86_64.whl/async_tiff/_async_tiff.pyi
@@ -0,0 +1,8 @@
+from ._decoder import Decoder as Decoder
+from ._decoder import DecoderRegistry as DecoderRegistry
+from ._geo import GeoKeyDirectory as GeoKeyDirectory
+from ._ifd import ImageFileDirectory as ImageFileDirectory
+from ._thread_pool import ThreadPool as ThreadPool
+from ._tiff import ObspecInput as ObspecInput
+from ._tiff import TIFF as TIFF
+from ._tile import Tile as Tile
