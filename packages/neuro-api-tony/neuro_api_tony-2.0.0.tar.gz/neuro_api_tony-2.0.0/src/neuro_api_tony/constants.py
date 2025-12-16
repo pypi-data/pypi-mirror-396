bcc05701c04c5df12687576f01f7be989ed4ee48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 21:06:13 +0000

    Add package neuro-api-tony/2.0.0/neuro_api_tony-2.0.0.tar.gz

---
+++ packages/neuro-api-tony/neuro_api_tony-2.0.0.tar.gz/neuro_api_tony-2.0.0/src/neuro_api_tony/constants.py
@@ -0,0 +1,10 @@
+"""Constants - Application global constants."""
+
+from typing import Final
+
+APP_NAME: Final = "Tony"
+VERSION: Final = "2.0.0"
+PACKAGE_NAME: Final = "neuro-api-tony"
+GIT_REPO_URL: Final = "https://github.com/Pasu4/neuro-api-tony"
+PYPI_URL: Final = "https://pypi.org/project/neuro-api-tony"
+PYPI_API_URL: Final = "https://pypi.org/pypi/neuro-api-tony/json"
