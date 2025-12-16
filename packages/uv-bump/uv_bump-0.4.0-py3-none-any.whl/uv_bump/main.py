3511ba3c158d220160e3ff330ad4e350a2e65a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 20:06:51 +0000

    Add package uv-bump/0.4.0/uv_bump-0.4.0-py3-none-any.whl

---
+++ packages/uv-bump/uv_bump-0.4.0-py3-none-any.whl/uv_bump/main.py
@@ -0,0 +1,145 @@
+from __future__ import annotations
+
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib  # type: ignore[import-untyped]  # pragma: no cover
+
+LOCK_FILE_NAME = "uv.lock"
+PYPROJECT_FILE_NAME = "pyproject.toml"
+
+
+class UVSyncError(Exception):  # noqa: D101
+    exit_code: int
+    msg: str
+
+    def __init__(self, exit_code: int, msg: str) -> None:  # noqa: D107
+        self.exit_code = exit_code
+        self.msg = msg
+
+    def __str__(self) -> str:  # noqa: D105
+        return f"UVSyncError(exit_code={self.exit_code}, message=\n" + self.msg + ")"
+
+
+def upgrade(root_pyproject_toml_file: Path | None = None) -> None:
+    """Upgrade minimum versions of dependencies in specified pyproject.toml."""
+    if root_pyproject_toml_file is None:
+        root_pyproject_toml_file = Path(PYPROJECT_FILE_NAME)
+
+    lock_path = root_pyproject_toml_file.parent / LOCK_FILE_NAME
+
+    run_uv_sync()
+
+    package_versions = collect_package_versions_from_lock_file(lock_path)
+    pyproject_files = collect_all_pyproject_files(lock_path)
+    for pyproject_file in pyproject_files:
+        update_pyproject_toml(pyproject_file, package_versions)
+
+
+def run_uv_sync() -> None:
+    """
+    Find package upgrades through uv sync.
+
+    Raises UVSyncError.
+    """
+    try:
+        subprocess.run(
+            ["uv", "sync", "--upgrade", "--all-extras"],  # noqa: S607
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+    except subprocess.CalledProcessError as error:
+        raise UVSyncError(error.returncode, error.stderr) from error
+
+
+def collect_package_versions_from_lock_file(lock_path: Path) -> dict[str, str]:
+    """
+    Gather all dependency versions.
+
+    Args:
+        lock_path: path to uv.lock file
+
+    Returns:
+        dict with the package name as key and package version as value
+
+    """
+    contents = tomllib.loads(lock_path.read_text(encoding="utf-8"))
+    return {p["name"]: p["version"] for p in contents["package"] if "version" in p}
+
+
+def collect_all_pyproject_files(lock_path: Path) -> list[Path]:
+    """
+    Determine all pyproject.toml file locations in the project from uv.lock file.
+
+    Args:
+        lock_path: the full path to the lock file
+
+    Returns:
+        list of paths to pyproject.toml files
+
+    """
+    contents = tomllib.loads(lock_path.read_text(encoding="utf-8"))
+
+    if "manifest" in contents:
+        # workspaces
+        member_paths = []
+
+        for member in contents["manifest"]["members"]:
+            for pkg in contents["package"]:
+                if pkg["name"] == member:
+                    source = pkg["source"]
+                    for source_type in ["editable", "virtual"]:
+                        if source_type in source:
+                            member_paths.append(
+                                lock_path.parent
+                                / source[source_type]
+                                / PYPROJECT_FILE_NAME
+                            )
+                            continue
+
+        return member_paths
+
+    return [lock_path.parent / PYPROJECT_FILE_NAME]
+
+
+def update_pyproject_toml(file: Path, package_versions: dict[str, str]) -> None:
+    """
+    Update specified pyproject.toml file with minimum version bounds (>=, ~=).
+
+    Params:
+         file: the path to the pyproject.toml file
+         package_version_updated: dict of package names and package versions.
+    """
+    contents = file.read_text(encoding="utf-8")
+    contents_updated = _update_pyproject_contents(contents, package_versions)
+    file.write_text(contents_updated, encoding="utf-8")
+
+
+def _update_pyproject_contents(
+    contents: str, package_version_updated: dict[str, str]
+) -> str:
+    for package, version in package_version_updated.items():
+        contents = _replace_package_version(contents, package, version)
+    return contents
+
+
+def _replace_package_version(text: str, package: str, version: str) -> str:
+    # we assume the following:
+    # 1. the package name is directly preceded by a double quote
+    # 2. (?:\[[^\]]*\])? => after the package name there can be extras, if so, we
+    #    require it to be in square brackets
+    # 3. (>|>=|~=) there will be a version specifier that allows updating
+    # 4. [^"`,;]+' =>  we need to stop. There can be more version specifiers, separated
+    #    by comma, and/or system specifiers (separated by semicolon), or none, in which
+    #    case we encounter the double quotes.
+    pattern = r'"(' + package + r'(?:\[[^\]]*\])?)(>|>=|~=)[^"`,;]+'
+    replacement = r'"\1>=' + version
+
+    text, _ = re.subn(pattern, replacement, text)
+    return text
