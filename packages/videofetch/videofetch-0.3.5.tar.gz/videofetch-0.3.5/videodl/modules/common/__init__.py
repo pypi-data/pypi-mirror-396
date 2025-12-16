9273326b7033669b45c49276c7ef9ccc7216ca96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 05:08:31 +0000

    Add package videofetch/0.3.5/videofetch-0.3.5.tar.gz

---
+++ packages/videofetch/videofetch-0.3.5.tar.gz/videofetch-0.3.5/videodl/modules/common/__init__.py
@@ -0,0 +1,23 @@
+'''initialize'''
+from .vget import VgetVideoClient
+from .xmflv import XMFlvVideoClient
+from .gvvip import GVVIPVideoClient
+from .kedou import KedouVideoClient
+from .iiilab import IIILabVideoClient
+from ..utils import BaseModuleBuilder
+from .snapany import SnapAnyVideoClient
+from .iloveapi import ILoveAPIVideoClient
+from .qzxdptools import QZXDPToolsVideoClient
+
+
+'''CommonVideoClientBuilder'''
+class CommonVideoClientBuilder(BaseModuleBuilder):
+    REGISTERED_MODULES = {
+        'IIILabVideoClient': IIILabVideoClient, 'KedouVideoClient': KedouVideoClient, 'SnapAnyVideoClient': SnapAnyVideoClient,
+        'GVVIPVideoClient': GVVIPVideoClient, 'VgetVideoClient': VgetVideoClient, 'ILoveAPIVideoClient': ILoveAPIVideoClient,
+        'XMFlvVideoClient': XMFlvVideoClient, 'QZXDPToolsVideoClient': QZXDPToolsVideoClient,
+    }
+
+
+'''BuildCommonVideoClient'''
+BuildCommonVideoClient = CommonVideoClientBuilder().build
