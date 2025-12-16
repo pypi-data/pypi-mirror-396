4fa7c49e4f36f7500eadd8fe01acc4c9acd584c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:08:05 +0000

    Add package liblaf-grapes/8.9.3/liblaf_grapes-8.9.3.tar.gz

---
+++ packages/liblaf-grapes/liblaf_grapes-8.9.3.tar.gz/liblaf_grapes-8.9.3/src/liblaf/grapes/magic/_frame.py
@@ -0,0 +1,63 @@
+import inspect
+import types
+from collections.abc import Callable
+
+from liblaf.grapes._config import config
+
+from ._release_type import is_pre_release
+
+_HIDDEN_FROM_LOGGING_NAMES: tuple[str, ...] = ("rich.progress.",)
+
+
+def hidden_from_logging(frame: types.FrameType | None) -> bool:
+    if frame is None:
+        return True
+    if frame.f_locals.get("__tracebackhide__", False):
+        return True
+    name: str = frame.f_globals.get("__name__", "")
+    return f"{name}.".startswith(_HIDDEN_FROM_LOGGING_NAMES)
+
+
+def hidden_from_traceback(
+    frame: types.FrameType | None, *, hide_stable_release: bool | None = None
+) -> bool:
+    if frame is None:
+        return True
+    if frame.f_locals.get("__tracebackhide__", False):
+        return True
+    if hide_stable_release is None:
+        hide_stable_release = config.traceback.hide_stable_release.get()
+    if hide_stable_release:
+        name: str | None = frame.f_globals.get("__name__")
+        if not is_pre_release(frame.f_code.co_filename, name):
+            return True
+    return False
+
+
+def get_frame(
+    depth: int = 1, hidden: Callable[[types.FrameType], bool] | None = None
+) -> types.FrameType | None:
+    frame: types.FrameType | None = inspect.currentframe()
+    while frame is not None and depth > 0:
+        frame = frame.f_back
+        depth -= 1
+        if hidden is not None:
+            while frame is not None and hidden(frame):
+                frame = frame.f_back
+    return frame
+
+
+def get_frame_with_stacklevel(
+    depth: int = 1, hidden: Callable[[types.FrameType], bool] | None = None
+) -> tuple[types.FrameType | None, int]:
+    frame: types.FrameType | None = inspect.currentframe()
+    stacklevel: int = 0
+    while frame is not None and depth > 0:
+        frame = frame.f_back
+        depth -= 1
+        stacklevel += 1
+        if hidden is not None:
+            while frame is not None and hidden(frame):
+                frame = frame.f_back
+                stacklevel += 1
+    return frame, stacklevel
