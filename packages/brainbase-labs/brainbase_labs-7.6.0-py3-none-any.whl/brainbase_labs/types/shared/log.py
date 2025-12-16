10a6ae515a0dd6c6654374a737fa76ac610647f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 11:05:19 +0000

    Add package brainbase-labs/7.6.0/brainbase_labs-7.6.0-py3-none-any.whl

---
+++ packages/brainbase-labs/brainbase_labs-7.6.0-py3-none-any.whl/brainbase_labs/types/shared/log.py
@@ -0,0 +1,40 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from datetime import datetime
+
+from pydantic import Field as FieldInfo
+
+from ..._models import BaseModel
+
+__all__ = ["Log"]
+
+
+class Log(BaseModel):
+    id: str
+
+    call_sid: Optional[str] = None
+
+    data: Optional[object] = None
+
+    direction: Optional[str] = None
+
+    end_time: Optional[datetime] = FieldInfo(alias="endTime", default=None)
+
+    external_call_id: Optional[str] = FieldInfo(alias="externalCallId", default=None)
+
+    from_number: Optional[str] = FieldInfo(alias="fromNumber", default=None)
+
+    messages: Optional[object] = None
+
+    recording_url: Optional[str] = FieldInfo(alias="recordingUrl", default=None)
+
+    start_time: Optional[datetime] = FieldInfo(alias="startTime", default=None)
+
+    status: Optional[str] = None
+
+    telephony_source: Optional[str] = FieldInfo(alias="Telephony_source", default=None)
+
+    to_number: Optional[str] = FieldInfo(alias="toNumber", default=None)
+
+    transcription: Optional[str] = None
