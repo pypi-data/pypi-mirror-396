4dd936705eaf38cf49694b2cdce1ac3ed0ccc14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 08:05:49 +0000

    Add package tiledbsoma/2.2.0rc0/tiledbsoma-2.2.0rc0-cp312-cp312-macosx_13_0_arm64.whl

---
+++ packages/tiledbsoma/tiledbsoma-2.2.0rc0-cp312-cp312-macosx_13_0_arm64.whl/tiledbsoma/io/__init__.py
@@ -0,0 +1,62 @@
+# Copyright (c) TileDB, Inc. and The Chan Zuckerberg Initiative Foundation
+#
+# Licensed under the MIT License.
+
+
+import importlib.metadata
+
+import anndata as ad
+import scipy
+from packaging.version import Version
+
+from ._registration import (
+    ExperimentAmbientLabelMapping,
+)
+from .ingest import (
+    add_matrix_to_collection,
+    add_X_layer,
+    from_anndata,
+    from_h5ad,
+    register_anndatas,
+    register_h5ads,
+    update_matrix,
+    update_obs,
+    update_var,
+)
+from .outgest import (
+    to_anndata,
+    to_h5ad,
+)
+from .shaping import (
+    get_experiment_shapes,
+    resize_experiment,
+    show_experiment_shapes,
+    upgrade_experiment_shapes,
+)
+
+# https://github.com/single-cell-data/TileDB-SOMA/issues/3920
+scipy_version = Version(importlib.metadata.version("scipy"))
+anndata_version = Version(importlib.metadata.version("anndata"))
+if anndata_version >= Version("0.10.7") and anndata_version < Version("0.11.2") and scipy_version >= Version("1.15.0"):
+    raise RuntimeError(
+        f"anndata '{ad.__version__}' is incompatible with '{scipy.__version__}', and more generally, anndata 0.10.7-0.11.1 are incompatible with scipy 0.15. Please upgrade your anndata to >= 0.11.2, or downgrade your scipy to < 0.15.",
+    )
+
+__all__ = (
+    "ExperimentAmbientLabelMapping",
+    "add_X_layer",
+    "add_matrix_to_collection",
+    "from_anndata",
+    "from_h5ad",
+    "get_experiment_shapes",
+    "register_anndatas",
+    "register_h5ads",
+    "resize_experiment",
+    "show_experiment_shapes",
+    "to_anndata",
+    "to_h5ad",
+    "update_matrix",
+    "update_obs",
+    "update_var",
+    "upgrade_experiment_shapes",
+)
---
+++ packages/tiledbsoma/tiledbsoma-2.2.0rc0-cp312-cp312-macosx_13_0_arm64.whl/tiledbsoma/io/spatial/outgest.py
@@ -0,0 +1,94 @@
+# Copyright (c) TileDB, Inc. and The Chan Zuckerberg Initiative Foundation
+#
+# Licensed under the MIT License.
+
+from __future__ import annotations
+
+import warnings
+from collections.abc import Mapping, Sequence
+from typing import Any
+
+try:
+    import spatialdata as sd
+except ImportError as err:
+    warnings.warn("Experimental spatial outgestor requires the spatialdata package.", stacklevel=1)
+    raise err
+
+
+from tiledbsoma import Experiment
+from tiledbsoma._constants import SPATIAL_DISCLAIMER
+from tiledbsoma.io import to_anndata
+
+from ._spatialdata_util import _spatial_to_spatialdata
+
+
+def to_spatialdata(
+    experiment: Experiment,
+    *,
+    measurement_names: Sequence[str] | None = None,
+    scene_names: Sequence[str] | None = None,
+    obs_id_name: str = "obs_id",
+    var_id_name: str = "var_id",
+    table_kwargs: Mapping[str, dict[str, Any]] | None = None,
+) -> sd.SpatialData:
+    """Converts the experiment group to SpatialData format.
+
+    Args:
+        experiment:
+        measurement_names: The names of measurements to export. If ``None``, all
+            measurements are included. Defaults to ``None``.
+        scene_names: The names of the scenes to export. If ``None``, all scenes are
+            included. Defaults to ``None``.
+        obs_id_name: Column name to use for ``obs`` dataframes. Defaults to
+            ``"obs_id"``.
+        var_id_name: Column name to use for ``var`` dataframes. Defaults to
+            ``"var_id|``.
+        table_kwargs: Optional mapping from measurment name to keyword arguments to
+            pass to table conversions. See :method:`to_anndata` for possible keyword
+            arguments.
+    """
+    warnings.warn(SPATIAL_DISCLAIMER, stacklevel=2)
+
+    # Read non-spatial data into Anndata tables.
+    if measurement_names is not None:
+        measurement_names = tuple(measurement_names)
+    if "ms" in experiment:
+        ms_keys = tuple(experiment.ms.keys()) if measurement_names is None else measurement_names
+        if table_kwargs is None:
+            table_kwargs = {}
+        tables = {
+            measurement_name: (
+                to_anndata(
+                    experiment,
+                    measurement_name,
+                    obs_id_name=obs_id_name,
+                    var_id_name=var_id_name,
+                    **table_kwargs[measurement_name],
+                )
+                if measurement_name in table_kwargs
+                else to_anndata(
+                    experiment,
+                    measurement_name,
+                    obs_id_name=obs_id_name,
+                    var_id_name=var_id_name,
+                )
+            )
+            for measurement_name in ms_keys
+        }
+    else:
+        tables = {}
+
+    # If no spatial data, return just the tables.
+    if "spatial" not in experiment:
+        return sd.SpatialData(tables=tables)
+
+    scene_names = tuple(experiment.spatial.keys()) if scene_names is None else tuple(scene_names)
+
+    return _spatial_to_spatialdata(
+        experiment.spatial,
+        tables,
+        scene_names=scene_names,
+        measurement_names=measurement_names,
+        obs_id_name=obs_id_name,
+        var_id_name=var_id_name,
+    )
