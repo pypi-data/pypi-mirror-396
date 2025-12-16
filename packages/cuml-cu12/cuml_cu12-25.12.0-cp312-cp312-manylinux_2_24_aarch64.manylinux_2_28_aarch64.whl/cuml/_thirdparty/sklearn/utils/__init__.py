f74f016961a335973c8dcaefaf35b8860b5a52a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:31:34 +0000

    Add package cuml-cu12/25.12.0/cuml_cu12-25.12.0-cp312-cp312-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/cuml-cu12/cuml_cu12-25.12.0-cp312-cp312-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/cuml/_thirdparty/sklearn/utils/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright (c) 2020-2025, NVIDIA CORPORATION.
+# SPDX-License-Identifier: BSD-3-Clause
+
+# This code originates from the Scikit-Learn library,
+# it was since modified to allow GPU acceleration.
+# This code is under BSD 3 clause license.
+
+
+from . import extmath, validation
