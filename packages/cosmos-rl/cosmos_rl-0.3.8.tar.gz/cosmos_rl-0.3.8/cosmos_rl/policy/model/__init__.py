e9b53cc1e6e417733b996abfe6a4fa59a7eb2474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 11:05:59 +0000

    Add package cosmos-rl/0.3.8/cosmos_rl-0.3.8.tar.gz

---
+++ packages/cosmos-rl/cosmos_rl-0.3.8.tar.gz/cosmos_rl-0.3.8/cosmos_rl/policy/model/__init__.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cosmos_rl.policy.model.gpt import GPT
+from cosmos_rl.policy.model.qwen2_5_vl import Qwen2_5_VLConditionalModel
+from cosmos_rl.policy.model.qwen3_moe import Qwen3MoE
+from cosmos_rl.policy.model.qwen3_vl_moe import Qwen3VLMoeModel
+from cosmos_rl.policy.model.hf_models import HFModel
+from cosmos_rl.policy.model.deepseek_v3 import DeepseekV3MoEModel
+from cosmos_rl.policy.model.internvl import InternVLChatModel
+from cosmos_rl.policy.model.base import ModelRegistry, BaseModel, WeightMapper
+
+__all__ = [
+    "GPT",
+    "Qwen2_5_VLConditionalModel",
+    "Qwen3MoE",
+    "Qwen3VLMoeModel",
+    "HFModel",
+    "DeepseekV3MoEModel",
+    "InternVLChatModel",
+    "BaseModel",
+    "WeightMapper",
+    "ModelRegistry",
+]
