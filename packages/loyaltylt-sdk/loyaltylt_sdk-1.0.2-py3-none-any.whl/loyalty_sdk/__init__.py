6abb0a6ac12070c7f31c1515c5e1a95210178318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 08:05:19 +0000

    Add package loyaltylt-sdk/1.0.2/loyaltylt_sdk-1.0.2-py3-none-any.whl

---
+++ packages/loyaltylt-sdk/loyaltylt_sdk-1.0.2-py3-none-any.whl/loyalty_sdk/__init__.py
@@ -0,0 +1,24 @@
+"""
+Loyalty.lt SDK for Python - Official Partner API client.
+
+Usage:
+    from loyalty_sdk import LoyaltySDK
+    
+    sdk = LoyaltySDK(
+        api_key='lty_your_api_key',
+        api_secret='your_api_secret'
+    )
+    
+    shops = sdk.get_shops()
+"""
+
+from .client import LoyaltySDK
+from .exceptions import LoyaltySDKError, LoyaltyAPIError, LoyaltyAuthError
+
+__version__ = "1.0.0"
+__all__ = [
+    'LoyaltySDK',
+    'LoyaltySDKError',
+    'LoyaltyAPIError',
+    'LoyaltyAuthError',
+]
