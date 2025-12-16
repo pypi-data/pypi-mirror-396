160a00a303ecb1fbd215daf7b2df06ed0b5192c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 13:17:48 +0000

    Add package tomsup/1.4.0/tomsup-1.4.0.tar.gz

---
+++ packages/tomsup/tomsup-1.4.0.tar.gz/tomsup-1.4.0/tomsup/payoffmatrix.py
@@ -0,0 +1,189 @@
+"""
+This scripts contains the PayoffMatrix
+"""
+
+from typing import Optional
+import numpy as np
+
+
+class PayoffMatrix:
+    """
+    A class of 2 by 2 payoff matrices.
+
+    Currently include the following games:
+    The staghunt game: 'staghunt',
+    The matching pennies game (coop and competive): 'penny_competive', 'penny_cooperative',
+    The party dilemma: 'party',
+    The Battle of the sexes: 'sexes',
+    The chicken game: 'chicken',
+    The deadlock: 'deadlock',
+    The prisoners dilemma: 'prisoners_dilemma'.
+
+    For custom payoff matrix supply a 2x2x2 numpy array to the predefined argument.
+
+    Examples:
+        >>> import tomsup as ts
+        >>> staghunt = ts.PayoffMatrix(name="staghunt")
+        >>> staghunt.payoff(action_agent0=1, action_agent1=1, agent=0)
+        5
+        >>> staghunt.payoff(action_agent0=1, action_agent1=0, agent=0)
+        0
+        >>> staghunt.payoff(action_agent0=0, action_agent1=1, agent=0)
+        3
+        >>> chicken = ts.PayoffMatrix(name="chicken")
+        >>> chicken.payoff(0, 1, 0)
+        -1
+        >>> dead = ts.PayoffMatrix(name="deadlock")
+        >>> dead.payoff(1, 0, 1)
+        0
+        >>> sexes = ts.PayoffMatrix(name="sexes")
+        >>> sexes.payoff(1, 1, 0)
+        5
+        >>> custom = ts.PayoffMatrix(name="custom", np.array(([(10, 0), (0, 5)],
+                                                        [(5, 0), (0, 10)])))
+    """
+
+    def __init__(self, name: str, predefined: Optional[np.array] = None):
+        """
+        Args:
+            name (str): The name of the either predefined matrix or your custom matrix.
+                Currently include the following games:
+                The staghunt game ('staghunt'),
+                the matching pennies game ('penny_competive', 'penny_cooperative'),
+                the party dilemma ('party'),
+                the Battle of the sexes ('sexes'), the chicken game ('chicken'),
+                the deadlock ('deadlock'), nad the prisoners dilemma ('prisoners_dilemma').
+            predefined (Optional[np.array], optional): A custom 2x2x2 matrix. Defaults to None.
+        """
+        self.name = name
+        if name == "staghunt":
+            self.matrix = np.array(
+                (
+                    [(3, 3), (0, 5)],
+                    [(3, 0), (3, 5)],
+                )
+            )
+        elif name == "prisoners_dilemma":
+            self.matrix = np.array(([(1, 5), (0, 3)], [(1, 0), (5, 3)]))
+        elif name == "penny_competitive":
+            self.matrix = np.array(([(-1, 1), (1, -1)], [(1, -1), (-1, 1)]))
+        elif name == "penny_cooperative":
+            self.matrix = np.array(([(1, -1), (-1, 1)], [(1, -1), (-1, 1)]))
+        elif name == "party":
+            self.matrix = np.array(([(5, 0), (0, 10)], [(5, 0), (0, 10)]))
+        elif name == "sexes":  # battle of the sexes
+            self.matrix = np.array(([(10, 0), (0, 5)], [(5, 0), (0, 10)]))
+        elif name == "chicken":
+            self.matrix = np.array(([(-1000, -1), (1, 0)], [(-1000, 1), (-1, 0)]))
+        elif name == "deadlock":
+            self.matrix = np.array(([(1, 0), (3, 2)], [(1, 3), (0, 2)]))
+        else:
+            if predefined is not None:
+                matrix = np.array(predefined)
+                if matrix.shape == (2, 2, 2):
+                    self.matrix = np.array(predefined)
+                else:
+                    raise TypeError(
+                        "Predefined should be a valid matrix where \
+                                     matrix.shape == (2, 2, 2), e.g. a 2x2x2 \
+                                     matrix"
+                    )
+            else:
+                raise TypeError(
+                    "Invalid name and no predefined matrix given. \
+                                 Please input a valid name or input a \
+                                 predefined matrix of dimension 2x2x2."
+                )
+
+    def payoff(self, choice_agent0: int, choice_agent1: int, agent: int = 0) -> float:
+        """
+        Args:
+            choice_agent0 (int): choice of agent 0
+            choice_agent1 (int): choice of agent 1
+            agent (int, optional): The perspective agent which should get the payoff, either 0 or 1.
+                Defaults to 0.
+
+        Returns:
+            float: The payoff of the agent
+        """
+
+        return self.matrix[agent, choice_agent0, choice_agent1]
+
+    def __str__(self):
+        print_len = max([len(str(self().min())), len(str(self().max()))])
+
+        def add_pl(string):
+            return (print_len - len(str(string))) * " " + str(string)
+
+        str1 = f"<Class PayoffMatrix, Name = {self.name}> "
+        str2 = "The payoff matrix of agent 0"
+        str3 = "       |  Choice agent 1"
+        str4 = (
+            "       | "
+            + f"{add_pl(' ')}"
+            + " | "
+            + f"{add_pl(0)}"
+            + " | "
+            + f"{add_pl(1)}"
+            + " |"
+        )
+        str5 = "       | " + print_len * 3 * "-" + 2 * "---" + " |"
+        str6 = (
+            "Choice | " + f"{add_pl(0)}" + " | " + f"{add_pl(self()[0][0, 0])}" + " | "
+            f"{add_pl(self()[0][0, 1])}" + " |"
+        )
+        str7 = (
+            "agent 0| " + f"{add_pl(1)}" + " | " + f"{add_pl(self()[0][1, 0])}" + " | "
+            f"{add_pl(self()[0][1, 1])}" + " |"
+        )
+        str8 = " "
+        str9 = "The payoff matrix of agent 1"
+        str10 = "       |  Choice agent 1"
+        str11 = (
+            "       | "
+            + f"{add_pl(' ')}"
+            + " | "
+            + f"{add_pl(0)}"
+            + " | "
+            + f"{add_pl(1)}"
+            + " |"
+        )
+        str12 = str5
+        str13 = (
+            "Choice | " + f"{add_pl(0)}" + " | " + f"{add_pl(self()[1][0, 0])}" + " | "
+            f"{add_pl(self()[1][0, 1])}" + " |"
+        )
+        str14 = (
+            "agent 0| " + f"{add_pl(1)}" + " | " + f"{add_pl(self()[1][1, 0])}" + " | "
+            f"{add_pl(self()[1][1, 1])}" + " |"
+        )
+        str15 = str8
+        return "\n".join(
+            [
+                str1,
+                str2,
+                str3,
+                str4,
+                str5,
+                str6,
+                str7,
+                str8,
+                str9,
+                str10,
+                str11,
+                str12,
+                str13,
+                str14,
+                str15,
+            ]
+        )
+
+    def get_matrix(self) -> np.array:
+        """
+        Returns:
+            np.array: The payoff matrix
+        """
+        return self.matrix
+
+    def __call__(self):
+        return self.matrix
---
+++ packages/tomsup/tomsup-1.4.0.tar.gz/tomsup-1.4.0/tomsup/utils.py
@@ -0,0 +1,67 @@
+"""
+This script contains utility functions used in the tomsup package
+"""
+
+import json
+import os
+from typing import List, Optional, Union
+
+from tomsup.agent import Agent, AgentGroup
+
+
+def valid_agents() -> dict:
+    """
+    Returns:
+        dict: A dictionary of valid agent, where each key is an valid agent shorthand.
+            Each dictionary key (e.g. shorthand) refers to a new dictionary containing,
+            additional information, such as a short description of the strategy, and a reference.
+            See examples for more.
+    Examples:
+        >>> output = valid_agents()
+        >>> isinstance(output, dict)
+        True
+        >>> output.keys()
+        dict_keys(['RB', 'WSLS'])
+        >>> output['RB']
+        {'name': 'Random Bias',
+        'shorthand': 'RB',
+        'example': 'RB(bias = 0.5)',
+        'reference': 'Devaine, et al. (2017)',
+        'strategy': 'Chooses 1 randomly based on a probability or bias'}
+    """
+    this_dir, this_filename = os.path.split(__file__)
+    path = os.path.join(this_dir, "agent_info.json")
+    with open(path, "r") as fp:
+        agent_dict = json.load(fp)
+    return agent_dict
+
+
+def create_agents(
+    agents: Union[str, list], start_params: Optional[List[dict]] = None, **kwargs
+) -> Union[AgentGroup, Agent]:
+    """
+    Args:
+        agents (Union[str, list]): A str denoting an agent or a list of agents. To see option use `valid_agents()`.
+        start_params (Optional[List[dict]], optional): The starting parameters of the agent.
+            Defaults to None, indicating the default starting parameters.
+
+    Returns:
+        Union[AgentGroup, Agent]: If only one agent is specified returns an Agent otherwise returns an AgentGroup.
+
+    Examples:
+        >>> group = create_agents(agents = ['RB']*3)
+        >>> isinstance(group, AgentGroup)
+        True
+        >>> group.get_names()
+        ['RB_0', 'RB_1', 'RB_2']
+        >>> RB_agent = group.get_agent('RB_0')
+        >>> RB_agent.get_strategy()
+        'RB'
+    """
+    if isinstance(agents, str):
+        if (start_params is not None) and ("save_history" in start_params):
+            save_history = start_params["save_history"]
+        else:
+            save_history = False
+        return Agent(strategy=agents, save_history=save_history, **kwargs)
+    return AgentGroup(agents=agents, start_params=start_params)
