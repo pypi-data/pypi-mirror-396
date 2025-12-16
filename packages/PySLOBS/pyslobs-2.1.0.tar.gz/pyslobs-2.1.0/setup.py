f4dd0ddde28c7acb51b898f02bab0e9a7234e90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 09:09:28 +0000

    Add package PySLOBS/2.1.0/pyslobs-2.1.0.tar.gz

---
+++ packages/PySLOBS/pyslobs-2.1.0.tar.gz/pyslobs-2.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PySLOBS",
+    # If you want to bump the version, consider also bumping the
+    # 'tested on' versions in the README.md.
+    version="2.1.0",
+    author="Julian-O",
+    # author_email="",
+    description="Python wrapper to StreamLabs Desktop API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Julian-O/pyslobs",
+    packages=setuptools.find_packages(),
+    install_requires=["websocket-client"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Video",
+    ],
+    python_requires=">=3.10",
+)
