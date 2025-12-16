140d6d4ce561490339b7ce1800996c44762ad712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 11:05:54 +0000

    Add package fred-framework/1.0.8/fred_framework-1.0.8-py3-none-any.whl

---
+++ packages/fred-framework/fred_framework-1.0.8-py3-none-any.whl/fred_framework/demo/frontend/src/views/directives/dragDirect/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="card content-box">
+    <span class="text">拖拽指令 🍇🍇🍇🍓🍓🍓</span>
+    <div v-draggable class="drag-box flx-center">我可以拖拽哦~</div>
+  </div>
+</template>
+
+<script setup lang="ts" name="dragDirect"></script>
+
+<style scoped lang="scss">
+@use "./index.scss" as *;
+</style>
