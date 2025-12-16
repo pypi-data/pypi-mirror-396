a0d31bd04e8911ac7af3116ef5aec52de18ad0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 15:11:57 +0000

    Add package shox-arithmagic/0.1/shox_arithmagic-0.1.tar.gz

---
+++ packages/shox-arithmagic/shox_arithmagic-0.1.tar.gz/shox_arithmagic-0.1/arithmagic/core.py
@@ -0,0 +1,25 @@
+import math
+
+def summa_3(a, b, c):
+    return a + b + c
+
+def summa_4(a, b, c, d):
+    return a + b + c + d
+
+def square(a, b, c):
+    return a**2 + b**2 + c**2
+
+def circle_diameter(radius):
+    return radius * 2
+
+def circle_area(radius):
+    return math.pi * (radius ** 2)
+
+def speed_finder(time, distance):
+    return distance / time
+
+def time_finder(speed, distance):
+    return distance / speed
+
+def distance_finder(speed, time):
+    return speed * time
---
+++ packages/shox-arithmagic/shox_arithmagic-0.1.tar.gz/shox_arithmagic-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+setup(
+    name='shox-arithmagic',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.11',
+    description='arithmagic',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+
+
+)
