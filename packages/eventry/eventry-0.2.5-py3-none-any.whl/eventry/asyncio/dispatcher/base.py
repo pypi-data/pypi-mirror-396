94403e96be32352b1c452dd46fbd62055e4bd55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 20:06:15 +0000

    Add package eventry/0.2.5/eventry-0.2.5-py3-none-any.whl

---
+++ packages/eventry/eventry-0.2.5-py3-none-any.whl/eventry/asyncio/dispatcher/base.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+
+__all__ = ['Dispatcher', 'ErrorContext']
+
+
+from collections.abc import Callable
+
+from typing_extensions import TYPE_CHECKING, Any
+
+from eventry.config import DispatcherConfig
+from eventry.loggers import dispatcher_logger
+from eventry.asyncio.event import Event
+from eventry.asyncio.router import Router
+
+
+if TYPE_CHECKING:
+    pass
+
+
+class Dispatcher(Router):
+    def __init__(
+        self,
+        error_event_factory: Callable[[Event, Exception], Event],
+        workflow_data: dict[str, Any] | None = None,
+        config: DispatcherConfig | None = None,
+    ) -> None:
+        Router.__init__(self, name='Dispatcher')
+
+        self._workflow_data = workflow_data if workflow_data is not None else {}
+        self._config = config or DispatcherConfig()
+        self._error_event_factory: Callable[[Event, Exception], Event] = error_event_factory
+
+    async def event_entry(
+        self,
+        event: Event,
+        event_context_injection: dict[str, Any] | None = None,
+        silent: bool = False,
+    ) -> None:
+        dispatcher_logger.debug(f'New event {id(event)}: {type(event)}')
+
+        if event_context_injection is None:
+            event_context_injection = {}
+
+        event_context: dict[str, Any] = {
+            **self._workflow_data,
+            **event.event_context_injection,
+            **event_context_injection,
+            self._config.default_names_remap.get('event', 'event'): event,
+            self._config.default_names_remap.get('dispatcher', 'dispatcher'): self,
+        }
+        event_context[self._config.default_names_remap.get('data', 'data')] = event_context
+
+        async for exception in self.propagate_event(self._config, event, event_context, silent):
+            if silent:
+                continue
+
+            try:
+                error_event = self._error_event_factory(event, exception)
+            except Exception as e:
+                dispatcher_logger.error(f'An error occurred while creating error event: {e}')
+                continue
+
+            await self.event_entry(error_event, silent=True)
