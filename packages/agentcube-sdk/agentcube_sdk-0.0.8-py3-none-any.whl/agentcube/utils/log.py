1c85c58ccefb7d33be6141ccaf8cc50f98a3c97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 12:05:59 +0000

    Add package agentcube-sdk/0.0.8/agentcube_sdk-0.0.8-py3-none-any.whl

---
+++ packages/agentcube-sdk/agentcube_sdk-0.0.8-py3-none-any.whl/agentcube/utils/log.py
@@ -0,0 +1,21 @@
+import logging
+from typing import Union
+
+# Initialize logger configuration once
+def get_logger(name: str, level: Union[int, str] = logging.INFO) -> logging.Logger:
+    """Get a logger instance with basic configuration"""
+    logger = logging.getLogger(name)
+    
+    # Always update level if requested
+    logger.setLevel(level)
+
+    # Configure only if no handlers are already set
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s | %(levelname)-4s | %(name)-20s | %(message)s'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    
+    return logger
