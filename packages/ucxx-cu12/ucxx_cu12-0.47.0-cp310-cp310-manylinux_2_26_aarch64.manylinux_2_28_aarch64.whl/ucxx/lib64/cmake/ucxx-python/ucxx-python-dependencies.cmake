a9991166fe60d9e2b7bef97395d7fa6767b59d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 06:05:28 +0000

    Add package ucxx-cu12/0.47.0/ucxx_cu12-0.47.0-cp310-cp310-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/ucxx-cu12/ucxx_cu12-0.47.0-cp310-cp310-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/ucxx/lib64/cmake/ucxx-python/ucxx-python-dependencies.cmake
@@ -0,0 +1,27 @@
+#=============================================================================
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
+#=============================================================================
+
+include(CMakeFindDependencyMacro)
+
+find_dependency(ucxx)
+
+find_dependency(Python3 COMPONENTS Development)
+
+
+set(rapids_global_targets )
+
+
+foreach(target IN LISTS rapids_global_targets)
+  if(TARGET ${target})
+    get_target_property(_is_imported ${target} IMPORTED)
+    get_target_property(_already_global ${target} IMPORTED_GLOBAL)
+    if(_is_imported AND NOT _already_global)
+        set_target_properties(${target} PROPERTIES IMPORTED_GLOBAL TRUE)
+    endif()
+  endif()
+endforeach()
+
+unset(rapids_global_targets)
+unset(rapids_clear_cpm_cache)
