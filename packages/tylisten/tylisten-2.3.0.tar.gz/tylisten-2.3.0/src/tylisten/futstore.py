05b1747696dd198d16eb609c34fc3caa5f88c5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 19:09:32 +0000

    Add package tylisten/2.3.0/tylisten-2.3.0.tar.gz

---
+++ packages/tylisten/tylisten-2.3.0.tar.gz/tylisten-2.3.0/src/tylisten/futstore.py
@@ -0,0 +1,57 @@
+import asyncio
+import typing as t
+
+T = t.TypeVar("T")
+
+
+class FutureStore:
+    """
+    A store for keeping references of running futures,
+    in order to keep them from being garbage collected.
+    """
+
+    _futs: t.Set[asyncio.Future]
+
+    def __init__(self, loop: asyncio.AbstractEventLoop | None = None) -> None:
+        super().__init__()
+        self._futs = set()
+        self.loop = loop
+
+    def add_awaitable(self, func: t.Awaitable[T]) -> "asyncio.Future[T]":
+        """Add an awaitable into this store.
+
+        :param func: the awaitable
+        :return: the wrapped task
+        """
+        if not (func := asyncio.ensure_future(func, loop=self.loop)).done():
+            self._futs.add(func)
+            func.add_done_callback(self._futs.discard)
+        return func
+
+    __call__ = add_awaitable
+
+    async def wait(self, wait_new=True):
+        """Wait for all tasks in the specific group(s).
+
+        :param wait_new: also wait for new-added futures during waiting for existing futures.
+
+        .. note:: Even if `wait_new` is False, newly added tasks may still be scheduled.
+        """
+        if not self._futs:
+            return
+        await asyncio.wait(self._futs)
+        if wait_new and self._futs:
+            # await potential new tasks in this store
+            await self.wait()
+
+    def clear(self):
+        """Clear current future store, cancel all saved tasks. If you do not want a task to be cancelled,
+        wrap it in a `~asyncio.shield`.
+
+        :param exc: if not given, all futures will be cancelled; else they will be set with this exception.
+        """
+        for task in self._futs:
+            task.cancel()
+
+    def __bool__(self):
+        return bool(self._futs)
