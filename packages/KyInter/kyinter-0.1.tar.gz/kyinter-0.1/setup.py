92150f586ee607103e5337d6384dd90f8dcc31f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 19:09:37 +0000

    Add package KyInter/0.1/kyinter-0.1.tar.gz

---
+++ packages/KyInter/kyinter-0.1.tar.gz/kyinter-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="KyInter",
+    version="0.1",
+    packages=find_packages(),
+    author="bzNAK",
+    description="A library for creating personalization window.",
+    long_description=open("README.md", encoding="UTF-8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+
+)
