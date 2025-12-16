c4e81e1808b6f947c31f45f12c2599fbb14363f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 20:05:49 +0000

    Add package KyInter/0.6/kyinter-0.6-py3-none-any.whl

---
+++ packages/KyInter/kyinter-0.6-py3-none-any.whl/KyInter/KyCore.py
@@ -0,0 +1,9 @@
+class Position():
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+class Size():
+    def __init__(self, width: int, height: int) -> None:
+        self.width = width
+        self.height = height
