0366e53e00c1be5c803a744648538b72fef71605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 15:09:53 +0000

    Add package icegrams/1.1.6/icegrams-1.1.6-cp39-cp39-win_amd64.whl

---
+++ packages/icegrams/icegrams-1.1.6-cp39-cp39-win_amd64.whl/icegrams/trie_build.py
@@ -0,0 +1,133 @@
+"""
+
+Icegrams: A trigrams library for Icelandic
+
+CFFI builder for _trie module
+
+Copyright (C) 2019-2025 Miðeind ehf.
+Original author: Vilhjálmur Þorsteinsson
+
+This software is licensed under the MIT License:
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without restriction,
+    including without limitation the rights to use, copy, modify, merge,
+    publish, distribute, sublicense, and/or sell copies of the Software,
+    and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+This module only runs at setup/installation time. It is invoked
+from setup.py as requested by the cffi_modules=[] parameter of the
+setup() function. It causes the _trie.*.so CFFI wrapper library
+to be built from its source in trie.cpp.
+
+"""
+
+import os
+import platform
+import cffi
+
+# Don't change the name of this variable unless you
+# change it in setup.py as well
+ffibuilder = cffi.FFI()
+
+WINDOWS = platform.system() == "Windows"
+MACOS = platform.system() == "Darwin"
+IMPLEMENTATION = platform.python_implementation()
+
+# What follows is the actual Python-wrapped C interface to trie.*.so
+# It must be kept in sync with trie.h
+
+declarations = """
+
+    typedef unsigned int UINT;
+    typedef uint8_t BYTE;
+    typedef uint32_t UINT32;
+    typedef uint64_t UINT64;
+    typedef void VOID;
+
+    UINT mapping(const BYTE* pbMap, const BYTE* pbWord);
+    UINT bitselect(const BYTE* pb, UINT n);
+    UINT retrieve(const BYTE* pb, UINT nStart, UINT n);
+
+    UINT lookupFrequency(const BYTE* pb,
+        UINT nQuantumSize, UINT nIndex);
+
+    UINT64 lookupMonotonic(const BYTE* pb,
+        UINT nQuantumSize, UINT nIndex);
+    VOID lookupPairMonotonic(const BYTE* pb,
+        UINT nQuantumSize, UINT nIndex,
+        UINT64* pn1, UINT64* pn2);
+
+    UINT64 lookupPartition(const BYTE* pb,
+        UINT nOuterQuantum, UINT nInnerQuantum, UINT nIndex);
+    VOID lookupPairPartition(const BYTE* pb,
+        UINT nQuantumSize, UINT nInnerQuantum, UINT nIndex,
+        UINT64* pn1, UINT64* pn2);
+
+    UINT searchMonotonic(const BYTE* pb,
+        UINT nQuantumSize, UINT nP1, UINT nP2, UINT64 n);
+    UINT searchMonotonicPrefix(const BYTE* pb,
+        UINT nQuantumSize, UINT nP1, UINT nP2, UINT64 n);
+
+    UINT searchPartition(const BYTE* pb,
+        UINT nOuterQuantum, UINT nInnerQuantum,
+        UINT nP1, UINT nP2, UINT64 n);
+    UINT searchPartitionPrefix(const BYTE* pb,
+        UINT nOuterQuantum, UINT nInnerQuantum,
+        UINT nP1, UINT nP2, UINT64 n);
+
+"""
+
+# Do the magic CFFI incantations necessary to get CFFI and setuptools
+# to compile trie.cpp at setup time, generate a .so library and
+# wrap it so that it is callable from Python and PyPy as _trie
+
+if WINDOWS:
+    extra_compile_args = ["/Zc:offsetof-"]
+elif MACOS:
+    os.environ["CFLAGS"] = "-stdlib=libc++"  # Fixes PyPy build on macOS 10.15.6+
+    os.environ["MACOSX_DEPLOYMENT_TARGET"] = "10.9"
+    extra_compile_args = ["-mmacosx-version-min=10.9", "-stdlib=libc++"]
+else:
+    # Adding -O3 to the compiler arguments doesn't seem to make
+    # any discernible difference in lookup speed
+    extra_compile_args = ["-std=c++11"]
+
+# On some systems, the linker needs to be told to use the C++ compiler
+# under PyPy due to changes in the default behaviour of distutils.
+if IMPLEMENTATION == "PyPy":
+    os.environ["LDCXXSHARED"] = "c++ -shared"
+
+# Use the Python stable ABI (abi3) for CPython, allowing a single wheel
+# to work across multiple Python versions (3.9+). PyPy doesn't support abi3.
+py_limited_api = "cp39" if IMPLEMENTATION == "CPython" else False
+
+ffibuilder.set_source(
+    "icegrams._trie",
+    # trie.cpp is written in C++ but must export a pure C interface.
+    # This is the reason for the "extern 'C' { ... }" wrapper.
+    'extern "C" {\n' + declarations + "\n}\n",
+    source_extension=".cpp",
+    sources=["src/icegrams/trie.cpp"],
+    extra_compile_args=extra_compile_args,
+    py_limited_api=py_limited_api,
+)
+
+ffibuilder.cdef(declarations)
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=False)
