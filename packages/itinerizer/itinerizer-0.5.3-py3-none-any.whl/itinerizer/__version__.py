63bde88fadc79fd3d350c2966d212c0b08ec1893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 22:06:03 +0000

    Add package itinerizer/0.5.3/itinerizer-0.5.3-py3-none-any.whl

---
+++ packages/itinerizer/itinerizer-0.5.3-py3-none-any.whl/itinerizer/__version__.py
@@ -0,0 +1,9 @@
+"""
+Itinerizer version information.
+"""
+
+__version__ = "0.5.3"
+__author__ = "Itinerizer Contributors"
+__email__ = "contact@itinerizer.io"
+__description__ = "A comprehensive travel itinerary management system with JSON storage"
+__url__ = "https://github.com/bobmatnyc/itinerizer"
