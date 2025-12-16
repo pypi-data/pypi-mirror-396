ff402726cd8f6f70572b4c2f6414d5f3c33ead4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 09:08:34 +0000

    Add package tylereq/0.1.4/tylereq-0.1.4-py3-none-any.whl

---
+++ packages/tylereq/tylereq-0.1.4-py3-none-any.whl/tylereq/__init__.py
@@ -0,0 +1,36 @@
+""" 
+tylereq
+=======
+
+A simple equations module made by Tyler.
+
+Currently includes:
+-------------------
+- `shapes` : geometry-related formulas for perimeters, areas, surface areas, and volumes.
+
+But, I have plans to expand outside of shape related formulas
+
+Examples
+--------
+>>> import tylereq
+>>> tylereq.a_circle(5)
+78.53981633974483
+
+>>> from tylereq import p_square
+>>> p_square(10)
+40
+"""
+
+from importlib.metadata import version as _get_version
+from . import shapes
+
+__author__ = "TylerUploads"
+__license__ = "MIT"
+__docformat__ = "numpy"
+__version__ = "0.1.4"
+
+__all__ = ["shapes"]
+
+# Quick list of all the functions in shapes
+# "p_rectangle", "p_square", "p_triangle", "c_circle", "a_rectangle", "a_square", 
+# "a_triangle", "a_circle", "sa_box",  "sa_sphere", "sa_cylinder", "v_box", "v_sphere", "v_cylinder"
