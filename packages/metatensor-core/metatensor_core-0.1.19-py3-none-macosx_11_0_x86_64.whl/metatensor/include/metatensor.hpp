a83b8a861d44438355dfe0fe2509487d1aaf7103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:32:20 +0000

    Add package metatensor-core/0.1.19/metatensor_core-0.1.19-py3-none-macosx_11_0_x86_64.whl

---
+++ packages/metatensor-core/metatensor_core-0.1.19-py3-none-macosx_11_0_x86_64.whl/metatensor/include/metatensor.hpp
@@ -0,0 +1,6 @@
+#include "metatensor/errors.hpp"   // IWYU pragma: export
+#include "metatensor/arrays.hpp"   // IWYU pragma: export
+#include "metatensor/labels.hpp"   // IWYU pragma: export
+#include "metatensor/block.hpp"    // IWYU pragma: export
+#include "metatensor/tensor.hpp"   // IWYU pragma: export
+#include "metatensor/io.hpp"       // IWYU pragma: export
