1083a8d8104074632e3292a15612683a334d7843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 05:06:24 +0000

    Add package raft-dask-cu12/25.12.0/raft_dask_cu12-25.12.0-cp312-cp312-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/raft-dask-cu12/raft_dask_cu12-25.12.0-cp312-cp312-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/raft_dask/include_test/raft_include_test.pyx
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright (c) 2020-2022, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+def raft_include_test():
+    print("RAFT Setup successfully")
+    return True
