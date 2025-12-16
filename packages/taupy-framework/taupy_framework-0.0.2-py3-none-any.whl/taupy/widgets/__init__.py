bebac698ee686a6b0f35c105fc26cb0a860b62d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 18:06:47 +0000

    Add package taupy-framework/0.0.2/taupy_framework-0.0.2-py3-none-any.whl

---
+++ packages/taupy-framework/taupy_framework-0.0.2-py3-none-any.whl/taupy/widgets/__init__.py
@@ -0,0 +1,4 @@
+from .component import Component
+from .elements import Div, Button, Text, Input, Table, Image, Modal
+from .layout import HStack, VStack, Center, Spacer, Scroll, Container
+from .factory import Component
