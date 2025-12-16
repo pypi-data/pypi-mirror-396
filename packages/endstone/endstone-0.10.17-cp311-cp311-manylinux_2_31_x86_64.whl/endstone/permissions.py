14753e4223c2c3e1bc4602aece47f1ab0cddaed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 06:07:31 +0000

    Add package endstone/0.10.17/endstone-0.10.17-cp311-cp311-manylinux_2_31_x86_64.whl

---
+++ packages/endstone/endstone-0.10.17-cp311-cp311-manylinux_2_31_x86_64.whl/endstone/permissions.py
@@ -0,0 +1,17 @@
+from endstone._internal.endstone_python import (
+    Permissible,
+    Permission,
+    PermissionAttachment,
+    PermissionAttachmentInfo,
+    PermissionDefault,
+    PermissionLevel,
+)
+
+__all__ = [
+    "Permission",
+    "Permissible",
+    "PermissionAttachment",
+    "PermissionAttachmentInfo",
+    "PermissionDefault",
+    "PermissionLevel",
+]
