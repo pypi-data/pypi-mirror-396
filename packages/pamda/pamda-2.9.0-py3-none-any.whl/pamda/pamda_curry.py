c9325d67219430c390593de6d6e24f4049fa0f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 16:05:28 +0000

    Add package pamda/2.9.0/pamda-2.9.0-py3-none-any.whl

---
+++ packages/pamda/pamda-2.9.0-py3-none-any.whl/pamda/pamda_curry.py
@@ -0,0 +1,202 @@
+import types, threading, ctypes
+from functools import update_wrapper
+import type_enforced
+
+
+class curry_obj:
+    def __init__(
+        self,
+        __fn__,
+        *__args__,
+        __flips__=[],
+        __fnExecute__=None,
+        __isThunk__=False,
+        __isTypeEnforced__=False,
+        **__kwargs__,
+    ):
+        update_wrapper(self, __fn__)
+        self.__fn__ = __fn__
+        self.__fnExecute__ = (
+            __fnExecute__ if __fnExecute__ is not None else __fn__
+        )
+        self.__args__ = __args__
+        self.__kwargs__ = __kwargs__
+        self.__isCurried__ = True
+        self.__isThunk__ = __isThunk__
+        self.__isTypeEnforced__ = __isTypeEnforced__
+        self.__flips__ = __flips__
+        self.__fnArity__ = self.__getFnArity__()
+        self.__arity__ = self.__getArity__(__args__, __kwargs__)
+        self.__thread__ = None
+        self.__thread_results__ = None
+        self.__get_fn_arg_default_keys__()
+
+    def __call__(self, *args, **kwargs):
+        new_args = self.__args__ + args
+        new_kwargs = dict(**self.__kwargs__, **kwargs)
+        # Create a comprehensive set of assigned variable names to determine arity
+        assigned_vars = set(
+            self.__fn_arg_default_keys__
+            + self.__fn_arg_keys__[: len(new_args)]
+            + list(new_kwargs.keys())
+        )
+        arity = self.__fnArity__ - len(assigned_vars)
+        if arity < 0:
+            self.__exception__("Too many arguments were supplied")
+        elif arity == 0:
+            if len(self.__flips__) > 0:
+                new_args = self.__unflipArgs__(new_args)
+            if (not self.__isThunk__) or (len(args) + len(kwargs) == 0):
+                results = self.__fnExecute__(*new_args, **new_kwargs)
+                if self.__thread__ != None:
+                    self.__thread_results__ = results
+                return results
+        return curry_obj(
+            self.__fn__,
+            *new_args,
+            __flips__=self.__flips__,
+            __isThunk__=self.__isThunk__,
+            __isTypeEnforced__=self.__isTypeEnforced__,
+            __fnExecute__=self.__fnExecute__,
+            **new_kwargs,
+        )
+
+    def __get__(self, instance, owner):
+        def bind(*args, **kwargs):
+            if instance is not None and self.__arity__ == self.__fnArity__:
+                return self.__call__(instance, *args, **kwargs)
+            else:
+                return self.__call__(*args, **kwargs)
+
+        update_wrapper(bind, self)
+        return bind
+
+    def __repr__(self):
+        return f"<curried {self.__fn__.__module__}.{self.__fn__.__qualname__} object at {hex(id(self))}>"
+
+    def __getArity__(self, args, kwargs):
+        return self.__fnArity__ - (len(args) + len(kwargs))
+
+    def __getFnArity__(self):
+        if not isinstance(self.__fn__, (types.MethodType, types.FunctionType)):
+            self.__exception__(
+                "A non function was passed as a function and does not have any arity. See the stack trace above for more information."
+            )
+        extra_method_input_count = (
+            1 if isinstance(self.__fn__, (types.MethodType)) else 0
+        )
+        return self.__fn__.__code__.co_argcount - extra_method_input_count
+
+    def __get_fn_arg_default_keys__(self):
+        """
+        Get the default values of the passed function or method and store them in `self.__fn_defaults__`.
+        """
+        self.__fn_var_keys__ = list(self.__fn__.__code__.co_varnames)
+        self.__fn_arg_keys__ = self.__fn_var_keys__[
+            : self.__fn__.__code__.co_argcount
+        ]
+        if self.__fn__.__defaults__ is not None:
+            self.__fn_arg_default_keys__ = self.__fn_arg_keys__[
+                -len(self.__fn__.__defaults__) :
+            ]
+        else:
+            self.__fn_arg_default_keys__ = []
+        if self.__fn__.__kwdefaults__ is not None:
+            self.__fn_arg_default_keys__.extend(
+                list(self.__fn__.__kwdefaults__.keys())
+            )
+
+    def thunkify(self):
+        self.__isThunk__ = True
+        return self
+
+    def flip(self):
+        if self.__arity__ < 2:
+            self.__exception__(
+                "To `flip` a function, it  must have an arity of at least 2 (take two or more inputs)"
+            )
+        self.__flips__ = [len(self.__args__)] + self.__flips__
+        return self
+
+    def __unflipArgs__(self, args):
+        args = list(args)
+        for flip in self.__flips__:
+            arg = args.pop(flip + 1)
+            args.insert(flip, arg)
+        return tuple(args)
+
+    def __exception__(self, message):
+        pre_message = (
+            f"({self.__fn__.__module__}.{self.__fn__.__qualname__}_curried): "
+        )
+        raise Exception(pre_message + message)
+
+    def typeEnforce(self):
+        if not self.__isTypeEnforced__:
+            self.__fnExecute__ = type_enforced.Enforcer(self.__fnExecute__)
+            self.__isTypeEnforced__ = True
+        return self
+
+    def asyncRun(self, daemon=False):
+        if not self.__isThunk__ and self.__arity__ == 0:
+            self.__exception__(
+                f"To `asyncRun` a Function, it must be a thunk with arity 0"
+            )
+        if self.__thread__ is not None:
+            self.__exception__(
+                "`asyncRun` has already been executed on this thunk"
+            )
+        self.__thread_completed__ = False
+        self.__thread__ = threading.Thread(target=self)
+        self.__thread__.setDaemon(daemon)
+        self.__thread__.start()
+        return self
+
+    def asyncWait(self):
+        if self.__thread__ == None:
+            self.__exception__(
+                f"To `asyncWait` a Function, it must be `asyncRun` first"
+            )
+        if not self.__thread_completed__:
+            self.__thread__.join()
+            self.__thread_completed__ = True
+        return self.__thread_results__
+
+    def asyncKill(self):
+        if self.__thread__ == None:
+            self.__exception__(
+                f"To `asyncKill` a Function, it must be `asyncRun` first"
+            )
+        if self.__thread_completed__:
+            return self.__thread_results__
+
+        thread_id = self.__thread__.ident
+        if thread_id is None:
+            self.__exception__(
+                f"Cannot `asyncKill` a Function that does not have a thread id"
+            )
+
+        # Use ctypes to set the async exception
+        try:
+            res = ctypes.pythonapi.PyThreadState_SetAsyncExc(
+                ctypes.c_long(thread_id), ctypes.py_object(SystemExit)
+            )
+        except:
+            self.__exception__(
+                f"Failed to `asyncKill`: Something failed when seting the Exit state for thread id ({thread_id})"
+            )
+        if res == 0:
+            self.__exception__(
+                f"Failed to `asyncKill`: Thread id not found ({thread_id})"
+            )
+        elif res == 1:
+            # Success, thread killed join the thread to clean up resources
+            self.__thread_completed__ = True
+            self.__thread__.join()
+            return self.__thread_results__
+        elif res > 1:
+            # Something is wrong, set it back to 0
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+            self.__exception__(
+                f"Failed to `asyncKill`: ctypes returned multiple results for thread id ({thread_id}). This should not happen. Returned thread state back to normal."
+            )
---
+++ packages/pamda/pamda-2.9.0-py3-none-any.whl/pamda/pamda_timer.py
@@ -0,0 +1,181 @@
+import time
+from functools import update_wrapper
+from type_enforced.utils import Partial
+
+
+class PamdaTimer:
+    def __init__(
+        self,
+        __fn__,
+        units="ms",
+        iterations=1,
+        print_call=True,
+        print_time_stats=False,
+    ):
+        """
+        Function:
+
+        Initialize a pamda_timer object.
+        - Note: This object is used for bare minimum in script timing purposes.
+
+        Required:
+
+        - `fn`:
+            - Type: function | method
+            - What: The name of the process being timed
+
+        Optional:
+
+        - `units`:
+            - Type: str
+            - What: The units for the time measurement. Can be "s" (seconds), "ms" (milliseconds), or "us" (microseconds). Default is "ms".
+        - `iterations`:
+            - Type: int
+            - What: The number of iterations to run the function when getting time statistics. Default is 1.
+        - `print_call`:
+            - Type: bool
+            - What: Whether to print the function call time when this object is called. Default is True.
+        - `print_time_stats`:
+            - Type: bool
+            - What: Whether to print a simple output line for get_time_stats after all iterations run. Default is False.
+        """
+        update_wrapper(self, __fn__)
+        self.__fn__ = __fn__
+        self.units = units
+        self.iterations = iterations
+        self.print_call = print_call
+        self.print_time_stats = print_time_stats
+        assert (
+            isinstance(self.iterations, int) and self.iterations > 0
+        ), "Iterations must be a positive integer."
+        if units not in ["s", "ms", "us"]:
+            raise ValueError("Invalid units. Use 's', 'ms', or 'us'.")
+        if units == "s":
+            self.__divisor__ = 1
+        elif units == "ms":
+            self.__divisor__ = 1000
+        elif units == "us":
+            self.__divisor__ = 1000000
+
+    def __call__(self, *args, **kwargs):
+        """
+        Function:
+
+        Test the function processing time.
+
+        Optional:
+
+        - `args`:
+            - Type: any
+            - What: The arguments to pass to the function
+        - `kwargs`:
+            - Type: any
+            - What: The keyword arguments to pass to the function
+        """
+        start = time.time()
+        out = self.__fn__(*args, **kwargs)
+        time_taken = time.time() - start
+        if self.print_call:
+            print(
+                f"{self.__fn__.__qualname__}: {(time_taken * self.__divisor__):.4f}{self.units}"
+            )
+        return out
+
+    def __repr__(self):
+        return f"<timed {self.__fn__.__module__}.{self.__fn__.__qualname__} object at {hex(id(self))}>"
+
+    def get_time_stats(self, *args, **kwargs):
+        """
+        Function:
+
+        Get the time statistics of the function given the number of iterations (specified during initialization).
+
+        Optional:
+
+        - `args`:
+            - Type: any
+            - What: The arguments to pass to the function
+        - `kwargs`:
+            - Type: any
+            - What: The keyword arguments to pass to the function
+
+        Returns:
+            - A dictionary containing the:
+                - module: (str) - The module where the function is defined
+                - function: (str) - The name of the function being timed
+                - unit: (str) - The time unit used for measurements
+                - iterations: (int) - The number of iterations run
+                - avg: (float) - The average time taken per iteration in the specified units
+                - min: (float) - The minimum time taken for a single iteration in the specified units
+                - max: (float) - The maximum time taken for a single iteration in the specified units
+                - std: (float) - The standard deviation of time taken by the function in the specified units
+                - raw: (list[float]) - The raw timing history in the specified units
+        """
+        timing_history = []
+
+        for _ in range(self.iterations):
+            start = time.time()
+            self.__fn__(*args, **kwargs)
+            time_taken = time.time() - start
+            timing_history.append(time_taken)
+
+        avg_time = sum(timing_history) / len(timing_history)
+        min_time = min(timing_history)
+        max_time = max(timing_history)
+        if self.iterations == 1:
+            stdev_time = 0
+        else:
+            stdev_time = (
+                sum((x - avg_time) ** 2 for x in timing_history)
+                / len(timing_history)
+            ) ** 0.5
+
+        output = {
+            "module": self.__fn__.__module__,
+            "function": self.__fn__.__qualname__,
+            "unit": self.units,
+            "iterations": self.iterations,
+            "avg": avg_time * self.__divisor__,
+            "min": min_time * self.__divisor__,
+            "max": max_time * self.__divisor__,
+            "std": stdev_time * self.__divisor__,
+            "raw": [i * self.__divisor__ for i in timing_history],
+        }
+        if self.print_time_stats:
+            print(
+                f"{output['function']}: {output['avg']:.3f} ± {output['std']:.3f} {output['unit']} over {output['iterations']} iterations"
+            )
+        return output
+
+
+def pamda_timer(
+    fn, units="ms", iterations=1, print_call=True, print_time_stats=False
+):
+    """
+    Function:
+    Create a pamda_timer object.
+    A wrapper function to create a pamda_timer object with the specified parameters.
+
+    Optional:
+
+    - `units`: str, the units for the time measurement (default is "ms")
+    - `iterations`: int, the number of iterations to run the function when getting time statistics (default is 1)
+    - `print_call`: bool, whether to print the function call time when this object is called (default is True)
+    - `print_time_stats`: bool, whether to print the time statistics when get_time_stats is called (default is False)
+    """
+    return PamdaTimer(
+        fn,
+        units=units,
+        iterations=iterations,
+        print_call=print_call,
+        print_time_stats=print_time_stats,
+    )
+
+
+pamda_timer = Partial(
+    pamda_timer,
+    units="ms",
+    iterations=1,
+    print_call=True,
+    print_time_stats=False,
+)
