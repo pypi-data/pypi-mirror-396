febdf41fa2de480732e10369edafcf7a7fe6fe17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 20:06:02 +0000

    Add package drf-response-wrappers/1.2.7/drf_response_wrappers-1.2.7.tar.gz

---
+++ packages/drf-response-wrappers/drf_response_wrappers-1.2.7.tar.gz/drf_response_wrappers-1.2.7/drf_response_wrapper/__init__.py
@@ -0,0 +1,4 @@
+from .middleware import APIResponseWrapperMiddleware
+from .version import __version__
+
+__all__ = ["APIResponseWrapperMiddleware", "__version__"]
---
+++ packages/drf-response-wrappers/drf_response_wrappers-1.2.7.tar.gz/drf_response_wrappers-1.2.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="drf_response_wrappers",
+    version="1.2.7" ,
+    packages=find_packages(),
+    include_package_data=True,
+    description="DRF middleware to wrap all API responses",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Md Harun Or Roshed Riyad",
+    author_email="rieadhasan499@gmail.com",
+    url="https://github.com/yourusername/drf-response-wrapper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.8",
+)
