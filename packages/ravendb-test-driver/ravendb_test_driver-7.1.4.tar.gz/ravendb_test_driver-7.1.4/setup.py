8fa0d82c01f5465a4a245f3c7ce3981f48e0ea6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:31:18 +0000

    Add package ravendb-test-driver/7.1.4/ravendb_test_driver-7.1.4.tar.gz

---
+++ packages/ravendb-test-driver/ravendb_test_driver-7.1.4.tar.gz/ravendb_test_driver-7.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ravendb-test-driver",
+    packages=find_packages(exclude=["*.tests.*", "tests", "*.tests", "tests.*"]),
+    version="7.1.4",
+    description="RavenDB package for writing integration tests against RavenDB server",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    author="RavenDB",
+    author_email="support@ravendb.net",
+    url="https://github.com/ravendb/ravendb-python-testdriver",
+    license="MIT",
+    keywords=["ravendb", "nosql", "database", "test", "driver"],
+    python_requires="~=3.9",
+    license_files="LICENSE",
+    install_requires=["ravendb-embedded==7.1.4", "ravendb~=7.1.4"],
+)
