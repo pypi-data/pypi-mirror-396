fe6429bc238a098f0d3a63572efb5de17239d492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 15:10:10 +0000

    Add package fias-public-api/1.0.3/fias_public_api-1.0.3-py3-none-any.whl

---
+++ packages/fias-public-api/fias_public_api-1.0.3-py3-none-any.whl/fias_public_api/__init__.py
@@ -0,0 +1,17 @@
+from .constants import STANDART_HEADERS, AddressType, retry_on_error
+from .fias_async import get_token_async, AsyncFPA
+from .fias_sync import get_token_sync, SyncFPA
+from . import constants, fias_async, fias_sync
+
+__all__ = [
+    "STANDART_HEADERS",
+    "AddressType",
+    "retry_on_error",
+    "get_token_async",
+    "get_token_sync",
+    "AsyncFPA",
+    "SyncFPA",
+    "constants",
+    "fias_async",
+    "fias_sync",
+]
