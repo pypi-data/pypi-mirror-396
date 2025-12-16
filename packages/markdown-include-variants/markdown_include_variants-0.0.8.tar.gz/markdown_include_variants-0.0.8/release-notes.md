bb22ad24abcd2b98896888f67b249b13ac242fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 15:10:35 +0000

    Add package markdown-include-variants/0.0.8/markdown_include_variants-0.0.8.tar.gz

---
+++ packages/markdown-include-variants/markdown_include_variants-0.0.8.tar.gz/markdown_include_variants-0.0.8/release-notes.md
@@ -0,0 +1,92 @@
+# Release Notes
+
+## Latest Changes
+
+## 0.0.8
+
+### Fixes
+
+* 🐛 Fix case with single folder with non-default version. PR [#41](https://github.com/tiangolo/markdown-include-variants/pull/41) by [@YuriiMotov](https://github.com/YuriiMotov).
+
+## 0.0.7
+
+### Features
+
+* ✨ Support `title` option. PR [#39](https://github.com/tiangolo/markdown-include-variants/pull/39) by [@YuriiMotov](https://github.com/YuriiMotov).
+
+### Upgrades
+
+* ⬆️ Upgrade uv config in pyproject.toml. PR [#40](https://github.com/tiangolo/markdown-include-variants/pull/40) by [@tiangolo](https://github.com/tiangolo).
+
+## 0.0.6
+
+### Features
+
+* ✨ Search for variants by folder name if no variants by file name found. PR [#38](https://github.com/tiangolo/markdown-include-variants/pull/38) by [@YuriiMotov](https://github.com/YuriiMotov).
+
+### Internal
+
+* ⬆ Bump actions/checkout from 5 to 6. PR [#37](https://github.com/tiangolo/markdown-include-variants/pull/37) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump actions/checkout from 5 to 6. PR [#34](https://github.com/tiangolo/markdown-include-variants/pull/34) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* 👷 Upgrade `latest-changes` GitHub Action and pin `actions/checkout@v5`. PR [#35](https://github.com/tiangolo/markdown-include-variants/pull/35) by [@tiangolo](https://github.com/tiangolo).
+* ⬆ Bump actions/upload-artifact from 4 to 5. PR [#32](https://github.com/tiangolo/markdown-include-variants/pull/32) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump actions/download-artifact from 5 to 6. PR [#33](https://github.com/tiangolo/markdown-include-variants/pull/33) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump astral-sh/setup-uv from 6 to 7. PR [#31](https://github.com/tiangolo/markdown-include-variants/pull/31) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump tiangolo/issue-manager from 0.5.1 to 0.6.0. PR [#30](https://github.com/tiangolo/markdown-include-variants/pull/30) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump actions/labeler from 5 to 6. PR [#29](https://github.com/tiangolo/markdown-include-variants/pull/29) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump actions/setup-python from 5 to 6. PR [#27](https://github.com/tiangolo/markdown-include-variants/pull/27) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump pypa/gh-action-pypi-publish from 1.12.4 to 1.13.0. PR [#26](https://github.com/tiangolo/markdown-include-variants/pull/26) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump actions/checkout from 4 to 5. PR [#25](https://github.com/tiangolo/markdown-include-variants/pull/25) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump actions/download-artifact from 4 to 5. PR [#24](https://github.com/tiangolo/markdown-include-variants/pull/24) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump tiangolo/latest-changes from 0.3.2 to 0.4.0. PR [#23](https://github.com/tiangolo/markdown-include-variants/pull/23) by [@dependabot[bot]](https://github.com/apps/dependabot).
+
+## 0.0.5
+
+### Features
+
+* ✨ Support Python 3.11. PR [#28](https://github.com/tiangolo/markdown-include-variants/pull/28) by [@svlandeg](https://github.com/svlandeg).
+
+### Internal
+
+* ⬆ Bump astral-sh/setup-uv from 5 to 6. PR [#22](https://github.com/tiangolo/markdown-include-variants/pull/22) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump pypa/gh-action-pypi-publish from 1.12.3 to 1.12.4. PR [#21](https://github.com/tiangolo/markdown-include-variants/pull/21) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* 👷 Publish on release released. PR [#20](https://github.com/tiangolo/markdown-include-variants/pull/20) by [@tiangolo](https://github.com/tiangolo).
+
+## 0.0.4
+
+### Features
+
+* ✨ Include variant tab in full file preview. PR [#19](https://github.com/tiangolo/markdown-include-variants/pull/19) by [@tiangolo](https://github.com/tiangolo).
+* ✨ Support including only a file preview. PR [#18](https://github.com/tiangolo/markdown-include-variants/pull/18) by [@tiangolo](https://github.com/tiangolo).
+
+### Internal
+
+* ⬆ Bump astral-sh/setup-uv from 3 to 5. PR [#17](https://github.com/tiangolo/markdown-include-variants/pull/17) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump tiangolo/latest-changes from 0.3.1 to 0.3.2. PR [#14](https://github.com/tiangolo/markdown-include-variants/pull/14) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump pypa/gh-action-pypi-publish from 1.11.0 to 1.12.3. PR [#16](https://github.com/tiangolo/markdown-include-variants/pull/16) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* ⬆ Bump pypa/gh-action-pypi-publish from 1.10.3 to 1.11.0. PR [#11](https://github.com/tiangolo/markdown-include-variants/pull/11) by [@dependabot[bot]](https://github.com/apps/dependabot).
+
+## 0.0.3
+
+### Features
+
+* ✨ Add highlight for full file preview. PR [#10](https://github.com/tiangolo/markdown-include-variants/pull/10) by [@tiangolo](https://github.com/tiangolo).
+
+## 0.0.2
+
+### Refactors
+
+* ♻️ Make the preferred variant the one used in the include. PR [#9](https://github.com/tiangolo/markdown-include-variants/pull/9) by [@tiangolo](https://github.com/tiangolo).
+
+### Docs
+
+* 📝 Fix MkDocs config example. PR [#8](https://github.com/tiangolo/markdown-include-variants/pull/8) by [@tiangolo](https://github.com/tiangolo).
+* 📝 Update README with mkdocs.yml configs. PR [#6](https://github.com/tiangolo/markdown-include-variants/pull/6) by [@tiangolo](https://github.com/tiangolo).
+
+### Internal
+
+* ⬆ Bump pypa/gh-action-pypi-publish from 1.9.0 to 1.10.3. PR [#1](https://github.com/tiangolo/markdown-include-variants/pull/1) by [@dependabot[bot]](https://github.com/apps/dependabot).
+* 👷 Tweak labeler GitHub Action. PR [#7](https://github.com/tiangolo/markdown-include-variants/pull/7) by [@tiangolo](https://github.com/tiangolo).
+* 👷 Fix smokeshow setup. PR [#4](https://github.com/tiangolo/markdown-include-variants/pull/4) by [@tiangolo](https://github.com/tiangolo).
+* 👷 Tweak labeler GitHub Action to not change labels on manual labels. PR [#5](https://github.com/tiangolo/markdown-include-variants/pull/5) by [@tiangolo](https://github.com/tiangolo).
+* 👷 Tweak CI and types. PR [#2](https://github.com/tiangolo/markdown-include-variants/pull/2) by [@tiangolo](https://github.com/tiangolo).
---
+++ packages/markdown-include-variants/markdown_include_variants-0.0.8.tar.gz/markdown_include_variants-0.0.8/src/markdown_include_variants/_main.py
@@ -0,0 +1,462 @@
+import re
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Literal, Union
+
+from markdown import Extension, Markdown
+from markdown.preprocessors import Preprocessor
+
+AN = "_an"
+PY39 = "_py39"
+PY310 = "_py310"
+PY311 = "_py311"
+
+SUFFIXES_TO_REMOVE = ["_an", "_py39", "_py310", "_py311"]
+
+
+@dataclass(frozen=True)
+class Variant:
+    path: Path
+    title: str
+    is_annotated: Union[bool, None]
+    version: Literal["py38", "py39", "py310", "py311"]
+
+
+def remove_suffixes(base_name: str, suffixes: list[str]) -> str:
+    for suffix in suffixes:
+        if base_name.endswith(suffix):
+            base_name = base_name[: -len(suffix)]
+            return remove_suffixes(base_name, suffixes)
+    return base_name
+
+
+def calculate_variants(base_path: Path) -> dict[tuple[str, Union[bool, None]], Variant]:
+    preferred_name = base_path.stem
+    base_name = remove_suffixes(preferred_name, SUFFIXES_TO_REMOVE)
+    base_dir = base_path.parent
+
+    an_p311_path = base_dir / f"{base_name}{AN}{PY311}.py"
+    an_p310_path = base_dir / f"{base_name}{AN}{PY310}.py"
+    an_p39_path = base_dir / f"{base_name}{AN}{PY39}.py"
+    an_p38_path = base_dir / f"{base_name}{AN}.py"
+    p311_path = base_dir / f"{base_name}{PY311}.py"
+    p310_path = base_dir / f"{base_name}{PY310}.py"
+    p39_path = base_dir / f"{base_name}{PY39}.py"
+    p38_path = base_dir / f"{base_name}.py"
+
+    return _create_variants(
+        an_p311_path=an_p311_path,
+        an_p310_path=an_p310_path,
+        an_p39_path=an_p39_path,
+        an_p38_path=an_p38_path,
+        p311_path=p311_path,
+        p310_path=p310_path,
+        p39_path=p39_path,
+        p38_path=p38_path,
+    )
+
+
+def calculate_variants_folder(
+    base_path: Path,
+) -> dict[tuple[str, Union[bool, None]], Variant]:
+    file_name = base_path.name
+    folder = base_path.parent
+    base_dir = folder.parent
+    base_name = remove_suffixes(folder.name, SUFFIXES_TO_REMOVE)
+
+    an_p311_path = base_dir / f"{base_name}{AN}{PY311}" / file_name
+    an_p310_path = base_dir / f"{base_name}{AN}{PY310}" / file_name
+    an_p39_path = base_dir / f"{base_name}{AN}{PY39}" / file_name
+    an_p38_path = base_dir / f"{base_name}{AN}" / file_name
+    p311_path = base_dir / f"{base_name}{PY311}" / file_name
+    p310_path = base_dir / f"{base_name}{PY310}" / file_name
+    p39_path = base_dir / f"{base_name}{PY39}" / file_name
+    p38_path = base_dir / f"{base_name}" / file_name
+
+    return _create_variants(
+        an_p311_path=an_p311_path,
+        an_p310_path=an_p310_path,
+        an_p39_path=an_p39_path,
+        an_p38_path=an_p38_path,
+        p311_path=p311_path,
+        p310_path=p310_path,
+        p39_path=p39_path,
+        p38_path=p38_path,
+    )
+
+
+def _create_variants(
+    an_p311_path: Path,
+    an_p310_path: Path,
+    an_p39_path: Path,
+    an_p38_path: Path,
+    p311_path: Path,
+    p310_path: Path,
+    p39_path: Path,
+    p38_path: Path,
+) -> dict[tuple[str, Union[bool, None]], Variant]:
+    variants: dict[tuple[str, Union[bool, None]], Variant] = {}
+    if an_p311_path.exists():
+        v = Variant(
+            path=an_p311_path,
+            title="Python 3.11+",
+            is_annotated=True,
+            version="py311",
+        )
+        variants[(v.version, v.is_annotated)] = v
+        if p311_path.exists():
+            v = Variant(
+                path=p311_path,
+                title="Python 3.11+ - non-Annotated",
+                is_annotated=False,
+                version="py311",
+            )
+            variants[(v.version, v.is_annotated)] = v
+    elif p311_path.exists():
+        v = Variant(
+            path=p311_path,
+            title="Python 3.11+",
+            is_annotated=None,
+            version="py311",
+        )
+        variants[(v.version, v.is_annotated)] = v
+    if an_p310_path.exists():
+        v = Variant(
+            path=an_p310_path,
+            title="Python 3.10+",
+            is_annotated=True,
+            version="py310",
+        )
+        variants[(v.version, v.is_annotated)] = v
+        if p310_path.exists():
+            v = Variant(
+                path=p310_path,
+                title="Python 3.10+ - non-Annotated",
+                is_annotated=False,
+                version="py310",
+            )
+            variants[(v.version, v.is_annotated)] = v
+    elif p310_path.exists():
+        v = Variant(
+            path=p310_path,
+            title="Python 3.10+",
+            is_annotated=None,
+            version="py310",
+        )
+        variants[(v.version, v.is_annotated)] = v
+    if an_p39_path.exists():
+        v = Variant(
+            path=an_p39_path,
+            title="Python 3.9+",
+            is_annotated=True,
+            version="py39",
+        )
+        variants[(v.version, v.is_annotated)] = v
+        if p39_path.exists():
+            v = Variant(
+                path=p39_path,
+                title="Python 3.9+ - non-Annotated",
+                is_annotated=False,
+                version="py39",
+            )
+            variants[(v.version, v.is_annotated)] = v
+    elif p39_path.exists():
+        v = Variant(
+            path=p39_path,
+            title="Python 3.9+",
+            is_annotated=None,
+            version="py39",
+        )
+        variants[(v.version, v.is_annotated)] = v
+    if an_p38_path.exists():
+        v = Variant(
+            path=an_p38_path,
+            title="Python 3.8+",
+            is_annotated=True,
+            version="py38",
+        )
+        variants[(v.version, v.is_annotated)] = v
+        if p38_path.exists():
+            v = Variant(
+                path=p38_path,
+                title="Python 3.8+ - non-Annotated",
+                is_annotated=False,
+                version="py38",
+            )
+            variants[(v.version, v.is_annotated)] = v
+    elif p38_path.exists():
+        v = Variant(
+            path=p38_path,
+            title="Python 3.8+",
+            is_annotated=None,
+            version="py38",
+        )
+        variants[(v.version, v.is_annotated)] = v
+    return variants
+
+
+def parse_lines_index(include_lines_config: str) -> list[tuple[int, int]]:
+    lines = []
+    for line in include_lines_config.strip().split(","):
+        clean_line = line.strip()
+        if ":" in clean_line:
+            start, end = clean_line.split(":")
+            lines.append((int(start), int(end)))
+        else:
+            lines.append((int(line), int(line)))
+    lines.sort()
+    return lines
+
+
+@dataclass
+class IncludeInfo:
+    start: int
+    end: int
+    offset: int
+    hl_lines: list[tuple[int, int]]
+
+    def computed_hl_lines(self) -> list[tuple[int, int]]:
+        return [
+            (
+                hl_line[0] - self.start + self.offset + 1,
+                hl_line[1] - self.start + self.offset + 1,
+            )
+            for hl_line in self.hl_lines
+        ]
+
+
+def calculate_includes(
+    lines_to_include: list[tuple[int, int]], hl_lines: list[tuple[int, int]]
+) -> dict[tuple[int, int], IncludeInfo]:
+    lines_groups: dict[tuple[int, int], IncludeInfo] = {}
+    offset = 0
+    for i, line_group in enumerate(lines_to_include):
+        if line_group[0] > 1:
+            if i == 0:
+                offset = 2
+            else:
+                offset += 3
+        info = IncludeInfo(
+            start=line_group[0],
+            end=line_group[1],
+            offset=offset,
+            hl_lines=[],
+        )
+        offset += line_group[1] - line_group[0] + 1
+        for hl_line in hl_lines:
+            if hl_line[0] >= line_group[0] and hl_line[1] <= line_group[1]:
+                info.hl_lines.append(hl_line)
+        lines_groups[line_group] = info
+    return lines_groups
+
+
+def generate_hl_string(hl_lines: list[tuple[int, int]]) -> str:
+    hl_lines_strs: list[str] = []
+    for hl_line in hl_lines:
+        if hl_line[0] == hl_line[1]:
+            hl_lines_strs.append(str(hl_line[0]))
+        else:
+            hl_lines_strs.append(f"{hl_line[0]}-{hl_line[1]}")
+    return " ".join(hl_lines_strs)
+
+
+re_line = r"^\{\*\s*(\S+)\s*(.*)\*\}$"
+re_config = r"(\w+)\[([^\]]+)\]"
+
+
+class IncludeVariantsPreprocessor(Preprocessor):
+    def run(self, lines: list[str]) -> list[str]:
+        new_lines = []
+        for line in lines:
+            line_match = re.match(re_line, line)
+            if not line_match:
+                new_lines.append(line)
+                continue
+            block_lines = []
+            src_file_str = line_match.group(1)
+            base_path = Path(src_file_str)
+
+            configs_str = line_match.group(2).strip()
+            include_lines: list[tuple[int, int]] = []
+            highlight_lines: list[tuple[int, int]] = []
+            title: Union[str, None] = None
+            for config_match in re.finditer(re_config, configs_str):
+                if config_match:
+                    name = config_match.group(1)
+                    value_str = config_match.group(2)
+                    if name == "ln":
+                        include_lines.extend(parse_lines_index(value_str))
+                    elif name == "hl":
+                        highlight_lines.extend(parse_lines_index(value_str))
+                    elif name == "title":
+                        assert value_str.startswith('"') and value_str.endswith('"')
+                        title = value_str[1:-1]
+            use_hl_lines: list[tuple[int, int]] = []
+            if highlight_lines:
+                if include_lines:
+                    include_infos = calculate_includes(include_lines, highlight_lines)
+                    for info in include_infos.values():
+                        use_hl_lines.extend(info.computed_hl_lines())
+                else:
+                    use_hl_lines = highlight_lines
+            use_hl_lines_str = generate_hl_string(use_hl_lines)
+
+            variants = calculate_variants(base_path)
+            if len(variants) == 0:
+                raise FileNotFoundError(f"Could not find any variants for {base_path}")
+            if len(variants) == 1:
+                folder_variants = calculate_variants_folder(base_path)
+                if len(folder_variants) > 1:
+                    variants = folder_variants
+                elif len(folder_variants) == 1:
+                    file_variant_is_default = variants.get(("py38", None)) is not None
+                    folder_variant_is_default = (
+                        folder_variants.get(("py38", None)) is not None
+                    )
+                    # File name doesn't contain any suffixes, but folder does - use variants from folder
+                    if file_variant_is_default and not folder_variant_is_default:
+                        variants = folder_variants
+            sorted_variants: list[Variant] = []
+            preferred_variant = None
+            for an in [True, False, None]:
+                for version in ["py311", "py310", "py39", "py38"]:
+                    v = variants.get((version, an))
+                    if v:
+                        if v.path == base_path:
+                            assert preferred_variant is None
+                            preferred_variant = v
+                        else:
+                            sorted_variants.append(v)
+            assert preferred_variant is not None
+            content_lines = preferred_variant.path.read_text().splitlines()
+
+            internal_block_lines: list[str] = []
+            if not include_lines:
+                internal_block_lines.append(
+                    f"{{!{preferred_variant.path}!}}",
+                )
+            if include_lines != [(0, 0)]:
+                for i, line_group in enumerate(include_lines):
+                    if i == 0 and line_group[0] > 1:
+                        internal_block_lines.extend(
+                            [
+                                "# Code above omitted 👆",
+                                "",
+                            ]
+                        )
+                    if i >= 1:
+                        internal_block_lines.extend(
+                            [
+                                "",
+                                "# Code here omitted 👈",
+                                "",
+                            ]
+                        )
+                    internal_block_lines.append(
+                        f"{{!{preferred_variant.path}[ln:{line_group[0]}-{line_group[1]}]!}}"
+                    )
+                    if i == len(include_lines) - 1 and line_group[1] < len(
+                        content_lines
+                    ):
+                        internal_block_lines.extend(
+                            [
+                                "",
+                                "# Code below omitted 👇",
+                            ]
+                        )
+                first_line = "```python"
+                if use_hl_lines:
+                    first_line += f' hl_lines="{use_hl_lines_str}"'
+                if title:
+                    first_line += f' title="{title}"'
+
+                block_lines.extend(
+                    [
+                        f"//// tab | {preferred_variant.title}",
+                        first_line,
+                        *internal_block_lines,
+                        "```",
+                        "",
+                        "////",
+                        "",
+                    ]
+                )
+            if include_lines:
+                first_line_full = "```python"
+                if highlight_lines:
+                    hl_lines_full_str = generate_hl_string(highlight_lines)
+                    first_line_full += f' hl_lines="{hl_lines_full_str}"'
+                if title:
+                    first_line_full += f' title="{title}"'
+                block_lines.extend(
+                    [
+                        "///// details | 👀 Full file preview",
+                        "",
+                        f"//// tab | {preferred_variant.title}",
+                        "",
+                        first_line_full,
+                        f"{{!{preferred_variant.path}!}}",
+                        "```",
+                        "",
+                        "////",
+                        "",
+                        "/////",
+                        "",
+                    ]
+                )
+            if len(sorted_variants):
+                first_line_variant = "```python"
+                if title:
+                    first_line_variant += f' title="{title}"'
+                block_lines.extend(
+                    [
+                        "///// details | 🤓 Other versions and variants",
+                        "",
+                    ]
+                )
+
+                for variant in sorted_variants:
+                    block_lines.extend([f"//// tab | {variant.title}", ""])
+                    if variant.is_annotated is False:
+                        block_lines.extend(
+                            [
+                                "/// tip",
+                                "",
+                                "Prefer to use the `Annotated` version if possible.",
+                                "",
+                                "///",
+                                "",
+                            ]
+                        )
+
+                    block_lines.extend(
+                        [
+                            first_line_variant,
+                            f"{{!{variant.path}!}}",
+                            "```",
+                            "////",
+                        ]
+                    )
+
+                block_lines.extend(
+                    [
+                        "",
+                        "/////",
+                        "",
+                    ]
+                )
+            new_lines.extend(block_lines)
+        return new_lines
+
+
+class IncludeVariantsExtension(Extension):
+    def extendMarkdown(self, md: Markdown) -> None:
+        md.preprocessors.register(
+            IncludeVariantsPreprocessor(md), "include_variants", 110
+        )
+
+
+def makeExtension(
+    *args: Any, **kwargs: Any
+) -> IncludeVariantsExtension:  # pragma: no cover
+    return IncludeVariantsExtension(*args, **kwargs)
