63e2987bcede9b3822b73d8286ed63d8df99defb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:07:51 +0000

    Add package leetcode-terminal/0.1.0/leetcode_terminal-0.1.0.tar.gz

---
+++ packages/leetcode-terminal/leetcode_terminal-0.1.0.tar.gz/leetcode_terminal-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="leetcode-terminal",  # CLI command
+    version="0.1.0",
+    description="LeetCLI - Solve, practice, and fetch LeetCode challenges from your terminal",
+     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Chitransh Saxena",
+    author_email="geniussaxena007@gmail.com",
+    url="https://github.com/Chitransh2309",  # optional, GitHub repo URL if any
+    packages=find_packages(include=["*", "leet*"]),
+    python_requires=">=3.7",
+    install_requires=[
+        "typer[all]>=0.9.0",
+        "requests>=2.30.0",
+        "rich>=13.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "lc=leetcli.main:app",  # CLI command points to main.py Typer app
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # change if needed
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Utilities",
+    ],
+    include_package_data=True,
+)
