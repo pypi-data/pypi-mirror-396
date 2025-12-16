5c99a0150cae9dbe3c462b772b948b0192a2ba35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 11:05:25 +0000

    Add package tree-processing/0.4.1/tree_processing-0.4.1.tar.gz

---
+++ packages/tree-processing/tree_processing-0.4.1.tar.gz/tree_processing-0.4.1/src/tree_processing/actions/filesystem.py
@@ -0,0 +1,70 @@
+from os import link
+from shutil import copy
+from . import chainable, filterable, Node
+
+
+@chainable
+def not_hidden(node: Node):
+    '''A helper to filter out "hidden" files and folders on UNIX-like systems
+    (i.e. those whose names start with a dot).'''
+    return not node.name.startswith('.')
+
+
+@chainable
+def src_is_regular_file(node: Node):
+    '''A helper to filter out symlinks and other non-regular files.'''
+    src, dst = node.current
+    return src.is_file()
+
+
+@filterable
+def recurse_into_folders(node: Node):
+    '''A helper to do nothing special with folders.'''
+    pass
+
+
+# Adapt a function that accepts src and dst paths,
+# into one that expects a `(src, dst)` pair in `node.current`.
+def _mirror(func):
+    return filterable(lambda node: func(*node.current))
+
+
+# Path.hardlink_to requires 3.10.
+# For better compatibility, we fall back to the os module
+# and also fall back on copying if hardlinks fail for any reason.
+def hardlink_or_copy(src, dst):
+    '''A helper to hard-link from the source to the destination when possible,
+    and copy otherwise.'''
+    try:
+        link(src, dst)
+    except:
+        copy(src, dst)
+hardlink_or_copy_files = _mirror(hardlink_or_copy)
+
+
+copy_files = _mirror(copy)
+
+
+def _fake_copy(src, dst):
+    '''A helper for testing. Displays diagnostic messages.'''
+    print(f'Would copy {src} to {dst}')
+
+
+fake_copy_regular_files = _mirror(_fake_copy).which(src_is_regular_file)
+
+
+@filterable
+def propagate_folders(node: Node):
+    '''A helper to create folders in the destination file tree
+    which correspond to the source file tree.'''
+    src, dst = node.current
+    assert src.is_dir()
+    dst.mkdir(exist_ok=True)
+
+
+@filterable
+def fake_propagate_folders(node: Node):
+    '''A helper for testing. Displays diagnostic messages.'''
+    src, dst = node.current
+    assert src.is_dir()
+    print(f'Would create folder {dst} if missing')
