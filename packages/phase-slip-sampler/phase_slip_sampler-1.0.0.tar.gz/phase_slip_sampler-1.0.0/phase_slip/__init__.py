55b017ec897ff484d62d32ef5837d91b57779b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 19:10:34 +0000

    Add package phase-slip-sampler/1.0.0/phase_slip_sampler-1.0.0.tar.gz

---
+++ packages/phase-slip-sampler/phase_slip_sampler-1.0.0.tar.gz/phase_slip_sampler-1.0.0/phase_slip/__init__.py
@@ -0,0 +1,11 @@
+# phase_slip/__init__.py
+
+"""
+ Original Author: Michael Christian Morgan
+ 2025.12.03
+ Github: https://github.com/Mmorgan-ML
+ Twitter: @Mmorgan_ML
+ Email: mmorgankorea@gmail.com
+"""
+
+from .sampler import PhaseSlipSampler
