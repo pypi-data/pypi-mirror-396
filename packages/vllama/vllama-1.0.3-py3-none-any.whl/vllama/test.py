50fd2048ddabf151a80a6f8852488499a12b1dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 14:06:49 +0000

    Add package vllama/1.0.3/vllama-1.0.3-py3-none-any.whl

---
+++ packages/vllama/vllama-1.0.3-py3-none-any.whl/vllama/test.py
@@ -0,0 +1,8 @@
+from core import *
+
+if __name__ == "__main__":
+    # text_to_speech("Hello world this is Manvith building the text to speech module with in built library of python")
+    # text_to_speech()
+    # text_to_speech("exit")
+
+    list_downloads()
