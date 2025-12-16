1fc0f52f963d99bc47dab7f876ebb29118d313f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 06:05:28 +0000

    Add package vscodenb/0.1.14/vscodenb-0.1.14.tar.gz

---
+++ packages/vscodenb/vscodenb-0.1.14.tar.gz/vscodenb-0.1.14/src/vscodenb/utils.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import numpy as np
+
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    if isinstance(cmap, str):
+        cmap = plt.get_cmap(cmap)
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
+        cmap(np.linspace(minval, maxval, n)))
+    return new_cmap
