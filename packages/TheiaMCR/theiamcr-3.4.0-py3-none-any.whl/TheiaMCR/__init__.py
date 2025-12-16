38e17873f3126bde230171355de5ed3904153aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 16:05:57 +0000

    Add package TheiaMCR/3.4.0/theiamcr-3.4.0-py3-none-any.whl

---
+++ packages/TheiaMCR/theiamcr-3.4.0-py3-none-any.whl/TheiaMCR/__init__.py
@@ -0,0 +1,12 @@
+# TheiaMCR module for controling motors using MCR600
+#
+# Theia Technologies
+# https://www.theiatech.com
+# (c) 2023 Theia Technologies
+__author__ = 'Mark Peterson'
+__credits__ = 'Theia Technologies'
+
+#from TheiaMCR.TheiaMCR import *
+from .TheiaMCR import MCRControl
+from .errList import *
+from .rotatingLogFiles import *
