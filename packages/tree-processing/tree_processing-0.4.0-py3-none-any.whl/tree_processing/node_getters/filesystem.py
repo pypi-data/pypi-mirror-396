cd061f25ce499c94f6718585f5c1cb5f16e2b2da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 09:08:53 +0000

    Add package tree-processing/0.4.0/tree_processing-0.4.0-py3-none-any.whl

---
+++ packages/tree-processing/tree_processing-0.4.0-py3-none-any.whl/tree_processing/node_getters/filesystem.py
@@ -0,0 +1,32 @@
+from os import scandir as _scan_directory
+from pathlib import Path
+
+from . import make_getter
+from ..actions import Node
+
+
+# Client code can customize the get and make routines by attaching filters
+# and/or error handling and then calling make_getter().
+
+
+def raw_get(node):
+    to_scan = node.current
+    if isinstance(to_scan, tuple): # "mirroring" traversal
+        to_scan = to_scan[0]
+    yield from _scan_directory(to_scan)
+
+
+def make_node(parent_node, entry):
+    new_current, new_parent = Path(entry.path), parent_node.current
+    if isinstance(new_parent, tuple): # "mirroring" traversal.
+        new_current = (new_current, new_parent[1] / entry.name)
+    return Node(entry.is_dir(), entry.name, new_current, new_parent)
+
+
+default_get = make_getter(raw_get, make_node)
+
+
+def make_root(src, dst=None):
+    src = Path(src)
+    current = src if dst is None else (src, Path(dst))
+    return Node(src.is_dir(), src.name, current, None)
