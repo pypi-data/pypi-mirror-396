e50cfabdc1fe6501ab8f4043d8b0debff3068431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 11:05:50 +0000

    Add package onnxruntime-genai-cuda/0.11.4/onnxruntime_genai_cuda-0.11.4-cp312-cp312-win_amd64.whl

---
+++ packages/onnxruntime-genai-cuda/onnxruntime_genai_cuda-0.11.4-cp312-cp312-win_amd64.whl/onnxruntime_genai/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License
+
+from onnxruntime_genai import _dll_directory
+
+__version__ = "0.11.4"
+__id__ = "onnxruntime-genai-cuda"
+
+_dll_directory.add_onnxruntime_dependency(__id__)
+
+try:
+    from onnxruntime_genai.onnxruntime_genai import *
+except ImportError as e:
+    if __id__ == "onnxruntime-genai-cuda":
+        # Try importing onnxruntime_genai. If an ImportError is raised,
+        # it could be because the cuda dlls could not be found on windows.
+        # Try adding the cuda dlls path to the dll search directory
+        # and import onnxruntime_genai again.
+        _dll_directory.add_cuda_dependency()
+        from onnxruntime_genai.onnxruntime_genai import *
+    else:
+        raise e
