4ef83d9c517fd61b4006eed6fbbdbce268172a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:07:04 +0000

    Add package recallbricks/1.5.0/recallbricks-1.5.0.tar.gz

---
+++ packages/recallbricks/recallbricks-1.5.0.tar.gz/recallbricks-1.5.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="recallbricks",
+    version="1.5.0",
+    author="RecallBricks",
+    author_email="support@recallbricks.com",
+    description="Enterprise-Grade Memory Layer for AI - Persistent memory with advanced metacognition",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/recallbricks/recallbricks-python",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "requests>=2.31.0",
+    ],
+)
