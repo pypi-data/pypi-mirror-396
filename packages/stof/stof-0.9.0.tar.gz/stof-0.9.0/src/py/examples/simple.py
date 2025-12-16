319ae888f77b13e6cb88e72ab43090d14c6bae38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 19:10:26 +0000

    Add package stof/0.9.0/stof-0.9.0.tar.gz

---
+++ packages/stof/stof-0.9.0.tar.gz/stof-0.9.0/src/py/examples/simple.py
@@ -0,0 +1,22 @@
+
+from pystof import Doc
+
+STOF = """
+#[main]
+fn main() {
+    const name = Example.name('Stof,', 'with Python');
+    pln(`Hello, ${name}!!`)
+}
+"""
+
+def name(first, last):
+    return first + ' ' + last
+
+def main():
+    doc = Doc()
+    doc.lib('Example', 'name', name)
+    doc.parse(STOF)
+    doc.run()
+
+if __name__ == "__main__":
+    main()
