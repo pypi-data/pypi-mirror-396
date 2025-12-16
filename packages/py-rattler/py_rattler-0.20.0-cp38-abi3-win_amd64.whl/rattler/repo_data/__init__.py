3894089ed3e436ca9e9e00ab0474e3de01ba4e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 04:15:10 +0000

    Add package py-rattler/0.20.0/py_rattler-0.20.0-cp38-abi3-win_amd64.whl

---
+++ packages/py-rattler/py_rattler-0.20.0-cp38-abi3-win_amd64.whl/rattler/repo_data/__init__.py
@@ -0,0 +1,17 @@
+from rattler.repo_data.package_record import PackageRecord
+from rattler.repo_data.repo_data import RepoData
+from rattler.repo_data.patch_instructions import PatchInstructions
+from rattler.repo_data.record import RepoDataRecord
+from rattler.repo_data.sparse import SparseRepoData, PackageFormatSelection
+from rattler.repo_data.gateway import Gateway, SourceConfig
+
+__all__ = [
+    "PackageRecord",
+    "RepoData",
+    "PatchInstructions",
+    "RepoDataRecord",
+    "SparseRepoData",
+    "Gateway",
+    "SourceConfig",
+    "PackageFormatSelection",
+]
