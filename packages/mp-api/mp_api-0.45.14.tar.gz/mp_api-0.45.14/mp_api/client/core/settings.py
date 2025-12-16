3697a6ea6118bc6a86123f78fcaf24c218e4afdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 16:06:22 +0000

    Add package mp-api/0.45.14/mp_api-0.45.14.tar.gz

---
+++ packages/mp-api/mp_api-0.45.14.tar.gz/mp_api-0.45.14/mp_api/client/core/settings.py
@@ -0,0 +1,90 @@
+import os
+from multiprocessing import cpu_count
+from typing import List
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pymatgen.core import _load_pmg_settings
+
+from mp_api.client import __file__ as root_dir
+
+PMG_SETTINGS = _load_pmg_settings()
+_NUM_PARALLEL_REQUESTS = min(PMG_SETTINGS.get("MPRESTER_NUM_PARALLEL_REQUESTS", 4), 4)
+_MAX_RETRIES = min(PMG_SETTINGS.get("MPRESTER_MAX_RETRIES", 3), 3)
+_MUTE_PROGRESS_BAR = PMG_SETTINGS.get("MPRESTER_MUTE_PROGRESS_BARS", False)
+_MAX_HTTP_URL_LENGTH = PMG_SETTINGS.get("MPRESTER_MAX_HTTP_URL_LENGTH", 2000)
+_MAX_LIST_LENGTH = min(PMG_SETTINGS.get("MPRESTER_MAX_LIST_LENGTH", 10000), 10000)
+
+try:
+    CPU_COUNT = cpu_count()
+except NotImplementedError:
+    pass
+
+
+class MAPIClientSettings(BaseSettings):
+    """Special class to store settings for MAPI client
+    python module.
+    """
+
+    TEST_FILES: str = Field(
+        os.path.join(os.path.dirname(os.path.abspath(root_dir)), "../../test_files"),
+        description="Directory with test files",
+    )
+
+    QUERY_NO_PARALLEL: List[str] = Field(
+        [
+            "elements",
+            "exclude_elements",
+            "possible_species",
+            "coordination_envs",
+            "coordination_envs_anonymous",
+            "has_props",
+            "gb_plane",
+            "rotation_axis",
+            "keywords",
+            "substrate_orientation",
+            "film_orientation",
+            "synthesis_type",
+            "operations",
+            "condition_mixing_device",
+            "condition_mixing_media",
+            "condition_heating_atmosphere",
+            "operations",
+            "_fields",
+        ],
+        description="List API query parameters that do not support parallel requests.",
+    )
+
+    NUM_PARALLEL_REQUESTS: int = Field(
+        _NUM_PARALLEL_REQUESTS,
+        description="Number of parallel requests to send.",
+    )
+
+    MAX_RETRIES: int = Field(
+        _MAX_RETRIES, description="Maximum number of retries for requests."
+    )
+
+    BACKOFF_FACTOR: float = Field(
+        0.1,
+        description="A backoff factor to apply between retry attempts. To disable, set to 0.",
+    )
+
+    MUTE_PROGRESS_BARS: bool = Field(
+        _MUTE_PROGRESS_BAR,
+        description="Whether to mute progress bars when data is retrieved.",
+    )
+
+    MAX_HTTP_URL_LENGTH: int = Field(
+        _MAX_HTTP_URL_LENGTH,
+        description="Number of characters to use to define the maximum length of a given HTTP URL.",
+    )
+
+    MIN_EMMET_VERSION: str = Field(
+        "0.54.0", description="Minimum compatible version of emmet-core for the client."
+    )
+
+    MAX_LIST_LENGTH: int = Field(
+        _MAX_LIST_LENGTH, description="Maximum length of query parameter list"
+    )
+
+    model_config = SettingsConfigDict(env_prefix="MPRESTER_")
---
+++ packages/mp-api/mp_api-0.45.14.tar.gz/mp_api-0.45.14/tests/materials/test_tasks.py
@@ -0,0 +1,59 @@
+import os
+from core_function import client_search_testing
+import pytest
+
+from emmet.core.utils import utcnow
+from mp_api.client.routes.materials.tasks import TaskRester
+
+
+@pytest.fixture
+def rester():
+    rester = TaskRester(monty_decode=False)
+    yield rester
+    rester.session.close()
+
+
+excluded_params = [
+    "sort_fields",
+    "chunk_size",
+    "num_chunks",
+    "all_fields",
+    "fields",
+    "formula",
+    "elements",
+    "exclude_elements",
+]
+
+sub_doc_fields = []  # type: list
+
+alt_name_dict = {
+    "formula": "task_id",
+    "task_ids": "task_id",
+    "exclude_elements": "task_id",
+}  # type: dict
+
+custom_field_tests = {
+    "chemsys": "Si-O",
+    "last_updated": (None, utcnow()),
+    "task_ids": ["mp-149"],
+}  # type: dict
+
+
+@pytest.mark.skipif(os.getenv("MP_API_KEY") is None, reason="No API key found.")
+def test_client(rester):
+    search_method = rester.search
+
+    client_search_testing(
+        search_method=search_method,
+        excluded_params=excluded_params,
+        alt_name_dict=alt_name_dict,
+        custom_field_tests=custom_field_tests,
+        sub_doc_fields=sub_doc_fields,
+    )
+
+
+def test_get_trajectories(rester):
+    trajectories = [traj for traj in rester.get_trajectory("mp-149")]
+
+    for traj in trajectories:
+        assert ("@module", "pymatgen.core.trajectory") in traj.items()
