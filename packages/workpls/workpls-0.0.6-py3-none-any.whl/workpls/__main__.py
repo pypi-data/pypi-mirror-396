849d6c4aa8cddced015e055041f42c6eadf88c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 20:06:44 +0000

    Add package workpls/0.0.6/workpls-0.0.6-py3-none-any.whl

---
+++ packages/workpls/workpls-0.0.6-py3-none-any.whl/workpls/__main__.py
@@ -0,0 +1,19 @@
+import argparse
+
+def main():
+ cmd = argparse.ArgumentParser()
+
+ cmd.add_argument("-hello")
+ cmd.add_argument("-hello2")
+
+ comp = cmd.parse_args()
+
+ if comp.hello:
+    print("hello")
+ elif comp.hello2:
+    print("hello butt better")
+ else:
+    print("uknown comman d")
+
+if __name__ == "__main__":
+    main()
