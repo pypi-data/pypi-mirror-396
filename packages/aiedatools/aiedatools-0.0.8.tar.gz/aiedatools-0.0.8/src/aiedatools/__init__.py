704e892516e25bf5b458f7475b114c20f8dd2ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 09:09:05 +0000

    Add package aiedatools/0.0.8/aiedatools-0.0.8.tar.gz

---
+++ packages/aiedatools/aiedatools-0.0.8.tar.gz/aiedatools-0.0.8/src/aiedatools/__init__.py
@@ -0,0 +1,6 @@
+from .single_variable_analysis import profile_table, plot_cat_bar, plot_numeric
+from .two_variables_analysis import plot_corr_heatmap, plot_pvalue_heatmap
+from .trend_analysis import plot_trend, plot_trend_num
+
+from .bigquery_data_prepare import bq_to_polars
+
