4f0593e5eac24b6b3211a5e313f301640a87ca29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:31:05 +0000

    Add package tardis-spac/0.6.1/tardis_spac-0.6.1.tar.gz

---
+++ packages/tardis-spac/tardis_spac-0.6.1.tar.gz/tardis_spac-0.6.1/tardis_spac/__init__.py
@@ -0,0 +1,10 @@
+from . import utils, stats
+
+try:
+    from . import external
+except ImportError:
+    pass
+
+__author__ = __maintainer__ = "zenglabPKU"
+__email__ = "barry_2001@pku.edu.cn"
+__version__ = "0.2.0"
