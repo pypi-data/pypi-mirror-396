f156b7ac4987f0a31242b272183fb9203f66d03b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 21:07:06 +0000

    Add package DGtal/2.1.0/dgtal-2.1.0-cp312-cp312-musllinux_1_2_x86_64.whl

---
+++ packages/DGtal/dgtal-2.1.0-cp312-cp312-musllinux_1_2_x86_64.whl/include/DGtal/topology/tables/NeighborhoodTables.h
@@ -0,0 +1,44 @@
+/**
+* @file   NeighborhoodTables.h
+* @author Pablo Hernandez-Cerdan. Institute of Fundamental Sciences.
+* Massey University. Palmerston North, New Zealand
+* @date 2018/01/01
+*
+* Configuration header for DGtal look up tables.
+
+* You can use loadTable(table)
+* @see NeighborhoodConfigurations.h
+*
+**/
+#pragma once
+#include <string>
+
+namespace DGtal {
+  namespace simplicity  {
+  ///Path to the DGtal look up tables. Compressed with zlib.
+  inline const std::string tableDir = "/tmp/tmp8ifgx9c6/wheel/platlib/include/DGtal/topology/tables";
+  inline const std::string tableSimple26_6 =
+    "/tmp/tmp8ifgx9c6/wheel/platlib/include/DGtal/topology/tables/simplicity_table26_6.zlib";
+  inline const std::string tableSimple18_6 =
+    "/tmp/tmp8ifgx9c6/wheel/platlib/include/DGtal/topology/tables/simplicity_table18_6.zlib";
+  inline const std::string tableSimple6_26 =
+    "/tmp/tmp8ifgx9c6/wheel/platlib/include/DGtal/topology/tables/simplicity_table6_26.zlib";
+  inline const std::string tableSimple6_18 =
+    "/tmp/tmp8ifgx9c6/wheel/platlib/include/DGtal/topology/tables/simplicity_table6_18.zlib";
+  inline const std::string tableSimple8_4 =
+    "/tmp/tmp8ifgx9c6/wheel/platlib/include/DGtal/topology/tables/simplicity_table8_4.zlib";
+  inline const std::string tableSimple4_8 =
+    "/tmp/tmp8ifgx9c6/wheel/platlib/include/DGtal/topology/tables/simplicity_table4_8.zlib";
+  } // simplicity namespace
+
+  namespace isthmusicity {
+    inline const std::string tableIsthmus =
+      "/tmp/tmp8ifgx9c6/wheel/platlib/include/DGtal/topology/tables/isthmusicity_table26_6.zlib";
+    inline const std::string tableOneIsthmus =
+      "/tmp/tmp8ifgx9c6/wheel/platlib/include/DGtal/topology/tables/isthmusicityOne_table26_6.zlib";
+    inline const std::string tableTwoIsthmus =
+      "/tmp/tmp8ifgx9c6/wheel/platlib/include/DGtal/topology/tables/isthmusicityTwo_table26_6.zlib";
+  } // isthmusicity namespace
+} // DGtal namespace
+
+
