db20dab80e6d07e0fd6ca96aa960eb2c141d284c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:07:00 +0000

    Add package scrapscraper/0.2.0/scrapscraper-0.2.0.tar.gz

---
+++ packages/scrapscraper/scrapscraper-0.2.0.tar.gz/scrapscraper-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scrapscraper",  
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "colorama"
+    ],
+    entry_points={
+        "console_scripts": [
+            "scraper=scraper.scrapscraper:main",  
+        ],
+    },
+    author="skelliyB/lemonsrC00l",  # your name or handle
+    description="A fun Python web scraper if you need me comment on the github page lemonsrC00l is just my alt",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/skelliyB/scrapscraper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
