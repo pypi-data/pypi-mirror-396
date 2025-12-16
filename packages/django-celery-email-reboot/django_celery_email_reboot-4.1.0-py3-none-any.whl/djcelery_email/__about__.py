d68bde5d0a26dcb19ae1d333726700aa128d6765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 18:07:15 +0000

    Add package django-celery-email-reboot/4.1.0/django_celery_email_reboot-4.1.0-py3-none-any.whl

---
+++ packages/django-celery-email-reboot/django_celery_email_reboot-4.1.0-py3-none-any.whl/djcelery_email/__about__.py
@@ -0,0 +1,17 @@
+__all__ = [
+    '__title__', '__summary__', '__uri__', '__version__', '__author__',
+    '__email__', '__license__', '__copyright__',
+]
+
+__title__ = 'django-celery-email-reboot'
+__summary__ = 'An async Django email backend using celery (forked from django-celery-email)'
+__uri__ = 'https://github.com/Panevo/django-celery-email'
+
+__version__ = '4.1.0'
+
+__originalauthor__ = 'Paul McLanahan'
+__author__ = 'Karlo Krakan'
+__email__ = 'karlo.krakan@panevo.com'
+
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2015 {0}'.format(__originalauthor__)
