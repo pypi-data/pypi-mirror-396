90994646781c3487e2146cfb12abd467ccd043bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 05:06:30 +0000

    Add package net-franky/1.0.0/net_franky-1.0.0-py3-none-any.whl

---
+++ packages/net-franky/net_franky-1.0.0-py3-none-any.whl/net_franky/franky/motion.pyi
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import Union
+
+from ._franky import (
+    BaseCartesianPoseMotion,
+    BaseCartesianVelocityMotion,
+    BaseJointPositionMotion,
+    BaseJointVelocityMotion,
+    BaseTorqueMotion,
+)
+
+Motion = Union[
+    BaseCartesianPoseMotion,
+    BaseCartesianVelocityMotion,
+    BaseJointPositionMotion,
+    BaseJointVelocityMotion,
+    BaseTorqueMotion,
+]
