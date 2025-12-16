01fd4da8fafb59d64ae754efa1b88f13aab06fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 08:06:40 +0000

    Add package colation/0.0.1/colation-0.0.1.tar.gz

---
+++ packages/colation/colation-0.0.1.tar.gz/colation-0.0.1/README.md
@@ -0,0 +1,4 @@
+# colation
+
+This is a placeholder release for the `colation` package name on PyPI.
+
---
+++ packages/colation/colation-0.0.1.tar.gz/colation-0.0.1/colation/__init__.py
@@ -0,0 +1,9 @@
+"""
+:Colation - minimal placeholder package for namespace reservation.
+
+This version 0.0.1 only secures the 'colation' name on PyPI.
+Actual functionality will be added in later releases.
+"""
+
+__all__ = []
+__version__ = "0.0.1"
