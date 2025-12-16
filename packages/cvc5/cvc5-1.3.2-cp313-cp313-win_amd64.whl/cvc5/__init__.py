5745337759cabb8a9de6e1f1d963adabe5c3f2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 16:05:34 +0000

    Add package cvc5/1.3.2/cvc5-1.3.2-cp313-cp313-win_amd64.whl

---
+++ packages/cvc5/cvc5-1.3.2-cp313-cp313-win_amd64.whl/cvc5/__init__.py
@@ -0,0 +1,15 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'cvc5.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+import sys
+from .cvc5_python_base import *
+__file__ = cvc5_python_base.__file__
+__version__ = "1.3.2"
