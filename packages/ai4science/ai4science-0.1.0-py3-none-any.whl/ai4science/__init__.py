0d7023aac51e34e7b23b63f8cb238002b49ca5f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 12:07:07 +0000

    Add package ai4science/0.1.0/ai4science-0.1.0-py3-none-any.whl

---
+++ packages/ai4science/ai4science-0.1.0-py3-none-any.whl/ai4science/__init__.py
@@ -0,0 +1,11 @@
+"""
+@Project  : ai4science
+@File     : __init__.py.py
+@Author   : Shaobo Cui
+@Date     : 12.12.2025 11:54
+"""
+
+__all__ = ["hello"]
+__version__ = "0.1.0"
+
+from .core import hello
---
+++ packages/ai4science/ai4science-0.1.0-py3-none-any.whl/ai4science/core.py
@@ -0,0 +1,10 @@
+"""
+@Project  : ai4science
+@File     : core.py
+@Author   : Shaobo Cui
+@Date     : 12.12.2025 11:55
+"""
+
+
+def hello() -> str:
+    return "Hello from ai4science!"
