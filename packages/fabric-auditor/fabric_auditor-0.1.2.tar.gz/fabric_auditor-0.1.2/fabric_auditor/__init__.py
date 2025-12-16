1a0bb0e047c3adf28278d52f8282ac635915b4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 13:17:33 +0000

    Add package fabric-auditor/0.1.2/fabric_auditor-0.1.2.tar.gz

---
+++ packages/fabric-auditor/fabric_auditor-0.1.2.tar.gz/fabric_auditor-0.1.2/fabric_auditor/__init__.py
@@ -0,0 +1,3 @@
+from .core import FabricAuditor
+
+__all__ = ["FabricAuditor"]
---
+++ packages/fabric-auditor/fabric_auditor-0.1.2.tar.gz/fabric_auditor-0.1.2/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+# Configuration is defined in pyproject.toml
+if __name__ == "__main__":
+    setup()
