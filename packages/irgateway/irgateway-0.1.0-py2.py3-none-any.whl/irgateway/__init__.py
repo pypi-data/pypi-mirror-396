9f06d7894ba7c50dbcc2d5d5b0c9c22b19ce557c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 22:06:14 +0000

    Add package irgateway/0.1.0/irgateway-0.1.0-py2.py3-none-any.whl

---
+++ packages/irgateway/irgateway-0.1.0-py2.py3-none-any.whl/irgateway/__init__.py
@@ -0,0 +1,3 @@
+"""Python SDK for Zarinpal Payment Gateway with sync and async support."""
+
+__version__ = "0.1.0"
---
+++ packages/irgateway/irgateway-0.1.0-py2.py3-none-any.whl/irgateway/zarinpal/__init__.py
@@ -0,0 +1,4 @@
+"""Python SDK for Zarinpal Payment Gateway with sync and async support."""
+
+from irgateway.zarinpal.client import ZarinpalClient, AsyncZarinpalClient
+from irgateway.zarinpal.types import ZarinpalConfig
