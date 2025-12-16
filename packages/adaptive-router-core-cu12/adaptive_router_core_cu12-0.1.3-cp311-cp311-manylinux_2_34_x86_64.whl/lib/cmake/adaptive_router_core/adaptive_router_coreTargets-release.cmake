2375f76c6f646bba35caa37d6def1e8414dc7397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 05:07:18 +0000

    Add package adaptive-router-core-cu12/0.1.3/adaptive_router_core_cu12-0.1.3-cp311-cp311-manylinux_2_34_x86_64.whl

---
+++ packages/adaptive-router-core-cu12/adaptive_router_core_cu12-0.1.3-cp311-cp311-manylinux_2_34_x86_64.whl/lib/cmake/adaptive_router_core/adaptive_router_coreTargets-release.cmake
@@ -0,0 +1,39 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "adaptive::adaptive_core" for configuration "Release"
+set_property(TARGET adaptive::adaptive_core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(adaptive::adaptive_core PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libadaptive_core.a"
+  )
+
+list(APPEND _cmake_import_check_targets adaptive::adaptive_core )
+list(APPEND _cmake_import_check_files_for_adaptive::adaptive_core "${_IMPORT_PREFIX}/lib/libadaptive_core.a" )
+
+# Import target "adaptive::adaptive_c" for configuration "Release"
+set_property(TARGET adaptive::adaptive_c APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(adaptive::adaptive_c PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libadaptive_c.so.0.1.0"
+  IMPORTED_SONAME_RELEASE "libadaptive_c.so.0"
+  )
+
+list(APPEND _cmake_import_check_targets adaptive::adaptive_c )
+list(APPEND _cmake_import_check_files_for_adaptive::adaptive_c "${_IMPORT_PREFIX}/lib/libadaptive_c.so.0.1.0" )
+
+# Import target "adaptive::adaptive_core_cuda" for configuration "Release"
+set_property(TARGET adaptive::adaptive_core_cuda APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(adaptive::adaptive_core_cuda PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CUDA"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libadaptive_core_cuda.a"
+  )
+
+list(APPEND _cmake_import_check_targets adaptive::adaptive_core_cuda )
+list(APPEND _cmake_import_check_files_for_adaptive::adaptive_core_cuda "${_IMPORT_PREFIX}/lib/libadaptive_core_cuda.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
