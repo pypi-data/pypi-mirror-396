403076a875ff55d03a8c513707608fa2fd9b49ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 18:06:56 +0000

    Add package messthaler-wulff/2025.12.8/messthaler_wulff-2025.12.8-py3-none-any.whl

---
+++ packages/messthaler-wulff/messthaler_wulff-2025.12.8-py3-none-any.whl/messthaler_wulff/mode_minimisers.py
@@ -0,0 +1,35 @@
+import logging
+
+from prettytable import PrettyTable
+
+from messthaler_wulff.additive_simulation import OmniSimulation, SimpleNeighborhood, wipe_screen
+from messthaler_wulff.explorative_simulation import crystal_data
+from messthaler_wulff.minimiser_simulation import MinimiserSimulation
+from messthaler_wulff.progress import debounce
+
+log = logging.getLogger("messthaler_wulff")
+
+
+def show_results(energies, counts, intermediate_value=False):
+    if intermediate_value:
+        wipe_screen()
+        print("Intermediate results:")
+    else:
+        print("Final results:")
+
+    table = PrettyTable(["nr atoms", "nr crystals", "min energy"], align='r')
+    table.custom_format = lambda f, v: f"{v:,}"
+
+    for i in counts.keys():
+        table.add_row([i, counts[i], energies[i]])
+
+    print(table)
+
+
+def run_mode(goal, lattice, dimension):
+    omni_simulation = OmniSimulation(SimpleNeighborhood(lattice), None, tuple([0] * (dimension + 1)))
+    sim = MinimiserSimulation(omni_simulation)
+
+    for n in range(goal):
+        print(f"{n:3}: {sim.data(n)}")
+
