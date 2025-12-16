3c5122340021a019ffeea2d9d170f24d3601e4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 16:05:38 +0000

    Add package simple-openai/5.2.0/simple_openai-5.2.0-py3-none-any.whl

---
+++ packages/simple-openai/simple_openai-5.2.0-py3-none-any.whl/simple_openai/chat_manager.py
@@ -0,0 +1,182 @@
+"""Chat manager for the simple openai app
+
+This module contains the chat manager for the simple openai app.
+
+The chat manager is used to manage the chat messages and create the chat, it limits the number of messages in the chat to 21 by default and adds the system message to the start of the list.
+"""
+
+from collections import deque
+from datetime import datetime
+from pathlib import Path
+from zoneinfo import ZoneInfo
+
+from .models import open_ai_models
+from .constants import MAX_CHAT_HISTORY, CHAT_HISTORY_FILE, DEFAULT_CHAT_ID
+
+
+class ChatManager:
+    """The chat manager
+
+    This class is used to manage the chat messages and create the chat, it limits the number of messages in the chat to 21 by default and adds the system message to the start of the list.
+
+    It can optionally handle messages from multiple chats separately and store them in a file.
+
+    On initialisation, the chat manager will try to load the chat history from the file.  If the file does not exist, it will create a new chat history.
+
+    Args:
+        system_message (str): The system message to add to the start of the chat
+        max_messages (int, optional): The maximum number of messages in the chat. Defaults to 21.
+        storage_path (Path, optional): The path to the storage directory. Defaults to None.
+        timezone (str, optional): The timezone to use for the chat messages. Defaults to 'UTC'.
+    """
+
+    def __init__(
+        self,
+        system_message: str,
+        max_messages: int = MAX_CHAT_HISTORY,
+        storage_path: Path | None = None,
+        timezone: str = "UTC",
+    ) -> None:
+        self._system_message = system_message
+        self._max_messages = max_messages
+        self._storage_path = storage_path
+        self._timezone = ZoneInfo(timezone)
+
+        # If a storage path is provided, create the storage directory
+        if self._storage_path is not None:
+            self._storage_path.mkdir(parents=True, exist_ok=True)
+
+            # Try to load the chat history
+            try:
+                # Load the chat history
+                with open(self._storage_path / CHAT_HISTORY_FILE, "rb") as f:
+                    # Load the chat history
+                    self._chat_history = open_ai_models.ChatHistory.model_validate_json(f.read())
+            except FileNotFoundError:
+                # initialise a deque of messages not including the system message
+                self._chat_history = open_ai_models.ChatHistory(messages={})
+        else:
+            # initialise a deque of messages not including the system message
+            self._chat_history = open_ai_models.ChatHistory(messages={})
+
+    def update_system_message(self, system_message: str) -> None:
+        """Update the system message
+
+        Args:
+            system_message (str): The new system message
+        """
+        self._system_message = system_message
+
+    def add_message(
+        self,
+        message: open_ai_models.ChatMessage,
+        chat_id: str = DEFAULT_CHAT_ID,
+        add_date_time: bool = False,
+    ) -> open_ai_models.Chat:
+        """Add a message to the chat
+
+        Args:
+            message (open_ai_models.ChatMessage): The message to add to the chat
+            chat_id (str, optional): The ID of the chat to add the message to. Defaults to DEFAULT_CHAT_ID.
+            add_date_time (bool, optional): Whether to add the date and time to the start of the prompt. Defaults to False.
+
+        Returns:
+            open_ai_models.Chat: The chat
+        """
+        # If the chat ID is not in the chat history, create a new deque
+        if chat_id not in self._chat_history.messages:
+            self._chat_history.messages[chat_id] = deque(maxlen=self._max_messages)
+
+        # Add the message to the deque
+        self._chat_history.messages[chat_id].append(message)
+
+        # If a storage path is provided, save the chat history
+        if self._storage_path is not None:
+            # Save the chat history
+            with open(self._storage_path / CHAT_HISTORY_FILE, "w") as f:
+                f.write(self._chat_history.model_dump_json(exclude_none=True, indent=2))
+
+        # Update the system message with the date and time in iso format if required
+        if add_date_time:
+            system_message = f"The date and time is {datetime.now(tz=self._timezone).isoformat()} give answers in timezone {self._timezone.key}.\n{self._system_message}"
+        else:
+            system_message = self._system_message
+
+        # Create the chat adding the system message to the start
+        chat = open_ai_models.Chat(
+            messages=[
+                open_ai_models.ChatMessage(
+                    role="system", content=system_message, name="System"
+                )
+            ]
+            + list(self._chat_history.messages[chat_id])
+        )
+
+        # Return the chat
+        return chat
+
+    def get_chat(self, chat_id: str = DEFAULT_CHAT_ID) -> str:
+        """Get the chat
+
+        Args:
+            chat_id (str, optional): The ID of the chat to get. Defaults to DEFAULT_CHAT_ID.
+
+        Returns:
+            str: The chat
+        """
+        # If the chat ID is not in the messages, create a new deque
+        if chat_id not in self._chat_history.messages:
+            return ""
+
+        # Get the chat
+        chat = self._chat_history.messages[chat_id]
+
+        # Parse the most recent 10 chat messages to a string with each name and message on a new line
+        chat_str = "\n".join([f"{message.name}: {message.content}" for message in chat])
+
+        # Return the chat
+        return chat_str
+
+    def get_truncated_chat(self, chat_id: str = DEFAULT_CHAT_ID) -> str:
+        """Get the truncated chat, limited to the last 4,000 characters
+
+        Args:
+            chat_id (str, optional): The ID of the chat to get. Defaults to DEFAULT_CHAT_ID.
+
+        Returns:
+            str: The truncated chat
+        """
+        # If the chat ID is not in the messages, create a new deque
+        if chat_id not in self._chat_history.messages:
+            return ""
+
+        # Get the chat
+        chat = self._chat_history.messages[chat_id]
+
+        # Parse the most recent 10 chat messages to a string with each name and message on a new line
+        chat_str = "\n".join([f"{message.name}: {message.content}" for message in chat])
+
+        # Get the last 4,000 characters of the chat
+        chat_str = chat_str[-4000:]
+
+        # Return the chat
+        return chat_str
+
+    def clear_chat(self, chat_id: str = DEFAULT_CHAT_ID) -> None:
+        """Clear the chat
+
+        Args:
+            chat_id (str, optional): The ID of the chat to clear. Defaults to DEFAULT_CHAT_ID.
+        """
+        # If the chat ID is not in the messages, create a new deque
+        if chat_id not in self._chat_history.messages:
+            return
+
+        # Clear the chat
+        self._chat_history.messages[chat_id].clear()
+
+        # If a storage path is provided, save the chat history
+        if self._storage_path is not None:
+            # Save the chat history
+            with open(self._storage_path / CHAT_HISTORY_FILE, "w") as f:
+                f.write(self._chat_history.model_dump_json(exclude_none=True, indent=2))
---
+++ packages/simple-openai/simple_openai-5.2.0-py3-none-any.whl/simple_openai/models/open_ai_models.py
@@ -0,0 +1,159 @@
+"""OpenAI API models
+
+This module contains the models for the OpenAI API.
+
+The models are used to validate the data sent to and received from the OpenAI API.
+
+The models are based on the [OpenAI API documentation](https://beta.openai.com/docs/api-reference/introduction) and use [Pydantic](https://pydantic-docs.helpmanual.io/) to help serialise and deserialise the JSON.
+"""
+
+from collections import deque
+from pydantic import BaseModel, field_validator
+
+from simple_openai.constants import MAX_CHAT_HISTORY
+
+
+class OpenAIParameter(BaseModel):
+    """OpenAI parameter
+
+    This class represents an OpenAI parameter.
+
+    Attributes:
+        type (str): The type of the parameter
+        description (str): The description of the parameter, used by OpenAI to decide whether to use the parameter
+    """
+
+    type: str
+    description: str
+
+
+class OpenAIParameters(BaseModel):
+    """OpenAI parameters
+
+    This class represents a list of OpenAI parameters.
+
+    Attributes:
+        type (str): The type of the parameters
+        properties (dict[str, OpenAIParameter]): The parameters
+        required (list[str], optional): The required parameters. Defaults to [].
+    """
+
+    type: str = "object"
+    properties: dict[str, OpenAIParameter]
+    required: list[str] = []
+
+
+class OpenAIFunction(BaseModel):
+    """OpenAI function
+
+    This class represents an OpenAI function.
+
+    Attributes:
+        name (str): The name of the function
+        description (str): The description of the function, used by OpenAI to decide whether to use the function
+        parameters (OpenAIParameters): The parameters of the function
+    """
+
+    name: str
+    description: str
+    parameters: OpenAIParameters
+
+
+class OpenAITool(BaseModel):
+    type: str = "function"
+    function: OpenAIFunction
+
+
+class FunctionCall(BaseModel):
+    name: str
+    arguments: str
+
+
+class ToolCall(BaseModel):
+    id: str
+    type: str
+    function: FunctionCall
+
+
+class ChatMessage(BaseModel):
+    role: str
+    tool_calls: list[ToolCall] | None = None
+    tool_call_id: str | None = None
+    content: str | None = None
+    name: str = "Botto"
+
+
+class Chat(BaseModel):
+    messages: list[ChatMessage]
+
+
+class ChatHistory(BaseModel):
+    messages: dict[str, deque[ChatMessage]]
+
+    # Ensure maxlen is always enforced
+    @field_validator("messages", mode="after")
+    def enforce_maxlen(cls, v: dict[str, deque[ChatMessage]]) -> dict[str, deque[ChatMessage]]:
+        # Always reconstruct with maxlen=MAX_CHAT_HISTORY
+        return {k: deque(v, maxlen=MAX_CHAT_HISTORY) for k, v in v.items()}
+
+
+class ChatRequest(Chat):
+    tools: list[OpenAITool] | None = None
+    tool_choice: str
+    model: str = "gpt-5.2"
+
+
+class ResponseMessage(BaseModel):
+    role: str
+    content: str | None
+    tool_calls: list[ToolCall] | None = None
+
+
+class Choice(BaseModel):
+    index: int
+    message: ResponseMessage
+    finish_reason: str
+
+
+class Usage(BaseModel):
+    prompt_tokens: int
+    completion_tokens: int
+    total_tokens: int
+
+
+class ChatResponse(BaseModel):
+    id: str
+    object: str
+    created: int
+    choices: list[Choice]
+    usage: Usage
+
+
+class ImageRequest(BaseModel):
+    model: str = "dall-e-3"
+    prompt: str
+    n: int = 1
+    size: str = "1024x1024"
+    response_format: str = "url"
+    quality: str = "hd"
+    style: str = "vivid"
+
+
+class Url(BaseModel):
+    url: str
+
+
+class ImageResponse(BaseModel):
+    created: int
+    data: list[Url]
+
+
+class Error(BaseModel):
+    code: str | None
+    message: str
+    param: str | None
+    type: str
+
+
+class ErrorResponse(BaseModel):
+    error: Error
