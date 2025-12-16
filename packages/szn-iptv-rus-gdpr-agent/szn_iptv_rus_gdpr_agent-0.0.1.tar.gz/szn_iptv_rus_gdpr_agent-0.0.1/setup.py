7a611644c3b8e92594aee1eb9b53b0ff3f6c43ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:31:54 +0000

    Add package szn-iptv-rus-gdpr-agent/0.0.1/szn_iptv_rus_gdpr_agent-0.0.1.tar.gz

---
+++ packages/szn-iptv-rus-gdpr-agent/szn_iptv_rus_gdpr_agent-0.0.1.tar.gz/szn_iptv_rus_gdpr_agent-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="szn-iptv-rus-gdpr-agent",
+    version="0.0.1",
+    description="Security placeholder package",
+    long_description="This package is a security placeholder to prevent dependency confusion attacks.",
+    long_description_content_type="text/plain",
+    author="Security Team",
+    author_email="security@company.com",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
