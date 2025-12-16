e82425a97ed3bf597565a464c5a5efc24e79959f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 22:05:58 +0000

    Add package Mopidy-Pibox/3.0.5/mopidy_pibox-3.0.5-py3-none-any.whl

---
+++ packages/Mopidy-Pibox/mopidy_pibox-3.0.5-py3-none-any.whl/mopidy_pibox/api.py
@@ -0,0 +1,130 @@
+from __future__ import absolute_import, unicode_literals
+
+import json
+
+from mopidy import config
+import tornado.web
+
+import logging
+
+from mopidy.models import ModelJSONEncoder, Track
+from . import socket
+
+
+class PiboxHandler(tornado.web.RequestHandler):
+    def initialize(self, core, frontend):
+        self.core = core
+        self.frontend = frontend
+        self.logger = logging.getLogger(__name__)
+
+    def _get_body(self):
+        return tornado.escape.json_decode(self.request.body)
+
+    def _get_user_fingerprint(self):
+        return self.request.headers["X-Pibox-Fingerprint"]
+
+
+class TracklistHandler(PiboxHandler):
+    def initialize(self, core, frontend):
+        super(TracklistHandler, self).initialize(core, frontend)
+
+    def post(self):
+        data = self._get_body()
+        fingerprint = self._get_user_fingerprint()
+        track_uri = data["track"]
+        (_success, error) = self.frontend.add_track_to_queue(track_uri).get()
+        tracklist = self.frontend.get_queued_tracks(fingerprint).get()
+        self.set_header("Content-Type", "application/json")
+        self.write(
+            json.dumps({"tracklist": tracklist, "error": error}, cls=ModelJSONEncoder)
+        )
+
+    def get(self):
+        fingerprint = self._get_user_fingerprint()
+        tracklist = self.frontend.get_queued_tracks(fingerprint).get()
+        self.set_header("Content-Type", "application/json")
+        self.write(json.dumps({"tracklist": tracklist}, cls=ModelJSONEncoder))
+
+
+class VoteHandler(PiboxHandler):
+    def initialize(self, core, frontend):
+        super(VoteHandler, self).initialize(core, frontend)
+
+    def post(self):
+        data = self._get_body()
+        fingerprint = self._get_user_fingerprint()
+        track = Track(uri=data["uri"])
+
+        if self.frontend.pibox.has_user_voted_on_track(fingerprint, track).get():
+            self.set_status(400)
+            response = {
+                "code": "15",
+                "title": "Voted Already",
+                "message": "User has already used their 1 vote to skip on this track",
+            }
+            self.write(response)
+        else:
+            self.frontend.add_vote_for_user_on_queued_track(fingerprint, track)
+
+            socket.PiboxWebSocket.send(
+                "VOTE_ADDED",
+                {},
+            )
+
+            self.set_status(200)
+
+
+class SessionHandler(PiboxHandler):
+    def initialize(self, core, frontend):
+        super(SessionHandler, self).initialize(core, frontend)
+
+    def post(self):
+        data = self._get_body()
+        skip_threshold = data["skipThreshold"]
+        playlists = data.get("playlists", [])
+        auto_start = data.get("autoStart", True)
+        shuffle = data.get("shuffle", True)
+
+        self.frontend.start_session(int(skip_threshold), playlists, auto_start, shuffle)
+        session = self.frontend.pibox.to_json().get()
+
+        socket.PiboxWebSocket.send(
+            "SESSION_STARTED",
+            session,
+        )
+        self.set_status(200)
+
+    def get(self):
+        session = self.frontend.pibox.to_json().get()
+        self.write(session)
+
+    def delete(self):
+        self.frontend.end_session().get()
+        socket.PiboxWebSocket.send("SESSION_ENDED", {})
+        self.set_status(200)
+
+
+class SuggestionsHandler(PiboxHandler):
+    def initialize(self, core, frontend):
+        super(SuggestionsHandler, self).initialize(core, frontend)
+
+    def get(self):
+        suggestions = self.frontend.get_suggestions(3).get()
+        self.set_header("Content-Type", "application/json")
+        self.write(json.dumps({"suggestions": suggestions}, cls=ModelJSONEncoder))
+
+
+class ConfigHandler(tornado.web.RequestHandler):
+    def initialize(self, config: config.Proxy):
+        self.config = config
+        self.logger = logging.getLogger(__name__)
+
+    def get(self):
+        pibox_config = self.config.get("pibox")
+        self.write(
+            {
+                "offline": pibox_config.get("offline"),
+                "defaultPlaylists": list(pibox_config.get("default_playlists")),
+                "defaultSkipThreshold": pibox_config.get("default_skip_threshold"),
+            }
+        )
---
+++ packages/Mopidy-Pibox/mopidy_pibox-3.0.5-py3-none-any.whl/mopidy_pibox/frontend.py
@@ -0,0 +1,172 @@
+import pykka
+import logging
+from random import sample, shuffle
+
+from mopidy import core
+
+from mopidy_pibox import Extension
+from mopidy_pibox.pibox import Pibox
+
+PUSSYCAT_LIST = [
+    "spotify:track:0asT0RDbe4Vrf6pxLHgpkn",
+    "spotify:track:2HkHE4EeZyx9AncSN042q3",
+]
+
+
+class PiboxFrontend(pykka.ThreadingActor, core.CoreListener):
+    def __init__(self, config, core, pussycat_list=PUSSYCAT_LIST):
+        super(PiboxFrontend, self).__init__()
+        self.core = core
+        self.config = config["pibox"]
+        self.pussycat_list = pussycat_list
+        self.logger = logging.getLogger(__name__)
+
+        data_dir = Extension.get_data_dir(config)
+        self.pibox = pykka.traversable(Pibox(data_dir=data_dir))
+
+        self.core.tracklist.set_consume(value=True)
+
+    def start_session(self, skip_threshold, playlists, auto_start, shuffle):
+        self.pibox.start_session(skip_threshold, playlists, shuffle)
+        if auto_start:
+            self.__queue_song_from_session_playlists()
+            self.__start_playing()
+
+    def track_playback_ended(self, tl_track, time_position):
+        if not self.pibox.started:
+            return
+
+        self.__update_played_tracks(tl_track)
+
+        if self.__should_play_whats_new_pussycat(tl_track):
+            self.core.tracklist.add(uris=[self.pussycat_list[0]], at_position=0).get()
+            self.logger.info("Meow")
+            self.__start_playing()
+        elif self.core.tracklist.get_length().get() == 0:
+            self.__queue_song_from_session_playlists()
+            self.__start_playing()
+
+    def get_queued_tracks(self, user_fingerprint):
+        return [
+            {
+                "info": track,
+                "votes": self.pibox.get_votes_for_track(track),
+                "voted": self.pibox.has_user_voted_on_track(user_fingerprint, track),
+            }
+            for track in self.core.tracklist.get_tracks().get()
+        ]
+
+    def add_track_to_queue(self, track_uri):
+        if track_uri in self.pibox.played_tracks:
+            return (False, "ALREADY_PLAYED")
+
+        if self.__is_queued(track_uri):
+            return (False, "ALREADY_QUEUED")
+
+        self.core.tracklist.add(uris=[track_uri]).get()
+        self.pibox.manually_queued_tracks.append(track_uri)
+
+        return (True, None)
+
+    def add_vote_for_user_on_queued_track(self, user_fingerprint, track):
+        vote_count = self.pibox.add_vote_for_user_on_track(user_fingerprint, track)
+        self.logger.info(
+            f"Vote added for {track.uri} by {user_fingerprint} ({vote_count}/{self.pibox.skip_threshold})"
+        )
+        if vote_count >= self.pibox.skip_threshold:
+            self.logger.info(f"Skipping {track.uri} due to votes")
+            self.core.tracklist.remove({"uri": [track.uri]}).get()
+
+            self.logger.info("Track removed from tracklist")
+            self.pibox.skip_queued_track(track)
+
+    def end_session(self):
+        self.core.playback.stop()
+        self.core.tracklist.clear()
+
+        self.pibox.end_session()
+
+    def get_suggestions(self, length):
+        suggestions = self.pibox.get_suggestions()
+
+        unqueued_suggestions = [
+            track for track in suggestions if not self.__is_queued(track)
+        ]
+        size = (
+            len(unqueued_suggestions) if len(unqueued_suggestions) < length else length
+        )
+        unplayed_tracks = [
+            track
+            for tracks in self.core.library.lookup(sample(unqueued_suggestions, size))
+            .get()
+            .values()
+            for track in tracks
+        ]
+
+        return unplayed_tracks
+
+    def __queue_song_from_session_playlists(self):
+        self.logger.info("Pibox is trying to queue a song")
+
+        playlist_items = self.__get_session_playlist_items()
+
+        if self.pibox.shuffle:
+            shuffle(playlist_items)
+
+        seen = set()
+
+        remaining_playlist = [
+            ref
+            for ref in playlist_items
+            if (
+                self.__can_play(ref.uri)
+                and ref.uri not in seen
+                and not seen.add(ref.uri)
+            )
+        ]
+        self.__update_remaining_playlist_tracks(remaining_playlist)
+
+        if len(remaining_playlist) == 0:
+            self.logger.info("No more tracks to play")
+            self.end_session()
+            return
+
+        next_track = remaining_playlist[0]
+
+        self.core.tracklist.add(uris=[next_track.uri], at_position=0).get()
+        self.logger.info("Pibox auto-added " + next_track.name + " to tracklist")
+
+    def __get_session_playlist_items(self):
+        if self.config["offline"]:
+            return self.core.library.browse(uri="local:directory?type=track").get()
+        else:
+            return [
+                track
+                for playlist in self.pibox.playlists
+                for track in self.core.playlists.get_items(playlist["uri"]).get()
+            ]
+
+    def __update_played_tracks(self, tl_track):
+        self.pibox.played_tracks.append(tl_track.track.uri)
+
+    def __update_remaining_playlist_tracks(self, remaining_playlist):
+        self.pibox.remaining_playlist_tracks = [
+            track.uri for track in remaining_playlist
+        ]
+
+    def __can_play(self, uri):
+        return (uri not in self.pibox.played_tracks) and (
+            uri not in self.pibox.denylist
+        )
+
+    def __is_queued(self, uri):
+        return self.core.tracklist.filter({"uri": [uri]}).get() != []
+
+    def __start_playing(self):
+        if self.core.playback.get_state().get() == core.PlaybackState.STOPPED:
+            self.core.playback.play().get()
+            self.logger.info("Pibox started playback")
+
+    def __should_play_whats_new_pussycat(self, tl_track):
+        tracklist = self.core.tracklist.get_tracks().get()
+        return tl_track.track.uri in self.pussycat_list and len(tracklist) == 0
