829cbcdd88efb1113fa771c6fc47b1d121fd3da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:07:02 +0000

    Add package qbt-rules/0.3.2rc2/qbt_rules-0.3.2rc2.tar.gz

---
+++ packages/qbt-rules/qbt_rules-0.3.2rc2.tar.gz/qbt_rules-0.3.2rc2/src/qbt_rules/__init__.py
@@ -0,0 +1,10 @@
+"""
+qBittorrent Automation - A rules engine for automating torrent management.
+
+This package provides a declarative YAML-based rules engine for managing
+torrents in qBittorrent through the Web API.
+"""
+
+from qbt_rules.__version__ import __version__
+
+__all__ = ["__version__"]
