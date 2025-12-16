4278fa78dd35112d3c7a21bed09a5d4fdc10dea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:31:25 +0000

    Add package cerebras-pytorch/2.7.1/cerebras_pytorch-2.7.1-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/cerebras-pytorch/cerebras_pytorch-2.7.1-cp311-cp311-manylinux2014_x86_64.whl/cerebras/pytorch/experimental/inference/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2016-2024 Cerebras Systems
+# SPDX-License-Identifier: BSD-3-Clause
+
+from .utils import (
+    TensorID,
+    hoist_function,
+    register_id,
+    register_id_with_op,
+    register_kv_cache,
+    register_state,
+    register_weight,
+)
