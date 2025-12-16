cd861630e330899167c7c136e56385fc1c7c4997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:07:30 +0000

    Add package mathesis/0.7.2/mathesis-0.7.2.tar.gz

---
+++ packages/mathesis/mathesis-0.7.2.tar.gz/mathesis-0.7.2/mathesis/deduction/sequent_calculus/__init__.py
@@ -0,0 +1,5 @@
+from mathesis.deduction.sequent_calculus.sequent import Sequent, SequentItem
+from mathesis.deduction.sequent_calculus.sequent_tree import SequentTree
+from mathesis.deduction.sequent_calculus import rules
+
+__all__ = ["Sequent", "SequentItem", "SequentTree", "rules"]
