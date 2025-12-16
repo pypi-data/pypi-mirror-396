d4a4839c1ea944164f28ce067c252b590ecc409d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 06:06:35 +0000

    Add package Invenio-Theme-TUW/2025.2.14/invenio_theme_tuw-2025.2.14-py3-none-any.whl

---
+++ packages/Invenio-Theme-TUW/invenio_theme_tuw-2025.2.14-py3-none-any.whl/invenio_theme_tuw/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2025 TU Wien.
+#
+# Invenio-Theme-TUW is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""TU Wien theme for Invenio (RDM)."""
+
+from .ext import InvenioThemeTUW
+
+__version__ = "2025.2.14"
+
+__all__ = (
+    "__version__",
+    "InvenioThemeTUW",
+)
