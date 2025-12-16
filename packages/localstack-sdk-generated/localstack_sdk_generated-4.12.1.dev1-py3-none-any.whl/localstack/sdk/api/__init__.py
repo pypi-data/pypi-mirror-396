e2f2bddda877a7d5fe59038ba29fcd4a331a6eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:32:01 +0000

    Add package localstack-sdk-generated/4.12.1.dev1/localstack_sdk_generated-4.12.1.dev1-py3-none-any.whl

---
+++ packages/localstack-sdk-generated/localstack_sdk_generated-4.12.1.dev1-py3-none-any.whl/localstack/sdk/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from localstack.sdk.api.aws_api import AwsApi
+from localstack.sdk.api.chaos_api import ChaosApi
+from localstack.sdk.api.default_api import DefaultApi
+from localstack.sdk.api.localstack_api import LocalstackApi
+from localstack.sdk.api.pods_api import PodsApi
+from localstack.sdk.api.pro_api import ProApi
+from localstack.sdk.api.replicator_api import ReplicatorApi
+from localstack.sdk.api.state_api import StateApi
+
