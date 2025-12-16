ec3229c6997cc0655c8f86f1083db7059093ea0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 19:10:39 +0000

    Add package my-awesome-library/0.0.1/my_awesome_library-0.0.1-py3-none-any.whl

---
+++ packages/my-awesome-library/my_awesome_library-0.0.1-py3-none-any.whl/my_awesome_library/main_module.py
@@ -0,0 +1,7 @@
+def greet(name):
+    """ترجع رسالة ترحيب مخصصة."""
+    return f"Hello, {name}! Welcome to my awesome library."
+
+def add_two_numbers(a, b):
+    """تضيف عددين معا."""
+    return a + b
