a827822ed82d8509d7d0f9e8e112dc1328186f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 20:06:13 +0000

    Add package auralis-worker/0.2.3/auralis_worker-0.2.3.tar.gz

---
+++ packages/auralis-worker/auralis_worker-0.2.3.tar.gz/auralis_worker-0.2.3/src/auralis_worker/__init__.py
@@ -0,0 +1,10 @@
+"""
+Auralis Cloud Worker - Distributed AI Compute Platform
+"""
+
+__version__ = "0.2.3"
+__author__ = "Aman Satyawani"
+
+from .cli import main, AuralisWorker
+
+__all__ = ["main", "AuralisWorker", "__version__"]
