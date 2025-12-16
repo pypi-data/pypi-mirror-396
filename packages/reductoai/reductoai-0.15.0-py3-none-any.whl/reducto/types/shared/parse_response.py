a2e275bbe430968024292d518eeafc4a2598bdc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 09:08:18 +0000

    Add package reductoai/0.15.0/reductoai-0.15.0-py3-none-any.whl

---
+++ packages/reductoai/reductoai-0.15.0-py3-none-any.whl/reducto/types/shared/parse_response.py
@@ -0,0 +1,118 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Dict, List, Union, Optional
+from typing_extensions import Literal, TypeAlias
+
+from ..._models import BaseModel
+from .parse_usage import ParseUsage
+from .bounding_box import BoundingBox
+
+__all__ = [
+    "ParseResponse",
+    "Result",
+    "ResultFullResult",
+    "ResultFullResultChunk",
+    "ResultFullResultOcr",
+    "ResultFullResultOcrLine",
+    "ResultFullResultOcrWord",
+    "ResultURLResult",
+]
+
+
+class ResultFullResultChunk(BaseModel):
+    blocks: List[Dict[str, object]]
+
+    content: str
+    """The content of the chunk extracted from the document."""
+
+    embed: str
+    """Chunk content optimized for embedding and retrieval."""
+
+    enriched: Optional[str] = None
+    """The enriched content of the chunk extracted from the document."""
+
+    enrichment_success: Optional[bool] = None
+    """Whether the enrichment was successful."""
+
+
+class ResultFullResultOcrLine(BaseModel):
+    bbox: BoundingBox
+
+    text: str
+
+    chunk_index: Optional[int] = None
+    """The index of the chunk that the line belongs to."""
+
+    confidence: Optional[float] = None
+    """OCR confidence score between 0 and 1, where 1 indicates highest confidence"""
+
+    rotation: Optional[int] = None
+    """The rotation angle in degrees, from 0 to 360, counterclockwise."""
+
+
+class ResultFullResultOcrWord(BaseModel):
+    bbox: BoundingBox
+
+    text: str
+
+    chunk_index: Optional[int] = None
+    """The index of the chunk that the word belongs to."""
+
+    confidence: Optional[float] = None
+    """OCR confidence score between 0 and 1, where 1 indicates highest confidence"""
+
+    rotation: Optional[int] = None
+    """The rotation angle in degrees, from 0 to 360, counterclockwise."""
+
+
+class ResultFullResultOcr(BaseModel):
+    lines: List[ResultFullResultOcrLine]
+
+    words: List[ResultFullResultOcrWord]
+
+
+class ResultFullResult(BaseModel):
+    chunks: List[ResultFullResultChunk]
+
+    type: Literal["full"]
+    """type = 'full'"""
+
+    custom: Optional[object] = None
+
+    ocr: Optional[ResultFullResultOcr] = None
+
+
+class ResultURLResult(BaseModel):
+    result_id: str
+
+    type: Literal["url"]
+    """type = 'url'"""
+
+    url: str
+
+
+Result: TypeAlias = Union[ResultFullResult, ResultURLResult]
+
+
+class ParseResponse(BaseModel):
+    duration: float
+    """The duration of the parse request in seconds."""
+
+    job_id: str
+
+    result: Result
+    """The response from the document processing service.
+
+    Note that there can be two types of responses, Full Result and URL Result. This
+    is due to limitations on the max return size on HTTPS. If the response is too
+    large, it will be returned as a presigned URL in the URL response. You should
+    handle this in your application.
+    """
+
+    usage: ParseUsage
+
+    pdf_url: Optional[str] = None
+    """The storage URL of the converted PDF file."""
+
+    studio_link: Optional[str] = None
+    """The link to the studio pipeline for the document."""
---
+++ packages/reductoai/reductoai-0.15.0-py3-none-any.whl/reducto/types/split_run_params.py
@@ -0,0 +1,56 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Union, Iterable
+from typing_extensions import Literal, Required, TypeAlias, TypedDict
+
+from .shared_params.upload import Upload
+from .shared_params.parse_options import ParseOptions
+from .shared_params.split_category import SplitCategory
+
+__all__ = ["SplitRunParams", "Input", "Settings"]
+
+
+class SplitRunParams(TypedDict, total=False):
+    input: Required[Input]
+    """For parse/split/extract pipelines, the URL of the document to be processed.
+
+    You can provide one of the following: 1. A publicly available URL 2. A presigned
+    S3 URL 3. A reducto:// prefixed URL obtained from the /upload endpoint after
+    directly uploading a document 4. A jobid:// prefixed URL obtained from a
+    previous /parse invocation
+
+                For edit pipelines, this should be a string containing the edit instructions
+    """
+
+    split_description: Required[Iterable[SplitCategory]]
+    """The configuration options for processing the document."""
+
+    parsing: ParseOptions
+    """The configuration options for parsing the document.
+
+    If you are passing in a jobid:// URL for the file, then this configuration will
+    be ignored.
+    """
+
+    settings: Settings
+    """The settings for split processing."""
+
+    split_rules: str
+    """The prompt that describes rules for splitting the document."""
+
+
+Input: TypeAlias = Union[str, Upload]
+
+
+class Settings(TypedDict, total=False):
+    """The settings for split processing."""
+
+    table_cutoff: Literal["truncate", "preserve"]
+    """
+    If tables should be truncated to the first few rows or if all content should be
+    preserved. truncate improves latency, preserve is recommended for cases where
+    partition_key is being used and the partition_key may be included within the
+    table. Defaults to truncate
+    """
