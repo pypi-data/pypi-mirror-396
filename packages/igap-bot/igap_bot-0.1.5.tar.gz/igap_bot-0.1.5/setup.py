f83e9ce171f5f06cf16f13bf98a0bfcbd08afec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 22:05:59 +0000

    Add package igap-bot/0.1.5/igap_bot-0.1.5.tar.gz

---
+++ packages/igap-bot/igap_bot-0.1.5.tar.gz/igap_bot-0.1.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="igap-bot",
+    version="0.1.5",
+    description="Unofficial iGap Bot API client for Python",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Ehsan",
+    author_email="Botkaplus@gmail.com",
+    url="https://github.com/sinyor-ehsan/igap-bot",
+    packages=find_packages(),
+    install_requires=[
+        "protobuf>=3.19.0,<4.0.0",
+        "aiohttp>=3.8.0",
+        "Pillow>=9.0.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
