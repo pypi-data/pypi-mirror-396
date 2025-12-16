90575b80797c714a3faa1770ba637a0365db38b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 15:10:13 +0000

    Add package python-easyverein/2.1.0/python_easyverein-2.1.0-py3-none-any.whl

---
+++ packages/python-easyverein/python_easyverein-2.1.0-py3-none-any.whl/easyverein/__init__.py
@@ -0,0 +1,14 @@
+"""
+Middleware for FastAPI that supports authenticating users against Keycloak
+"""
+
+__version__ = "2.1.0"
+
+# Export EasyVerein API directly
+from .api import EasyvereinAPI  # noqa: F401
+from .core.exceptions import (  # noqa: F401
+    EasyvereinAPIException,
+    EasyvereinAPINotFoundException,
+    EasyvereinAPITooManyRetriesException,
+)
+from .core.responses import BearerToken
