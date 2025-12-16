b38513dde5fce77f897e91621eac2d49670b981e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 21:06:12 +0000

    Add package visionprint/0.2.3/visionprint-0.2.3-py3-none-any.whl

---
+++ packages/visionprint/visionprint-0.2.3-py3-none-any.whl/visionprint/ansi.py
@@ -0,0 +1,19 @@
+class Colors:
+    BLACK   = "\033[30m"
+    RED     = "\033[31m"
+    GREEN   = "\033[32m"
+    YELLOW  = "\033[33m"
+    BLUE    = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN    = "\033[36m"
+    WHITE   = "\033[37m"
+    GREY    = "\033[90m"
+
+    RESET   = "\033[0m"
+
+class Styles:  
+    RESET     = "\033[0m"
+    BOLD      = "\033[1m"
+    FAINT     = "\033[2m"
+    ITALIC    = "\033[3m"
+    UNDERLINE = "\033[4m"
