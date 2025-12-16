b808a4bd8f49584f9e238605487704a8993f3643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 04:15:31 +0000

    Add package mdvcontainment/2.0.0a1/mdvcontainment-2.0.0a1.tar.gz

---
+++ packages/mdvcontainment/mdvcontainment-2.0.0a1.tar.gz/mdvcontainment-2.0.0a1/setup.py
@@ -0,0 +1,24 @@
+import numpy
+from Cython.Build import cythonize
+from setuptools import setup
+
+setup(
+    name='mdvcontainment',
+    version='v2.0.0a1',
+    packages=['mdvcontainment'],
+    author='BMH Bruininks',
+    description = "Containment analysis for (periodic) point clouds.",
+    long_description = 'README.md',
+    long_description_content_type = 'text/markdown',
+    url = "https://github.com/BartBruininks/mdvcontainment",
+    python_requires = ">=3.12",
+    ext_modules=cythonize(["mdvcontainment/find_label_contacts.pyx", 'mdvcontainment/find_bridges.pyx', "mdvcontainment/atoms_voxels_mapping.pyx"], language_level=3),
+    include_dirs=[numpy.get_include()],
+    install_requires=[
+        "numpy>=2.2",
+        "networkx>=3.4",
+        "scipy>=1.15",
+        "MDAnalysis>=2.8",
+        "matplotlib>=3.10",
+    ],
+)
