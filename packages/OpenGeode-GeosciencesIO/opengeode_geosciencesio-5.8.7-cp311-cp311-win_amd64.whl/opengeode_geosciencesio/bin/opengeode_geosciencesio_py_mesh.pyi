8654b60e69181f5419e9a39e233c419df92f089a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 09:08:55 +0000

    Add package OpenGeode-GeosciencesIO/5.8.7/opengeode_geosciencesio-5.8.7-cp311-cp311-win_amd64.whl

---
+++ packages/OpenGeode-GeosciencesIO/opengeode_geosciencesio-5.8.7-cp311-cp311-win_amd64.whl/opengeode_geosciencesio/bin/opengeode_geosciencesio_py_mesh.pyi
@@ -0,0 +1,9 @@
+"""
+OpenGeode-GeosciencesIO Python binding for mesh
+"""
+from __future__ import annotations
+__all__: list[str] = ['GeosciencesIOMeshLibrary']
+class GeosciencesIOMeshLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
