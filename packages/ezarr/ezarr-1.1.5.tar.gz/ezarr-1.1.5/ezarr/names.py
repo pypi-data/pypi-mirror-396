48aa70cfc6dbb410bf066029ed14c47ba863e630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 04:16:28 +0000

    Add package ezarr/1.1.5/ezarr-1.1.5.tar.gz

---
+++ packages/ezarr/ezarr-1.1.5.tar.gz/ezarr-1.1.5/ezarr/names.py
@@ -0,0 +1,16 @@
+from typing import final
+
+
+@final
+class Attribute:
+    EZType = "__ez_type__"
+    EZClass = "__ez_class__"
+
+
+@final
+class EZType:
+    Object = "object"
+    List = "list"
+
+
+UNKNOWN = "<UNKNWON>"
