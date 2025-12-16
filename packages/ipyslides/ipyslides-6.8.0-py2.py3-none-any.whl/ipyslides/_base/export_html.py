f2a13166df4370163f8cfa684ff75642cbe53964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:08:19 +0000

    Add package ipyslides/6.8.0/ipyslides-6.8.0-py2.py3-none-any.whl

---
+++ packages/ipyslides/ipyslides-6.8.0-py2.py3-none-any.whl/ipyslides/_base/export_html.py
@@ -0,0 +1,183 @@
+"""
+Export Slides to static HTML slides. It is used by program itself, 
+not by end user.
+"""
+import os
+import textwrap
+from contextlib import suppress
+
+from .export_template import doc_html
+from . import styles
+from ..writer import _fmt_html
+from ..formatters import _inline_style
+
+_script = '''<script>
+    let box = document.querySelector('.SlideBox');
+    let slide = box.querySelector('.SlideArea');
+
+    window.onresize = function() {
+        let rectBox = box.getBoundingClientRect();
+        let rectSlide = slide.getBoundingClientRect();
+        console.log(rectBox, rectSlide);
+        let oldScale = document.documentElement.style.getPropertyValue('--contentScale');
+        let old = oldScale ? oldScale : 1;
+        let scaleH = old*rectBox.height/rectSlide.height;
+        let scaleW = old*rectBox.width/rectSlide.width;
+        let scale = scaleH > scaleW ? scaleW : scaleH;
+        if (scale) { // Only add if not null or somethings
+            document.documentElement.style.setProperty('--contentScale',scale);
+            document.documentElement.style.setProperty('--paddingBottom',Number(__PADBTM__/scale) + "px");
+        };
+    };
+    window.dispatchEvent(new window.Event('resize')); // First time programatically
+
+    let slides = document.querySelector(".SlidesWrapper");
+
+    slides.addEventListener("scroll", (event) => {
+        slides.classList.add("Scrolling");
+        setTimeout(() => { slides.classList.remove("Scrolling");}, 300); // ensure scrollend if link clicked
+    })
+    slides.addEventListener("scrollend", (event) => {
+        slides.classList.remove("Scrolling");
+    })
+</script>'''
+
+
+class _HhtmlExporter:
+    # Should be used inside Slides class only.
+    def __init__(self, _instance_BaseSlides):
+        self.main = _instance_BaseSlides
+        self.main.widgets.buttons.export.on_click(self._export) # Export button
+        
+    def _htmlize(self):
+        content = ''
+        for item in self.main:
+            objs = item.contents # get conce
+            frames, snums = [], []
+            
+            if not item._export_fidxs:
+                frames = [objs]
+                snums = [str(item.index)]
+            else:
+                for fi, frame in enumerate(item._export_fidxs):
+                    head, start, end, part = [frame.get(k, -1) - item._offset for k in ('head','start','end','part')]
+                    frame_objs = []
+                    
+                    if head >= 0:
+                        frame_objs.extend(objs[:head + 1])
+                    
+                    if not "part" in frame: # full content in range
+                        frame_objs.extend(objs[start:end + 1])
+                    else: # partial content in range
+                        for i in range(start, end + 1):
+                            if i < part:
+                                frame_objs.append(objs[i])
+                            elif i > part:
+                                frame_objs.append(f"<div style='visibility:hidden;'>{_fmt_html(objs[i])}</div>")
+                            else: # i == part, can be Writer
+                                if "col" in frame and hasattr(objs[i], "fmt_html"): # Writer with columns
+                                    frame_objs.append(objs[i].fmt_html(frame))
+                                else: # normal Writer
+                                    frame_objs.append(objs[i])
+                
+                    frames.append(frame_objs) 
+                    snums.append(item._get_snum(fi))         
+            
+            for k, (snum, objs) in enumerate(zip(snums, frames)):
+                _html = item._speaker_notes(returns=True) # speaker notes at top if any, returns string
+                for out in objs:
+                    _html += f'<div style="width: 100%;">{_fmt_html(out)}</div>' # Important to have each content in a div, so that it can be same as notebook content
+
+                _html = f'<div class="jp-OutputArea">{_html}</div>'
+
+                number = ""
+                if item.index > 0 and self.main.settings.footer.numbering:
+                    number = f'<span class="Number">{snum}</span>' 
+
+                content += textwrap.dedent(f'''
+                    <section id="{item._sec_id}">
+                        {self._get_css(item)}
+                        <div class="SlideBox">
+                            {item._get_bg_image(f'#{item._sec_id}')}
+                            <div class="{item._css_class} export-only">
+                                {_html}
+                            </div>
+                            {number}
+                            {self._get_progress(item,k)}
+                        </div>
+                    </section>''')
+        
+        navui_class = '' if self.main.settings.footer._print_padding > 16 else 'NavHidden' 
+        content += f'<div class="Footer {navui_class}">{self.main.settings.footer._to_html()}</div>'
+        content += self._get_logo() # Both of these fixed
+            
+        theme_kws = self.main.settings._theme_kws
+        
+        if self.main.widgets.theme.value == "Jupyter":  # jupyterlab themes colors to export
+            if self.main.widgets.iw._colors:
+                theme_kws["colors"] = self.main.widgets.iw._colors
+
+        return doc_html(
+            code_css    = self.main.widgets.htmls.hilite.value.replace(f'.{self.main.uid}',''), # remove id from code here
+            style_css   = styles.style_css(**theme_kws, _root=True), 
+            content     = content, 
+            script      = _script, 
+            click_btns  = self.main.settings._get_clickers(), 
+            css_class   = ' '.join(c for c in self.main._box._dom_classes if c.startswith('Slides')),
+            padding_bottom = self.main.settings.footer._print_padding,
+            )
+    
+    def _get_progress(self, slide, fidx=0):
+        if not self.main.settings.footer.progress:
+            return ''
+        
+        unit = 100/(self.main._iterable[-1].index or 1) # avoid zero division error or None
+        pv = round(unit * ((slide.index or 0) - (slide.nf - fidx - 1)/slide.nf), 4)
+        gradient = f'linear-gradient(to right, var(--accent-color) 0%,  var(--accent-color) {pv}%, var(--bg2-color) {pv}%, var(--bg2-color) 100%)'
+        return f'<div class="Progress" style="background: {gradient};"></div>'
+    
+    def _get_css(self, slide):
+        "uclass.SlidesWrapper → sec_id , .SlidesWrapper → sec_id, FooterBox → Footer"
+        return (f'{slide.css}').replace( # xtml or str
+            f".{self.main.uid}.SlidesWrapper", f"#{slide._sec_id}").replace(
+            f".{self.main.uid}", f"#{slide._sec_id}").replace(
+            ".FooterBox", ".Footer"
+            )
+    def _get_logo(self):
+        return f'''<div class="SlideLogo" {_inline_style(self.main.widgets.htmls.logo)}"> 
+            {self.main.widgets.htmls.logo.value} 
+        </div>'''
+                
+    def _writefile(self, path, overwrite = False):
+        if os.path.isfile(path) and not overwrite:
+            return print(f'File {path!r} already exists. Use overwrite=True to overwrite.')
+        
+        with open(path,'w', encoding="utf-8") as f: # encode to utf-8 to handle emojis
+            f.write(self._htmlize()) 
+            
+    
+    def export_html(self, path = 'Slides.html', overwrite = False):
+        """Build html slides that you can print.
+        
+        - Use 'overrides.css' file in same folder to override CSS styles.
+        - If a slide has only widgets or does not have single object with HTML representation, it will be skipped.
+        - You can take screenshot (using system's tool) of a widget, save it using Clips GUI in side panel and laod as image to keep PNG view of a widget. 
+        
+        ::: note-info
+            - PDF printing of slide width is 210mm (8.25in). Height is determined by aspect ratio provided.
+            - Use `Save as PDF` option instead of Print PDF in browser to make links work in output PDF. Alsp enable background graphics in print dialog.
+        """
+        _path = os.path.splitext(path)[0] + '.html' if path != 'Slides.html' else path
+        export_func = lambda: self._writefile(_path, overwrite)
+        self.main.widgets.iw._try_exec_with_fallback(export_func)
+        
+    def _export(self,btn):
+        "Export to HTML slides on button click."
+        path = 'Slides.html'
+        if os.path.isfile(path) and not self.main.widgets.checks.confirm.value:
+            return self.main.notify(f'File {path!r} already exists. Select overwrite checkbox if you want to replace it.')
+        
+        self.export_html(path, overwrite = self.main.widgets.checks.confirm.value)
+        self.main.notify(f'File saved: {path!r}',10)
+        with suppress(BaseException): # Does not work on some systems, so safely continue.
+            os.startfile(path) 
---
+++ packages/ipyslides/ipyslides-6.8.0-py2.py3-none-any.whl/ipyslides/_base/export_template.py
@@ -0,0 +1,194 @@
+# Template for building HTML from slides 
+
+def doc_html(code_css, style_css, content, script, click_btns, css_class, padding_bottom):
+    return f'''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,%3Csvg viewBox='0 0 50 50' xmlns='http://www.w3.org/2000/svg' fill='none' stroke='currentColor' stroke-linecap='butt' stroke-linejoin='round' stroke-width='7.07'%3E%3Cpath d='M22.5 7.5L10 20L20 30L30 20L40 30L27.5 42.5' stroke='%2343D675'/%3E%3Cpath d='M7.5 27.5L22.5 42.5' stroke='%234F8EF7'/%3E%3Cpath d='M32.5 32.5L20 20L30 10L42.5 22.5' stroke='%234F8EF7'/%3E%3C/svg%3E">
+    <title>Slides</title>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script> 
+    
+    <style>{style_css}</style>
+    {slides_css.replace("__PADBTM__",str(padding_bottom))}
+    {code_css}
+
+    <!-- Custom stylesheet, it must be in the same directory as the html file -->
+    <link rel="stylesheet" href="overrides.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Loading mathjax macro -->
+    <!-- Load mathjax -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-AMS_HTML"></script>
+    <!-- MathJax configuration -->
+    <script type="text/x-mathjax-config">
+    MathJax.Hub.Config({{
+        tex2jax: {{
+            inlineMath: [ ["$","$"] ],
+            displayMath: [ ["$$","$$"] ],
+            processEscapes: true,
+            processEnvironments: true
+        }},
+        // align-center justify equations in code and markdown cells. Elsewhere
+        // we use CSS to left justify single line equations in code cells.
+        displayAlign: "center",
+        "HTML-CSS": {{
+            styles: {{".MathJax_Display": {{"margin": "0.5em auto"}}}},
+            linebreaks: {{ automatic: true }}
+        }}
+    }});
+    </script>
+    <!-- End of mathjax configuration -->
+</head>
+<body>
+<div>
+    {click_btns}
+    <!-- Classes below work for both scenerios -->
+    <div class="{css_class}">
+    {content}
+    </div>
+</div>
+</body>
+{script.replace("__PADBTM__",str(padding_bottom))}
+</html>
+'''
+
+slides_css = """<style>
+.jupyter-only { display:none !important;}
+.speaker-notes { display:none;} /* hide notes in HTML */
+a {text-decoration:none !important;}
+a:hover {text-decoration:underline !important;}
+p {margin-block: 0.25em !important;}
+pre {white-space: break-spaces;} /* General rule unless specified in elements */
+.toc-item {margin-block: 0.25em}
+.SlidesWrapper {
+	scroll-snap-type: x mandatory !important;
+    display: flex !important;
+    overflow-x: auto !important;
+    overflow-y: hidden !important; 
+    position: fixed !important;
+    scroll-behavior: smooth;
+    top: 0 !important;
+    left: 0 !important;
+    height: 100vh !important;
+    width: 100vw !important;
+    line-height: 1.5 !important; /* Default line-height for all text to match Jupyter */
+}
+section {
+	scroll-snap-align:start !important;
+    scroll-snap-stop: always !important;
+	display: block !important;
+	height: 100vh !important;
+	max-height: 100vh !important;
+	min-width: 100vw !important;
+    background: var(--bg1-color) !important; /* need for each slide */
+}
+section .SlideBox {
+    width: 100vw !important;
+    height: 100vh !important; 
+    /* Extra CSS will come from export here */
+    padding: 0 !important;
+}
+section .SlideBox > .Progress { 
+    display: block !important; 
+    margin: 0 !important;
+    height: 2px !important;
+    width: 100% !important;
+    position: absolute !important;
+    left:0 !important;
+    bottom: 0 !important;
+}
+section:first-of-type .SlideBox > .Progress {width: 0 !important;}  /* avoid non-zero progress in title of print*/
+.SlidesWrapper .Footer { 
+    padding: 0 !important; margin: 0 !important; 
+    position: fixed !important;
+    left:0;
+    width: 100%;
+    bottom: -2px !important; /* something off */
+    overflow: hidden !important;
+}
+.SlidesWrapper .Footer.NavHidden {
+    background: none; /* no important here */
+}
+.SlidesWrapper .Footer > p {
+    font-size: 14px !important;
+    padding: 4px !important;
+    padding-left: 8px !important; 
+    display:block !important;
+    margin:0 !important;
+}
+.SlidesWrapper .Footer.NavHidden > p {
+    display:none !important;
+}
+.SlidesWrapper .SlideLogo {
+    position: fixed !important;
+    line-height: 0 !important; /* suppress bad line-height here */
+}
+.SlidesWrapper::-webkit-scrollbar,
+.SlidesWrapper::-webkit-scrollbar-button,
+.SlidesWrapper::-webkit-scrollbar-corner {
+    display:none !important;
+}
+
+.SlidesWrapper.Scrolling .Progress,
+.SlidesWrapper.Scrolling .SlideBox > .Number {
+    visibility: hidden !important;
+    transition: visbility 200ms ease-in;
+}
+.SlidesWrapper .SlideBox > .Number {
+    color:var(--fg2-color);
+    position:absolute;
+    right:6px;
+    bottom:3px;
+    font-size:12px;
+    z-index:5;
+}
+
+@media print {
+    * {
+        -webkit-print-color-adjust: exact !important;
+        print-color-adjust: exact !important;
+        color-adjust: exact !important;
+        ::-webkit-scrollbar { height: 0 !important; width: 0 !important; }
+    }
+    /* Page size is set in style_css */
+    .speaker-notes { display:block !important;} /* show notes in print */
+    .SlidesWrapper {
+        --contentScale : 1 !important; /* Defualt for printing at same value */
+        display: flex !important;
+        flex-direction: column !important;
+        overflow: hidden !important;
+        position: absolute !important;
+        top: 0 !important;
+        left: 0 !important;
+        height: auto !important;
+    }
+    section {
+        display: block !important;
+        margin: 0 !important;
+        padding: 0 !important;
+        page-break-inside: avoid !important;
+        page-break-after: always !important;
+        overflow: hidden !important; /* otherwise it throws text to next page */
+        padding-bottom: __PADBTM__px !important; /* for avoiding overflow to bottom,same as under SlideArea */
+    }
+    section:last-of-type {
+        page-break-after: avoid !important;
+    }
+    section .SlideBox {
+        margin-bottom: 0 !important;
+        padding-bottom: 0 !important;
+        page-break-inside: avoid !important;
+    }
+    section .SlideArea {
+        position: static !important; /*override from document as printing absolute is issue */
+        overflow: hidden !important;
+        page-break-inside: avoid !important;
+        --paddingBottom: __PADBTM__px; /* fixed for printing, padding set in style_css */
+    }
+}
+</style>
+"""
