f1e4e3f1d71ddb6bdcb93396db821ff3bca25eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:31:33 +0000

    Add package wikipedia-tablo-cekici/1.0.0/wikipedia_tablo_cekici-1.0.0.tar.gz

---
+++ packages/wikipedia-tablo-cekici/wikipedia_tablo_cekici-1.0.0.tar.gz/wikipedia_tablo_cekici-1.0.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="wikipedia-tablo-cekici",  # PyPI'de başka birisi kullanıyorsa değiştirin
+    version="1.0.0",
+    
+    author="özgürözen",  # Virgül eksikti
+    author_email="oozen760@gmail.com",  # Virgül eksikti
+    
+    description="Wikipedia sayfalarından tablo verilerini çeken Python kütüphanesi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
+    url="https://github.com/ozgurrozennn/my_project",  # Tırnak içine alınmalı ve virgül eksikti
+    
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "requests>=2.25.0",
+        "beautifulsoup4>=4.9.0",
+        "pandas>=1.2.0",
+        "lxml>=4.6.0",
+        "openpyxl>=3.0.0",
+    ],
+    keywords="wikipedia table scraper data extraction tablo çekici",
+)
