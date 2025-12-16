367126d6d356509d0a7632f2d29deab9e511c049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:07:21 +0000

    Add package kompress/0.3.20241214/kompress-0.3.20241214-py3-none-any.whl

---
+++ packages/kompress/kompress-0.3.20241214-py3-none-any.whl/kompress/__init__.py
@@ -0,0 +1,178 @@
+from __future__ import annotations
+
+import gzip
+import io
+import os
+import pathlib
+import sys
+import warnings
+from pathlib import Path
+from typing import IO, TYPE_CHECKING
+
+from .tar import TarPath
+from .zip import ZipPath
+
+
+class Ext:
+    # fmt: off
+    xz    = '.xz'
+    zip   = '.zip'
+    lz4   = '.lz4'
+    zstd  = '.zstd'
+    zst   = '.zst'
+    targz = '.tar.gz'
+    gz    = '.gz'
+    # fmt: on
+
+
+def is_compressed(p: Path | str) -> bool:
+    pp = p if isinstance(p, Path) else Path(p)
+    # todo kinda lame way for now.. use mime ideally?
+    # should cooperate with kompress.kopen?
+    return pp.name.endswith((Ext.xz, Ext.zip, Ext.lz4, Ext.zstd, Ext.zst, Ext.targz, Ext.gz))
+
+
+class CPath(Path):
+    """
+    Hacky way to support compressed files.
+    If you can think of a better way to do this, please let me know! https://github.com/karlicoss/HPI/issues/20
+
+    Ugh. So, can't override Path because of some _flavour thing.
+    Path only has _accessor and _closed slots, so can't directly set .open method
+    _accessor.open has to return file descriptor, doesn't work for compressed stuff.
+    """
+
+    if sys.version_info[:2] < (3, 12):
+        # older version of python need _flavour defined
+        _flavour = pathlib._windows_flavour if os.name == 'nt' else pathlib._posix_flavour  # type: ignore[attr-defined]
+
+    def __new__(cls, *args, **kwargs):
+        # TODO shortcut if args[0] is already Cpath?
+
+        path = Path(*args)
+        if path.name.endswith(Ext.zip):
+            if path.exists():
+                # if path doesn't exist, zipfile can't open it to read the index etc
+                # so it's the best we can do in this case?
+                # TODO move this into ZipPath.__new__?
+                return ZipPath(path)
+        if path.name.endswith(Ext.targz):
+            return TarPath(path)
+        return super().__new__(cls, *args, **kwargs)
+
+    def open(  # type: ignore[override]
+        self,
+        mode: str = 'r',
+        buffering: int = -1,
+        encoding: str | None = None,
+        errors: str | None = None,
+        **kwargs,
+    ):
+        if buffering not in {-1, 0}:
+            # buffering is unsupported by most compressed formats
+            # -1 is 'default', 0 means 'buffering off' (pathlib passes it since 3.14)
+            warnings.warn(f"while opening {self}: CPath doesn't support buffering", stacklevel=2)
+        # simply forward the rest of positional args
+        kwargs['encoding'] = encoding
+        kwargs['errors'] = errors
+        return _cpath_open(
+            path=str(self),
+            mode=mode,
+            **kwargs,
+        )
+
+
+def _cpath_open(*, path: Path | str, mode: str, **kwargs) -> IO:
+    if 'w' in mode:
+        raise RuntimeError(f"Tring to open {path} in {mode=}. CPath only supports reading.")
+
+    pp = Path(path)
+    name = pp.name
+    if name.endswith((Ext.zstd, Ext.zst)):
+        if sys.version_info[:2] >= (3, 14):
+            from compression import zstd
+
+            # ugh. default r for zstd is rb
+            # see https://docs.python.org/3.15/library/compression.zstd.html#compression.zstd.open
+            if mode == 'r':
+                mode = 'rt'
+
+            return zstd.open(path, mode=mode, **kwargs)  # type: ignore[call-overload]
+        else:
+            # ty does checks in both branches atm? see https://github.com/astral-sh/ty/issues/160
+            import zstandard as zstd  # ty: ignore[unresolved-import]
+
+            fh = pp.open('rb')
+            dctx = zstd.ZstdDecompressor()
+            reader = dctx.stream_reader(fh)
+
+            if mode == 'rb':
+                return reader
+            else:
+                # must be text mode
+                # NOTE: no need to pass mode, TextIOWrapper doesn't like it
+                return io.TextIOWrapper(reader, **kwargs)  # meh
+    elif name.endswith(Ext.xz):
+        import lzma
+
+        # for lzma, 'r' means 'rb'
+        # https://github.com/python/cpython/blob/d01cf5072be5511595b6d0c35ace6c1b07716f8d/Lib/lzma.py#L97
+        # whereas for Path.open, 'r' means 'rt'
+        if mode == 'r':
+            mode = 'rt'
+        return lzma.open(pp, mode=mode, **kwargs)
+    elif name.endswith(Ext.lz4):
+        import lz4.frame  # type: ignore[import-untyped]
+
+        if mode == 'r':
+            # lz4 uses rb by default
+            # whereas for Path.open, 'r' means 'rt'
+            mode = 'rt'
+
+        return lz4.frame.open(str(pp), mode=mode, **kwargs)
+    elif name.endswith(Ext.gz):
+        if mode == 'r':
+            # for gzip 'r' means 'rb' returns a gzip.Gzipfile (in binary mode)
+            # whereas for Path.open, 'r' means 'rt'
+            mode = 'rt'
+
+        # gzip does not support encoding in binary mode
+        # TODO tbh, open() docs are saying encoding shouldn't be passed in binary mode, not sure what this was for?
+        if 'b' in mode:
+            del kwargs['encoding']
+
+        # gzip.open already returns a io.TextIOWrapper if encoding is specified
+        # and its not in binary mode
+        return gzip.open(pp, mode=mode, **kwargs)  # type: ignore[return-value]
+    elif name.endswith(Ext.zip):
+        # this should be handled by ZipPath (see CPath.__new__)
+        raise RuntimeError("shouldn't happen")
+    elif name.endswith(Ext.targz):
+        # this should be handled by TarPath (see CPath.__new__)
+        raise RuntimeError("shouldn't happen")
+    else:
+        return pp.open(mode=mode, **kwargs)
+
+
+if not TYPE_CHECKING:
+    # FIXME deprecate properly
+    # still used in promnesia legacy takeout module? could migrate off
+    # ah ok, promnesia works off my.core.kompress (which is itself deprecated)
+    # so we could perhaps add kopen/kexists adapters that just do Cpath(first_arg) / Path(rest)?
+    # pass kwargs to open? like mode/encoding
+
+    from .compat import deprecated
+
+    @deprecated('use Cpath(...).open() instead')
+    def kopen(path, *args, **kwargs):
+        cpath = CPath(path) / Path(*args)
+        return cpath.open(**kwargs)
+
+    @deprecated('use Cpath(...).open() instead')
+    def open(*args, **kwargs):  # noqa: A001
+        return kopen(*args, **kwargs)
+
+    @deprecated('use Cpath(...).exists() instead')
+    def kexists(path, *args) -> bool:
+        cpath = CPath(path) / Path(*args)
+        return cpath.exists()
---
+++ packages/kompress/kompress-0.3.20241214-py3-none-any.whl/kompress/tests/kompress.py
@@ -0,0 +1,270 @@
+import gzip
+import io
+import lzma
+import sys
+import zipfile
+from pathlib import Path
+
+import pytest
+
+from .. import CPath, ZipPath
+
+structure_data: Path = Path(__file__).parent / "structure_data"
+
+
+def test_zip(tmp_path: Path) -> None:
+    subpath = 'path/in/archive'
+
+    assert (CPath(tmp_path / 'file.zip') / subpath).open().read() == 'data in zip'
+
+    # CPath should dispatch zips to ZipPath
+    cpath = CPath(tmp_path / 'file.zip')
+    assert isinstance(cpath, ZipPath)
+
+    assert (cpath / subpath).read_text() == 'data in zip'
+
+    # make sure construction from parts works as expected
+    assert isinstance(CPath(*cpath.parts), ZipPath)
+
+    assert isinstance(CPath(cpath), ZipPath)
+
+
+def test_cpath_zip(tmp_path: Path) -> None:
+    assert (CPath(tmp_path / 'file.zip') / 'path/in/archive').exists()
+    assert not (CPath(tmp_path / 'file.zip') / 'path/notin/archive').exists()
+
+    assert not (CPath(tmp_path / 'nosuchzip.zip') / 'path/in/archive').exists()
+
+
+@pytest.mark.parametrize(
+    ('filename', 'expected'),
+    [
+        ('file'    , 'just plaintext'),
+        ('file.xz' , 'compressed text'),
+        ('file.zst', 'compressed text'),
+        ('file.gz' , 'compressed text'),
+        ('file.lz4', 'compressed text'),
+    ],
+)  # fmt: skip
+def test_cpath_regular(filename: str, expected: str, tmp_path: Path) -> None:
+    """
+    Check different ways of interacting with CPath
+    """
+    path = tmp_path / filename
+
+    with CPath(path).open() as fo:
+        assert fo.read() == expected
+
+    with CPath(path).open(mode='rt') as fo:
+        assert fo.read() == expected
+
+    with CPath(path).open('r') as fo:
+        assert fo.read() == expected
+
+    with CPath(path).open('r', encoding='utf8') as fo:
+        assert fo.read() == expected
+
+    with CPath(path).open('rb') as fo:
+        assert fo.read() == expected.encode('ascii')
+
+    assert CPath(path).read_text() == expected
+    assert CPath(path).read_bytes() == expected.encode('ascii')
+
+    for args in [
+        [str(path)],
+        [path],
+        [CPath(path)],
+        path.parts,
+    ]:
+        Path(*args)  # type: ignore[misc] # just a sanity check that regular Path can be constructed this way
+        assert CPath(*args).read_text() == expected  # type: ignore[misc]
+
+
+def test_zippath(tmp_path: Path) -> None:
+    # TODO support later...
+    # zz = ZipPath(tmp_path / 'doesntexist.zip')
+
+    zp = ZipPath(tmp_path / 'file.zip', 'path/in/archive')
+
+    assert zp.read_text() == 'data in zip'
+
+    assert zp.open(mode='rb').read() == b'data in zip'
+
+    assert zp.open(mode='r').read() == 'data in zip'
+    assert zp.open(mode='rt').read() == 'data in zip'  # type: ignore[call-overload]
+
+    target = structure_data / 'gdpr_export.zip'
+    assert target.exists(), target  # precondition
+
+    zp = ZipPath(target)
+
+    ZipPath(zp)  # make sure it doesn't crash
+
+    # magic! convenient to make third party libraries agnostic of ZipPath
+    assert isinstance(zp, Path)
+    assert isinstance(zp, ZipPath)
+    assert isinstance(zp / 'subpath', Path)
+    # TODO maybe change __str__/__repr__? since it's a bit misleading:
+    # Path('/code/hpi/tests/core/structure_data/gdpr_export.zip', 'gdpr_export/')
+
+    assert ZipPath(target) == ZipPath(target)
+    assert zp.absolute() == zp
+    assert zp / '.' == zp
+
+    # shouldn't crash
+    hash(zp)
+
+    assert zp.exists()
+    assert (zp / 'gdpr_export').exists()
+    assert (zp / 'gdpr_export' / 'comments').exists()
+    assert (zp / Path('gdpr_export', 'comments')).exists()
+    ## NOTE: in pathlib.Path these work, however not in zipfile.Path
+    ## for now we don't support them either, need to be really careful if we wanna diverge from zipfile.Path
+    ## but in
+    # assert (zp / '.').exists()
+    # assert (zp / '.' / 'gdpr_export').exists()
+    # assert (zp / 'gdpr_export' / './comments').exists()
+    ##
+
+    # check str constructor just in case
+    assert (ZipPath(str(target)) / 'gdpr_export' / 'comments').exists()
+    assert not (ZipPath(str(target)) / 'whatever').exists()
+
+    matched = list(zp.rglob('*'))
+    assert len(matched) > 0
+    assert all(p.filepath == target for p in matched), matched
+
+    rpaths = [p.relative_to(zp) for p in matched]
+    gdpr_export = Path('gdpr_export')
+    # fmt: off
+    assert rpaths == [
+        gdpr_export,
+        gdpr_export / 'comments',
+        gdpr_export / 'comments' / 'comments.json',
+        gdpr_export / 'profile',
+        gdpr_export / 'profile' / 'settings.json',
+        gdpr_export / 'messages',
+        gdpr_export / 'messages' / 'index.csv',
+    ], rpaths
+    # fmt: on
+
+    # TODO hmm this doesn't work atm, whereas Path does
+    # not sure if it should be defensive or something...
+    # ZipPath('doesnotexist')
+    # same for this one
+    # assert ZipPath(Path('test'), 'whatever').absolute() == ZipPath(Path('test').absolute(), 'whatever')
+
+    assert (ZipPath(target) / 'gdpr_export' / 'comments').exists()
+
+    jsons = [p.relative_to(zp / 'gdpr_export') for p in zp.rglob('*.json')]
+    # fmt: off
+    assert jsons == [
+        Path('comments', 'comments.json'),
+        Path('profile' , 'settings.json'),
+    ]
+    # fmt: on
+
+    # NOTE: hmm interesting, seems that ZipPath is happy with forward slash regardless OS?
+    assert list(zp.rglob('mes*')) == [ZipPath(target, 'gdpr_export/messages')]
+
+    iterdir_res = list((zp / 'gdpr_export').iterdir())
+    assert len(iterdir_res) == 3
+    assert all(isinstance(p, Path) for p in iterdir_res)
+
+    # date recorded in the zip archive
+    assert (zp / 'gdpr_export' / 'comments' / 'comments.json').stat().st_mtime > 1625000000
+    # TODO ugh.
+    # unzip -l shows the date  as 2021-07-01 09:43
+    # however, python reads it as 2021-07-01 01:43 ??
+    # don't really feel like dealing with this for now, it's not tz aware anyway
+
+    json_gz = zp / 'gdpr_export' / 'comments' / 'comments.json.gz'
+    assert json_gz.suffixes == ['.json', '.gz']
+    assert json_gz.suffix == '.gz'
+
+
+def test_gz(tmp_path: Path) -> None:
+    gzf = tmp_path / 'file.gz'
+    with gzip.open(gzf, 'wb') as f:
+        f.write(b'compressed text')
+
+    # test against gzip magic number
+    assert gzf.read_bytes()[:2] == b'\x1f\x8b'
+
+    with CPath(gzf).open() as f:
+        assert hasattr(f, 'read')
+        assert hasattr(f, 'readable')
+        assert f.readable()
+        assert not f.writable()
+        assert f.read() == 'compressed text'  # if not specified, defaults to rt
+
+    with CPath(gzf).open(mode='rb') as f:
+        assert isinstance(f, gzip.GzipFile)
+        assert f.read() == b'compressed text'
+
+    # should return text
+    with CPath(gzf).open(mode='r') as f:
+        assert isinstance(f, io.TextIOWrapper)
+        assert f.read() == 'compressed text'
+
+    # if you specify, rt, does what you expect
+    with CPath(gzf).open(mode='rt') as f:
+        assert isinstance(f, io.TextIOWrapper)
+        assert f.read() == 'compressed text'
+
+    assert CPath(gzf).read_text() == 'compressed text'
+    assert CPath(gzf).read_bytes() == b'compressed text'
+
+
+def test_kopen_kexists(tmp_path: Path) -> None:
+    """
+    Testing deprecations, can remove when we remove kexists/kopen
+    """
+    from .. import kexists, kopen  # type: ignore[attr-defined]
+
+    path = Path(tmp_path / 'file.zip')
+
+    read_res = kopen(path, 'path', 'in', 'archive').read()
+    assert read_res == 'data in zip'
+    assert kexists(path, 'path/in/archive')
+    assert not kexists(path, 'does/not/exist')
+
+
+@pytest.fixture(autouse=True)
+def prepare_data(tmp_path: Path):
+    (tmp_path / 'file').write_text('just plaintext')
+
+    # xz
+    with (tmp_path / 'file.xz').open('wb') as f:
+        with lzma.open(f, 'w') as lzf:
+            lzf.write(b'compressed text')
+
+    # zst
+    if sys.version_info[:2] >= (3, 14):
+        from compression import zstd
+
+        with zstd.open(tmp_path / 'file.zst', 'wb') as f:
+            f.write(b'compressed text')
+    else:
+        # ty does checks in both branches atm? see https://github.com/astral-sh/ty/issues/160
+        import zstandard as zstd  # ty: ignore[unresolved-import]
+
+        zst_ctx = zstd.ZstdCompressor()
+        (tmp_path / 'file.zst').write_bytes(zst_ctx.compress(b'compressed text'))
+
+    # gz
+    gzf = tmp_path / 'file.gz'
+    with gzip.open(gzf, 'wb') as f:
+        f.write(b'compressed text')
+
+    # lz4
+    import lz4.frame  # type: ignore[import-untyped]
+
+    (tmp_path / 'file.lz4').write_bytes(lz4.frame.compress(b'compressed text'))
+
+    with zipfile.ZipFile(tmp_path / 'file.zip', 'w') as zf:
+        zf.writestr('path/in/archive', 'data in zip')
+    try:
+        yield None
+    finally:
+        pass
