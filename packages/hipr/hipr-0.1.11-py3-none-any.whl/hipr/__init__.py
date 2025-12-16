e14c85bc1c26918166e009ddd42d4e21f5401e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:07:13 +0000

    Add package hipr/0.1.11/hipr-0.1.11-py3-none-any.whl

---
+++ packages/hipr/hipr-0.1.11-py3-none-any.whl/hipr/__init__.py
@@ -0,0 +1,25 @@
+"""hipr - Automatic Pydantic config generation from function signatures."""
+
+__version__ = "0.1.11"
+
+from hipr.constraints import Ge, Gt, Le, Lt, MaxLen, MinLen, MultipleOf, Pattern
+from hipr.generation import configurable
+from hipr.models import BoundFunction, MakeableModel
+from hipr.typedefs import DEFAULT, Hyper
+
+__all__ = [
+  "DEFAULT",
+  "BoundFunction",
+  "Ge",
+  "Gt",
+  "Hyper",
+  "Le",
+  "Lt",
+  "MakeableModel",
+  "MaxLen",
+  "MinLen",
+  "MultipleOf",
+  "Pattern",
+  "__version__",
+  "configurable",
+]
