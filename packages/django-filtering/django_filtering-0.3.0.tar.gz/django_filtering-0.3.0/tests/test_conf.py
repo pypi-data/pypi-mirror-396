a4d7e319904aab4d16c2d8c7bcaf8541b9df08c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 09:09:45 +0000

    Add package django-filtering/0.3.0/django_filtering-0.3.0.tar.gz

---
+++ packages/django-filtering/django_filtering-0.3.0.tar.gz/django_filtering-0.3.0/tests/test_conf.py
@@ -0,0 +1,15 @@
+import pytest
+
+from django_filtering.conf import _APP_SETTINGS_KEY, settings as filtering_settings
+
+
+def test_invalid_setting():
+    setting = 'FOO'
+    with pytest.raises(AttributeError, match=f"Invalid setting name: '{setting}'"):
+        getattr(filtering_settings, setting)
+
+def test_settings_modification(settings):
+    expected_setting = {'exact': 'equals'}
+    setattr(settings, _APP_SETTINGS_KEY, {"LOOKUP_LABELS": expected_setting})
+
+    assert filtering_settings.LOOKUP_LABELS == expected_setting
