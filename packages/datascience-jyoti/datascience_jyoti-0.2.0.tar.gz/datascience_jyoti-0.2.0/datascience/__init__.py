b3ab02b3c61ba4897700880a652fba679f652cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 22:06:56 +0000

    Add package datascience-jyoti/0.2.0/datascience_jyoti-0.2.0.tar.gz

---
+++ packages/datascience-jyoti/datascience_jyoti-0.2.0.tar.gz/datascience_jyoti-0.2.0/datascience/__init__.py
@@ -0,0 +1,7 @@
+"""
+Data Science Practicals and Utilities
+Author: Jyoti Rahate
+"""
+
+__version__ = "0.2.0"
+__author__ = "Jyoti Rahate"
---
+++ packages/datascience-jyoti/datascience_jyoti-0.2.0.tar.gz/datascience_jyoti-0.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="datascience-jyoti",
+    version="0.2.0",
+    author="Jyoti Rahate",
+    author_email="your.email@example.com",
+    description="Data Science practicals and utilities - Python scripts",
+    long_description="Data Science practicals including database connections, data processing, and analysis scripts",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
