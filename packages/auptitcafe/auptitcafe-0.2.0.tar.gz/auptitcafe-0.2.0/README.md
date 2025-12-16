f292da502925ae51dddead55d75b6f07d1cb4528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 22:06:38 +0000

    Add package auptitcafe/0.2.0/auptitcafe-0.2.0.tar.gz

---
+++ packages/auptitcafe/auptitcafe-0.2.0.tar.gz/auptitcafe-0.2.0/README.md
@@ -0,0 +1,82 @@
+![PyPI - Implementation](https://img.shields.io/pypi/implementation/auptitcafe)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/auptitcafe)
+![PyPI - Wheel](https://img.shields.io/pypi/wheel/auptitcafe)
+![PyPI - Format](https://img.shields.io/pypi/format/auptitcafe)
+![PyPI](https://img.shields.io/pypi/v/auptitcafe)
+
+# ❔ About
+
+> Finally a Python package to make **getting Au p'it café's menus a piece of (cheese) cake.😅**
+
+With this [package](https://pypi.org/project/auptitcafe/) you can:
+
+- ✔️ Get [menus](http://auptitcafe.nc/menu/)
+- ✔️ Get [takeways](http://auptitcafe.nc/a-emporter/)
+
+... and get them as list of objects or `csv`.
+
+👉 See it live on [Kaggle](https://www.kaggle.com/adriensales/au-p-tit-caf-pypi-package-intro) 👈
+
+
+# 🔖 Social networks
+
+- [Official website](http://auptitcafe.nc/)
+- [TripAdvisor](https://www.tripadvisor.com/Restaurant_Review-g294130-d1952994-Reviews-Au_P_tit_Cafe-Noumea_Grand_Terre.html)
+- [Instagram](https://www.instagram.com/auptitcafe.nc/?hl=en) 
+- [Facebook](https://www.facebook.com/auptitcafe.nc/)
+- [Google Maps](https://goo.gl/maps/4UcxegSnxMsE8qKs8)
+
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/iRSInif_Zwc/0.jpg)](https://www.youtube.com/watch?v=iRSInif_Zwc)
+
+# 🤓 Nerd resources
+
+- [🐍 `pypi`](https://pypi.org/project/auptitcafe/)
+- [😋 Au p'tit café (pypi package intro on Kaggle) 🚀](https://www.kaggle.com/adriensales/au-p-tit-caf-pypi-package-intro)
+
+# 🚀 Quickstart
+
+For the impatients, here is a quick and ready to use code snippet:
+
+```python
+# Install the package
+!pip install auptitcafe --upgrade
+
+# Make some imports
+from auptitcafe.menus import Menus
+import pandas as pd
+
+# Create the main utility instance
+menu_instance = Menus()
+
+# Dump menus as a csv file
+menus = 'menus.csv'
+menu_instance.to_csv(menus)
+
+# Load menus in a panda dataframe
+df = pd.read_csv(menus)
+# Diplay dataframe
+df
+```
+
+# Build it
+
+```shell
+poetry show --tree
+```
+
+```shell
+poetry build
+poetry install
+```
+
+## test
+
+```shell
+poetry run pytest
+```
+## Publish
+
+```shell
+poetry config pypi-token.pypi $PYPI_TOKEN
+poetry publish
+```
---
+++ packages/auptitcafe/auptitcafe-0.2.0.tar.gz/auptitcafe-0.2.0/auptitcafe/menus.py
@@ -0,0 +1,185 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+from auptitcafe.plat import Plat
+
+class Menus:
+    def __init__(self):
+        self.menus_url = "http://auptitcafe.nc/menu/"
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        }
+
+    @staticmethod
+    def extract_price(menu_item):
+        # Find the dash separator that precedes the price
+        if '-' in menu_item:
+            parts = menu_item.rsplit('-', 1)
+            if len(parts) == 2:
+                price_str = parts[1].strip()
+                # Extract digits from price string
+                price_digits = ''.join(c for c in price_str if c.isdigit())
+                if price_digits:
+                    return int(price_digits)
+        
+        # Fallback: find the last occurrence of digits followed by F or Frs
+        import re
+        match = re.search(r'(\d+(?:\s+\d+)*)\s*F(?:rs)?', menu_item)
+        if match:
+            price_str = match.group(1).replace(' ', '')
+            return int(price_str)
+        
+        return 0
+    
+    @staticmethod
+    def extract_name(menu_item):
+        # Find the dash separator that precedes the price
+        if '-' in menu_item:
+            parts = menu_item.rsplit('-', 1)
+            if len(parts) == 2:
+                name = parts[0].strip()
+                name = name.replace('"', "'")
+                return name
+        
+        # Fallback: remove price pattern from end
+        import re
+        name = re.sub(r'\s*-?\s*\d+(?:\s+\d+)*\s*F(?:rs)?$', '', menu_item)
+        name = name.strip()
+        name = name.replace('"', "'")
+        return name
+
+
+
+    def get_title(self):
+        response = requests.get(self.menus_url, headers=self.headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.find('h2', class_='elementor-heading-title elementor-size-default')
+        out = title.text.strip()
+        # remove special characters
+        caracteres_speciaux = "~!@#$%^&*()_+{}:\"<>?|\\-=[];,./"
+        for caractere in caracteres_speciaux:
+            out = out.replace(caractere, "")
+        out = out.strip()
+        return out
+
+    def get_all(self):
+        out = []
+        response = requests.get(self.menus_url, headers=self.headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.find('h2', class_='elementor-heading-title elementor-size-default')
+        #print('title : <' + title.text.strip() + '>')
+        # Plats
+
+        menus = soup.find_all('div', class_='col-sm-6 col-md-6 mb-4 selfer-contact-info')
+
+        for menu in menus:
+            name = menu.find('h5').text.strip()
+            # get the menu photo
+            if menu.find('img'):
+                image = menu.find('img')['src']
+            else:
+                image = ""
+            # Get the details fo the receipe
+            recette = menu.find('div', class_='contact_descriptions').text.strip()
+            #print('name : <' + name + '>')
+            #titre_plat = name.split("-")[0].strip()
+            #titre_plat = re.sub(r'\s+\d+\s*F$', '', titre_plat)
+            titre_plat = Menus.extract_name(name)
+
+            #print('Titre plat : <' + titre_plat + '>')
+            #print('url image : <' + image + '>')
+            #print('recette : <' + recette + '>')
+            #image_url = menu.find('img')['src']
+            #print(name + " : " + image_url)
+            #numbers = [int(s) for s in re.findall(r'\d+\.\d+|\d+', name)]
+            #print('Prix : <' + str(numbers[0]) + '>')
+            #prix = Menus.extract_price(name)
+            prix = Menus.extract_price(name)
+
+            if prix < 1500:
+                category = 'DESSERT'
+            else:
+                category = 'PLAT'
+            #print("Catgory : <" + category + '>')
+            plat = Plat(title = titre_plat,
+                        price = prix,
+                        cat = category,
+                        details = recette,
+                        img_url = image)
+            #print("================================================================")
+            out.append(plat)
+        return out
+    
+    def to_csv(self, csv_filename='menus.csv', header=True):
+        menu_instance = Menus()
+        plats = []
+        plats = menu_instance.get_all()
+        # Menus
+        with open(csv_filename, 'w') as file:
+            if header:
+                file.write('titre_plat,prix,category,recette,image_url\n')
+            for plat in plats:
+                file.write('"' + plat.title + '","' + str(plat.price) + '","' + plat.cat + '","' + plat.details + '","' + plat.img_url +  '"\n')
+    
+class Emporter:
+    def __init__(self):
+        self.menus_url = "http://auptitcafe.nc/a-emporter/"
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        }
+
+    def get_title(self):
+        response = requests.get(self.menus_url, headers=self.headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.find('h2', class_='elementor-heading-title elementor-size-default')
+        out = title.text.strip()
+        # remove special characters
+        caracteres_speciaux = "~!@#$%^&*()_+{}:\"<>?|\\-=[];,./"
+        for caractere in caracteres_speciaux:
+            out = out.replace(caractere, "")
+        out = out.strip()
+        return out
+
+
+    def get_all(self):
+        out = []
+        response = requests.get(self.menus_url, headers=self.headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Plats
+
+        menus = soup.find_all('div', class_='col-sm-6 col-md-6 mb-4 selfer-contact-info')
+
+        for menu in menus:
+            # name
+            name = menu.find('h5').text.strip()
+            
+            # get the menu photo
+            if menu.find('img'):
+                image = menu.find('img')['src']
+            else:
+                image = ""
+            # Get the details fo the receipe
+            recette = menu.find('div', class_='contact_descriptions').text.strip()
+            #print('name : <' + name + '>')
+            titre_plat = ""
+            
+            category = 'EMPORTER'
+            #print("Catgory : <" + category + '>')
+            plat = Plat(title = name,
+                        cat = category,
+                        details = recette,
+                        img_url = image,
+                        price = 0)
+            out.append(plat)
+        return out
+    
+    def to_csv(self, csv_filename='menus-emporter.csv', header=True):
+        menu_instance = Menus()
+        plats = []
+        plats = menu_instance.get_all()
+        # Menus
+        with open(csv_filename, 'w') as file:
+            if header:
+                file.write('titre_plat,category,recette,image_url\n')
+            for plat in plats:
+                file.write('"' + plat.title + '","' + plat.cat + '","' + plat.details + '","' + plat.img_url +  '"\n')
