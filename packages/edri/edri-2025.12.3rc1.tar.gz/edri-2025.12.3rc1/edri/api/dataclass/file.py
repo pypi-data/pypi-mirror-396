e1a215a0f48719f1176e40c704fb87786236cddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 13:18:15 +0000

    Add package edri/2025.12.3rc1/edri-2025.12.3rc1.tar.gz

---
+++ packages/edri/edri-2025.12.3rc1.tar.gz/edri-2025.12.3rc1/edri/api/dataclass/file.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from edri.utility.shared_memory_pipe import SharedMemoryPipe
+
+
+@dataclass
+class File:
+    file_name: str | None
+    mime_type: str
+    path: Path | SharedMemoryPipe
