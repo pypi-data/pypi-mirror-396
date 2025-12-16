3c61ff20d22a523d0fab2fe26987098dbda43db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 23:07:00 +0000

    Add package scrapscraper/0.2.0/scrapscraper-0.2.0-py3-none-any.whl

---
+++ packages/scrapscraper/scrapscraper-0.2.0-py3-none-any.whl/scrapscraper/__init__.py
@@ -0,0 +1,12 @@
+
+__version__ = "0.1.0"
+
+from .scrapscraper import (
+    scrape_website,
+    crawl_with_depth,
+    is_valid_url,
+    scrapedepth,
+    scrapelinks,
+    scrapetext,
+    scrapetitle
+)
