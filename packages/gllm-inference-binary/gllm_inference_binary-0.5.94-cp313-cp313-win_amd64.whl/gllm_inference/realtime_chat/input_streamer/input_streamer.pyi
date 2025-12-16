987465b8f98e25a3a07bced1ab72563844bb1a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 12:06:30 +0000

    Add package gllm-inference-binary/0.5.94/gllm_inference_binary-0.5.94-cp313-cp313-win_amd64.whl

---
+++ packages/gllm-inference-binary/gllm_inference_binary-0.5.94-cp313-cp313-win_amd64.whl/gllm_inference/realtime_chat/input_streamer/input_streamer.pyi
@@ -0,0 +1,36 @@
+import abc
+import asyncio
+from abc import ABC, abstractmethod
+from pydantic import BaseModel as BaseModel
+
+class BaseInputStreamer(ABC, metaclass=abc.ABCMeta):
+    """[BETA] A base class for input streamers.
+
+    Attributes:
+        state (BaseModel | None): The state of the input streamer.
+        input_queue (asyncio.Queue | None): The queue to put the input events.
+    """
+    state: BaseModel | None
+    input_queue: asyncio.Queue | None
+    async def initialize(self, state: BaseModel, input_queue: asyncio.Queue) -> None:
+        """Initializes the input streamer.
+
+        Args:
+            input_queue (asyncio.Queue): The queue to put the input events.
+            state (BaseModel): The state of the input streamer.
+        """
+    @abstractmethod
+    async def stream_input(self) -> None:
+        """Streams the input from a certain source.
+
+        This method must be implemented by subclasses to define the logic for streaming the input.
+
+        Raises:
+            NotImplementedError: If the method is not implemented in a subclass.
+        """
+    async def close(self) -> None:
+        """Closes the input streamer.
+
+        This method is used to close the input streamer.
+        It is used to clean up the input streamer.
+        """
