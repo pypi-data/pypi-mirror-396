37b89a7c0b293536e6e9aa005da17c57b3118566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 09:08:15 +0000

    Add package apyds-bnf/0.0.6/apyds_bnf-0.0.6-py3-none-any.whl

---
+++ packages/apyds-bnf/apyds_bnf-0.0.6-py3-none-any.whl/apyds_bnf/__init__.py
@@ -0,0 +1,88 @@
+__all__ = ["parse", "unparse"]
+
+from antlr4 import InputStream, CommonTokenStream
+from .DspLexer import DspLexer
+from .DspParser import DspParser
+from .DspVisitor import DspVisitor
+from .DsLexer import DsLexer
+from .DsParser import DsParser
+from .DsVisitor import DsVisitor
+
+
+class ParseVisitor(DspVisitor):
+    def visitRule_pool(self, ctx):
+        return "\n\n".join(self.visit(r) for r in ctx.rule_())
+
+    def visitRule(self, ctx):
+        result = [self.visit(t) for t in ctx.term()]
+        if len(result) == 1:
+            return f"----\n{result[0]}"
+        else:
+            conclusion = result.pop()
+            length = max(len(premise) for premise in result)
+            result.append("-" * max(length, 4))
+            result.append(conclusion)
+            return "\n".join(result)
+
+    def visitSymbol(self, ctx):
+        return ctx.SYMBOL().getText()
+
+    def visitParentheses(self, ctx):
+        return self.visit(ctx.term())
+
+    def visitSubscript(self, ctx):
+        return f"(subscript {' '.join(self.visit(t) for t in ctx.term())})"
+
+    def visitFunction(self, ctx):
+        return f"(function {' '.join(self.visit(t) for t in ctx.term())})"
+
+    def visitUnary(self, ctx):
+        return f"(unary {ctx.getChild(0).getText()} {self.visit(ctx.term())})"
+
+    def visitBinary(self, ctx):
+        return f"(binary {ctx.getChild(1).getText()} {self.visit(ctx.term(0))} {self.visit(ctx.term(1))})"
+
+
+class UnparseVisitor(DsVisitor):
+    def visitRule_pool(self, ctx):
+        return "\n".join(self.visit(r) for r in ctx.rule_())
+
+    def visitRule(self, ctx):
+        result = [self.visit(t) for t in ctx.term()]
+        conclusion = result.pop()
+        return ", ".join(result) + " -> " + conclusion
+
+    def visitSymbol(self, ctx):
+        return ctx.SYMBOL().getText()
+
+    def visitSubscript(self, ctx):
+        return f"{self.visit(ctx.term(0))}[{', '.join(self.visit(t) for t in ctx.term()[1:])}]"
+
+    def visitFunction(self, ctx):
+        return f"{self.visit(ctx.term(0))}({', '.join(self.visit(t) for t in ctx.term()[1:])})"
+
+    def visitUnary(self, ctx):
+        return f"({ctx.getChild(1).getText()} {self.visit(ctx.term())})"
+
+    def visitBinary(self, ctx):
+        return f"({self.visit(ctx.term(0))} {ctx.getChild(1).getText()} {self.visit(ctx.term(1))})"
+
+
+def parse(input: str) -> str:
+    chars = InputStream(input)
+    lexer = DspLexer(chars)
+    tokens = CommonTokenStream(lexer)
+    parser = DspParser(tokens)
+    tree = parser.rule_pool()
+    visitor = ParseVisitor()
+    return visitor.visit(tree)
+
+
+def unparse(input: str) -> str:
+    chars = InputStream(input)
+    lexer = DsLexer(chars)
+    tokens = CommonTokenStream(lexer)
+    parser = DsParser(tokens)
+    tree = parser.rule_pool()
+    visitor = UnparseVisitor()
+    return visitor.visit(tree)
