615d35a0e7643a485016d44cb292424bb7fb7de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 15:12:21 +0000

    Add package pyvoa/0.4.0/pyvoa-0.4.0.tar.gz

---
+++ packages/pyvoa/pyvoa-0.4.0.tar.gz/pyvoa-0.4.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyvoa",  
+    version="0.4.0",  
+    author="pyvoa.org",
+    author_email="contact@pyvoa.org",
+    description="Python virus open analysis. See more on pyvoa.org",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pyvoa/pyvoa",  # Link to github deposit
+    packages=find_packages(), 
+    package_data={'pyvoa' :['data/*.json']},
+    install_requires=[
+        "pandas",
+        "geopandas",
+        "shapely",
+        "bs4",
+        "numpy",
+        "pycountry",
+        "pycountry_convert",
+        "requests",
+        "unidecode",
+        "lxml",
+        "IPython",
+        "contextily",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",  # Version minimale de Python
+)
