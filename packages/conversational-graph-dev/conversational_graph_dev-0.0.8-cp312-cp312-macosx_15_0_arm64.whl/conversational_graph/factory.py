c530a79bcc67d1746f00e86c47d6044ae1b907b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 11:05:14 +0000

    Add package conversational-graph-dev/0.0.8/conversational_graph_dev-0.0.8-cp312-cp312-macosx_15_0_arm64.whl

---
+++ packages/conversational-graph-dev/conversational_graph_dev-0.0.8-cp312-cp312-macosx_15_0_arm64.whl/conversational_graph/factory.py
@@ -0,0 +1,48 @@
+from abc import ABC
+from typing import Literal, Optional
+from ._internal.factory import ConversationalGraphEngine
+from ._internal.tools import PreDefinedToolEngine,DataModel,AuthDetails, HttpReqTool
+
+
+class ConversationalDataModel(ABC):
+    """
+    Public interface for Data Models for the Conversational Graph.
+    """
+    
+    def __new__(cls, **data):
+        return DataModel(**data)
+
+
+class ConversationalGraph(ABC):
+    """
+    Public interface for the Conversational Graph.
+    """
+    
+    def __new__(cls, name: str, DataModel: ConversationalDataModel = None, off_topic_threshold: int = 5, graph_type: Literal["STRICT"] = "STRICT"):
+        return ConversationalGraphEngine(name, DataModel, off_topic_threshold, graph_type)
+
+
+class PreDefinedTool(ABC):
+    """
+    Public interface for PreDefined Tools in the Conversational Graph.
+    """
+    
+    def __new__(cls):
+        return PreDefinedToolEngine()
+
+class ToolAuthDetails(ABC):
+    """
+    Public interface for Auth Details for HTTP Tools in the Conversational Graph.
+    """
+    
+    def __new__(cls, enabled: bool, auth_type: Optional[Literal["bearer","basic"]] = None, token: Optional[str] = None):
+        return AuthDetails(enabled=enabled, auth_type=auth_type, token=token)
+
+
+class HttpToolRequest(ABC):
+    """
+    Public interface for HTTP Tools Request in the Conversational Graph.
+    """
+    
+    def __new__(cls, name:str, description:str, url:str, method:Literal["GET","POST"] = "GET", auth:ToolAuthDetails = None, params:dict = None,payload:dict = None):
+        return HttpReqTool(name=name, description=description, url=url, method=method, auth=auth, params=params, payload=payload)
