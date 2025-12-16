e30a9bc90f2e0b213af2c940e5b055819262a503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 14:05:54 +0000

    Add package pingshui-rhyme/0.19/pingshui_rhyme-0.19.tar.gz

---
+++ packages/pingshui-rhyme/pingshui_rhyme-0.19.tar.gz/pingshui_rhyme-0.19/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pingshui-rhyme',
+    version='0.19',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    description='A Python package for classifying Chinese characters phonologically based on the Middle Chinese Pingshui rhyme scheme',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='rbnyng',
+    author_email='',
+    url='https://github.com/rbnyng/pingshui_rhyme',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+    ],
+    package_data={
+        'pingshui_rhyme': ['data/*.json'],
+    },
+)
