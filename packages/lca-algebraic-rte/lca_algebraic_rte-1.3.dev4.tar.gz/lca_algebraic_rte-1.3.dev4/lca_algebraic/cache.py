73e88fa7c95dfa60ff920cd71819ca7d76303cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 04:16:19 +0000

    Add package lca-algebraic-rte/1.3.dev4/lca_algebraic_rte-1.3.dev4.tar.gz

---
+++ packages/lca-algebraic-rte/lca_algebraic_rte-1.3.dev4.tar.gz/lca_algebraic_rte-1.3.dev4/lca_algebraic/cache.py
@@ -0,0 +1,114 @@
+import os
+from os import path
+from pickle import UnpicklingError
+
+import brightway2 as bw
+from cloudpickle import dump, load
+from sympy.core.function import UndefinedFunction
+from .log import logger
+from .settings import Settings
+
+LCIA_CACHE = "lcia"
+EXPR_CACHE = "expr"
+
+
+# Overide the behaviour for pickling sympy.UndefineFunction
+"""
+class MyCloudPickler(cloudpickle.CloudPickler):
+    from sympy.core.function import UndefinedFunction
+
+    def reducer_override(self, obj):
+        # FIXME: maybe too gready, we may check if obj is an instance of
+        #        registered functions instead.
+        if obj.__class__ is UndefinedFunction:
+            return type, (obj.__name__, obj.__bases__, dict(obj.__dict__))
+        return NotImplemented
+"""
+
+
+def last_db_update():
+    """Get the last update of current database project"""
+    filename = path.join(bw.projects.dir, "lci", "databases.db")
+
+    return path.getmtime(filename)
+
+
+def disable_cache():
+    Settings.cache_enabled = False
+
+
+class _Caches:
+    "Singleton instance holding caches"
+    caches = dict()
+
+
+class _CacheDict:
+    """A smart cache that get cleared whenever database changes, and dumped to file whenever we exit from it"""
+
+    def __init__(self, name):
+        self.name = name
+
+        # No cache ? => LOCAL DICT
+        if not Settings.cache_enabled:
+            self.data = dict()
+            return
+
+        filename = _CacheDict.filename(self.name)
+        if path.exists(filename):
+            if last_db_update() > path.getmtime(filename):
+                logger.info(f"Db changed recently, clearing cache {self.name}")
+
+                # Reset cache on disk and locally
+                os.remove(filename)
+                _Caches.caches[name] = dict()
+
+            else:
+                # Cache not already loaded in memory ?
+                if name not in _Caches.caches:
+                    # Load cache from disk
+                    with open(filename, "rb") as pickleFile:
+                        try:
+                            _Caches.caches[name] = load(pickleFile)
+                        except UnpicklingError as e:
+                            logger.warn(f"Error while unpickling cache {filename}. Ignoring/overwriting it")
+
+        else:
+            # No file yet, init local cache
+            _Caches.caches[name] = dict()
+
+        # Point to local cache
+        self.data = _Caches.caches[name]
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Save data on exit
+        if Settings.cache_enabled and self.data:
+            with open(_CacheDict.filename(self.name), "wb") as pickleFile:
+                self.data = dump(self.data, pickleFile)
+
+    @classmethod
+    def filename(cls, name):
+        return path.join(bw.projects.dir, f"lca_algebraic_cache-{name}.pickle")
+
+
+class LCIACache(_CacheDict):
+    def __init__(self):
+        _CacheDict.__init__(self, LCIA_CACHE)
+
+
+class ExprCache(_CacheDict):
+    def __init__(self):
+        _CacheDict.__init__(self, EXPR_CACHE)
+
+
+def clear_caches(local=True, disk=True):
+    if local:
+        _Caches.caches = dict()
+
+    if disk:
+        for cache_name in [LCIA_CACHE, EXPR_CACHE]:
+            filename = _CacheDict.filename(cache_name)
+            if path.exists(filename):
+                os.remove(filename)
