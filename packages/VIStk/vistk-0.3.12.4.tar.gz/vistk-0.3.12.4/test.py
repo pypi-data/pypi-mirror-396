e049421a3a51749ab17ebfdab279e85246ea56c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 21:05:31 +0000

    Add package VIStk/0.3.12.4/vistk-0.3.12.4.tar.gz

---
+++ packages/VIStk/vistk-0.3.12.4.tar.gz/vistk-0.3.12.4/test.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from VIS.Objects import *
+from VIS.Widgets import *
+import time
+
+root = Root()
+root.WindowGeometry.setGeometry(width=40,height=40,align="center",size_style="screen_relative")
+
+def subWindow():
+    subroot = QuestionWindow(parent=root,
+                             question=["Some info...",
+                                       "Additional information given",
+                                       "Do you want to continue"],
+                             answer = "xu",
+                             ycommand=print,
+                             droplist=["test",
+                                       "aagin"]
+                            )
+
+vb_test = Button(root, text="Open Submenu", command=subWindow)
+vb_test.grid(row=1,column=1,sticky=(N, S, E, W))
+
+root.mainloop()
