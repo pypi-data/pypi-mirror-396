b85e5134b2e2c6d57487184f5bf8734660dc12a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 08:05:13 +0000

    Add package jsonql-db/0.1.5/jsonql_db-0.1.5.tar.gz

---
+++ packages/jsonql-db/jsonql_db-0.1.5.tar.gz/jsonql_db-0.1.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+setup(
+    name="jsonql-db",
+    version="0.1.5",
+    description="A lightweight, file-based JSON database with SQL-like interface",
+    long_description=open("README-pypi.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/takouzlo/jsonql-db",
+    author="FBF",
+    author_email="f.bfalik@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.7",
+    extras_require={
+        "browser": ["flet>=0.25.0"],
+    },
+    keywords="json database sqlite alternative lightweight file-based",
+)
