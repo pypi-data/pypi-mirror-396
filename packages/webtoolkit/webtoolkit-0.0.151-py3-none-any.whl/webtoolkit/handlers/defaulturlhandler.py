6219a319567e48cd6b35e30e818d3b0102ec4c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 13:17:43 +0000

    Add package webtoolkit/0.0.151/webtoolkit-0.0.151-py3-none-any.whl

---
+++ packages/webtoolkit/webtoolkit-0.0.151-py3-none-any.whl/webtoolkit/handlers/defaulturlhandler.py
@@ -0,0 +1,222 @@
+"""
+Default url handler.
+"""
+import copy
+from collections import OrderedDict
+from concurrent.futures import ThreadPoolExecutor
+
+from ..utils.dateutils import DateUtils
+from ..pages import DefaultContentPage
+from ..request import PageRequestObject, copy_request
+from ..webconfig import WebLogger
+from .handlerhttppage import HttpPageHandler
+
+
+class DefaultUrlHandler(HttpPageHandler):
+    """
+    Default URL handler
+    """
+
+    def __init__(self, url=None, contents=None, request=None, url_builder=None):
+        self.code = None
+        super().__init__(url=url, request=request, url_builder=url_builder)
+        self.code = self.input2code(self.url)
+
+    def get_page_url(self, url, crawler_name=None):
+        """
+        Obtains a custom, another URL using a crawler
+        Necessary for more advanced handlers that in order to provide necessary data
+        check multiple source of data.
+        """
+        if not url:
+            WebLogger.error("Default Url Handler - no url")
+            return
+
+        if self.request:
+            request = copy_request(self.request)
+        else:
+            request = PageRequestObject(url)
+
+        request.url = url
+        request.handler_name = "HttpPageHandler"
+        #request.handler_type = HttpPageHandler
+        if crawler_name:
+            request.crawler_name = crawler_name
+
+        if self.url_builder:
+            url = self.url_builder(
+                url=url, request=request, url_builder=self.url_builder
+            )
+            return url
+
+        WebLogger.error("Default Url Handler - no builder")
+
+    def build_default_url(self, url, crawler_name=None):
+        """
+        TODO reneme get_page_url to build_http_url
+        """
+        if not url:
+            return
+
+        if self.request:
+            request = copy.copy(self.request)
+        else:
+            request = PageRequestObject(url)
+
+        request.url = url
+
+        request.crawler_type = None
+        if crawler_name:
+            request.crawler_name = crawler_name
+
+        if self.url_builder:
+            url = self.url_builder(
+                url=url, request=request, url_builder=self.url_builder
+            )
+            return url
+
+    def get_code(self):
+        return self.code
+
+
+class DefaultChannelHandler(DefaultUrlHandler):
+    """
+    Default handler for channels
+    """
+
+    def get_channel_name(self):
+        return self.get_title()
+
+    def get_channel_url(self):
+        if self.code:
+            return self.code2url(self.code)
+
+
+class DefaultCompoundChannelHandler(DefaultChannelHandler):
+    """
+    Default URL handler which is capable of obtaining data from many network sources automatically.
+    """
+    def __init__(self, url=None, contents=None, request=None, url_builder=None):
+        self.channel_sources_urls = OrderedDict()
+        super().__init__(url=url, request=request, url_builder=url_builder)
+
+    def get_channel_sources(self):
+        sources = []
+
+        feeds = self.get_feeds()
+        if feeds and len(feeds) > 0:
+            feed = feeds[0]
+            if feed:
+                sources.append(feed)
+
+        channel_url = self.get_channel_url()
+        if channel_url:
+            sources.append(channel_url)
+
+        if len(sources) == 0:
+            sources.append(self.url)
+
+        return sources
+
+    def get_contents(self):
+        """
+        We obtain information about channel.
+        We cannot use HTML page to obtain thumbnail - as web page asks to log in to view this
+        """
+        if self.contents:
+            return self.contents
+
+        if self.get_response():
+            return self.get_response().get_text()
+
+    def get_response(self):
+        if self.response:
+            return self.response
+
+        handles = []
+        with ThreadPoolExecutor() as executor:
+            for channel_source in self.get_channel_sources():
+                handles.append(executor.submit(self.get_response_source, channel_source))
+
+            for handle in handles:
+                url = handle.result()
+
+                if self.response is None:
+                    self.response = url.get_response()
+
+                response = url.get_response()
+
+                self.channel_sources_urls[url.url] = url
+
+        return self.response
+
+    def get_streams(self):
+        for page_url in self.channel_sources_urls.values():
+            self.streams[page_url.url] = page_url.get_response()
+
+        return self.streams
+
+    def get_response_source(self, page_url):
+        if page_url in self.channel_sources_urls.values():
+            return self.channel_sources_urls[page_url]
+
+        url = self.get_page_url(page_url)
+        if url:
+            url.get_response()
+
+        return url
+
+    def get_entries(self):
+        for url in self.channel_sources_urls.values():
+            entries = url.get_entries()
+            if entries and len(list(entries)) > 0:
+                return entries
+        return []
+
+    def get_title(self):
+        for url in self.channel_sources_urls.values():
+            title = url.get_title()
+            if title:
+                return title
+
+    def get_description(self):
+        for url in self.channel_sources_urls.values():
+            description = url.get_description()
+            if description:
+                return description
+
+    def get_language(self):
+        for url in self.channel_sources_urls.values():
+            language = url.get_language()
+            if language:
+                return language
+
+    def get_thumbnail(self):
+        for url in self.channel_sources_urls.values():
+            thumbnail = url.get_thumbnail()
+            if thumbnail:
+                return thumbnail
+
+    def get_author(self):
+        for url in self.channel_sources_urls.values():
+            author = url.get_author()
+            if author:
+                return author
+
+    def get_album(self):
+        for url in self.channel_sources_urls.values():
+            album = url.get_author()
+            if album:
+                return album
+
+    def get_tags(self):
+        for url in self.channel_sources_urls.values():
+            tags = url.get_tags()
+            if tags:
+                return tags
+
+    def get_date_published(self):
+        for url in self.channel_sources_urls.values():
+            date_published = url.get_date_published()
+            if date_published:
+                return date_published
---
+++ packages/webtoolkit/webtoolkit-0.0.151-py3-none-any.whl/webtoolkit/handlers/handlerchannelyoutube.py
@@ -0,0 +1,201 @@
+import traceback
+from concurrent.futures import ThreadPoolExecutor
+
+from ..response import PageResponseObject
+from ..urllocation import UrlLocation
+from ..pages import RssPage
+from ..webtools import WebLogger
+from .defaulturlhandler import DefaultCompoundChannelHandler
+from .handlerhttppage import HttpPageHandler
+
+
+class YouTubeChannelHandler(DefaultCompoundChannelHandler):
+    """
+    Natively since we inherit RssPage, the contents should be RssPage
+    """
+
+    def __init__(self, url=None, request=None, url_builder=None):
+        self.social_data = {}
+        self.user_name = None
+
+        super().__init__(
+            url=url,
+            request=request,
+            url_builder=url_builder,
+        )
+
+        if not self.is_handled_by():
+            return
+
+        if request:
+            request.cookies = {}
+            request.cookies["CONSENT"] = "YES+cb.20210328-17-p0.en+F+678"
+
+    def is_handled_by(self):
+        if not self.url:
+            return False
+
+        short_url = UrlLocation(self.url).get_protocolless()
+
+        if (
+            short_url.startswith("www.youtube.com/channel")
+            or short_url.startswith("youtube.com/channel")
+            or short_url.startswith("m.youtube.com/channel")
+            or short_url.startswith("www.youtube.com/@")
+            or short_url.startswith("youtube.com/@")
+            or short_url.startswith("m.youtube.com/@")
+            or short_url.startswith("www.youtube.com/user")
+            or short_url.startswith("youtube.com/user")
+            or short_url.startswith("m.youtube.com/user")
+        ):
+            return True
+        if (
+            short_url.startswith("www.youtube.com/feeds")
+            or short_url.startswith("youtube.com/feeds")
+            or short_url.startswith("m.youtube.com/feeds")
+        ):
+            return True
+
+    def is_channel_name(self, url):
+        if not url:
+            return False
+
+        short_url = UrlLocation(url).get_protocolless()
+
+        if (
+            short_url.startswith("www.youtube.com/@")
+            or short_url.startswith("youtube.com/@")
+            or short_url.startswith("m.youtube.com/@")
+            or short_url.startswith("www.youtube.com/user")
+            or short_url.startswith("youtube.com/user")
+            or short_url.startswith("m.youtube.com/user")
+        ):
+            return True
+
+    def input2url(self, item):
+        code = self.input2code(item)
+        return self.code2url(code)
+
+    def code2url(self, code):
+        if code:
+            return "https://www.youtube.com/channel/{}".format(code)
+
+    def code2feed(self, code):
+        return "https://www.youtube.com/feeds/videos.xml?channel_id={}".format(code)
+
+    def get_feeds(self):
+        if not self.code:
+            return []
+
+        feeds = super().get_feeds()
+        if self.code:
+            feeds.append(
+                "https://www.youtube.com/feeds/videos.xml?channel_id={}".format(
+                    self.code
+                )
+            )
+
+        return feeds
+
+    def input2code(self, url):
+        wh = url.find("youtube.com")
+        if wh == -1:
+            return None
+
+        if self.is_channel_name(url):
+            return
+        if url.find("/channel/") >= 0:
+            return self.input2code_channel(url)
+        if url.find("/feeds/") >= 0:
+            return self.input2code_feeds(url)
+
+    def input2code_handle(self, url):
+        page_handler = self.get_page_url(url)
+        if not page_handler:
+            return
+
+        response = page_handler.get_response()
+        if not response:
+            return
+
+        props = page_handler.get_properties()
+        if not props:
+            return
+
+        if "feeds" in props:
+            if len(props["feeds"]) > 0:
+                feed = props["feeds"][0]
+                return self.input2code(feed)
+
+    def intpu2handlename(self, url):
+        wh = url.find("?")
+        if wh >= 0:
+            url = url[:wh]
+
+        wh1 = url.find("youtube.com/user")
+        if wh1 >= 0:
+            start = wh1 + len("youtube.com/user") + 1
+            wh2 = url.find("/", start+1)
+            if wh2 == -1:
+                return url[start-1:]
+            else:
+                return url[start-1:wh2]
+
+        wh1 = url.find("youtube.com/@")
+        if wh1 >= 0:
+            start = wh1 + len("youtube.com/@") + 1
+            wh2 = url.find("/", start + 1)
+            if wh2 == -1:
+                return url[start-1:]
+            else:
+                return url[start-1:wh2]
+
+    def input2code_channel(self, url):
+        wh = url.rfind("/")
+        return url[wh + 1 :]
+
+    def input2code_feeds(self, url):
+        wh = url.find("=")
+        if wh >= 0:
+            return url[wh + 1 :]
+
+    def get_channel_code(self):
+        return self.code
+
+    def get_channel_url(self):
+        if self.code:
+            return self.code2url(self.code)
+
+    def get_canonical_url(self):
+        if self.url.find("feeds") >= 0:
+            return self.url
+        else:
+            return self.get_channel_url()
+
+    def get_response(self):
+        if self.request:
+            if not self.request.cookies or len(self.request.cookies) == 0:
+                self.request.cookies = {}
+                self.request.cookies["CONSENT"] = "YES+cb.20210328-17-p0.en+F+678"
+
+        if self.code:
+            return super().get_response()
+        elif self.url:
+            url = self.get_page_url(self.url)
+            if not url.get_response():
+                return
+
+            for feed in url.get_feeds():
+                handler = YouTubeChannelHandler(url=feed)
+                if handler.is_handled_by():
+                    self.code = handler.get_code()
+
+                    return super().get_response()
+
+    def get_language(self):
+        """
+        Social media platforms host very different videos in different locations
+        Currently I have no means of identifying og:locale, or lang, it is commonly
+        locale of platform, not contents
+        """
+        return None
