001f6df14350607c25f33951859ec8a92f9cd77f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 05:08:59 +0000

    Add package pymob/0.5.26/pymob-0.5.26-py3-none-any.whl

---
+++ packages/pymob/pymob-0.5.26-py3-none-any.whl/pymob/__init__.py
@@ -0,0 +1,11 @@
+from . import inference
+from . import sim
+from . import utils
+from . import solvers
+from . import examples
+
+__version__ = "0.5.26"
+
+
+from .simulation import SimulationBase
+from .sim.config import Config
