bd8680ee02444c49937dd58787d7c9d59f7ecef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:31:38 +0000

    Add package xcd/2.0.4/xcd-2.0.4.tar.gz

---
+++ packages/xcd/xcd-2.0.4.tar.gz/xcd-2.0.4/src/xcd/django/forms.py
@@ -0,0 +1,26 @@
+from email.policy import default
+from django import forms
+from xcd.core import XCD_kits
+
+class ReportForm(forms.Form):
+    txt_file = forms.FileField(label="Vyber soubor s profily DNA (GM-idx)", required=True)
+    csv_files = forms.FileField(label="Vyber kvantifikační soubor(y)", required=False, widget=forms.ClearableFileInput())
+
+    kit_autosomal = forms.ChoiceField(
+        label="Pořadí lokusů podle kitu",
+        choices=sorted([(k, k) for k, t in XCD_kits.KIT_TYPE.items() if t.upper() == "AUTOSOMAL"]),
+        required=False
+    )
+
+    kit_y = forms.ChoiceField(
+        label="Pořadí lokusů podle kitu (Y)",
+        choices=sorted([(k, k) for k, t in XCD_kits.KIT_TYPE.items() if t.upper() == "Y"]),
+        required=False
+    )
+
+    mode_filter = forms.ChoiceField(label="Typ odečtu", choices=[("VŠE", "Vše"), ("SOUPISKA", "Soupiska"),("PŘÍPAD", "Případ"), ("EXPERT", "Expert")], required=True)
+    filter_value = forms.CharField(label="Zvol položku", required=False)
+
+    highlight_major = forms.BooleanField(label="Zvýraznit majoritní složku", required=False)
+    slope_effect = forms.BooleanField(label="Ski slope effect", required=False)
+
