081d8c8faea81e35eac8553ff5185cd03dec9d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 12:07:12 +0000

    Add package bdgs/2.0.0/bdgs-2.0.0.tar.gz

---
+++ packages/bdgs/bdgs-2.0.0.tar.gz/bdgs-2.0.0/bdgs/algorithms/oyedotun_khashman/oyedotun_khashman_payload.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+from bdgs.models.image_payload import ImagePayload
+
+
+class OyedotunKhashmanPayload(ImagePayload):
+    def __init__(self, image: np.ndarray, coords: list[tuple[int, int]]):
+        super().__init__(image)
+        self.coords = coords
---
+++ packages/bdgs/bdgs-2.0.0.tar.gz/bdgs-2.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bdgs",
+    version="2.0.0",
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python~=4.11.0.86',
+        'numpy~=2.1.3',
+        'tensorflow-cpu~=2.19.0',
+        'scikit-learn~=1.6.1',
+        "keras~=3.9.2",
+        'scikit-image~=0.25.2',
+        'silence-tensorflow~=1.2.3'
+    ],
+    include_package_data=True,
+    package_data={
+        'bdgs_trained_models': ['*'],
+    },
+    py_modules=['definitions'],
+    author="marcinbator",
+    author_email="marcinbator.ofc@gmail.com",
+    description="Static gestures recognition tool",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
