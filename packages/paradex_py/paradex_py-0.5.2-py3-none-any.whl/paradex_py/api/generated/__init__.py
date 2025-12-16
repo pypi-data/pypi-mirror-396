d56e41dc9d5046515dca114e73ffb0bf9c9e1f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 14:06:19 +0000

    Add package paradex_py/0.5.2/paradex_py-0.5.2-py3-none-any.whl

---
+++ packages/paradex_py/paradex_py-0.5.2-py3-none-any.whl/paradex_py/api/generated/__init__.py
@@ -0,0 +1,11 @@
+# Generated from Paradex API spec version 1.101.8
+
+"""Generated API models from Paradex OpenAPI spec v1.101.8."""
+
+# Import all generated models
+from .requests import *
+from .responses import *
+
+__all__: list[str] = [
+    # Re-export everything from sub-modules
+]
