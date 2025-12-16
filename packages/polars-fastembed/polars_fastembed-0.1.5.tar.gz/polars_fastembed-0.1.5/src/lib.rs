ec471fe508352c2ccc81e47afb06b250716172ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 21:05:45 +0000

    Add package polars-fastembed/0.1.5/polars_fastembed-0.1.5.tar.gz

---
+++ packages/polars-fastembed/polars_fastembed-0.1.5.tar.gz/polars_fastembed-0.1.5/src/lib.rs
@@ -0,0 +1,48 @@
+// See discussion 162
+// https://github.com/PyO3/maturin-action/discussions/162#discussioncomment-7978369
+#[cfg(feature = "openssl-vendored")]
+use openssl_probe;
+
+use pyo3::prelude::*;
+use pyo3_polars::PolarsAllocator;
+
+mod expressions;
+mod model_suggestions;
+mod registry;
+
+// See discussion 162
+#[cfg(feature = "openssl-vendored")]
+fn probe_ssl_certs() {
+    openssl_probe::init_ssl_cert_env_vars();
+}
+
+#[cfg(feature = "ort-dynamic")]
+fn init_ort() {
+    // TODO: actually initialise it (must first build ONNX runtime from source)
+}
+
+// --- Start of no-op zone ---
+
+#[cfg(not(feature = "openssl-vendored"))]
+fn probe_ssl_certs() {}
+
+#[cfg(not(feature = "ort-dynamic"))]
+fn init_ort() {}
+
+// --- End of no-op zone ---
+
+#[pymodule]
+fn _polars_fastembed(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    // See discussion 162
+    probe_ssl_certs();
+
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    m.add_function(wrap_pyfunction!(registry::register_model, m)?)?;
+    m.add_function(wrap_pyfunction!(registry::clear_registry, m)?)?;
+    m.add_function(wrap_pyfunction!(registry::list_models, m)?)?;
+
+    Ok(())
+}
+
+#[global_allocator]
+static ALLOC: PolarsAllocator = PolarsAllocator::new();
