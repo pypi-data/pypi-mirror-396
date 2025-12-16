d9fd06ef9720b0febbee782417f6376f6ca70832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 06:05:20 +0000

    Add package ternimator/0.0.7/ternimator-0.0.7-py3-none-any.whl

---
+++ packages/ternimator/ternimator-0.0.7-py3-none-any.whl/ternimator/__init__.py
@@ -0,0 +1,21 @@
+from . import animators
+from .utils import (
+    Animation,
+    AnimParams,
+    Lines,
+    animate,
+    animate_iter,
+    animated_lines,
+    consume,
+)
+
+__all__ = [
+    "AnimParams",
+    "Animation",
+    "Lines",
+    "animate",
+    "animate_iter",
+    "animated_lines",
+    "animators",
+    "consume",
+]
