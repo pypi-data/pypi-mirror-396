770397d559e8582e52193a4230fd1d253f75f7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 18:07:15 +0000

    Add package sphinx-automodapi/0.22.0/sphinx_automodapi-0.22.0-py3-none-any.whl

---
+++ packages/sphinx-automodapi/sphinx_automodapi-0.22.0-py3-none-any.whl/sphinx_automodapi/tests/example_module/attribute_class.py
@@ -0,0 +1,12 @@
+class ClassWithAttribute:
+    """A class with an attribute."""
+    my_attribute = 1
+
+    def my_method(self):
+        """A method."""
+        pass
+
+    @property
+    def my_property(self):
+        """A property."""
+        return 1
