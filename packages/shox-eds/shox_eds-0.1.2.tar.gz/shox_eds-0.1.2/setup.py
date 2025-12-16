9f229b85273083ad24ca93cb6a9a743f8f15327b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 15:10:38 +0000

    Add package shox-eds/0.1.2/shox_eds-0.1.2.tar.gz

---
+++ packages/shox-eds/shox_eds-0.1.2.tar.gz/shox_eds-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='shox-eds',
+    version='0.1.2',
+    packages=[],
+    description='Arithmetic mathematics library',
+    requires=[],
+    python_requires='>=3.11',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+
+)
