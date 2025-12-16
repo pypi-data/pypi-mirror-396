585cc15f34f8674d56c8b95304705d1db0f69bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 20:05:54 +0000

    Add package genai-pygame/0.1.1/genai_pygame-0.1.1.tar.gz

---
+++ packages/genai-pygame/genai_pygame-0.1.1.tar.gz/genai_pygame-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="genai_pygame",
+    description='A helpful PyGame tool for intergrating Generative AI models into game projects.',
+    author='Alex Khon',
+    author_email='alexoverkode@hotmail.com',
+    version="0.1.1",
+    packages=['genai_pygame'],
+    install_requires=[
+        'transformers'
+    ],
+)
