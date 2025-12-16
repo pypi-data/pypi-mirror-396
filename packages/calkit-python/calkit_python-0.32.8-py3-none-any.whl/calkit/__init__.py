612a86c62ccd90cd6c33a39ce03d574d8c03c1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 16:05:56 +0000

    Add package calkit-python/0.32.8/calkit_python-0.32.8-py3-none-any.whl

---
+++ packages/calkit-python/calkit_python-0.32.8-py3-none-any.whl/calkit/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "0.32.8"
+
+from .core import *  # noqa: F403, I001
+from . import git  # noqa: F401
+from . import dvc  # noqa: F401
+from . import cloud  # noqa: F401
+from . import jupyter  # noqa: F401
+from . import config  # noqa: F401
+from . import models  # noqa: F401
+from . import office  # noqa: F401
+from . import templates  # noqa: F401
+from . import conda  # noqa: F401
+from . import calc  # noqa: F401
+from . import check  # noqa: F401
+from . import github  # noqa: F401
+from . import invenio  # noqa: F401
+from . import releases  # noqa: F401
+from . import licenses  # noqa: F401
+from . import overleaf  # noqa: F401
+from .notebooks import declare_notebook  # noqa: F401
---
+++ packages/calkit-python/calkit_python-0.32.8-py3-none-any.whl/calkit/tests/test_dvc.py
@@ -0,0 +1,37 @@
+"""Tests for the ``dvc`` module."""
+
+import subprocess
+
+import calkit
+
+
+def test_get_remotes(tmp_dir):
+    subprocess.call(["git", "init"])
+    assert not calkit.dvc.get_remotes()
+    subprocess.call(["dvc", "init"])
+    assert not calkit.dvc.get_remotes()
+    subprocess.call(
+        [
+            "dvc",
+            "remote",
+            "add",
+            "something",
+            "https://sup.com",
+        ]
+    )
+    subprocess.call(
+        [
+            "dvc",
+            "remote",
+            "add",
+            "something-very-long-remote-that-will-be-more-than-one-line",
+            "https://sup.com/this/is/a/long/remote/url/so/test/this",
+        ]
+    )
+    resp = calkit.dvc.get_remotes()
+    assert resp == {
+        "something": "https://sup.com",
+        "something-very-long-remote-that-will-be-more-than-one-line": (
+            "https://sup.com/this/is/a/long/remote/url/so/test/this"
+        ),
+    }
