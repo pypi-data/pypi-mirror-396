788c42ec9bcbec5d49ee087c6116f3da0666bc8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 05:07:53 +0000

    Add package salstools/0.1.4.2/salstools-0.1.4.2-py3-none-any.whl

---
+++ packages/salstools/salstools-0.1.4.2-py3-none-any.whl/salstools/__init__.py
@@ -0,0 +1,19 @@
+#SalsTools
+'''
+add_pct = helper function for formulas - adds a Pct column to a Count column.
+
+smart_bar = smart bar chart with a number of variables to set visual parameters
+
+
+
+
+Make module importable system-wide:	"pip install -e ." if installing locally(use terminal from dir with files) 
+Must run from folder containing	setup.py
+Access functions with	import SalsTools as sals
+Control which functions are exposed	Use __init__.py
+
+'''
+
+
+
+from .main import *
