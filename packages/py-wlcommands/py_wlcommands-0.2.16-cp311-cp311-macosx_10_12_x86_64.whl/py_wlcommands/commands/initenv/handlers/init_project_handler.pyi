77557c67923bfc7b109a81b25db280522576e49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 21:05:29 +0000

    Add package py-wlcommands/0.2.16/py_wlcommands-0.2.16-cp311-cp311-macosx_10_12_x86_64.whl

---
+++ packages/py-wlcommands/py_wlcommands-0.2.16-cp311-cp311-macosx_10_12_x86_64.whl/py_wlcommands/commands/initenv/handlers/init_project_handler.pyi
@@ -0,0 +1,8 @@
+from py_wlcommands.commands.initenv.utils.git_initializer import GitInitializer as GitInitializer
+from py_wlcommands.commands.initenv.utils.platform_adapter import PlatformAdapter as PlatformAdapter
+from py_wlcommands.commands.initenv.utils.project_structure import ProjectStructureSetup as ProjectStructureSetup
+from py_wlcommands.utils.logging import log_error as log_error, log_info as log_info
+
+class InitProjectHandler:
+    @staticmethod
+    def initialize() -> None: ...
