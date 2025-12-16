1db7cea467b012ecdda61fdfe93350ab90bc1994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:31:54 +0000

    Add package pyvisgen/0.7.1/pyvisgen-0.7.1-py3-none-any.whl

---
+++ packages/pyvisgen/pyvisgen-0.7.1-py3-none-any.whl/pyvisgen/tools/create_dataset.py
@@ -0,0 +1,109 @@
+import rich_click as click
+
+from pyvisgen.dataset import SimulateDataSet
+from pyvisgen.io import Config
+from pyvisgen.utils.codecarbon import carbontracker
+
+
+@click.command()
+@click.argument(
+    "configuration_path",
+    type=click.Path(exists=True, dir_okay=False),
+)
+@click.option(
+    "--mode",
+    type=click.Choice(
+        [
+            "simulate",
+            "gridding",
+            "slurm",
+        ],
+        case_sensitive=False,
+    ),
+    default="simulate",
+)
+@click.option(
+    "-k",
+    "--key",
+    required=False,
+    type=str,
+    default="y",
+    help="Key under which the images are stored in the input HDF5 files",
+)
+@click.option("--slurm_job_id", required=False, type=int, default=None)
+@click.option("--slurm_n", required=False, type=int, default=None)
+@click.option(
+    "--num_images",
+    required=False,
+    type=int,
+    default=None,
+    help="""
+        Number of images in all bundles combined.
+        Will skip the automatic count.
+    """,
+)
+@click.option(
+    "-p",
+    "--multiprocess",
+    required=False,
+    help="""
+        Number of processes to run in parallel while
+        sampling and testing parameters. If -1 or
+        'all', will use all available cores.
+    """,
+)
+@click.option(
+    "-s",
+    "--stokes",
+    required=False,
+    type=str,
+    default="I",
+    help="""Stokes component to grid/simulate.""",
+)
+def main(
+    configuration_path: str | click.Path,
+    mode: str,
+    key: str = "x",
+    slurm_job_id=None,
+    slurm_n=None,
+    date_fmt="%d-%m-%Y %H:%M:%S",
+    num_images: int | None = None,
+    multiprocess: int | str = 1,
+    stokes: str = "I",
+):
+    """Dataset creation CLI tool of pyvisgen."""
+    config = Config.from_toml(configuration_path)
+
+    with carbontracker(config=config):
+        if mode == "simulate":
+            SimulateDataSet.from_config(
+                config,
+                image_key=key,
+                grid=False,
+                date_fmt=date_fmt,
+                num_images=num_images,
+                multiprocess=multiprocess,
+            )
+        if mode == "slurm":
+            SimulateDataSet.from_config(
+                config,
+                image_key=key,
+                slurm=True,
+                slurm_job_id=slurm_job_id,
+                slurm_n=slurm_n,
+                date_fmt=date_fmt,
+                num_images=num_images,
+            )
+        if mode == "gridding":
+            SimulateDataSet.from_config(
+                config,
+                image_key=key,
+                grid=True,
+                date_fmt=date_fmt,
+                num_images=num_images,
+                multiprocess=multiprocess,
+            )
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pyvisgen/pyvisgen-0.7.1-py3-none-any.whl/pyvisgen/utils/data.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import h5py
+import numpy as np
+from natsort import natsorted
+
+if TYPE_CHECKING:
+    from typing import String, Union
+
+__all__ = ["load_bundles", "open_bundles"]
+
+
+def load_bundles(
+    data_path: Union(String, Path), dataset_type: Union(String, None) = None
+) -> list:
+    """Loads bundle paths, filters for HDF5 files, and
+    returns them in a naturally ordered list.
+
+    Parameters
+    ----------
+    data_path : str or Path
+        Path to the directory containing the HDF5 files.
+    dataset_type : str, optional
+        Type of the dataset to filter, e.g. 'train', 'valid', or 'test'.
+
+    Returns
+    -------
+    bundles : list
+        Naturally ordered list containing paths to HDF5 files.
+    """
+    if isinstance(data_path, str):
+        data_path = Path(data_path)
+
+    filter_fmt = "*.h5" if not dataset_type else f"*{dataset_type}*.h5"
+
+    bundles = natsorted(list(data_path.glob(filter_fmt)))
+
+    return bundles
+
+
+def open_bundles(path: str | Path, key: str = "y") -> np.array:
+    """Opens a bundle HDF5 file.
+
+    Parameters
+    ----------
+    path : str or Path
+        Path to the bundle HDF5 file.
+
+    Returns
+    -------
+    bundle_y : :func:`~numpy.array`
+        :func:`~numpy.array` containing data from
+        the bundle file.
+    """
+    f = h5py.File(path, "r")
+    bundle_y = np.array(f[key])
+
+    return bundle_y
