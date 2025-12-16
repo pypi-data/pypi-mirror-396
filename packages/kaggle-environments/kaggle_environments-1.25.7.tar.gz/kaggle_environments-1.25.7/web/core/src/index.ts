1389fb7e38fd4263853617df242090f0726951ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 15:10:39 +0000

    Add package kaggle-environments/1.25.7/kaggle_environments-1.25.7.tar.gz

---
+++ packages/kaggle-environments/kaggle_environments-1.25.7.tar.gz/kaggle_environments-1.25.7/web/core/src/index.ts
@@ -0,0 +1,11 @@
+export * from './types';
+export * from './transformers/repeated_poker/v2/poker-steps-types';
+export type { ChessPlayer, ChessStep } from './transformers/chess/chessReplayTypes';
+export * from './adapter';
+export * from './preact-adapter';
+export * from './legacy-adapter';
+export * from './types';
+
+export * from './replay-visualizer-factory';
+export * from './timing';
+export * from './transformers';
