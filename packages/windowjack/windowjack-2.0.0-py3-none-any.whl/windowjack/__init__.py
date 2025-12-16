184e044acae838604a1bdc481ec97362f66ba9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 18:08:02 +0000

    Add package windowjack/2.0.0/windowjack-2.0.0-py3-none-any.whl

---
+++ packages/windowjack/windowjack-2.0.0-py3-none-any.whl/windowjack/__init__.py
@@ -0,0 +1,4 @@
+from .capture import WindowCapture, MonitorCapture, list_windows, list_monitors
+
+__version__ = "2.0.0"
+__all__ = ["WindowCapture", "MonitorCapture", "list_windows", "list_monitors"]
