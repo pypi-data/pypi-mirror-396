bc2e635c7e9731df7ac9908557a6a5a88789f7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 21:05:44 +0000

    Add package VKPyKit/0.4.4/vkpykit-0.4.4.tar.gz

---
+++ packages/VKPyKit/vkpykit-0.4.4.tar.gz/vkpykit-0.4.4/src/VKPyKit/__init__.py
@@ -0,0 +1,8 @@
+# Import all modules to make them available
+from . import EDA
+from . import DT
+from . import LR
+from . import MLM
+from . import VKPy
+
+__all__ = ["EDA", "DT", "LR", "MLM", "VKPy"]
