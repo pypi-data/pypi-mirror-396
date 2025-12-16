0bf4759510d9b844de99cbb4044dbeba325961f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 21:05:47 +0000

    Add package orgformat/2025.12.13.1/orgformat-2025.12.13.1.tar.gz

---
+++ packages/orgformat/orgformat-2025.12.13.1.tar.gz/orgformat-2025.12.13.1/setup.py
@@ -0,0 +1,28 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="orgformat",
+    version="2025.12.13.1",
+    description="Utility library for providing functions to generate and modify Org mode syntax elements like links, time-stamps, or date-stamps.",
+    license='GPLv3',
+    author="Karl Voit",
+    author_email="tools@Karl-Voit.at",
+    url="https://github.com/novoid/orgformat",
+    download_url="https://github.com/novoid/orgformat/zipball/master",
+    keywords=["orgmode", "datestamps", "timestamps", "links"],
+    packages=find_packages(),
+    #install_requires=["pyreadline3", "clint"],  # add dependencies if they are included in the code
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        ]
+)
