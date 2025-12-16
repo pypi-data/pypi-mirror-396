44a15dfc4911d8720817be1d9cdd82316b335ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 14:07:48 +0000

    Add package tiledbsoma/2.2.0/tiledbsoma-2.2.0-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/tiledbsoma/tiledbsoma-2.2.0-cp39-cp39-manylinux_2_28_x86_64.whl/tiledbsoma/stats.py
@@ -0,0 +1,24 @@
+# Copyright (c) TileDB, Inc. and The Chan Zuckerberg Initiative Foundation
+#
+# Licensed under the MIT License.
+
+"""TileDB query stats helpers."""
+
+import json
+from typing import Literal, Union, cast
+
+from .pytiledbsoma import tiledbsoma_stats_string
+
+ParsedStats = list[dict[Literal["counters", "timers"], dict[str, Union[float, int]]]]
+
+
+def tiledbsoma_stats_json() -> str:
+    """Returns tiledbsoma stats as a JSON string."""
+    # cast is needed for pybind11 things
+    return cast("str", tiledbsoma_stats_string())
+
+
+def tiledbsoma_stats_as_py() -> ParsedStats:
+    """Returns tiledbsoma stats as a Python dict."""
+    # cast is needed for pybind11 things
+    return cast("ParsedStats", json.loads(tiledbsoma_stats_string()))
