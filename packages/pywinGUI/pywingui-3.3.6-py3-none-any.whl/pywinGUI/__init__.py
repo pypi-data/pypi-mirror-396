451c78f579aec4f5eebd78a42f3f0e262976c524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 20:06:27 +0000

    Add package pywinGUI/3.3.6/pywingui-3.3.6-py3-none-any.whl

---
+++ packages/pywinGUI/pywingui-3.3.6-py3-none-any.whl/pywinGUI/__init__.py
@@ -0,0 +1,7 @@
+﻿# pywinGUI binary package (Nuitka compiled)
+from .controller import *
+from .readGUI import *
+from .error_engine import *
+from .error_reader import *
+
+__all__ = [n for n in globals() if not n.startswith('_')]
