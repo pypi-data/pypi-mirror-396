92d8bedf744f82a112af40e6e3c918659b24cca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 12:06:17 +0000

    Add package netbox-ip-permissions-synchronization/0.0.11/netbox_ip_permissions_synchronization-0.0.11.tar.gz

---
+++ packages/netbox-ip-permissions-synchronization/netbox_ip_permissions_synchronization-0.0.11.tar.gz/netbox_ip_permissions_synchronization-0.0.11/netbox_ip_permissions_synchronization/forms.py
@@ -0,0 +1,7 @@
+## forms.py
+from django import forms
+
+
+class IPPermissionsSyncForm(forms.Form):
+    sync_all = forms.BooleanField(required=False)
+    selected_ips = forms.CharField(required=False, widget=forms.HiddenInput())
