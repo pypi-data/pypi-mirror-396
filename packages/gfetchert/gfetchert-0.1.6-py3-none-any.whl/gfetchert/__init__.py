c3355b60842d135dbb097f307ade204b0d31cc4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 21:06:12 +0000

    Add package gfetchert/0.1.6/gfetchert-0.1.6-py3-none-any.whl

---
+++ packages/gfetchert/gfetchert-0.1.6-py3-none-any.whl/gfetchert/__init__.py
@@ -0,0 +1,7 @@
+from .core import get_rainfall
+from .geocode import get_coordinates
+from .file_ops import fetch_from_csv
+
+__all__ = ["get_rainfall", "get_coordinates", "fetch_from_csv"]
+__version__ = "0.1.1"
+
