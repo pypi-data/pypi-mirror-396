8152f8e5b6df48b25567f73fb865f593f5ef146b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-16 03:32:04 +0000

    Add package wikipedia-tablo-cekici/1.0.3/wikipedia_tablo_cekici-1.0.3.tar.gz

---
+++ packages/wikipedia-tablo-cekici/wikipedia_tablo_cekici-1.0.3.tar.gz/wikipedia_tablo_cekici-1.0.3/wikipedia_tablo_cekici/__init__.py
@@ -0,0 +1,4 @@
+from .main import wikipedia_tablo_cek, tablo_kaydet, tum_tablolari_goster
+
+__version__ = "1.0.1"
+__all__ = ["wikipedia_tablo_cek", "tablo_kaydet", "tum_tablolari_goster"]
---
+++ packages/wikipedia-tablo-cekici/wikipedia_tablo_cekici-1.0.3.tar.gz/wikipedia_tablo_cekici-1.0.3/wikipedia_tablo_cekici/main.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+
+
+def wikipedia_tablo_cek(url, tablo_index=0):
+    """
+    Wikipedia sayfasindan belirtilen indexteki tabloyu ceker
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    tablolar = soup.find_all('table', {'class': 'wikitable'})
+    
+    if not tablolar:
+        raise ValueError("Sayfada tablo bulunamadi!")
+    
+    if tablo_index >= len(tablolar):
+        raise ValueError(f"Gecersiz tablo indexi! Sayfada {len(tablolar)} tablo var.")
+    
+    tablo = tablolar[tablo_index]
+    df = pd.read_html(str(tablo))[0]
+    
+    return df
+
+
+def tablo_kaydet(df, klasor_adi='veriler', dosya_formati='csv'):
+    """
+    DataFrame'i belirtilen formatta kaydeder
+    """
+    os.makedirs(klasor_adi, exist_ok=True)
+    
+    dosya_adi = f"tablo.{dosya_formati}"
+    dosya_yolu = os.path.join(klasor_adi, dosya_adi)
+    
+    if dosya_formati == 'csv':
+        df.to_csv(dosya_yolu, index=False, encoding='utf-8-sig')
+    elif dosya_formati == 'excel':
+        df.to_excel(dosya_yolu, index=False)
+    elif dosya_formati == 'json':
+        df.to_json(dosya_yolu, orient='records', force_ascii=False)
+    else:
+        raise ValueError("Gecersiz format! 'csv', 'excel' veya 'json' olmali.")
+    
+    print(f"Tablo '{dosya_yolu}' olarak kaydedildi.")
+
+
+def tum_tablolari_goster(url):
+    """
+    Sayfadaki tum tablolari gosterir
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    tablolar = soup.find_all('table', {'class': 'wikitable'})
+    
+    print(f"Bu sayfada {len(tablolar)} tablo bulundu:\n")
+    
+    for i, tablo in enumerate(tablolar):
+        print(f"Tablo {i}:")
+        df = pd.read_html(str(tablo))[0]
+        print(df.head())
+        print("\n" + "="*50 + "\n")
