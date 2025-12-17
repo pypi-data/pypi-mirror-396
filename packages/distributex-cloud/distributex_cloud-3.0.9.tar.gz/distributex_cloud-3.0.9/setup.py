f3c000ca370da68b158ea571b0539ebb11ae0743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 03:31:17 +0000

    Add package distributex-cloud/3.0.9/distributex_cloud-3.0.9.tar.gz

---
+++ packages/distributex-cloud/distributex_cloud-3.0.9.tar.gz/distributex_cloud-3.0.9/setup.py
@@ -0,0 +1,42 @@
+"""
+DistributeX Cloud SDK Setup
+"""
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read README
+readme_file = Path(__file__).parent / "README.md"
+if readme_file.exists():
+    with open(readme_file, "r", encoding="utf-8") as f:
+        long_description = f.read()
+else:
+    long_description = "DistributeX - Distributed Computing SDK"
+
+setup(
+    name="distributex-cloud",
+    version = "3.0.9",
+    author="DistributeX Team",
+    author_email="unavailable",
+    description="Distributed computing platform - run code on global resource pool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DistributeX-Cloud/distributex-cli-public",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "requests>=2.28.0",
+    ],
+    keywords=["distributed", "computing", "cloud", "parallel", "gpu"],
+)
