91f98d1d5269e9a0d4f01e48a24f4dc959a5fcd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 04:13:39 +0000

    Add package bizkaibus/0.5.0/bizkaibus-0.5.0-py3-none-any.whl

---
+++ packages/bizkaibus/bizkaibus-0.5.0-py3-none-any.whl/bizkaibus/ServiceParams/StopInfoServiceParam.py
@@ -0,0 +1,10 @@
+from ServiceParams.BizkaibusServiceParam import BizkaibusServiceParam
+from const import _RESOURCE, STOP_INFO_SERVICE
+
+class StopInfoServiceParam(BizkaibusServiceParam):
+
+    def __init__(self):
+        """Retrieve the parameters for the service."""
+
+    def GetURL(self) -> str:
+        return _RESOURCE + STOP_INFO_SERVICE
