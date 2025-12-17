0e7845c25b52a1e2a046846ca04087d5f186f68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 00:07:45 +0000

    Add package upbge-stubs/0.3.1.27.dev20200728602/upbge_stubs-0.3.1.27.dev20200728602.tar.gz

---
+++ packages/upbge-stubs/upbge_stubs-0.3.1.27.dev20200728602.tar.gz/upbge_stubs-0.3.1.27.dev20200728602/node_socket_data_type_items/__init__.pyi
@@ -0,0 +1,65 @@
+"""
+
+
+Node Socket Data Type Items
+^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+:FLOAT:           
+  Float.
+
+:INT:             
+  Integer.
+
+:BOOLEAN:         
+  Boolean.
+
+:VECTOR:          
+  Vector.
+
+:RGBA:            
+  Color.
+
+:ROTATION:        
+  Rotation.
+
+:MATRIX:          
+  Matrix.
+
+:STRING:          
+  String.
+
+:MENU:            
+  Menu.
+
+:SHADER:          
+  Shader.
+
+:OBJECT:          
+  Object.
+
+:IMAGE:           
+  Image.
+
+:GEOMETRY:        
+  Geometry.
+
+:COLLECTION:      
+  Collection.
+
+:TEXTURE:         
+  Texture.
+
+:MATERIAL:        
+  Material.
+
+:BUNDLE:          
+  Bundle.
+
+:CLOSURE:         
+  Closure.
+
+.. _rna-enum-node-socket-data-type-items:
+
+"""
+
+import typing
