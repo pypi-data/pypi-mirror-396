b2fe3d4ec94c3ad1632aa5e62f650f1f44ee8a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 06:06:31 +0000

    Add package pyinsteon/1.6.4/pyinsteon-1.6.4.tar.gz

---
+++ packages/pyinsteon/pyinsteon-1.6.4.tar.gz/pyinsteon-1.6.4/pyinsteon/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for the command line tool."""
+
+from .tools import InsteonCmd
+
+if __name__ == "__main__":
+    InsteonCmd.start()
---
+++ packages/pyinsteon/pyinsteon-1.6.4.tar.gz/pyinsteon-1.6.4/pyinsteon/handlers/all_link_cleanup_failure_report.py
@@ -0,0 +1,19 @@
+"""Handle All-Link Cleanup Status Reports."""
+
+from . import inbound_handler
+from ..address import Address
+from ..topics import ALL_LINK_CLEANUP_FAILURE_REPORT
+from .inbound_base import InboundHandlerBase
+
+
+class AllLinkCleanupFailureReport(InboundHandlerBase):
+    """Handle All-Link Cleanup Failure Reports."""
+
+    def __init__(self):
+        """Init AllLinkCleanupFailureReport class."""
+        super().__init__(topic=ALL_LINK_CLEANUP_FAILURE_REPORT)
+
+    @inbound_handler
+    def handle_inbound(self, error: int, group: int, target: Address):
+        """Handle the All-Link Failure Report inbound message."""
+        self._call_subscribers(success=False, group=group, target=target)
