00bb00ac0cb4ebe7470772f19dd85a65873303d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 06:07:25 +0000

    Add package tms-mcp/0.4.0/tms_mcp-0.4.0.tar.gz

---
+++ packages/tms-mcp/tms_mcp-0.4.0.tar.gz/tms_mcp-0.4.0/tests/test_dummy.py
@@ -0,0 +1,6 @@
+# This file serves as a placeholder, ensuring that pytest does not return an error due to the absence of any test files.
+# Once you start developing your own tests, this file and its function can be safely removed.
+
+
+def test_dummy():
+    pass
