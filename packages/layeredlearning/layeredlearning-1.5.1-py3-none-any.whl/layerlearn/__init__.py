3e715b07914d36e27708b7e2e8070c89da9171b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 00:08:00 +0000

    Add package layeredlearning/1.5.1/layeredlearning-1.5.1-py3-none-any.whl

---
+++ packages/layeredlearning/layeredlearning-1.5.1-py3-none-any.whl/layerlearn/__init__.py
@@ -0,0 +1,16 @@
+# here is the package imports
+from layerlearn.flexiblestacked import FlexibleStackedClassifier
+from layerlearn.flexiblestacked import FlexibleStackedRegressor
+from layerlearn.baseLinear import LinearForestRegressor
+from layerlearn.baseLinear import LinearSupportRegressor
+from layerlearn.baseLinear import LinearDecisionRegressor
+from layerlearn.baseLinear import LinearKNNRegressor
+from layerlearn.baseLinear import LinearXGBRRegressor
+from layerlearn.baseLinear import LinearCatRegressor
+from layerlearn.baseLinear import LinearGradientRegressor
+from layerlearn.baseLinear import LinearAdaRegressor
+from layerlearn.baseLinear import LinearLightRegressor
+
+
+# here is the version of the package
+__version__ = "1.5.1"
