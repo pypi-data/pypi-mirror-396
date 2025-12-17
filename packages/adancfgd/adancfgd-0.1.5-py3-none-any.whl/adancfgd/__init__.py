be9d53ea09ea196e2f45ca3f613e798e6e86b81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 01:34:23 +0000

    Add package adancfgd/0.1.5/adancfgd-0.1.5-py3-none-any.whl

---
+++ packages/adancfgd/adancfgd-0.1.5-py3-none-any.whl/adancfgd/__init__.py
@@ -0,0 +1,42 @@
+from .adancfgd import AdaFGD, AdaNCFGD
+from .snn import (
+    ForwardFirstBackwardSecond,
+    SNNDropout,
+    SNNBatchNorm1d,
+    SNNBatchNorm2d,
+    SNNLinear,
+    SNNLinearWithBatchNorm,
+    SNNConv2d,
+    SNNConv2dWithBatchNorm,
+    SNN
+)
+from .snn_cnn import (
+    pool_spikes,
+    SNNCNN
+)
+
+__version__ = "0.1.5"
+__author__ = "Yihe Zhu"
+__email__ = "zhu.yihe@qq.com"
+__description__ = "Adaptive Fractional Gradient Descent Optimizers (AdaFGD & AdaNCFGD)"
+__url__ = "https://github.com/HunLuanZhiZhu/AdaNCFGD"
+
+# Export main classes and functions
+__all__ = [
+    # Optimizers
+    "AdaFGD",
+    "AdaNCFGD",
+    # SNN components
+    "ForwardFirstBackwardSecond",
+    "SNNDropout",
+    "SNNBatchNorm1d",
+    "SNNBatchNorm2d",
+    "SNNLinear",
+    "SNNLinearWithBatchNorm",
+    "SNNConv2d",
+    "SNNConv2dWithBatchNorm",
+    "SNN",
+    "SNNCNN",
+    # Utility functions
+    "pool_spikes"
+]
