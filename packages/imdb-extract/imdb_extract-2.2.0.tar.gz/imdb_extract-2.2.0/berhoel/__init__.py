4b00b032ceb2ef7be4b4064b86a0f28d9c00f211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 03:32:02 +0000

    Add package imdb-extract/2.2.0/imdb_extract-2.2.0.tar.gz

---
+++ packages/imdb-extract/imdb_extract-2.2.0.tar.gz/imdb_extract-2.2.0/berhoel/__init__.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Initialization for berhoel python namespace packages.
+"""
+
+__date__ = "2023/06/10 21:24:03 hoel"
+__author__ = "Berthold Höllmann"
+__copyright__ = "Copyright © 2017 by Berthold Höllmann"
+__credits__ = ["Berthold Höllmann"]
+__maintainer__ = "Berthold Höllmann"
+__email__ = "berhoel@gmail.com"
+
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+
+# Local Variables:
+# mode: python
+# compile-command: "cd ../ && python setup.py test"
+# time-stamp-pattern: "30/__date__ = \"%:y/%02m/%02d %02H:%02M:%02S %u\""
+# End:
