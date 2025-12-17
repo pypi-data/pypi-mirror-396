eed4e22a90c5fd0aff3e23283b12d5590304e80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 06:07:23 +0000

    Add package PyGPUkit/0.2.5/pygpukit-0.2.5-cp312-cp312-win_amd64.whl

---
+++ packages/PyGPUkit/pygpukit-0.2.5-cp312-cp312-win_amd64.whl/pygpukit/ops/__init__.py
@@ -0,0 +1,5 @@
+"""Operations module for PyGPUkit."""
+
+from pygpukit.ops.basic import add, div, exp, log, matmul, max, mean, mul, relu, sub, sum
+
+__all__ = ["add", "sub", "mul", "div", "exp", "log", "relu", "matmul", "sum", "mean", "max"]
