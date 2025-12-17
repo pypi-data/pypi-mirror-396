ebe87312b0b1c2a94e9e68e64780e172ecde104c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 07:09:27 +0000

    Add package ai-api-test-docs/1.0.1/ai_api_test_docs-1.0.1.tar.gz

---
+++ packages/ai-api-test-docs/ai_api_test_docs-1.0.1.tar.gz/ai_api_test_docs-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ai_api_test_docs",
+    version="1.0.1",
+    description="AI-powered API test and documentation generator using Gemini",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Ezeana Micheal",
+    packages=find_packages(),
+    install_requires=[
+        "google-generativeai>=0.6.9",
+        "python-dotenv>=1.0.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "ai_api_test_docs=ai_api_docs.cli:main"
+        ]
+    },
+    python_requires=">=3.9",
+    license="MIT",
+)
