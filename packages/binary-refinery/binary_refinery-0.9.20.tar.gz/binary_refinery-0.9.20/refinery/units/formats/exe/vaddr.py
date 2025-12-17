cafb464ba8130e67b58d90ae221c8e8fff2b5ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 01:34:41 +0000

    Add package binary-refinery/0.9.20/binary_refinery-0.9.20.tar.gz

---
+++ packages/binary-refinery/binary_refinery-0.9.20.tar.gz/binary_refinery-0.9.20/refinery/units/formats/exe/vaddr.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+from refinery.lib.argformats import metavars
+from refinery.lib.executable import Executable
+from refinery.lib.types import Param
+from refinery.units import Arg, Unit
+
+
+class vaddr(Unit):
+    """
+    Converts a metadata variable holding a file offset to a virtual address. This unit only works when the
+    chunk body contains a PE, ELF, or MachO executable. The variable will be substituted in place. If you
+    would like to retain the original value, it is recommended to use the `refinery.put` unit first to create
+    a copy of an already existing variable, and then convert the copy.
+    """
+
+    def __init__(
+        self, *name: Param[str, Arg.String(help='The name of a metadata variable holding an integer.')],
+        base: Param[int, Arg.Number('-b', metavar='ADDR', help='Optionally specify a custom base address B.')] = None
+    ):
+        return super().__init__(names=name, base=base)
+
+    def process(self, data):
+        try:
+            exe = Executable.Load(data, self.args.base)
+        except Exception:
+            self.log_warn('unable to parse input as executable; no variable conversion was performed')
+            return data
+        meta = metavars(data)
+        for name in self.args.names:
+            value = meta[name]
+            meta[name] = exe.location_from_offset(value).virtual.position
+        return data
+
+    def reverse(self, data):
+        try:
+            exe = Executable.Load(data, self.args.base)
+        except Exception:
+            self.log_warn('unable to parse input as executable; no variable conversion was performed')
+            return data
+        meta = metavars(data)
+        for name in self.args.names:
+            value = meta[name]
+            meta[name] = exe.location_from_address(value).physical.position
+        return data
