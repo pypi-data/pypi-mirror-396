82a3c574c524b9c7a76d3c7a4c205a698b4cfaa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 08:07:03 +0000

    Add package icclogger/1.2/icclogger-1.2.tar.gz

---
+++ packages/icclogger/icclogger-1.2.tar.gz/icclogger-1.2/test.py
@@ -0,0 +1,23 @@
+#%%
+from src.icclogger import log,LOGGER
+from src.icclogger.colors import ConsoleColors
+@log()
+def test():
+    # LOGGER.info(f"{ConsoleColors.R} red")
+    # LOGGER.info(f"{ConsoleColors.G} green")
+    # LOGGER.info(f"{ConsoleColors.C} cyan")
+    # LOGGER.info(f"{ConsoleColors.W} white")
+    # LOGGER.info(f"{ConsoleColors.Y} yellow")
+    # LOGGER.info(f"{ConsoleColors.P} purple")
+    # LOGGER.info(f"{ConsoleColors.GR} dark grey")
+    # LOGGER.info(f"{ConsoleColors.BR} bright red")
+    # LOGGER.info(f"{ConsoleColors.BG} bright green")
+    # LOGGER.info(f"{ConsoleColors.BY} bright yellow")
+    # LOGGER.info(f"{ConsoleColors.BBLUE} bright blue")
+    # LOGGER.info(f"{ConsoleColors.BP} bright purple")
+    # LOGGER.info(f"{ConsoleColors.BC} bright cyan")
+    # LOGGER.info(f"{ConsoleColors.BW} bright white")
+
+    return
+
+test()
