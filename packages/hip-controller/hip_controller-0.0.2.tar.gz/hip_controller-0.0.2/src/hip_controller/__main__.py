ff4c94325906b1a58a52e0d5d9b4023983bc7a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 08:06:57 +0000

    Add package hip-controller/0.0.2/hip_controller-0.0.2.tar.gz

---
+++ packages/hip-controller/hip_controller-0.0.2.tar.gz/hip_controller-0.0.2/src/hip_controller/__main__.py
@@ -0,0 +1,28 @@
+"""Sample doc string."""
+
+import argparse
+
+from hip_controller.app import main
+from hip_controller.definitions import DEFAULT_LOG_LEVEL, LogLevel
+
+if __name__ == "__main__":  # pragma: no cover
+    parser = argparse.ArgumentParser("Run the pipeline.")
+    parser.add_argument(
+        "--log-level",
+        default=DEFAULT_LOG_LEVEL,
+        choices=list(LogLevel()),
+        help="Set the log level.",
+        required=False,
+        type=str,
+    )
+    parser.add_argument(
+        "--stderr-level",
+        default=DEFAULT_LOG_LEVEL,
+        choices=list(LogLevel()),
+        help="Set the std err level.",
+        required=False,
+        type=str,
+    )
+    args = parser.parse_args()
+
+    main(log_level=args.log_level, stderr_level=args.stderr_level)
