bb69733bc91fa3d99355745089a2e037bb95f173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 00:08:37 +0000

    Add package n8nControl/1.7.4/n8ncontrol-1.7.4.tar.gz

---
+++ packages/n8nControl/n8ncontrol-1.7.4.tar.gz/n8ncontrol-1.7.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="n8nControl",
+    version="1.7.4",
+    author="LEV",
+    author_email="you@example.com",
+    description="Python agent to control remote n8n instances",
+    packages=find_packages(include=['n8nControl', 'n8nControl.*']),
+    include_package_data=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+    entry_points={
+        "console_scripts": [
+            "n8nagent = n8nControl.app:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
