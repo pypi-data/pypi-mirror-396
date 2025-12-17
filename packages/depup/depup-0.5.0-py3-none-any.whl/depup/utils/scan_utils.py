484fd1e82e95b4a682c94fde0f64ca217d14f846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 00:08:57 +0000

    Add package depup/0.5.0/depup-0.5.0-py3-none-any.whl

---
+++ packages/depup/depup-0.5.0-py3-none-any.whl/depup/utils/scan_utils.py
@@ -0,0 +1,24 @@
+def _convert_to_jsonable(deps, infos):
+    info_by_name = {i.name.lower(): i for i in infos}
+
+    items = []
+    for dep in deps:
+        info = info_by_name.get(dep.name.lower())
+
+        items.append({
+            "name": dep.name,
+            "declared": dep.version,
+            "current": dep.version,  # for env mode declared=None
+            "latest": info.latest if info else None,
+            "update_type": info.update_type if info else "none",
+            "source": dep.source_file.name if dep.source_file else None,
+        })
+    return items
+
+def _has_outdated(infos: list) -> bool:
+    return any(i.update_type != "none" for i in infos)
+
+__all__ = [
+    "_convert_to_jsonable",
+    "_has_outdated",
+]
