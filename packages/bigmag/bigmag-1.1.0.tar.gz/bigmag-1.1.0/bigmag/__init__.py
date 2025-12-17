3bcdc99a02d5d059cce1a84210f723f60945712b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 07:08:54 +0000

    Add package bigmag/1.1.0/bigmag-1.1.0.tar.gz

---
+++ packages/bigmag/bigmag-1.1.0.tar.gz/bigmag-1.1.0/bigmag/__init__.py
@@ -0,0 +1,7 @@
+"""BIgMAG - Board InteGrating Metagenome-Assembled Genomes"""
+
+__version__ = "1.0.0"
+__author__ = "Jeferyd Yepes García, Laurent Falquet"
+__email__ = "jeferyd.yepes@unibe.ch"
+
+__all__ = ["__version__", "__author__", "__email__"]
