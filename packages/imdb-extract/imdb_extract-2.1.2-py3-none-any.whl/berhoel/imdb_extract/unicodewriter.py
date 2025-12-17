94e9a815af49195d6c1f28c30b13bc156f3ee21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 03:32:02 +0000

    Add package imdb-extract/2.1.2/imdb_extract-2.1.2-py3-none-any.whl

---
+++ packages/imdb-extract/imdb_extract-2.1.2-py3-none-any.whl/berhoel/imdb_extract/unicodewriter.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Write UTF-8 CSV files.
+"""
+
+# Standard library imports.
+import csv
+
+__date__ = "2022/08/15 17:04:54 hoel"
+__author__ = "Berthold Höllmann"
+__copyright__ = "Copyright © 2012 by Berthold Höllmann"
+__credits__ = ["Berthold Höllmann"]
+__maintainer__ = "Berthold Höllmann"
+__email__ = "berhoel@gmail.com"
+
+UnicodeWriter = csv.writer
+
+
+# Local Variables:
+# mode: python
+# compile-command: "cd ../../ && python setup.py test"
+# time-stamp-pattern: "30/__date__ = \"%:y/%02m/%02d %02H:%02M:%02S %u\""
+# End:
