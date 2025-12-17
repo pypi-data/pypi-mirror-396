c3be712fd7bf83ccab9136553b5ecc67ca836b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 04:13:50 +0000

    Add package lldb-for-pwndbg/21.1.7.post1/lldb_for_pwndbg-21.1.7.post1-cp312-cp312-manylinux_2_39_riscv64.whl

---
+++ packages/lldb-for-pwndbg/lldb_for_pwndbg-21.1.7.post1-cp312-cp312-manylinux_2_39_riscv64.whl/lldb/utils/in_call_stack.py
@@ -0,0 +1,24 @@
+#!/nix/store/m2wi1qf09alzvqlkaszlvk2pdhhjxaif-python3-riscv64-unknown-linux-gnu-3.12.11/bin/python
+
+
+def __lldb_init_module(debugger, internal_dict):
+    debugger.HandleCommand(
+        f"command alias in_call_stack breakpoint command add --python-function {__name__}.in_call_stack -k name -v %1"
+    )
+
+
+def in_call_stack(frame, bp_loc, arg_dict, _):
+    """Only break if the given name is in the current call stack."""
+    name = arg_dict.GetValueForKey("name").GetStringValue(1000)
+    thread = frame.GetThread()
+    found = False
+    for frame in thread.frames:
+        # Check the symbol.
+        symbol = frame.GetSymbol()
+        if symbol and name in frame.GetSymbol().GetName():
+            return True
+        # Check the function.
+        function = frame.GetFunction()
+        if function and name in function.GetName():
+            return True
+    return False
