f25064cfcd19669d8a1bbf2dd057be150aeaabbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 08:07:33 +0000

    Add package ghsa-client/1.5.0/ghsa_client-1.5.0-py3-none-any.whl

---
+++ packages/ghsa-client/ghsa_client-1.5.0-py3-none-any.whl/ghsa_client/models/language.py
@@ -0,0 +1,18 @@
+from enum import StrEnum
+
+
+class Language(StrEnum):
+    PYTHON = "python"
+    RUBY = "ruby"
+    PHP = "php"
+    JAVA = "java"
+    GOLANG = "go"
+    NEXTJS = "nextjs"
+    NODEJS = "node"
+    RUST = "rust"
+    WORDPRESS = "wordpress"
+    CSHARP = "csharp"
+    ERLANG = "erlang"
+    FLUTTER = "flutter"
+    SWIFT = "swift"
+    ACTIONS = "actions"  # Github Actions
