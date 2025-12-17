473f47bff4d9c3e81e6620c9910f29bb7302123e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 04:14:19 +0000

    Add package lamindb_setup/1.17.0/lamindb_setup-1.17.0-py3-none-any.whl

---
+++ packages/lamindb_setup/lamindb_setup-1.17.0-py3-none-any.whl/lamindb_setup/core/__init__.py
@@ -0,0 +1,37 @@
+"""Core setup library.
+
+General
+-------
+
+.. autoclass:: SetupSettings
+
+User
+----
+
+.. autoclass:: UserSettings
+
+Instance
+--------
+
+.. autoclass:: InstanceSettings
+
+Storage
+-------
+
+.. autoclass:: StorageSettings
+
+"""
+
+from . import django, upath
+from ._clone import (
+    connect_local_sqlite,
+    connect_remote_sqlite,
+    init_local_sqlite,
+    upload_sqlite_clone,
+)
+from ._deprecated import deprecated  # documented in lamindb.base
+from ._docs import doc_args  # documented in lamindb.base
+from ._settings import SetupSettings
+from ._settings_instance import InstanceSettings
+from ._settings_storage import StorageSettings
+from ._settings_user import UserSettings
