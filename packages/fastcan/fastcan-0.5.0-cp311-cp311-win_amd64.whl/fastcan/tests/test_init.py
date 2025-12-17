160e56f4c2dc32580a3e6da2a0761e4523c398f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 06:06:37 +0000

    Add package fastcan/0.5.0/fastcan-0.5.0-cp311-cp311-win_amd64.whl

---
+++ packages/fastcan/fastcan-0.5.0-cp311-cp311-win_amd64.whl/fastcan/tests/test_init.py
@@ -0,0 +1,17 @@
+"""Basic unittests to test functioning of module's top-level"""
+
+try:
+    from fastcan import *  # noqa: F403
+
+    _TOP_IMPORT_ERROR = None
+
+except Exception as e:
+    _TOP_IMPORT_ERROR = e
+
+
+def test_import_fastcan():
+    """Test either above import has failed for some reason
+    "import *" is discouraged outside of the module level, hence we
+    rely on setting up the variable above
+    """
+    assert _TOP_IMPORT_ERROR is None
