89ff24c421370490565ba6382781cda496063ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 07:09:25 +0000

    Add package pglast/7.11/pglast-7.11-cp314-cp314-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/pglast/pglast-7.11-cp314-cp314-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/pglast/enums/lockoptions.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# :Project:   pglast — DO NOT EDIT: automatically extracted from lockoptions.h @ 17-6.2.0-0-ga192b38
+# :Author:    Lele Gaifax <lele@metapensiero.it>
+# :License:   GNU General Public License version 3 or later
+# :Copyright: © 2017-2025 Lele Gaifax
+#
+
+from enum import Enum, IntEnum, IntFlag, auto
+
+try:
+    from enum import StrEnum
+except ImportError:  # pragma: no cover
+    # Python < 3.10
+    class StrEnum(str, Enum):
+        pass
+
+
+class LockClauseStrength(IntEnum):
+    LCS_NONE = 0
+    LCS_FORKEYSHARE = auto()
+    LCS_FORSHARE = auto()
+    LCS_FORNOKEYUPDATE = auto()
+    LCS_FORUPDATE = auto()
+
+
+class LockTupleMode(IntEnum):
+    LockTupleKeyShare = 0
+    LockTupleShare = auto()
+    LockTupleNoKeyExclusive = auto()
+    LockTupleExclusive = auto()
+
+
+class LockWaitPolicy(IntEnum):
+    LockWaitBlock = 0
+    LockWaitSkip = auto()
+    LockWaitError = auto()
