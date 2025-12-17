43c97a0cc88fc6caa9c6b3d364a38a4b6a2bd581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 05:08:31 +0000

    Add package zelepy/0.0.3/zelepy-0.0.3.tar.gz

---
+++ packages/zelepy/zelepy-0.0.3.tar.gz/zelepy-0.0.3/README.md
@@ -0,0 +1,2 @@
+# What is this
+This is a python library for zelesis neo https://zelesis.com/
---
+++ packages/zelepy/zelepy-0.0.3.tar.gz/zelepy-0.0.3/zelepy/__init__.py
@@ -0,0 +1,17 @@
+"""
+Zelesis Neo API Python Bindings
+
+A Python library for interacting with the Zelesis Neo API.
+"""
+
+# Version should match pyproject.toml
+__version__ = "0.0.1"
+
+# Import your main classes/functions here when you create them
+# Example:
+# from .client import Client
+# from .api import API
+
+# Make them available at package level
+# __all__ = ['Client', 'API']
+
