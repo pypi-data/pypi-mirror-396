bfbe7c2aa806566f869e0fd1c5cda7605d88e7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 07:09:03 +0000

    Add package synriard/1.0.1/synriard-1.0.1.tar.gz

---
+++ packages/synriard/synriard-1.0.1.tar.gz/synriard-1.0.1/synriard/mjcf/__init__.py
@@ -0,0 +1,11 @@
+# This line makes the sub-folders available as attributes of this module
+from . import Alicia_D_v5_6
+from . import Alicia_M_v1_1
+from . import Bessica_D_v1_0
+from . import Bessica_D_v1_1
+__all__ = [
+    "Alicia_D_v5_6",
+    "Alicia_M_v1_1",
+    "Bessica_D_v1_0",
+    "Bessica_D_v1_1",
+]
