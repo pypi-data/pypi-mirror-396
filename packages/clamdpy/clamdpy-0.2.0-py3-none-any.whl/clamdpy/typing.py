7804c6990b69f5755a9f77a13a7db17838cbd98c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 01:34:55 +0000

    Add package clamdpy/0.2.0/clamdpy-0.2.0-py3-none-any.whl

---
+++ packages/clamdpy/clamdpy-0.2.0-py3-none-any.whl/clamdpy/typing.py
@@ -0,0 +1,13 @@
+from os import PathLike
+from typing import Protocol, TypeAlias, TypeVar
+
+_T_co = TypeVar("_T_co", covariant=True)
+
+
+# Taken from _typeshed/__init__.pyi
+class SupportsRead(Protocol[_T_co]):
+    def read(self, __length: int = ...) -> _T_co: ...
+
+
+# Taken from _typeshed/__init__.pyi
+StrPath: TypeAlias = str | PathLike[str]
