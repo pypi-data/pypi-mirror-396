dafcd7a3b7ffe87d5baaeed9418b6ba27a3ed588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 05:08:49 +0000

    Add package vellum-ai/1.11.18/vellum_ai-1.11.18-py3-none-any.whl

---
+++ packages/vellum-ai/vellum_ai-1.11.18-py3-none-any.whl/vellum/workflows/nodes/displayable/note_node/node.py
@@ -0,0 +1,21 @@
+from typing import Generic
+
+from vellum.workflows.nodes.bases import BaseNode
+from vellum.workflows.types import MergeBehavior
+from vellum.workflows.types.generics import StateType
+
+
+class NoteNode(BaseNode[StateType], Generic[StateType]):
+    """
+    A no-op Node purely used to display a note in the Vellum UI.
+    """
+
+    class Display(BaseNode.Display):
+        icon = "vellum:icon:note"
+        color = "cyan"
+
+    class Trigger(BaseNode.Trigger):
+        merge_behavior = MergeBehavior.AWAIT_ANY
+
+    def run(self) -> BaseNode.Outputs:
+        raise RuntimeError("NoteNode should never be run")
