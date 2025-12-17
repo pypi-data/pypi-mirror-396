2e178ec430909a129b9df1d23012f6dab6b01211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 06:07:04 +0000

    Add package scphylo-tools/0.0.5/scphylo_tools-0.0.5-cp313-cp313-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/scphylo-tools/scphylo_tools-0.0.5-cp313-cp313-manylinux_2_17_i686.manylinux2014_i686.whl/scphylo/external/mltd/mltd.h
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <limits>
+
+typedef struct {
+  int distance;
+  int similarity;
+  double normalized_similarity;
+} MLTDResult;
+MLTDResult calc_mltd(const char *tree1, const char *tree2);
