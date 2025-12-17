29cd4106a6af130217715b34257cbf373b40fc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 08:07:23 +0000

    Add package ghsa-client/1.3.0/ghsa_client-1.3.0-py3-none-any.whl

---
+++ packages/ghsa-client/ghsa_client-1.3.0-py3-none-any.whl/ghsa_client/exceptions/cvss.py
@@ -0,0 +1,7 @@
+"""CVSS-specific exceptions."""
+
+
+class InvalidCVSSError(Exception):
+    """Raised when CVSS data is invalid."""
+
+    pass
---
+++ packages/ghsa-client/ghsa_client-1.3.0-py3-none-any.whl/ghsa_client/models/__init__.py
@@ -0,0 +1,24 @@
+"""Models for GHSA operations."""
+
+from .advisory import Advisory, NoSourceCodeLocationFound
+from .cve_id import CVE_ID
+from .cvss import CVSS, CVSSVector
+from .ecosystem import Ecosystem
+from .ghsa_id import GHSA_ID, InvalidGHSAIDError
+from .package import Package
+from .version import VersionPredicate
+from .vulnerability import Vulnerability
+
+__all__ = [
+    "GHSA_ID",
+    "InvalidGHSAIDError",
+    "Advisory",
+    "NoSourceCodeLocationFound",
+    "CVE_ID",
+    "CVSS",
+    "CVSSVector",
+    "Ecosystem",
+    "Package",
+    "Vulnerability",
+    "VersionPredicate",
+]
