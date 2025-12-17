4586f31509b6783cacb27f05f8ffa2b148c89c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 05:09:01 +0000

    Add package artificer-objects/0.1.0a1/artificer_objects-0.1.0a1-py3-none-any.whl

---
+++ packages/artificer-objects/artificer_objects-0.1.0a1-py3-none-any.whl/artificer/object/__init__.py
@@ -0,0 +1,5 @@
+"""Object store module."""
+
+from artificer.object.store import objects
+
+__all__ = ["objects"]
---
+++ packages/artificer-objects/artificer_objects-0.1.0a1-py3-none-any.whl/artificer/object/store.py
@@ -0,0 +1,10 @@
+"""Object store implementation."""
+
+
+class ObjectStore:
+    """Object store for agents."""
+
+    pass
+
+
+objects = ObjectStore()
