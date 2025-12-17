f882d71cf620f202d4c42a5c2be350846486cdbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 02:38:36 +0000

    Add package zid/1.5.0/zid-1.5.0-cp312-cp312-manylinux_2_28_aarch64.whl

---
+++ packages/zid/zid-1.5.0-cp312-cp312-manylinux_2_28_aarch64.whl/zid/_lib.pyi
@@ -0,0 +1,14 @@
+def zid() -> int:
+    """
+    Generate a unique identifier.
+    """
+
+def zids(n: int, /) -> list[int]:
+    """
+    Generate a list of unique identifiers.
+    """
+
+def parse_zid_timestamp(zid: int) -> int:
+    """
+    Extract the UNIX timestamp in milliseconds from a ZID.
+    """
