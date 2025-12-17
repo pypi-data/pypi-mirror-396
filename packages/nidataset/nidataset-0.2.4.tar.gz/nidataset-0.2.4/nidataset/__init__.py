4a541f61884474b0d0714df6173898bb4bc51944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 03:32:02 +0000

    Add package nidataset/0.2.4/nidataset-0.2.4.tar.gz

---
+++ packages/nidataset/nidataset-0.2.4.tar.gz/nidataset-0.2.4/nidataset/__init__.py
@@ -0,0 +1,7 @@
+# nidataset/__init__.py
+
+from . import draw
+from . import preprocessing
+from . import slices
+from . import volume
+from . import utility
---
+++ packages/nidataset/nidataset-0.2.4.tar.gz/nidataset-0.2.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nidataset",
+    version="0.2.4",
+    description="NIfTI dataset management package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Giulio Russo, Ciro Russo",
+    author_email="russogiulio1998@icloud.com, ciro.russo2910@gmail.com",
+    url="https://github.com/GiulioRusso/Ni-Dataset",
+    packages=find_packages(),
+    license="MIT License",
+    install_requires=[
+        "Pillow>=9.4.0",
+        "nibabel>=5.1.0",
+        "numpy>=1.24.2",
+        "torch>=2.2.2",
+        "scikit-image>=0.19.3",
+        "pandas>=1.5.3",
+        "SimpleITK>=2.2.1",
+        "scipy>=1.10.0",
+        "tqdm>=4.67.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8.0",
+)
