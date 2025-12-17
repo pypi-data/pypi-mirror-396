c37453ad43749d2e62d46351963cf0aa1bd4ef7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 06:06:56 +0000

    Add package exif-sorter/1.0.0/exif_sorter-1.0.0-py3-none-any.whl

---
+++ packages/exif-sorter/exif_sorter-1.0.0-py3-none-any.whl/exif_sorter/__init__.py
@@ -0,0 +1,7 @@
+"""Media File Sorter - Organize photos and videos by EXIF creation date."""
+
+__version__ = "1.0.0"
+
+from .sorter import MediaFileSorter
+
+__all__ = ["MediaFileSorter", "__version__"]
