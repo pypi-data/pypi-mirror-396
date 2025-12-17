111a06cc8210d9bbd32401657f87193331570474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 08:07:52 +0000

    Add package PyGPUkit/0.2.6/pygpukit-0.2.6-cp312-cp312-win_amd64.whl

---
+++ packages/PyGPUkit/pygpukit-0.2.6-cp312-cp312-win_amd64.whl/pygpukit/ops/__init__.py
@@ -0,0 +1,33 @@
+"""Operations module for PyGPUkit."""
+
+from pygpukit.ops.basic import (
+    add,
+    div,
+    exp,
+    gelu,
+    layernorm,
+    log,
+    matmul,
+    max,
+    mean,
+    mul,
+    relu,
+    sub,
+    sum,
+)
+
+__all__ = [
+    "add",
+    "sub",
+    "mul",
+    "div",
+    "exp",
+    "log",
+    "relu",
+    "gelu",
+    "layernorm",
+    "matmul",
+    "sum",
+    "mean",
+    "max",
+]
