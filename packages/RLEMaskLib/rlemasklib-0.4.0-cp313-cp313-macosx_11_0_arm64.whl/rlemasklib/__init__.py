498eb2350a754acdab8ad37c8ba5f2a8d4f64457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 06:06:35 +0000

    Add package RLEMaskLib/0.4.0/rlemasklib-0.4.0-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/RLEMaskLib/rlemasklib-0.4.0-cp313-cp313-macosx_11_0_arm64.whl/rlemasklib/__init__.py
@@ -0,0 +1,112 @@
+"""Library for manipulating masks stored in run-length-encoded format.
+
+This library is an extended version of the pycocotools library's RLE functions, originally developed by Piotr Dollár and
+Tsung-Yi Linfor the COCO dataset :footcite:`lin2014coco`.
+
+There are two ways to use this library:
+
+1. with global functions, which take RLE masks in a dictionary representation, with the keys 'counts' and 'size'
+2. with the :class:`RLEMask` class, which is an object-oriented way to manipulate RLE masks
+
+"""
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"
+
+__all__ = [
+    "RLEMask",
+    "encode",
+    "decode",
+    "compress",
+    "complement",
+    "decompress",
+    "ones",
+    "zeros",
+    "ones_like",
+    "zeros_like",
+    "full",
+    "empty",
+    "any",
+    "all",
+    "area",
+    "centroid",
+    "intersection",
+    "union",
+    "iou",
+    "difference",
+    "symmetric_difference",
+    "from_bbox",
+    "to_bbox",
+    "from_polygon",
+    "crop",
+    "pad",
+    "connected_components",
+    "largest_connected_component",
+    "remove_small_components",
+    "fill_small_holes",
+    "erode",
+    "dilate",
+    "opening",
+    "closing",
+    "erode2",
+    "dilate2",
+    "opening2",
+    "closing2",
+    "shift",
+    "BoolFunc",
+    "merge",
+]
+
+from rlemasklib.oop import RLEMask
+
+
+from rlemasklib.rlemasklib import (
+    encode,
+    decode,
+    compress,
+    complement,
+    decompress,
+    ones,
+    zeros,
+    ones_like,
+    zeros_like,
+    ones as full,
+    zeros as empty,
+    any,
+    all,
+    area,
+    centroid,
+    intersection,
+    union,
+    iou,
+    difference,
+    symmetric_difference,
+    from_bbox,
+    to_bbox,
+    from_polygon,
+    crop,
+    pad,
+    connected_components,
+    largest_connected_component,
+    remove_small_components,
+    fill_small_holes,
+    erode,
+    dilate,
+    opening,
+    closing,
+    erode2,
+    dilate2,
+    opening2,
+    closing2,
+    shift,
+    BoolFunc,
+    merge,
+)
+
+# set the __module__ attribute of all functions/classes to this module
+for x in __all__:
+    obj = globals()[x]
+    obj._module_original_ = obj.__module__
+    obj.__module__ = __name__
