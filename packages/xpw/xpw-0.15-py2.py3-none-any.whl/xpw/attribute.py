8ca1adfa152cfd0901f41e0f3c2fd9bd5bdeb8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 00:07:45 +0000

    Add package xpw/0.15/xpw-0.15-py2.py3-none-any.whl

---
+++ packages/xpw/xpw-0.15-py2.py3-none-any.whl/xpw/attribute.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+__project__ = "xpw"
+__version__ = "0.15"
+__urlhome__ = "https://github.com/bondbox/xpw/"
+__description__ = "Password management and authentication"
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
