d5621e874d074bf3114593b8c01b3d8b5dbe06f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 06:06:33 +0000

    Add package pyTibber/0.34.0/pytibber-0.34.0-py3-none-any.whl

---
+++ packages/pyTibber/pytibber-0.34.0-py3-none-any.whl/tibber/exceptions.py
@@ -0,0 +1,47 @@
+"""Exceptions"""
+
+from .const import API_ERR_CODE_UNKNOWN
+
+
+class SubscriptionEndpointMissingError(Exception):
+    """Exception raised when subscription endpoint is missing"""
+
+
+class UserAgentMissingError(Exception):
+    """Exception raised when user agent is missing"""
+
+
+class HttpExceptionError(Exception):
+    """Exception base for HTTP errors
+
+    :param status: http response code
+    :param message: http response message if any
+    :param extension_code: http response extension if any
+    """
+
+    def __init__(
+        self,
+        status: int,
+        message: str = "HTTP error",
+        extension_code: str = API_ERR_CODE_UNKNOWN,
+    ) -> None:
+        self.status = status
+        self.message = message
+        self.extension_code = extension_code
+        super().__init__(self.message)
+
+
+class FatalHttpExceptionError(HttpExceptionError):
+    """Exception raised for HTTP codes that are non-retriable"""
+
+
+class RetryableHttpExceptionError(HttpExceptionError):
+    """Exception raised for HTTP codes that are possible to retry"""
+
+
+class InvalidLoginError(FatalHttpExceptionError):
+    """Invalid login exception."""
+
+
+class NotForDemoUserError(FatalHttpExceptionError):
+    """Exception raised when trying to use a feature not available for demo users"""
