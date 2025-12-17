a4b71fced26de25f9f4f36005387068d7aab17a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 03:31:10 +0000

    Add package oneurai/0.1.2/oneurai-0.1.2.tar.gz

---
+++ packages/oneurai/oneurai-0.1.2.tar.gz/oneurai-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="oneurai",
+    version="0.1.2", # 👈  تم تحديث الإصدار
+    author="MTMA",
+    author_email="mtma@example.com",
+    description="A powerful AI client library for Oneurai MLOps platform",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://amosb.fun",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "requests",
+        "torch",
+        "tqdm",
+    ],
+)
