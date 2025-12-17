b8f65e7cd142d98132eb8700f80cf6562decf348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 01:34:24 +0000

    Add package django-core-micha/2.1.20/django_core_micha-2.1.20.tar.gz

---
+++ packages/django-core-micha/django_core_micha-2.1.20.tar.gz/django_core_micha-2.1.20/src/django_core_micha/auth/exception_handler.py
@@ -0,0 +1,33 @@
+# auth/api/exception_handler.py
+from rest_framework.views import exception_handler as drf_exception_handler
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+
+def _flatten(detail, field=None):
+    out = []
+
+    if isinstance(detail, dict):
+        for k, v in detail.items():
+            nested_field = f"{field}.{k}" if field else k
+            out.extend(_flatten(v, nested_field))
+        return out
+
+    if isinstance(detail, list):
+        for item in detail:
+            out.extend(_flatten(item, field))
+        return out
+
+    code = getattr(detail, "code", "error")
+    out.append({"field": field, "code": str(code)})
+    return out
+
+
+def custom_exception_handler(exc, context):
+    resp = drf_exception_handler(exc, context)
+    if resp is None:
+        return None
+
+    if isinstance(exc, ValidationError):
+        return Response({"errors": _flatten(resp.data)}, status=resp.status_code)
+
+    return resp
