f503197c9a58974c2f958b9336bd4579c457e12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 05:08:58 +0000

    Add package cynn/0.1.5/cynn-0.1.5-cp312-cp312-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/cynn/cynn-0.1.5-cp312-cp312-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/cynn/__init__.py
@@ -0,0 +1,10 @@
+# cynn - Cython wrappers for lightweight neural network libraries
+#
+# Import directly from submodules:
+#   from cynn.tinn import TinnNetwork, seed
+#   from cynn.genann import GenannNetwork
+#   from cynn.fann import FannNetwork
+#   from cynn.cnn import CNNNetwork, CNNLayer
+#   from cynn.kann import KannNeuralNetwork, GraphBuilder, ...
+
+__version__ = "0.1.5"
