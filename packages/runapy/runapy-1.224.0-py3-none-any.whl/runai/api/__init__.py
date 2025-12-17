bb124c4110969c57282b97e57de6f6c0c8b48823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 07:09:22 +0000

    Add package runapy/1.224.0/runapy-1.224.0-py3-none-any.whl

---
+++ packages/runapy/runapy-1.224.0-py3-none-any.whl/runai/api/__init__.py
@@ -0,0 +1,71 @@
+# flake8: noqa
+
+# import apis into api package
+from runai.api.ai_applications_api import AIApplicationsApi
+from runai.api.access_keys_api import AccessKeysApi
+from runai.api.access_rules_api import AccessRulesApi
+from runai.api.administrator_command_line_interface_api import (
+    AdministratorCommandLineInterfaceApi,
+)
+from runai.api.applications_api import ApplicationsApi
+from runai.api.audit_logs_api import AuditLogsApi
+from runai.api.clusters_api import ClustersApi
+from runai.api.compute_api import ComputeApi
+from runai.api.config_map_api import ConfigMapApi
+from runai.api.credentials_api import CredentialsApi
+from runai.api.datasources_api import DatasourcesApi
+from runai.api.datavolumes_api import DatavolumesApi
+from runai.api.departments_api import DepartmentsApi
+from runai.api.distributed_api import DistributedApi
+from runai.api.distributed_inferences_api import DistributedInferencesApi
+from runai.api.environment_api import EnvironmentApi
+from runai.api.events_api import EventsApi
+from runai.api.git_api import GitApi
+from runai.api.host_path_api import HostPathApi
+from runai.api.idps_api import IdpsApi
+from runai.api.inferences_api import InferencesApi
+from runai.api.logo_api import LogoApi
+from runai.api.me_api import MeApi
+from runai.api.nfs_api import NFSApi
+from runai.api.nvidianim_api import NVIDIANIMApi
+from runai.api.network_topologies_api import NetworkTopologiesApi
+from runai.api.node_pools_api import NodePoolsApi
+from runai.api.nodes_api import NodesApi
+from runai.api.notification_channels_api import NotificationChannelsApi
+from runai.api.notification_state_api import NotificationStateApi
+from runai.api.notification_types_api import NotificationTypesApi
+from runai.api.org_unit_api import OrgUnitApi
+from runai.api.pvc_api import PVCApi
+from runai.api.permissions_api import PermissionsApi
+from runai.api.pods_api import PodsApi
+from runai.api.policy_api import PolicyApi
+from runai.api.projects_api import ProjectsApi
+from runai.api.registry_api import RegistryApi
+from runai.api.reports_api import ReportsApi
+from runai.api.researcher_command_line_interface_api import (
+    ResearcherCommandLineInterfaceApi,
+)
+from runai.api.researcher_command_line_interface_deprecated_api import (
+    ResearcherCommandLineInterfaceDeprecatedApi,
+)
+from runai.api.revisions_api import RevisionsApi
+from runai.api.roles_api import RolesApi
+from runai.api.s3_api import S3Api
+from runai.api.secret_api import SecretApi
+from runai.api.service_accounts_api import ServiceAccountsApi
+from runai.api.settings_api import SettingsApi
+from runai.api.storage_class_configuration_api import StorageClassConfigurationApi
+from runai.api.storage_classes_api import StorageClassesApi
+from runai.api.subscriptions_api import SubscriptionsApi
+from runai.api.template_api import TemplateApi
+from runai.api.tenant_api import TenantApi
+from runai.api.tokens_api import TokensApi
+from runai.api.trainings_api import TrainingsApi
+from runai.api.user_applications_api import UserApplicationsApi
+from runai.api.users_api import UsersApi
+from runai.api.workload_properties_api import WorkloadPropertiesApi
+from runai.api.workload_templates_api import WorkloadTemplatesApi
+from runai.api.workloads_api import WorkloadsApi
+from runai.api.workloads_v2_api import WorkloadsV2Api
+from runai.api.workloads_batch_api import WorkloadsBatchApi
+from runai.api.workspaces_api import WorkspacesApi
