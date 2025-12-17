f73a06cd079f35017cd5a7c3d79be261027a194c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 06:07:22 +0000

    Add package movoid-debug/1.5.4/movoid_debug-1.5.4.tar.gz

---
+++ packages/movoid-debug/movoid_debug-1.5.4.tar.gz/movoid_debug-1.5.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='movoid_debug',
+    version='1.5.4',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='movoid',
+    author_email='bobrobotsun@163.com',
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "movoid_config",
+        "movoid_function>=1.8.0"
+    ],
+    extras_require={
+        'debug': ['pyside6', 'requests']
+    }
+)
