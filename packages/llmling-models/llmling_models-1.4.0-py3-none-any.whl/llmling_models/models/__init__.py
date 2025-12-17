1267f3b210185fe9bf813e52afe7ca903782ea10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 05:09:32 +0000

    Add package llmling-models/1.4.0/llmling_models-1.4.0-py3-none-any.whl

---
+++ packages/llmling-models/llmling_models-1.4.0-py3-none-any.whl/llmling_models/models/__init__.py
@@ -0,0 +1,22 @@
+"""Multi-models."""
+
+from __future__ import annotations
+
+
+from llmling_models.models.delegation import DelegationMultiModel
+from llmling_models.models.augmented import AugmentedModel
+from llmling_models.models.userselect import UserSelectModel
+from llmling_models.models.input_model import InputModel
+from llmling_models.models.pyodide_model import SimpleOpenAIModel
+from llmling_models.models.remote_input import RemoteInputModel
+from llmling_models.models.remote_model import RemoteProxyModel
+
+__all__ = [
+    "AugmentedModel",
+    "DelegationMultiModel",
+    "InputModel",
+    "RemoteInputModel",
+    "RemoteProxyModel",
+    "SimpleOpenAIModel",
+    "UserSelectModel",
+]
