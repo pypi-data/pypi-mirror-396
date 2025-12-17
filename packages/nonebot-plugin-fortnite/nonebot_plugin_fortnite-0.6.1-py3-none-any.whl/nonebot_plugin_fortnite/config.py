46cab1758f0ca0249a25c8b01ba7918d6acfcf4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 00:07:52 +0000

    Add package nonebot-plugin-fortnite/0.6.1/nonebot_plugin_fortnite-0.6.1-py3-none-any.whl

---
+++ packages/nonebot-plugin-fortnite/nonebot_plugin_fortnite-0.6.1-py3-none-any.whl/nonebot_plugin_fortnite/config.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import nonebot_plugin_localstore as store
+from nonebot import get_plugin_config
+from pydantic import BaseModel
+
+_cache_dir: Path = store.get_plugin_cache_dir()
+_data_dir: Path = store.get_plugin_data_dir()
+
+CHINESE_FONT_PATH: Path = _data_dir / "SourceHanSansSC-Bold-2.otf"
+GITHUB_RAW = "https://raw.githubusercontent.com/fllesser/nonebot-plugin-fortnite"
+
+
+class Config(BaseModel):
+    fortnite_api_key: str | None = None
+    fortnite_screenshot_from_github: bool = False
+    fortnite_github_proxy_url: str = "https://proxy.pipers.cn"
+
+    @property
+    def api_key(self) -> str | None:
+        return self.fortnite_api_key
+
+    @property
+    def screenshot_from_github(self) -> bool:
+        return self.fortnite_screenshot_from_github
+
+    @property
+    def raw_base_url(self) -> str:
+        proxy = self.fortnite_github_proxy_url.rstrip("/")
+        return f"{proxy}/{GITHUB_RAW}"
+
+    @property
+    def cache_dir(self) -> Path:
+        return _cache_dir
+
+    @property
+    def data_dir(self) -> Path:
+        return _data_dir
+
+    @property
+    def chinese_font_path(self) -> Path:
+        return CHINESE_FONT_PATH
+
+
+fconfig: Config = get_plugin_config(Config)
