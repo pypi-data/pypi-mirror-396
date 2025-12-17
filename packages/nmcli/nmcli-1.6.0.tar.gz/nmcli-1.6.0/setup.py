6db66da0879bb5fad040be43b626570c7df0dac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 00:07:58 +0000

    Add package nmcli/1.6.0/nmcli-1.6.0.tar.gz

---
+++ packages/nmcli/nmcli-1.6.0.tar.gz/nmcli-1.6.0/setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# This file is kept for backwards compatibility.
+# All configuration is now in pyproject.toml
+from setuptools import setup
+
+setup()
---
+++ packages/nmcli/nmcli-1.6.0.tar.gz/nmcli-1.6.0/tests/test_helper.py
@@ -0,0 +1,16 @@
+from nmcli._helper import (add_fields_option_if_needed,
+                           add_wait_option_if_needed)
+
+
+def test_add_wait_option_if_needed():
+    assert add_wait_option_if_needed(10) == ['--wait', '10']
+    assert add_wait_option_if_needed(11) == ['--wait', '11']
+    assert not add_wait_option_if_needed()
+    assert not add_wait_option_if_needed(None)
+
+
+def test_add_fields_option_if_needed():
+    assert add_fields_option_if_needed('all') == ['-f', 'all']
+    assert add_fields_option_if_needed('common') == ['-f', 'common']
+    assert not add_fields_option_if_needed()
+    assert not add_fields_option_if_needed(None)
