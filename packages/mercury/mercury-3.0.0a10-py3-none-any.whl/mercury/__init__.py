ed6d25b4e8455dc2ce819f840622b4a2e4eaa5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 07:09:41 +0000

    Add package mercury/3.0.0a10/mercury-3.0.0a10-py3-none-any.whl

---
+++ packages/mercury/mercury-3.0.0a10-py3-none-any.whl/mercury/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "3.0.0a10"
+
+# input widgets
+from .button import Button
+from .checkbox import Checkbox
+from .multiselect import MultiSelect
+from .number import NumberInput
+from .select import Select
+from .slider import Slider
+from .text import TextInput
+from .file import UploadFile
+
+
+from .expander import Expander
+from .columns import Columns
+from .md import Markdown
+from .indicator import Indicator
+from .tabs import Tabs
+from .table import Table
+
+from .chat.chat import Chat
+from .chat.chatinput import ChatInput
+from .chat.message import Message 
+
+from IPython.display import display
