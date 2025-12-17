607a8b72cf3f95bd931d088301f1a30acbd1a5a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 08:07:13 +0000

    Add package sji/0.4.0/sji-0.4.0-py3-none-any.whl

---
+++ packages/sji/sji-0.4.0-py3-none-any.whl/sji/__init__.py
@@ -0,0 +1,9 @@
+"""
+SJI - Eine einfache Python-Bibliothek für Job-Initialisierung.
+"""
+
+__version__ = "0.4.0"
+
+from .sji import SimpleJobInit, get_script_version
+
+__all__ = ["SimpleJobInit", "get_script_version"]     
