2b7ca32c89e8546888b3dbf0449d0b927af3884b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 03:31:24 +0000

    Add package agentbill-py-langchain/7.2.0/agentbill_py_langchain-7.2.0-py3-none-any.whl

---
+++ packages/agentbill-py-langchain/agentbill_py_langchain-7.2.0-py3-none-any.whl/agentbill_langchain/__init__.py
@@ -0,0 +1,9 @@
+"""AgentBill LangChain Integration
+
+Zero-config callback handler for tracking LangChain usage in AgentBill.
+"""
+
+from .callback import AgentBillCallback
+
+__version__ = "7.2.0"
+__all__ = ["AgentBillCallback"]
