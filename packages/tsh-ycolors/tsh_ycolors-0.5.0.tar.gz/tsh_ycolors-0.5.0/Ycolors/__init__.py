f1088d08afecf1b768809c7e31ee1c7a4fe9e4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 05:08:35 +0000

    Add package tsh-ycolors/0.5.0/tsh_ycolors-0.5.0.tar.gz

---
+++ packages/tsh-ycolors/tsh_ycolors-0.5.0.tar.gz/tsh_ycolors-0.5.0/Ycolors/__init__.py
@@ -0,0 +1,8 @@
+def red(s):
+    return(f"\033[1;31m{s}\033[0m")
+def green(s):
+    return(f"\033[1;32m{s}\033[0m")
+def yellow(s):
+    return(f"\033[1;33m{s}\033[0m")
+def blue(s):
+    return(f"\033[1;34m{s}\033[0m")
