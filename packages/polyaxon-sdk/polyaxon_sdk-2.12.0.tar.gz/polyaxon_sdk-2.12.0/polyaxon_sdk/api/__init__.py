75d220f78b976658b479ef98a04867b9c695f288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 03:31:14 +0000

    Add package polyaxon-sdk/2.12.0/polyaxon_sdk-2.12.0.tar.gz

---
+++ packages/polyaxon-sdk/polyaxon_sdk-2.12.0.tar.gz/polyaxon_sdk-2.12.0/polyaxon_sdk/api/__init__.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+
+# import apis into api package
+from polyaxon_sdk.api.agents_v1_api import AgentsV1Api
+from polyaxon_sdk.api.artifacts_stores_v1_api import ArtifactsStoresV1Api
+from polyaxon_sdk.api.auth_v1_api import AuthV1Api
+from polyaxon_sdk.api.connections_v1_api import ConnectionsV1Api
+from polyaxon_sdk.api.dashboards_v1_api import DashboardsV1Api
+from polyaxon_sdk.api.organizations_v1_api import OrganizationsV1Api
+from polyaxon_sdk.api.policies_v1_api import PoliciesV1Api
+from polyaxon_sdk.api.presets_v1_api import PresetsV1Api
+from polyaxon_sdk.api.project_dashboards_v1_api import ProjectDashboardsV1Api
+from polyaxon_sdk.api.project_searches_v1_api import ProjectSearchesV1Api
+from polyaxon_sdk.api.projects_v1_api import ProjectsV1Api
+from polyaxon_sdk.api.queues_v1_api import QueuesV1Api
+from polyaxon_sdk.api.runs_v1_api import RunsV1Api
+from polyaxon_sdk.api.schemas_v1_api import SchemasV1Api
+from polyaxon_sdk.api.searches_v1_api import SearchesV1Api
+from polyaxon_sdk.api.service_accounts_v1_api import ServiceAccountsV1Api
+from polyaxon_sdk.api.tags_v1_api import TagsV1Api
+from polyaxon_sdk.api.teams_v1_api import TeamsV1Api
+from polyaxon_sdk.api.users_v1_api import UsersV1Api
+from polyaxon_sdk.api.versions_v1_api import VersionsV1Api
+
