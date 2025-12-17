c289e0787c51283dc981f2430ace383f99b1b400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 00:08:34 +0000

    Add package adancfgd/0.1.1/adancfgd-0.1.1.tar.gz

---
+++ packages/adancfgd/adancfgd-0.1.1.tar.gz/adancfgd-0.1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+# This is a simple setup.py file for backward compatibility
+# The actual configuration is in pyproject.toml and setup.cfg
+
+if __name__ == "__main__":
+    setup()
