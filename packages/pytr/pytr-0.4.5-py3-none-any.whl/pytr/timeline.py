73d4fca0279e975d0f2156f2da05ec5340ac9ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 06:07:16 +0000

    Add package pytr/0.4.5/pytr-0.4.5-py3-none-any.whl

---
+++ packages/pytr/pytr-0.4.5-py3-none-any.whl/pytr/timeline.py
@@ -0,0 +1,361 @@
+import json
+from datetime import datetime
+
+from .api import TradeRepublicError
+from .utils import get_logger, preview
+
+MAX_EVENT_REQUEST_BATCH = 1000
+
+
+def is_likely_same_but_newer(event, old_event):
+    if event["title"] != old_event["title"]:
+        return False
+
+    if (
+        event["subtitle"] != "Limit-Sell-Order"
+        and event["subtitle"] != "Limit-Buy-Order"
+        and event["subtitle"] != "Sparplan ausgeführt"
+    ):
+        return False
+
+    if event["subtitle"] != old_event["subtitle"]:
+        return False
+
+    # Check timestamps
+    fmt = "%Y-%m-%dT%H:%M:%S.%f%z"
+    date_new = datetime.strptime(event["timestamp"], fmt)
+    date_old = datetime.strptime(old_event["timestamp"], fmt)
+
+    if date_new < date_old:
+        return False
+
+    return abs((date_new - date_old).total_seconds() * 1000) <= 500
+
+
+class Timeline:
+    def __init__(
+        self,
+        tr,
+        output_path,
+        not_before=float(0),
+        not_after=float("inf"),
+        store_event_database=True,
+        scan_for_duplicates=False,
+        dump_raw_data=False,
+        event_callback=lambda *a, **kw: None,
+    ):
+        self.tr = tr
+        self.output_path = output_path
+        if not_before == -1:
+            self.fetch_from_tr = False
+            self.not_before = float(0)
+        else:
+            self.fetch_from_tr = True
+            self.not_before = not_before
+        self.not_after = not_after
+        self.store_event_database = store_event_database
+        self.scan_for_duplicates = scan_for_duplicates
+        self.dump_raw_data = dump_raw_data
+        self.event_callback = event_callback
+        self.log = get_logger(__name__)
+        self.dl_done = False
+        self.error_counts = {}
+        self.num_timelines = 0
+        self.all_detail = 0
+        self.requested_detail = 0
+        self.received_detail = 0
+        self.skipped_detail = 0
+        self.detail_digits = 0
+        self.timeline_transactions = {}
+        self.timeline_activities = {}
+        self.timeline_details = {}
+        self.events = []
+
+        output_path.mkdir(parents=True, exist_ok=True)
+
+    async def tl_loop(self):
+        await self.get_next_timeline_transactions(None)
+
+        # We might not want to download anything from TR but just create/update account_transactions.csv from available data
+        if not self.fetch_from_tr:
+            self.finish_timeline_details()
+
+        while not self.dl_done:
+            try:
+                _, subscription, response = await self.tr.recv()
+            except TradeRepublicError as e:
+                self.log.error(f'Error response for subscription "{e.subscription}".')
+                subscriptionid = e.subscription["id"]
+                curct = self.error_counts.get(subscriptionid, 0)
+                self.log.error(f'Errorcount for subscription {subscriptionid} is {curct}".')
+                if curct < 3:
+                    self.log.error(f'Error count for subscription {subscriptionid} is {curct}". Re-subscribing...')
+                    self.error_counts[subscriptionid] = curct + 1
+                    await self.tr.subscribe(e.subscription)
+                    continue
+                else:
+                    self.log.error(
+                        f'Error count for subscription {subscriptionid} is {curct}". Continuing with failure...'
+                    )
+                    subscription = e.subscription
+                    response = {}
+
+            if subscription.get("type", "") == "timelineTransactions":
+                await self.get_next_timeline_transactions(response)
+            elif subscription.get("type", "") == "timelineActivityLog":
+                await self.get_next_timeline_activity_log(response)
+            elif subscription.get("type", "") == "timelineDetailV2":
+                await self.process_timelineDetail(response, subscription.get("id"))
+            else:
+                self.log.warning(f"unmatched subscription of type '{subscription['type']}':\n{preview(response)}")
+
+    async def get_next_timeline_transactions(self, response):
+        """
+        Get timeline transactions and store them in list timeline_transactions
+        """
+        if response is None:
+            # empty response / first timeline
+            self.log.info("Timeline transactions: Subscribing to #1...")
+            self.num_timelines = 0
+            await self.tr.timeline_transactions()
+        else:
+            self.num_timelines += 1
+            added_last_event = False
+            for event in response["items"]:
+                event_timestamp = datetime.fromisoformat(event["timestamp"][:19]).timestamp()
+                if event_timestamp > self.not_before:
+                    if event_timestamp < self.not_after:
+                        event["source"] = "timelineTransaction"
+                        self.timeline_transactions[event["id"]] = event
+                    added_last_event = True
+                else:
+                    break
+
+            after = response["cursors"].get("after")
+            if (after is not None) and added_last_event:
+                self.log.info(
+                    f"Timeline transactions: Received #{self.num_timelines}, subscribing to #{self.num_timelines + 1}..."
+                )
+                await self.tr.timeline_transactions(after)
+            else:
+                # last timeline is reached
+                self.log.info(f"Timeline transactions: Received #{self.num_timelines} (last relevant).")
+                if self.dump_raw_data:
+                    with (self.output_path / "timeline_transactions.json").open("w") as f:
+                        json.dump(self.timeline_transactions, f, indent=4)
+                await self.get_next_timeline_activity_log(None)
+
+    async def get_next_timeline_activity_log(self, response):
+        """
+        Get timeline acvtivity log events and store them in list timeline_activities
+        """
+        if response is None:
+            # empty response / first timeline
+            self.log.info("Timeline activity log: Subscribing to #1...")
+            self.num_timelines = 0
+            await self.tr.timeline_activity_log()
+        else:
+            self.num_timelines += 1
+            added_last_event = False
+            for event in response["items"]:
+                event_timestamp = datetime.fromisoformat(event["timestamp"][:19]).timestamp()
+                if event_timestamp > self.not_before:
+                    if event_timestamp < self.not_after:
+                        event["source"] = "timelineActivity"
+                        self.timeline_activities[event["id"]] = event
+                    added_last_event = True
+                else:
+                    break
+
+            after = response["cursors"].get("after")
+            if (after is not None) and added_last_event:
+                self.log.info(
+                    f"Timeline activity log: Received #{self.num_timelines}, subscribing to #{self.num_timelines + 1}..."
+                )
+                await self.tr.timeline_activity_log(after)
+            else:
+                self.log.info(f"Timeline activity log: Received #{self.num_timelines} (last relevant).")
+                if self.dump_raw_data:
+                    with (self.output_path / "timeline_activities.json").open("w") as f:
+                        json.dump(self.timeline_activities, f, indent=4)
+
+                duplicates = set(self.timeline_transactions) & set(self.timeline_activities)
+                if duplicates:
+                    self.log.warning(f"Received duplicate events: {', '.join(duplicates)}")
+
+                self.timeline_details = {**self.timeline_transactions, **self.timeline_activities}
+
+                self.request_timeline_details_generator = self._request_timeline_details()
+                try:
+                    await self.request_timeline_details_generator.__anext__()
+                except StopAsyncIteration:
+                    pass
+
+    async def _request_timeline_details(self):
+        """
+        request timeline details
+        """
+        self.all_detail = len(self.timeline_details.values())
+        self.detail_digits = len(str(self.all_detail))
+
+        for event in self.timeline_details.values():
+            msg = None
+            action = event.get("action")
+            if action is None:
+                if event.get("actionLabel") is None:
+                    msg = "Skip timeline detail: No action/actionLabel section"
+            elif action.get("type") != "timelineDetail":
+                msg = f"Skip timeline detail: Action type {action['type']} is not timelineDetail"
+            elif action.get("payload") != event["id"]:
+                msg = f"Skip timeline detail: Action payload {action['payload']} does not match id {event['id']}"
+
+            self.requested_detail += 1
+            if msg is None:
+                await self.tr.timeline_detail_v2(event["id"])
+            else:
+                self.received_detail += 1
+                self.events.append(event)
+                self.log.warning(
+                    f"{self.received_detail + self.skipped_detail:>{self.detail_digits}}/{self.all_detail}: "
+                    + f"{event['title']} -- {event['subtitle']} - {event['timestamp'][:19]} {msg}"
+                )
+                self.log.debug("%s: %s", msg, json.dumps(event, indent=4))
+
+            if self.requested_detail % MAX_EVENT_REQUEST_BATCH == 0 and (
+                (self.received_detail + self.skipped_detail) < self.requested_detail
+            ):
+                self.log.info(f"Requested {self.requested_detail}/{self.all_detail} timeline details.")
+                yield
+
+        self.log.info(f"Requested all timeline details ({self.requested_detail}/{self.all_detail}).")
+        self.finish_if_done()
+
+    async def request_more_timeline_details(self):
+        if self.requested_detail == self.all_detail:
+            return
+        if (self.received_detail + self.skipped_detail) == self.requested_detail:
+            try:
+                await self.request_timeline_details_generator.__anext__()
+            except StopAsyncIteration:
+                pass
+
+    async def process_timelineDetail(self, response, subscription_id):
+        """
+        process timeline details response
+        """
+
+        event = self.timeline_details.get(subscription_id)
+
+        if event is None:
+            self.log.warning(f"Ignoring unrequested event response {json.dumps(response, indent=4)}")
+            self.skipped_detail += 1
+            self.finish_if_done()
+            return
+
+        self.received_detail += 1
+        event["details"] = response
+
+        self.log.info(
+            f"{self.received_detail + self.skipped_detail:>{self.detail_digits}}/{self.all_detail}: "
+            + f"{event['title']} -- {event['subtitle']} - {event['timestamp'][:19]}"
+        )
+        self.events.append(event)
+        self.event_callback(event)
+
+        await self.request_more_timeline_details()
+        self.finish_if_done()
+
+    def finish_if_done(self):
+        if self.requested_detail != self.all_detail:
+            return
+        if (self.received_detail + self.skipped_detail) == self.requested_detail:
+            self.finish_timeline_details()
+
+    def finish_timeline_details(self):
+        if self.fetch_from_tr:
+            self.log.info("Received all event details.")
+            if self.skipped_detail > 0:
+                self.log.warning(f"Skipped {self.skipped_detail} unsupported events")
+        else:
+            self.log.info("Skip fetching data from TR.")
+
+        if self.store_event_database:
+            self.log.info("Updating event database...")
+
+            # read old events from all_events.json
+            old_events = []
+            all_events_path = self.output_path / "all_events.json"
+            if all_events_path.exists():
+                self.log.info("Reading event database...")
+                with open(all_events_path, "r", encoding="utf-8") as f:
+                    old_events = json.load(f)
+
+            # if we have new data from a certain period, throw out old data
+            if self.fetch_from_tr and (self.not_before != 0 or self.not_after != float("inf")):
+                self.log.info("Throwing away outdated events...")
+                for i in range(len(old_events) - 1, -1, -1):
+                    ts = datetime.fromisoformat(old_events[i]["timestamp"][:19]).timestamp()
+                    if ts > self.not_before and ts < self.not_after:
+                        del old_events[i]
+
+            # merge new and old events
+            if old_events:
+                cur_events = {}
+
+                # drop duplicates in old events
+                if old_events:
+                    if self.scan_for_duplicates:
+                        self.log.info("Adding old events (scanning for duplicates)...")
+                        for event in old_events:
+                            idtodel = None
+                            for id in cur_events:
+                                cur_event = cur_events[id]
+                                if is_likely_same_but_newer(event, cur_event):
+                                    self.log.warning(
+                                        f"Dropping potential duplicate event {id} from {cur_event['timestamp']} due to newer event {event['id']} from {event['timestamp']}."
+                                    )
+                                    idtodel = id
+                                    break
+                            if idtodel is not None:
+                                cur_events.pop(idtodel)
+                            cur_events[event["id"]] = event
+                    else:
+                        self.log.info("Adding old events...")
+                        for event in old_events:
+                            cur_events[event["id"]] = event
+
+                # add new events
+                if self.events:
+                    if self.scan_for_duplicates:
+                        self.log.info("Adding new events (scanning for duplicates)...")
+                        for event in self.events:
+                            idtodel = None
+                            for id in cur_events:
+                                cur_event = cur_events[id]
+                                if event["id"] != id and is_likely_same_but_newer(event, cur_event):
+                                    self.log.warning(
+                                        f"Dropping existing event {id} from {cur_event['timestamp']} due to newer event {event['id']} from {event['timestamp']}."
+                                    )
+                                    idtodel = id
+                                    break
+                            if idtodel is not None:
+                                cur_events.pop(idtodel)
+                            cur_events[event["id"]] = event
+                    else:
+                        self.log.info("Adding new events...")
+                        for event in self.events:
+                            cur_events[event["id"]] = event
+
+                self.events = list(cur_events.values())
+
+            self.log.info("Sorting events...")
+            self.events.sort(key=lambda value: datetime.fromisoformat(value["timestamp"][:19]))
+
+            if self.fetch_from_tr:
+                self.log.info(f"Writing {all_events_path}...")
+                with open(all_events_path, "w", encoding="utf-8") as f:
+                    json.dump(self.events, f, ensure_ascii=False, indent=2, default=str)
+
+            self.log.info("Updated event database.")
+
+        self.dl_done = True
---
+++ packages/pytr/pytr-0.4.5-py3-none-any.whl/pytr/transactions.py
@@ -0,0 +1,291 @@
+import csv
+import json
+import platform
+from dataclasses import dataclass
+from locale import getdefaultlocale
+from typing import Any, Iterable, Literal, Optional, TextIO, TypedDict, Union
+
+from babel.numbers import format_decimal
+
+from .event import ConditionalEventType, Event, PPEventType
+from .translation import setup_translation
+from .utils import get_logger
+
+SUPPORTED_LANGUAGES = {
+    "cs",
+    "da",
+    "de",
+    "en",
+    "es",
+    "fr",
+    "it",
+    "nl",
+    "pl",
+    "pt",
+    "ru",
+    "zh",
+}
+
+CSVCOLUMN_TO_TRANSLATION_KEY = {
+    "date": "CSVColumn_Date",
+    "type": "CSVColumn_Type",
+    "value": "CSVColumn_Value",
+    "note": "CSVColumn_Note",
+    "isin": "CSVColumn_ISIN",
+    "shares": "CSVColumn_Shares",
+    "fees": "CSVColumn_Fees",
+    "taxes": "CSVColumn_Taxes",
+    "isin2": "CSVColumn_ISIN2",
+    "shares2": "CSVColumn_Shares2",
+}
+
+
+class _SimpleTransaction(TypedDict):
+    date: str
+    type: Union[str, None]
+    value: Union[str, float, None]
+    note: Union[str, None]
+    isin: Union[str, None]
+    shares: Union[str, float, None]
+    fees: Union[str, float, None]
+    taxes: Union[str, float, None]
+    isin2: Union[str, None]
+    shares2: Union[str, float, None]
+
+
+@dataclass
+class TransactionExporter:
+    """
+    A helper class to convert Trade Republic events each to one or more line items that are a simplified representation
+    useful for a importing for example into a portfolio manager.
+    """
+
+    lang: str = "en"
+    """ The language for the CSV header / JSON keys. """
+
+    date_with_time: bool = True
+    """ Include the timestamp in ISO8601 format in the date field. """
+
+    decimal_localization: bool = False
+    """ Whether to localize the decimal format. If enabled, decimal fields will be string values. """
+
+    csv_delimiter: str = ";"
+
+    def __post_init__(self):
+        self._log = get_logger(__name__)
+
+        if self.lang == "auto":
+            locale = getdefaultlocale()[0]
+            if locale is None:
+                self.lang = "en"
+            else:
+                self.lang = locale.split("_")[0]
+
+        if self.lang not in SUPPORTED_LANGUAGES:
+            self._log.info(f'Language not yet supported "{self.lang}", defaulting to "en"')
+            self.lang = "en"
+
+        self._translate = setup_translation(language=self.lang)
+
+    def _decimal_format(self, value: Optional[float], quantization: bool = True) -> Union[str, float, None]:
+        if value is None:
+            return None
+        return (
+            format_decimal(value, locale=self.lang, decimal_quantization=quantization)
+            if self.decimal_localization
+            else value
+        )
+
+    def _localize_keys(self, txn: _SimpleTransaction) -> dict[str, Any]:
+        return {self._translate(value): txn[key] for key, value in CSVCOLUMN_TO_TRANSLATION_KEY.items()}  # type: ignore[literal-required]
+
+    def fields(self) -> list[str]:
+        return [self._translate(value) for key, value in CSVCOLUMN_TO_TRANSLATION_KEY.items()]
+
+    def from_event(self, event: Event) -> Iterable[dict[str, Any]]:
+        """
+        Given an event, produces one or more JSON objects representing a transaction. The returned object contains
+        the given fields, localized in the selected language.
+
+        - `date`
+        - `type`
+        - `value`
+        - `note`
+        - `isin`
+        - `shares`
+        - `fees`
+        - `taxes`
+        - `isin2`
+        - `shares2`
+        """
+
+        if event.event_type is None:
+            return
+
+        kwargs: _SimpleTransaction = {
+            "date": event.date.isoformat() if self.date_with_time else event.date.date().isoformat(),
+            "type": self._translate(event.event_type.value) if isinstance(event.event_type, PPEventType) else None,
+            "value": self._decimal_format(event.value),
+            "note": self._translate(event.note) + " - " + event.title if event.note is not None else event.title,
+            "isin": event.isin,
+            "shares": self._decimal_format(event.shares, False),
+            "fees": self._decimal_format(-event.fees) if event.fees is not None else None,
+            "taxes": self._decimal_format(-event.taxes) if event.taxes is not None else None,
+            "isin2": event.isin2,
+            "shares2": self._decimal_format(event.shares2, False),
+        }
+
+        if event.event_type == ConditionalEventType.TRADE_INVOICE:
+            assert event.value is not None, event
+            ev_value = event.value
+            if event.shares2:
+                kwargs2 = kwargs.copy()
+                kwargs2["type"] = self._translate((PPEventType.BUY if event.value < 0 else PPEventType.SELL).value)
+                kwargs2["note"] = event.isin2
+                if event.isin2 == "ORSTED A/S EM.09/25 DK 10":
+                    kwargs2["isin"] = "DK0064307755"
+                else:
+                    kwargs2["isin"] = event.isin2
+                kwargs2["shares"] = self._decimal_format(event.shares2, False)
+                yield self._localize_keys(kwargs2)
+                kwargs["value"] = self._decimal_format(0)
+                ev_value = 0
+
+            kwargs["type"] = self._translate((PPEventType.BUY if ev_value < 0 else PPEventType.SELL).value)
+        elif event.event_type == PPEventType.SPINOFF:
+            if event.shares2:
+                kwargs2 = kwargs.copy()
+                kwargs2["type"] = self._translate(PPEventType.SELL.value)
+                yield self._localize_keys(kwargs2)
+
+            # kwargs["type"] = self._translate(PPEventType.BUY.value)
+            kwargs["isin2"] = event.isin
+            if event.note == "BlackRock Funding":
+                kwargs["isin"] = "US09290D1019"
+            elif event.note == "BYD":
+                kwargs["isin"] = "CNE100000296"
+            elif event.note == "Chipotle":
+                kwargs["isin"] = "US1696561059"
+            elif event.note == "Eckert & Ziegler":
+                kwargs["isin"] = "DE0005659700"
+            elif event.note == "Enovix Corp. WTS 01.10.26":
+                kwargs["isin"] = "US2935941318"
+            elif event.note == "Gamestop Corp. WTS 30.10.26":
+                kwargs["isin"] = "US36467W1172"
+            elif event.note == "GLOBALSTAR INC. O.N.":
+                kwargs["isin"] = "US3789735079"
+            elif event.note == "Magnum Ice Cream":
+                kwargs["isin"] = "NL0015002MS2"
+            elif event.note == "Netflix":
+                kwargs["isin"] = "US64110L1061"
+            elif event.note == "NVIDIA":
+                kwargs["isin"] = "US67066G1040"
+            elif event.note == "Orsted":
+                kwargs["isin"] = "DK0060094928"
+            elif event.note == "ORSTED A/S   -ANR-":
+                kwargs["isin"] = "DK0064307839"
+            elif event.note == "ROCKET LAB CORP. O.N.":
+                kwargs["isin"] = "US7731211089"
+            elif event.note == "TKMS":
+                kwargs["isin"] = "DE000TKMS001"
+            elif event.note == "Unilever":
+                kwargs["isin"] = "GB00BVZK7T90"
+            else:
+                kwargs["isin"] = event.isin2
+            if event.shares2:
+                kwargs["shares"] = self._decimal_format(event.shares2, False)
+            kwargs["note"] = event.note
+        # Special case for saveback events. Example payload: https://github.com/pytr-org/pytr/issues/116#issuecomment-2377491990
+        # With saveback, a small amount already invested into a savings plans is invested again, effectively representing
+        # a deposit (you get money from Trade Republic) and then a buy of the related asset.
+        elif event.event_type == ConditionalEventType.SAVEBACK:
+            assert event.value is not None, event
+            kwargs["type"] = self._translate(PPEventType.BUY.value)
+            yield self._localize_keys(kwargs)
+
+            kwargs = kwargs.copy()
+            kwargs["type"] = self._translate(PPEventType.DEPOSIT.value)
+            kwargs["value"] = self._decimal_format(-event.value)
+            kwargs["isin"] = None
+            kwargs["shares"] = None
+        elif event.event_type == ConditionalEventType.PRIVATE_MARKETS_ORDER:
+            if event.isin == "LU3176111881":
+                kwargs["note"] = "EQT"
+            elif event.isin == "LU3170240538":
+                kwargs["note"] = "Apollo"
+
+            assert event.value is not None, event
+            kwargs["type"] = self._translate((PPEventType.BUY if event.value < 0 else PPEventType.SELL).value)
+            if event.note == "1 % Bonus":
+                yield self._localize_keys(kwargs)
+
+                kwargs = kwargs.copy()
+                kwargs["type"] = self._translate(PPEventType.DEPOSIT.value)
+                kwargs["value"] = self._decimal_format(-event.value)
+                kwargs["isin"] = None
+                kwargs["shares"] = None
+        elif event.event_type == PPEventType.SWAP:
+            if event.note == "BlackRock Funding":
+                kwargs["isin2"] = "US09290D1019"
+            elif event.note == "BYD":
+                kwargs["isin2"] = "CNE100000296"
+            elif event.note == "Chipotle":
+                kwargs["isin2"] = "US1696561059"
+            elif event.note == "Eckert & Ziegler":
+                kwargs["isin2"] = "DE0005659700"
+            elif event.note == "Enovix Corp. WTS 01.10.26":
+                kwargs["isin2"] = "US2935941318"
+            elif event.note == "Gamestop Corp. WTS 30.10.26":
+                kwargs["isin2"] = "US36467W1172"
+            elif event.note == "GLOBALSTAR INC. O.N.":
+                kwargs["isin2"] = "US3789735079"
+            elif event.note == "Magnum Ice Cream":
+                kwargs["isin2"] = "NL0015002MS2"
+            elif event.note == "Netflix":
+                kwargs["isin2"] = "US64110L1061"
+            elif event.note == "NVIDIA":
+                kwargs["isin2"] = "US67066G1040"
+            elif event.note == "Orsted":
+                kwargs["isin2"] = "DK0060094928"
+            elif event.note == "ORSTED A/S   -ANR-":
+                kwargs["isin2"] = "DK0064307839"
+            elif event.note == "ORSTED A/S EM.09/25 DK 10":
+                kwargs["isin2"] = "DK0064307755"
+            elif event.note == "ROCKET LAB CORP. O.N.":
+                kwargs["isin2"] = "US7731211089"
+            elif event.note == "TKMS":
+                kwargs["isin2"] = "DE000TKMS001"
+            elif event.note == "Unilever":
+                kwargs["isin2"] = "GB00BVZK7T90"
+            else:
+                kwargs["isin2"] = event.note
+            kwargs["note"] = event.title
+
+        yield self._localize_keys(kwargs)
+
+    def export(
+        self,
+        fp: TextIO,
+        events: Iterable[Event],
+        sort: bool = False,
+        format: Literal["json", "csv"] = "csv",
+    ) -> None:
+        self._log.info("Exporting transactions...")
+        if sort:
+            events = sorted(events, key=lambda ev: ev.date)
+
+        transactions = (txn for event in events for txn in self.from_event(event))
+
+        if format == "csv":
+            lineterminator = "\n" if platform.system() == "Windows" else "\r\n"
+            writer = csv.DictWriter(
+                fp, fieldnames=self.fields(), delimiter=self.csv_delimiter, lineterminator=lineterminator
+            )
+            writer.writeheader()
+            writer.writerows(transactions)
+        elif format == "json":
+            for txn in transactions:
+                fp.write(json.dumps(txn))
+                fp.write("\n")
+
+        self._log.info("Transactions exported.")
