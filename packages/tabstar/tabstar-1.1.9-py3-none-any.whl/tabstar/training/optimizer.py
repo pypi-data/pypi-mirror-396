7c1f62111c8239bf2c29e3638041b39eae59f9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 04:14:13 +0000

    Add package tabstar/1.1.9/tabstar-1.1.9-py3-none-any.whl

---
+++ packages/tabstar/tabstar-1.1.9-py3-none-any.whl/tabstar/training/optimizer.py
@@ -0,0 +1,29 @@
+from torch import nn
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import OneCycleLR, LRScheduler
+
+WARMUP_PROPORTION = 0.1
+
+
+def get_optimizer(model: nn.Module, lr: float, wd: float) -> AdamW:
+    assert isinstance(model, nn.Module)
+    biases, weights = [], []
+    for name, p in model.named_parameters():
+        if "bias" in name:
+            biases.append(p)
+        else:
+            weights.append(p)
+    param_groups = [{"params": weights, "weight_decay": wd, "lr": lr, "name": "weights"},
+                    {"params": biases, "weight_decay": 0.0, "lr": lr, "name": "biases"}]
+    optimizer = AdamW(param_groups)
+    return optimizer
+
+def get_scheduler(optimizer: AdamW, max_lr: float, epochs: int) -> LRScheduler:
+    # TODO: we currently use total_steps=epochs, but it expects to be affected by accumulation steps.
+    # We might want to reconsider setting this depending on the expected number of updates.
+    try:
+        return OneCycleLR(optimizer=optimizer, max_lr=max_lr, total_steps=epochs,
+                          pct_start=WARMUP_PROPORTION, anneal_strategy='cos')
+    except ZeroDivisionError:
+        return OneCycleLR(optimizer=optimizer, max_lr=max_lr, total_steps=epochs+1,
+                          pct_start=WARMUP_PROPORTION, anneal_strategy='cos')
