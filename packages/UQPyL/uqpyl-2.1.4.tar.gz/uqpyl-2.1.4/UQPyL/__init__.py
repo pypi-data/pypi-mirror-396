426bdb579d72071c025c357296e889b445a33e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 00:08:18 +0000

    Add package UQPyL/2.1.4/uqpyl-2.1.4.tar.gz

---
+++ packages/UQPyL/uqpyl-2.1.4.tar.gz/uqpyl-2.1.4/UQPyL/__init__.py
@@ -0,0 +1,14 @@
+from . import problem, surrogate, optimization, analysis, doe, util
+
+__version__ = "2.1.4"
+__author__ = "wmtSky"
+
+__all__=[
+    "problem",
+    "surrogate",
+    "optimization",
+    "analysis",
+    "doe",
+    "inference",
+    "util"
+]
