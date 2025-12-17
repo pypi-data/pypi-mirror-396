2036fa25b4cb56f2c6f2daeaa0f9a976480296bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 08:08:07 +0000

    Add package log-surgeon-ffi/0.1.0b8/log_surgeon_ffi-0.1.0b8-cp311-cp311-musllinux_1_2_i686.whl

---
+++ packages/log-surgeon-ffi/log_surgeon_ffi-0.1.0b8-cp311-cp311-musllinux_1_2_i686.whl/log_surgeon_ffi/PyObjectCast.hpp
@@ -0,0 +1,127 @@
+#ifndef LOG_SURGEON_FFI_PY_OBJECT_CAST_HPP
+#define LOG_SURGEON_FFI_PY_OBJECT_CAST_HPP
+
+#include <wrapped_facade_headers/Python.hpp>
+
+#include <type_traits>
+
+namespace log_surgeon_ffi {
+/**
+ * Casts a given function pointer to a PyCFunction.
+ * The main purpose of this function is to silence clang-tidy checks on using
+ * `reinterpret_cast`. It does not perform any further function type checking.
+ * @tparam Src The source function pointer type.
+ * @param src The source function pointer.
+ * @return PyCFunction using reinterpret_cast.
+ */
+template <typename Src>
+auto py_c_function_cast(Src src) noexcept -> PyCFunction {
+    static_assert(std::is_pointer_v<Src>);
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+    return reinterpret_cast<PyCFunction>(src);
+}
+
+/**
+ * Casts a given function pointer to a `getbufferproc` CPython function. The
+ * main purpose of this function is to silence clang-tidy checks on using
+ * `reinterpret_cast`. It does not perform any further function type checking.
+ * @tparam Src The source function pointer type.
+ * @param src The source function pointer.
+ * @return `getbufferproc` using reinterpret_cast.
+ */
+template <typename Src>
+auto py_getbufferproc_cast(Src src) noexcept -> getbufferproc {
+    static_assert(std::is_pointer_v<Src>);
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+    return reinterpret_cast<getbufferproc>(src);
+}
+
+/**
+ * Casts a given function pointer to a `releasebufferproc` CPython function. The
+ * main purpose of this function is to silence clang-tidy checks on using
+ * `reinterpret_cast`. It does not perform any further function type checking.
+ * @tparam Src The source function pointer type.
+ * @param src The source function pointer.
+ * @return `releasebufferproc` using reinterpret_cast.
+ */
+template <typename Src>
+auto py_releasebufferproc_cast(Src src) noexcept -> releasebufferproc {
+    static_assert(std::is_pointer_v<Src>);
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+    return reinterpret_cast<releasebufferproc>(src);
+}
+
+/**
+ * This template struct is used as a compile-time flag that indicates whether
+ * type T is a PyObject or not. By default, `cValue` is set to false.
+ * @tparam T
+ */
+template <typename T>
+struct IsPythonObject {
+    static constexpr bool cValue = false;
+};
+
+/**
+ * This template const expression is a wrapper of underlying `cValue` stored in
+ * `IsPythonObject`, which is used to determine whether a type T is a valid
+ * Python object type.
+ * @tparam T
+ */
+template <typename T>  // NOLINTNEXTLINE(readability-identifier-naming)
+constexpr bool is_python_object_v{IsPythonObject<T>::cValue};
+
+/**
+ * The macro to create a specialization of `IsPythonObject` for a given type T.
+ * Only types that are marked with this macro will be considered as a valid
+ * Python object type.
+ */
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define LOG_SURGEON_FFI_MARK_AS_PYOBJECT(T) \
+    template <> \
+    struct IsPythonObject<T> { \
+        static constexpr bool cValue = true; \
+    }
+
+/**
+ * Casts a given Src pointer to a Dst pointer. It should behave as followed:
+ * 1. If Src and Dst are the same type, return `src` if their type is a valid
+ * Python object type, or they are both PyObject.
+ * 2. If Dst is PyObject, the cast is valid if Src is a PyObject. A pointer,
+ * Python*, should be returned.
+ * 3. If Src is PyObject, the cast is valid if Dst is a valid Python object
+ * type. A pointer, Dst*, should be returned.
+ * 4. Any other cases are considered as invalid cast.
+ * @tparam Dst The destination type. Must be given explicitly.
+ * @tparam Src The source type. Can be given implicitly.
+ * @param src The source pointer.
+ * @return Dst* The casted pointer.
+ */
+template <typename Dst, typename Src>
+auto py_reinterpret_cast(Src* src) noexcept -> Dst* {
+    if constexpr (std::is_same_v<Src*, Dst*>) {
+        static_assert(is_python_object_v<Src> || std::is_same_v<Src*, PyObject*>);
+        return src;
+    } else if constexpr (std::is_same_v<Dst*, PyObject*>) {
+        static_assert(is_python_object_v<Src>);
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+        return reinterpret_cast<PyObject*>(src);
+    } else {
+        static_assert(std::is_same_v<Src*, PyObject*>);
+        static_assert(is_python_object_v<Dst>);
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+        return reinterpret_cast<Dst*>(src);
+    }
+}
+
+class PyLogEvent;
+class PyReaderParser;
+
+LOG_SURGEON_FFI_MARK_AS_PYOBJECT(PyLogEvent);
+LOG_SURGEON_FFI_MARK_AS_PYOBJECT(PyReaderParser);
+LOG_SURGEON_FFI_MARK_AS_PYOBJECT(PyBytesObject);
+LOG_SURGEON_FFI_MARK_AS_PYOBJECT(PyDictObject);
+LOG_SURGEON_FFI_MARK_AS_PYOBJECT(PyTypeObject);
+LOG_SURGEON_FFI_MARK_AS_PYOBJECT(PyUnicodeObject);
+}  // namespace log_surgeon_ffi
+
+#endif  // LOG_SURGEON_FFI_PY_OBJECT_CAST_HPP
---
+++ packages/log-surgeon-ffi/log_surgeon_ffi-0.1.0b8-cp311-cp311-musllinux_1_2_i686.whl/log_surgeon_ffi/utils.hpp
@@ -0,0 +1,142 @@
+#ifndef LOG_SURGEON_FFI_UTILS_HPP
+#define LOG_SURGEON_FFI_UTILS_HPP
+
+#include <wrapped_facade_headers/Python.hpp>
+
+#include <cstddef>
+#include <cstdint>
+#include <limits>
+#include <optional>
+#include <span>
+#include <string>
+#include <string_view>
+#include <type_traits>
+
+namespace log_surgeon_ffi {
+/**
+ * Adds the given Python type to the given Python module.
+ * @param new_type Type object to add.
+ * @param type_name Type name identifier.
+ * @param module Python module to hold the new type.
+ * @return true on success.
+ * @return false on failure with the relevant Python exception and error set.
+ */
+[[nodiscard]] auto add_python_type(PyTypeObject* new_type, char const* type_name, PyObject* module)
+        -> bool;
+
+/**
+ * Parses a Python string into std::string.
+ * @param py_string PyObject that represents a Python level string. Only Python
+ * Unicode object or an instance of a Python Unicode subtype will be considered
+ * as valid input.
+ * @param out The string parsed.
+ * @return true on success.
+ * @return false on failure with the relevant Python exception and error set.
+ */
+[[nodiscard]] auto parse_py_string(PyObject* py_string, std::string& out) -> bool;
+
+/**
+ * Parses a Python string into std::string_view.
+ * @param py_string PyObject that represents a Python level string. Only Python
+ * Unicode object or an instance of a Python Unicode subtype will be considered
+ * as valid input.
+ * @param view The string_view of the underlying byte data of py_string.
+ * @return true on success.
+ * @return false on failure with the relevant Python exception and error set.
+ */
+[[nodiscard]] auto parse_py_string_as_string_view(PyObject* py_string, std::string_view& view)
+        -> bool;
+
+/**
+ * Gets the Python True/False object from a given `bool` value/expression.
+ * @param is_true A boolean value/expression.
+ * @return PyObject that is either Python True or Python False.
+ */
+[[nodiscard]] auto get_py_bool(bool is_true) -> PyObject*;
+
+/**
+ * Parses a Python integer into an int_type variable.
+ * @tparam IntType Output integer type (size and signed/unsigned).
+ * @param py_int PyObject that represents a Python level integer. Only
+ * PyLongObject or an instance of a subtype of PyLongObject will be considered
+ * as valid input.
+ * @param val The integer parsed.
+ * @return true on success.
+ * @return false on failure with the relevant Python exception and error set.
+ */
+// template <clp::IntegerType IntType>
+// [[nodiscard]] auto parse_py_int(PyObject* py_int, IntType& val) -> bool;
+
+/*
+ * Handles a `clp::TraceableException` by setting a Python exception
+ * accordingly.
+ * @param exception
+ */
+// auto handle_traceable_exception(clp::TraceableException& exception) noexcept -> void;
+
+/**
+ * @param sv
+ * @return A new reference to the constructed Python string object from the
+ * given string view `sv`.
+ * @Return nullptr on failure with the relevant Python exception and error set.
+ */
+[[nodiscard]] auto construct_py_str_from_string_view(std::string_view sv) -> PyObject*;
+
+/**
+ * A template that always evaluates as false.
+ */
+template <typename T>
+[[maybe_unused]] constexpr bool cAlwaysFalse{false};
+
+/**
+ * @param sv
+ * @return The underlying C-string of the given constexpr string view.
+ */
+[[nodiscard]] consteval auto get_c_str_from_constexpr_string_view(std::string_view const& sv)
+        -> char const*;
+
+/**
+ * @return A new reference to `Py_None`.
+ */
+[[nodiscard]] auto get_new_ref_to_py_none() -> PyObject*;
+
+// template <clp::IntegerType IntType>
+// auto parse_py_int(PyObject* py_int, IntType& val) -> bool {
+//     if (false == static_cast<bool>(PyLong_Check(py_int))) {
+//         PyErr_SetString(PyExc_TypeError, "parse_py_int receives none-integer argument.");
+//         return false;
+//     }
+
+//     if constexpr (std::is_same_v<IntType, size_t>) {
+//         val = PyLong_AsSize_t(py_int);
+//     } else if constexpr (std::is_same_v<IntType, clp::ir::epoch_time_ms_t>) {
+//         val = PyLong_AsLongLong(py_int);
+//     } else if constexpr (std::is_same_v<IntType, Py_ssize_t>) {
+//         val = PyLong_AsSsize_t(py_int);
+//     } else if constexpr (std::is_same_v<IntType, uint32_t>) {
+//         uint64_t const val_as_unsigned_long{PyLong_AsUnsignedLong(py_int)};
+//         if (nullptr != PyErr_Occurred()) {
+//             return false;
+//         }
+//         if (std::numeric_limits<uint32_t>::max() < val_as_unsigned_long) {
+//             PyErr_Format(
+//                     PyExc_OverflowError,
+//                     "The input integer %lu overflows the range of type `uint32_t`",
+//                     val_as_unsigned_long
+//             );
+//             return false;
+//         }
+//         val = static_cast<uint32_t>(val_as_unsigned_long);
+//     } else {
+//         static_assert(cAlwaysFalse<IntType>, "Given integer type not supported.");
+//     }
+
+//     return (nullptr == PyErr_Occurred());
+// }
+
+consteval auto get_c_str_from_constexpr_string_view(std::string_view const& sv) -> char const* {
+    return sv.data();
+}
+}  // namespace log_surgeon_ffi
+
+#endif  // LOG_SURGEON_FFI_UTILS_HPP
