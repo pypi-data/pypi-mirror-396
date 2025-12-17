3580c2751da82d46f7b02c7c79299df320e0ecce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 03:31:23 +0000

    Add package pnspy/0.2.1/pnspy-0.2.1-py3-none-any.whl

---
+++ packages/pnspy/pnspy-0.2.1-py3-none-any.whl/pns/pss.py
@@ -0,0 +1,140 @@
+"""Detect the principal subsphere by optimization."""
+
+import warnings
+
+import numpy as np
+from scipy.optimize import least_squares
+
+from .base import exp_map, log_map, rotation_matrix
+
+__all__ = [
+    "pss",
+]
+
+
+def pss(x, tol=1e-3, maxiter=None, lm_kwargs=None):
+    r"""Find the principal subsphere (PSS) from data on a hypersphere.
+
+    Parameters
+    ----------
+    x : (N, d+1) real array
+        Extrinsic coordinates of data on a ``d``-dimensional hypersphere,
+        embedded in a ``d+1``-dimensional space.
+    tol : float, default=1e-3
+        Convergence tolerance in radian.
+    maxiter : int, optional
+        Maximum number of iterations for the optimization.
+        If None, the number of iterations is not checked.
+    lm_kwargs : dict, optional
+        Additional keyword arguments to be passed for Levenberg-Marquardt optimization.
+        Follows the signature of :func:`scipy.optimize.least_squares`.
+
+    Returns
+    -------
+    v : (d+1,) real array
+        Estimated principal axis of the subsphere in extrinsic coordinates.
+    r : scalar in [0, pi]
+        Geodesic distance from the pole by *v* to the estimated principal subsphere.
+
+    Notes
+    -----
+    This function determines the best fitting subsphere
+    :math:`\hat{A}_{d-k} = A_{d-k}(\hat{v}_k, \hat{r}_k) \subset S^{d-k+1}` for
+    :math:`k = 1, 2, \ldots, d`.
+
+    The Fréchet mean :math:`\hat{A}_0` of the lowest level best fitting subsphere
+    :math:`\hat{A}_1` is also determined by this function.
+
+    Examples
+    --------
+    >>> from pns.pss import pss
+    >>> from pns.util import unit_sphere, circular_data, circle_3d
+    >>> x = circular_data([0, -1, 0])
+    >>> v, r = pss(x.reshape(-1, x.shape[-1]))
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    ... ax = plt.figure().add_subplot(projection='3d', computed_zorder=False)
+    ... ax.plot_surface(*unit_sphere(), color='skyblue', alpha=0.6, edgecolor='gray')
+    ... ax.scatter(*x.T, marker="x")
+    ... ax.plot(*circle_3d(v, r), color="tab:orange", zorder=10)
+    """
+    if lm_kwargs is None:
+        lm_kwargs = {}
+    else:
+        lm_kwargs = lm_kwargs.copy()
+        lm_kwargs.pop("method", None)
+        lm_kwargs.pop("args", None)
+
+    _, D = x.shape
+    if D <= 1:
+        raise ValueError("Data must be on at least 1-sphere.")
+    elif D == 2:
+        r = np.int_(0)
+        v = np.mean(x, axis=0)
+        norm = np.linalg.norm(v)
+        if norm != 0:
+            v /= norm
+        else:
+            v = np.array([1, 0])
+    else:
+        pole = np.array([0] * (D - 1) + [1])
+        R = np.eye(D)
+        _x = x
+        v, r = _pss(_x, lm_kwargs=lm_kwargs)
+
+        iter_count = 0
+        while np.arccos(np.dot(pole, v)) > tol:
+            if iter_count == maxiter:
+                warnings.warn(
+                    f"Maximum number of iterations ({maxiter}) reached. "
+                    "Optimization may not have converged.",
+                    UserWarning,
+                    stacklevel=2,
+                )
+                break
+
+            # Rotate so that v becomes the pole
+            _x, _R = _rotate(_x, v)
+            v, r = _pss(_x, lm_kwargs=lm_kwargs)
+            R = R @ _R.T
+            iter_count += 1
+
+        v = R @ v  # re-rotate back
+    return v.astype(x.dtype), r.astype(x.dtype)
+
+
+def _rotate(pts, v):
+    R = rotation_matrix(v)
+    return pts @ R.T, R
+
+
+def _pss(pts, lm_kwargs):
+    # Projection
+    x_dag = log_map(pts)
+    v_dag_init = np.mean(x_dag, axis=0)
+    r_init = np.mean(np.linalg.norm(x_dag - v_dag_init, axis=1))
+    init = np.concatenate([v_dag_init, [r_init]])
+    # Optimization
+    opt = least_squares(_res, init, _jac, method="lm", args=(x_dag,), **lm_kwargs).x
+    v_dag_opt, r_opt = opt[:-1], opt[-1]
+    v_opt = exp_map(v_dag_opt.reshape(1, -1)).reshape(-1)
+    r_opt = np.mod(r_opt, np.pi)
+    return v_opt, r_opt
+
+
+def _res(params, x_dag):
+    v_dag, r = params[:-1], params[-1]
+    return np.linalg.norm(x_dag - v_dag.reshape(1, -1), axis=1) - r
+
+
+def _jac(params, x_dag):
+    n = len(x_dag)
+    m = len(params)
+    v_dag = params[:-1].reshape(1, -1)
+    diff = x_dag - v_dag
+    dist = np.linalg.norm(diff, axis=1)
+    mask = dist > 1e-12
+    out = np.empty((n, m), dtype=float)
+    out[mask, :-1] = -diff[mask] / dist[mask][:, None]
+    out[~mask, :-1] = 0.0
+    out[:, -1] = -1.0
+    return out
---
+++ packages/pnspy/pnspy-0.2.1-py3-none-any.whl/pns/transform.py
@@ -0,0 +1,395 @@
+"""Classes to transform data."""
+
+import numpy as np
+
+from .base import rotation_matrix
+
+__all__ = [
+    "Project",
+    "project",
+    "Embed",
+    "embed",
+    "Reconstruct",
+    "reconstruct",
+    "ExtrinsicPNS",
+    "extrinsic_pns",
+]
+
+
+class Project:
+    r"""Minimum-geodesic projection of points to a subsphere.
+
+    Parameters
+    ----------
+    [op]_func : callable
+        Operator function.
+    dtype : type
+        Data type for intermediate vectors.
+
+    Notes
+    -----
+    The instance of this class is the function
+    :math:`P: S^{d-k+1} \to A_{d-k}(v_k, r_k ) \subset S^{d-k+1}` for
+    :math:`k = 1, 2, \ldots, d` in the original paper.
+    Here, :math:`A_{d-k}(v_k, r_k)` is a subsphere of the hypersphere :math:`S^{d-k+1}`.
+    The input and output data dimension are :math:`m+1`, where :math:`m = d-k+1`.
+
+    The resulting points have same number of components but their rank is reduced
+    by one in the manifold.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pns.transform import Project
+    >>> from pns.util import circular_data
+    >>> x = circular_data()
+    >>> project = Project(np.add, np.subtract, np.multiply, np.divide, np.sin,
+    ... np.acos, np.atan, np.matmul)
+    >>> xP, res = project(x, np.array([0, 0, 1]), 0.5)
+    """
+
+    def __init__(
+        self,
+        add_func,
+        sub_func,
+        mul_func,
+        div_func,
+        sin_func,
+        acos_func,
+        atan2_func,
+        matmul_func,
+        dtype=np.float64,
+    ):
+        self.add = add_func
+        self.sub = sub_func
+        self.mul = mul_func
+        self.div = div_func
+        self.sin = sin_func
+        self.acos = acos_func
+        self.atan2 = atan2_func
+        self.matmul = matmul_func
+        self.dtype = dtype
+
+    def __call__(self, X, v, r):
+        if v.shape[0] > 2:
+            rho = self.acos(self.matmul(X, v.reshape(-1, 1)))  # (N, 1)
+        else:
+            # For 2D case (circle), use arctan2 to preserve sign
+            rotated_v = (v @ np.array([[0, 1], [-1, 0]])).astype(self.dtype)
+            y = self.matmul(X, rotated_v.reshape(-1, 1))  # (N, 1)
+            x = self.matmul(X, v.reshape(-1, 1))  # (N, 1)
+            rho = self.atan2(y, x)  # (N, 1)
+
+        sin_r = np.sin(r).astype(self.dtype)
+        P = self.div(
+            self.add(
+                self.mul(sin_r, X),  # (N, d+1)
+                self.mul(
+                    self.sin(self.sub(rho, r)),  # (N, 1)
+                    v,  # (d+1,)
+                ),  # (N, d+1)
+            ),  # (N, d+1)
+            self.sin(rho),  # (N, 1)
+        )  # (N, d+1)
+        return P, self.sub(rho, r)
+
+
+_project = Project(
+    np.add,
+    np.subtract,
+    np.multiply,
+    np.divide,
+    np.sin,
+    np.acos,
+    np.atan,
+    np.matmul,
+)
+
+
+def project(X, v, r):
+    """Numpy-compatible instance of :class:`Project`.
+
+    Parameters
+    ----------
+    x : (N, m+1) real array
+        Extrinsic coordinates of data on a ``d``-dimensional hypersphere,
+        embedded in a ``d+1``-dimensional space.
+    v : (m+1,) real array
+        Subsphere axis.
+    r : scalar
+        Subsphere geodesic distance.
+
+    Returns
+    -------
+    xP : (N, m+1) real array
+        Extrinsic coordinates of data on a ``d``-dimensional hypersphere,
+        projected onto the found principal subsphere.
+    res : (N, 1) real array
+        Projection residuals.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pns.transform import project
+    >>> from pns.util import unit_sphere, circular_data
+    >>> x = circular_data()
+    >>> A, _ = project(x, np.array([0, 0, 1]), 0.5)
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    ... ax = plt.figure().add_subplot(projection='3d', computed_zorder=False)
+    ... ax.plot_surface(*unit_sphere(), color='skyblue', alpha=0.6, edgecolor='gray')
+    ... ax.scatter(*x.T, marker="x")
+    ... ax.scatter(*A.T, marker=".")
+    """
+    return _project(X, v, r)
+
+
+class Embed:
+    r"""Embed data on a sub-hypersphere to a low-dimensional unit hypersphere.
+
+    Parameters
+    ----------
+    matmul_func : callable
+        Matrix multiplication function.
+    dtype : type
+        Data type for intermediate vectors.
+
+    Notes
+    -----
+    The instance of this class is the function
+    :math:`f_k: A_{d-k}(v_k, r_k) \subset S^{d-k+1} \to S^{d-k}` for
+    :math:`k = 1, 2, \ldots, d-1` in the original paper.
+    Here, :math:`A_{d-k}(v_k, r_k)` is a subsphere of the hypersphere :math:`S^{d-k+1}`.
+    The input is :math:`x \in S^m \subset \mathbb{R}^{m+1}`
+    and the output is :math:`x^\dagger \in S^{m-1} \subset \mathbb{R}^{m}`,
+    where :math:`m = d-k+1`.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pns.transform import Embed
+    >>> from pns.util import circular_data
+    >>> x = circular_data()
+    >>> embed = Embed(np.matmul)
+    >>> x_embed = embed(x, np.array([0, 0, 1]), 0.5)
+    """
+
+    def __init__(self, matmul_func, dtype=np.float64):
+        self.matmul = matmul_func
+        self.dtype = dtype
+
+    def __call__(self, x, v, r, lastop_kwargs=None):
+        if lastop_kwargs is None:
+            lastop_kwargs = {}
+
+        R = rotation_matrix(v)
+        coeff = (1 / np.sin(r) * R[:-1:, :]).T.astype(self.dtype)
+        ret = self.matmul(x, coeff, **lastop_kwargs)
+        return ret
+
+
+_embed = Embed(np.matmul)
+
+
+def embed(x, v, r, *args, **kwargs):
+    r"""Numpy-compatible instance of :class:`Embed`.
+
+    Parameters
+    ----------
+    x : (N, m+1) real array
+        Data :math:`x \in A_{m-1} \subset S^m \subset \mathbb{R}^{m+1}`,
+        on a subsphere :math:`A_{m-1}` of a unit hypersphere :math:`S^m`.
+    v : (m+1,) real array
+        Sub-hypersphere axis.
+    r : scalar
+        Sub-hypersphere geodesic distance.
+
+    Returns
+    -------
+    (N, m) real array
+        Data :math:`x^\dagger` on a low-dimensional unit hypersphere :math:`S^{m-1}`.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pns.transform import embed
+    >>> from pns.util import unit_sphere, circular_data
+    >>> x = circular_data()
+    >>> x_embed = embed(x, np.array([0, 0, 1]), 0.5)
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    ... fig = plt.figure()
+    ... ax1 = fig.add_subplot(121, projection='3d', computed_zorder=False)
+    ... ax1.plot_surface(*unit_sphere(), color='skyblue', alpha=0.6, edgecolor='gray')
+    ... ax1.scatter(*x.T, marker=".", zorder=10)
+    ... ax2 = fig.add_subplot(122)
+    ... ax2.scatter(*x_embed.T, marker=".", zorder=10)
+    ... ax2.set_aspect("equal")
+    """
+    return _embed(x, v, r, *args, **kwargs)
+
+
+class Reconstruct:
+    r"""Reconstruct data on a low-dimensional unit hypersphere. to a sub-hypersphere.
+
+    Parameters
+    ----------
+    [op]_func : callable
+        Operator function.
+
+    Notes
+    -----
+    The instance of this class is the function
+    :math:`f^{-1}_k: S^{d-k} \to A_{d-k}(v_k, r_k) \subset S^{d-k+1}` for
+    :math:`k = 1, 2, \ldots, d-1` in the original paper.
+    Here, :math:`A_{d-k}(v_k, r_k)` is a subsphere of the hypersphere :math:`S^{d-k+1}`.
+    The input is :math:`x^\dagger \in S^{m-1} \subset \mathbb{R}^{m}`
+    and the output is :math:`x \in S^m \subset \mathbb{R}^{m+1}`,
+    where :math:`m = d-k+1`.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pns.transform import Reconstruct
+    >>> t = np.linspace(0, 2 * np.pi, 100)
+    >>> x = np.vstack((np.cos(t), np.sin(t))).T
+    >>> reconstruct = Reconstruct(np.multiply, np.sin, np.cos, np.full, np.hstack)
+    >>> x_rec = reconstruct(x, np.array([0, 0, 1]), 0.5)
+    """
+
+    # TODO: replace full_func and hstack_func
+    def __init__(
+        self,
+        mul_func,
+        sin_func,
+        cos_func,
+        full_func,
+        hstack_func,
+    ):
+        self.mul = mul_func
+        self.sin = sin_func
+        self.cos = cos_func
+        self.full = full_func
+        self.hstack = hstack_func
+
+    def __call__(self, x, v, r):
+        R = rotation_matrix(v)
+        vec = self.hstack(
+            [self.sin(r) * x, self.full(len(x), self.cos(r)).reshape(-1, 1)]
+        )
+        return vec @ R
+
+
+_reconstruct = Reconstruct(
+    np.multiply,
+    np.sin,
+    np.cos,
+    np.full,
+    np.hstack,
+)
+
+
+def reconstruct(x, v, r):
+    r"""Numpy-compatible instance of :class:`Reconstruct`.
+
+    Parameters
+    ----------
+    x : (N, m) real array
+        Data :math:`x^\dagger` on a low-dimensional unit hypersphere :math:`S^{m-1}`.
+    v : (m+1,) real array
+        Sub-hypersphere axis.
+    r : scalar
+        Sub-hypersphere geodesic distance.
+
+    Returns
+    -------
+    (N, m+1) real array
+        Data :math:`x \in A_{m-1} \subset S^m \subset \mathbb{R}^{m+1}`,
+        on a subsphere :math:`A_{m-1}` of a unit hypersphere :math:`S^m`.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pns.transform import reconstruct
+    >>> from pns.util import unit_sphere
+    >>> t = np.linspace(0, 2 * np.pi, 100)
+    >>> x = np.vstack((np.cos(t), np.sin(t))).T
+    >>> x_rec = reconstruct(x, np.array([0, 0, 1]), 0.5)
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    ... fig = plt.figure()
+    ... ax1 = fig.add_subplot(121)
+    ... ax1.scatter(*x.T)
+    ... ax1.set_aspect("equal")
+    ... ax2 = fig.add_subplot(122, projection='3d', computed_zorder=False)
+    ... ax2.plot_surface(*unit_sphere(), color='skyblue', alpha=0.6, edgecolor='gray')
+    ... ax2.scatter(*x_rec.T)
+    """
+    return _reconstruct(x, v, r)
+
+
+class ExtrinsicPNS:
+    r"""Transform data to low-dimensional hypersphere in extrinsic coordinates.
+
+    Parameters
+    ----------
+    project_func : pns.transform.Project
+    embed_func : pns.transform.Embed
+    dtype : type
+        Data type for intermediate vectors.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pns.transform import project, embed, ExtrinsicPNS
+    >>> from pns.util import circular_data
+    >>> x = circular_data()
+    >>> extrinsic_pns = ExtrinsicPNS(project, embed)
+    >>> x_transformed = extrinsic_pns(x, [np.array([0, 0, 1])], [np.float64(0.5)])
+    """
+
+    def __init__(self, project_func, embed_func, dtype=np.float64):
+        self.project = project_func
+        self.embed = embed_func
+        self.dtype = dtype
+
+    def __call__(self, X, vs, rs, lastop_kwargs=None):
+        for i, (v, r) in enumerate(zip(vs, rs)):
+            v, r = v.astype(self.dtype), r.reshape(1).astype(self.dtype)
+            P, _ = self.project(X, v, r)
+            if i < len(vs) - 1:
+                X = self.embed(P, v, r)
+            else:
+                X = self.embed(P, v, r, lastop_kwargs)
+        return X
+
+
+_extrinsic_pns = ExtrinsicPNS(project, embed)
+
+
+def extrinsic_pns(X, vs, rs, *args, **kwargs):
+    r"""Numpy-compatible instance of :class:`ExtrinsicPNS`.
+
+    Parameters
+    ----------
+    X : (N, d+1) real array
+        Extrinsic coordinates of data on a ``d``-dimensional hypersphere,
+        embedded in a ``d+1``-dimensional space.
+    vs : list of (m+1,) real arrays
+        Subsphere axes.
+    rs : list of scalars
+        Subsphere geodesic distances.
+
+    Returns
+    -------
+    (N, d-k+1) real array
+        Extrinsic coordinates of data on a low-dimensional unit hypersphere.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pns.transform import extrinsic_pns
+    >>> from pns.util import circular_data
+    >>> x = circular_data()
+    >>> x_transformed = extrinsic_pns(x, [np.array([0, 0, 1])], [np.float64(0.5)])
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    ... plt.scatter(*x_transformed.reshape(-1, 2).T)
+    """
+    return _extrinsic_pns(X, vs, rs, *args, **kwargs)
