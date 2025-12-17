3594eb0b1f911b3c472a3274bd54d28ab0e9726a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 01:34:56 +0000

    Add package shannon-sdk/0.4.1/shannon_sdk-0.4.1-py3-none-any.whl

---
+++ packages/shannon-sdk/shannon_sdk-0.4.1-py3-none-any.whl/shannon/__init__.py
@@ -0,0 +1,75 @@
+"""Shannon SDK - Python client for Shannon multi-agent AI platform."""
+
+__version__ = "0.4.1"
+
+from shannon.client import AsyncShannonClient, ShannonClient
+from shannon.models import (
+    ControlState,
+    Event,
+    EventType,
+    PendingApproval,
+    Session,
+    SessionEventTurn,
+    SessionHistoryItem,
+    SessionSummary,
+    TaskHandle,
+    TaskStatus,
+    TaskStatusEnum,
+    TaskSummary,
+    TokenUsage,
+)
+from shannon.errors import (
+    AuthenticationError,
+    ConnectionError,
+    PermissionDeniedError,
+    RateLimitError,
+    SessionError,
+    SessionExpiredError,
+    SessionNotFoundError,
+    ShannonError,
+    ServerError,
+    TaskCancelledError,
+    TaskError,
+    TaskNotFoundError,
+    TaskTimeoutError,
+    TemplateError,
+    TemplateNotFoundError,
+    ValidationError,
+)
+
+__all__ = [
+    # Client
+    "AsyncShannonClient",
+    "ShannonClient",
+    # Models
+    "ControlState",
+    "Event",
+    "EventType",
+    "PendingApproval",
+    "Session",
+    "SessionEventTurn",
+    "SessionHistoryItem",
+    "SessionSummary",
+    "TaskHandle",
+    "TaskStatus",
+    "TaskStatusEnum",
+    "TaskSummary",
+    "TokenUsage",
+    # Errors
+    "AuthenticationError",
+    "ConnectionError",
+    "PermissionDeniedError",
+    "RateLimitError",
+    "SessionError",
+    "SessionExpiredError",
+    "SessionNotFoundError",
+    "ShannonError",
+    "ServerError",
+    "TaskCancelledError",
+    "TaskError",
+    "TaskNotFoundError",
+    "TaskTimeoutError",
+    "TemplateError",
+    "TemplateNotFoundError",
+    "ValidationError",
+]
