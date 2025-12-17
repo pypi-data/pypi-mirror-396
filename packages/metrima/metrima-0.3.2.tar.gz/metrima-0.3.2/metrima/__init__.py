8f2d6b99855cbc31a6fa8965b7342f0f62d41167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-17 08:07:35 +0000

    Add package metrima/0.3.2/metrima-0.3.2.tar.gz

---
+++ packages/metrima/metrima-0.3.2.tar.gz/metrima-0.3.2/metrima/__init__.py
@@ -0,0 +1,32 @@
+from .lib import length, is_whole, is_whitespace, span, push_back, attach, locate, invert, trim, has, combinelst, duplicate, verify, chop
+
+from . import units
+from . import utils
+from . import math
+from . import core
+
+__all__ = [
+    "utils",
+
+    "units",
+    "math",
+    "core",
+
+    "length",
+    "is_whole",
+    "is_whitespace",
+    "span",
+    "push_back",
+    "attach",
+    "locate",
+    "invert",
+    "trim",
+    "has",
+    "combinelst",
+    "duplicate",
+    "verify",
+    "chop",
+]
+
+__version__ = "0.3.2"
+__author__ = "Razka Rizaldi"
