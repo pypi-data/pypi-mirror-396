d40c181cb36173cac00174ec3a71491014900f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 04:15:29 +0000

    Add package threaded-order/1.6.3/threaded_order-1.6.3-py3-none-any.whl

---
+++ packages/threaded-order/threaded_order-1.6.3-py3-none-any.whl/threaded_order/__init__.py
@@ -0,0 +1,44 @@
+from importlib import metadata as _metadata
+from os import getenv
+
+__all__ = [
+    'Scheduler',
+    'DAGraph',
+    'configure_logging',
+    'ThreadProxyLogger',
+    'dmark',
+    'tag',
+    'default_workers',
+    '__version__']
+
+def __getattr__(name):
+    if name == 'Scheduler':
+        from .scheduler import Scheduler
+        return Scheduler
+    if name == 'DAGraph':
+        from .graph import DAGraph
+        return DAGraph
+    if name == 'configure_logging':
+        from .logger import configure_logging
+        return configure_logging
+    if name == 'ThreadProxyLogger':
+        from .logger import ThreadProxyLogger
+        return ThreadProxyLogger
+    if name == 'dmark':
+        from .scheduler import dmark
+        return dmark
+    if name == 'tag':
+        from .scheduler import tag
+        return tag
+    if name == 'default_workers':
+        from .scheduler import default_workers
+        return default_workers
+    raise AttributeError(name)
+
+try:
+    __version__ = _metadata.version(__name__)
+except _metadata.PackageNotFoundError:
+    __version__ = '1.6.3'
+
+if getenv('DEV'):
+    __version__ = f'{__version__}+dev'
