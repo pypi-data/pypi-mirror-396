bee60ef328b77b3715dc9ea8c7c0349e5ee04f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:32:56 +0000

    Add package vorta/0.11.1/vorta-0.11.1.tar.gz

---
+++ packages/vorta/vorta-0.11.1.tar.gz/vorta-0.11.1/src/vorta/views/log_page.py
@@ -0,0 +1,68 @@
+from PyQt6 import uic
+from PyQt6.QtWidgets import (
+    QAbstractItemView,
+    QApplication,
+    QHeaderView,
+    QTableWidgetItem,
+)
+
+from vorta import config
+from vorta.store.models import BackupProfileMixin, EventLogModel
+from vorta.utils import get_asset
+
+uifile = get_asset('UI/log_page.ui')
+LogTableUI, LogTableBase = uic.loadUiType(uifile)
+
+
+class LogTableColumn:
+    Time = 0
+    Category = 1
+    Subcommand = 2
+    Repository = 3
+    ReturnCode = 4
+
+
+class LogPage(LogTableBase, LogTableUI, BackupProfileMixin):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.init_ui()
+        QApplication.instance().backup_finished_event.connect(self.populate_logs)
+        QApplication.instance().profile_changed_event.connect(self.populate_logs)
+
+    def init_ui(self):
+        self.logPage.setAlternatingRowColors(True)
+        header = self.logPage.horizontalHeader()
+        header.setVisible(True)
+        [header.setSectionResizeMode(i, QHeaderView.ResizeMode.ResizeToContents) for i in range(5)]
+        header.setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)
+        self.logPage.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
+        self.logPage.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+
+        self.logLink.setText(
+            f'<a href="file://{config.LOG_DIR}"><span style="text-decoration:'
+            'underline; color:#0984e3;">Click here</span></a> for complete logs.'
+        )
+
+        self.populate_logs()
+
+    def populate_logs(self):
+        profile = self.profile()
+        event_logs = [
+            s
+            for s in EventLogModel.select()
+            .where(EventLogModel.profile == profile.id)
+            .order_by(EventLogModel.start_time.desc())
+        ]
+
+        sorting = self.logPage.isSortingEnabled()
+        self.logPage.setSortingEnabled(False)
+        self.logPage.setRowCount(len(event_logs))
+        for row, log_line in enumerate(event_logs):
+            formatted_time = log_line.start_time.strftime('%Y-%m-%d %H:%M')
+            self.logPage.setItem(row, LogTableColumn.Time, QTableWidgetItem(formatted_time))
+            self.logPage.setItem(row, LogTableColumn.Category, QTableWidgetItem(log_line.category))
+            self.logPage.setItem(row, LogTableColumn.Subcommand, QTableWidgetItem(log_line.subcommand))
+            self.logPage.setItem(row, LogTableColumn.Repository, QTableWidgetItem(log_line.repo_url))
+            self.logPage.setItem(row, LogTableColumn.ReturnCode, QTableWidgetItem(str(log_line.returncode)))
+        self.logPage.setSortingEnabled(sorting)
---
+++ packages/vorta/vorta-0.11.1.tar.gz/vorta-0.11.1/src/vorta/views/networks_page.py
@@ -0,0 +1,60 @@
+from PyQt6 import uic
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QCheckBox, QLabel, QListWidget, QListWidgetItem
+
+from vorta.store.models import BackupProfileMixin, WifiSettingModel
+from vorta.utils import get_asset, get_sorted_wifis
+
+uifile = get_asset('UI/networks_page.ui')
+NetworksUI, NetworksBase = uic.loadUiType(uifile)
+
+
+class NetworksPage(NetworksBase, NetworksUI, BackupProfileMixin):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+        self.wifiListLabel: QLabel = self.findChild(QLabel, 'wifiListLabel')
+        self.meteredNetworksCheckBox: QCheckBox = self.findChild(QCheckBox, 'meteredNetworksCheckBox')
+        self.wifiListWidget: QListWidget = self.findChild(QListWidget, 'wifiListWidget')
+
+        # Connect signals
+        self.meteredNetworksCheckBox.stateChanged.connect(self.on_metered_networks_state_changed)
+        self.wifiListWidget.itemChanged.connect(self.save_wifi_item)
+        QApplication.instance().profile_changed_event.connect(self.populate_wifi)
+
+        self.populate_wifi()
+
+    def on_metered_networks_state_changed(self, state):
+        profile = self.profile()
+        attr = 'dont_run_on_metered_networks'
+        new_value = state != Qt.CheckState.Checked
+        self.save_profile_attr(attr, new_value)
+        self.meteredNetworksCheckBox.setChecked(False if profile.dont_run_on_metered_networks else True)
+
+    def populate_wifi(self):
+        self.wifiListWidget.clear()
+        profile = self.profile()
+        if profile:
+            for wifi in get_sorted_wifis(profile):
+                item = QListWidgetItem()
+                item.setText(wifi.ssid)
+                item.setFlags(item.flags() | Qt.ItemFlag.ItemIsUserCheckable)
+                if wifi.allowed:
+                    item.setCheckState(Qt.CheckState.Checked)
+                else:
+                    item.setCheckState(Qt.CheckState.Unchecked)
+                self.wifiListWidget.addItem(item)
+
+    def save_wifi_item(self, item):
+        profile = self.profile()
+        if profile:
+            db_item = WifiSettingModel.get(ssid=item.text(), profile=profile.id)
+            db_item.allowed = item.checkState() == Qt.CheckState.Checked
+            db_item.save()
+
+    def save_profile_attr(self, attr, new_value):
+        profile = self.profile()
+        if profile:
+            setattr(profile, attr, new_value)
+            profile.save()
