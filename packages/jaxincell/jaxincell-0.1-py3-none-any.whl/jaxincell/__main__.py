818181fec92fb145cdb6a84e090a8ec87ced81f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 04:15:13 +0000

    Add package jaxincell/0.1/jaxincell-0.1-py3-none-any.whl

---
+++ packages/jaxincell/jaxincell-0.1-py3-none-any.whl/jaxincell/__main__.py
@@ -0,0 +1,24 @@
+"""Main command line interface to JAX-in-Cell."""
+import sys
+from ._plot import plot
+from ._simulation import simulation, load_parameters
+from ._diagnostics import diagnostics
+
+def main(cl_args=sys.argv[1:]):
+    """Run the main JAX-in-Cell code from the command line.
+
+    Reads and parses user input from command line, runs the code,
+    and prints and plots the resulting simulation.
+
+    """
+    if len(cl_args) == 0:
+        print("Using standard input parameters instead of an input TOML file.")
+        output = simulation()
+    else:
+        input_parameters, solver_parameters = load_parameters(cl_args[0])
+        output = simulation(input_parameters, **solver_parameters)
+    diagnostics(output)
+    plot(output)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
