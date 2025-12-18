e2800fb653646956c962927b0ac7bbb80d575778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:32:21 +0000

    Add package dsfr_structure/0.8.1/dsfr_structure-0.8.1-py3-none-any.whl

---
+++ packages/dsfr_structure/dsfr_structure-0.8.1-py3-none-any.whl/dsfr_structure/extension/utils/__init__.py
@@ -0,0 +1,20 @@
+def absolute_url_if_needed(url):
+    if contains_subpath(url):
+        if url.endswith("/"):
+            return url[:-1]
+        return url
+    return ""
+
+def contains_subpath(url):
+    """Check if the URL contains a subpath, ie it contains a slash in the path, except http(s):// or at the end."""
+    if not url:
+        return False
+    # Check if the URL starts with http:// or https://
+    if url.startswith("http://") or url.startswith("https://"):
+        # Remove the protocol part
+        url = url.split("://", 1)[1]
+    # Remove the trailing slash if it exists
+    if url.endswith("/"):
+        url = url[:-1]
+    # Check if there is a slash in the path
+    return "/" in url
