4e35b196b88235e8256f2188f6d5278d121c8c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 04:15:11 +0000

    Add package easyfasta/1.0.14/easyfasta-1.0.14.tar.gz

---
+++ packages/easyfasta/easyfasta-1.0.14.tar.gz/easyfasta-1.0.14/easyfasta/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from .easyfata import(
+    wrap_sequence,
+    build_index,
+    get_sequence_index,
+    get_sequence_id,
+    fasta_iter,
+    load_fasta,
+    complement,
+    reverse,
+    reverse_complement
+)
+
+__all__ = [
+    "wrap_sequence",
+    "build_index",
+    "get_sequence_index",
+    "get_sequence_id",
+    "fasta_iter",
+    "load_fasta",
+    "complement",
+    "reverse",
+    "reverse_complement"
+]
+
+__version__ = "1.0.5"
+
